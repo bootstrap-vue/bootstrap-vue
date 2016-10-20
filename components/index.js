7aef6fa85488225945fefb6fbe4c06bd49da2273
vizo <miha.vizovisek@gmail.com>
2016-10-20 19:44:08 +0200

    Update index.js

--- components/index.js
+++ components/index.js
@@ -5,8 +5,10 @@ import bButtonGroup from './button-group.vue'
 import bButtonRadio from './button-radio.vue'
 import bButton from './button.vue'
 import bCard from './card.vue'
-import {slide, carousel} from './carousel.vue'
-import {collapse, collapseToggle} from './collapse.vue'
+import carousel from './carousel.vue'
+import carouselSlide from './carousel.vue'
+import collapse from './collapse.vue'
+import collapseToggle from './collapse-toggle.vue'
 import bDropdown from './dropdown.vue'
 import bDropdownSelect from './dropdown-select.vue'
 import bFormCheckbox from './form-checkbox.vue'
@@ -16,16 +18,19 @@ import bFormSelect from './form-select.vue'
 import bFormTextarea from './form-textarea.vue'
 import bJumbotron from './jumbotron.vue'
 import bTags from './tags.vue'
-import {listGroup, listGroupItem} from './list-group.vue'
+import listGroup from './list-group.vue'
+import listGroupItem from './list-group-item.vue'
 import bMedia from './media.vue'
 import bModal from './modal.vue'
-import {nav, navItem} from './nav.vue'
+import nav from './nav.vue'
+import navItem from './nav-item.vue'
 import bNavbar from './navbar.vue'
 import bPagination from './pagination.vue'
 import bPopover from './popover.vue'
 import bProgress from './progress.vue'
 import bTables from './tables.vue'
-import {tab, tabs} from './tabs.vue'
+import tabs from './tabs.vue'
+import tab from './tab.vue'
 import bTooltip from './tooltip.vue'
 
 var components = {
@@ -35,6 +40,7 @@ var components = {
   bButtonGroup,
   bButtonRadio,
   bButton,
+  bBtn: bButton,
   bCard,
   bDropdown,
   bDropdownSelect,
@@ -57,12 +63,15 @@ var components = {
   bTabs: tabs,
   bNav: nav,
   bNavItem: navItem,
-  bListGroup: listGroup,
+
   bListGroupItem: listGroupItem,
-  bSlide: slide,
+  bListGroup: listGroup,
+
+  bCarouselSlide: carouselSlide,
   bCarousel: carousel,
-  bCollapseToggle: collapseToggle,
+
   bCollapse: collapse,
+  bCollapseToggle: collapseToggle,
 };
 
 function plugin(Vue) {
