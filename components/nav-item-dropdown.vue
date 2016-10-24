960b9f4d6dcbd1cba0d59b2b6761a13c854e5384
vizo <miha.vizovisek@gmail.com>
2016-10-24 12:58:54 +0200

    Changed slots

--- components/nav-item-dropdown.vue
+++ components/nav-item-dropdown.vue
@@ -1,10 +1,10 @@
 <template>
   <li :class="{'nav-item': true, open: show, dropdown: !dropup, dropup: dropup}">
-    <a @click.stop="toggle($event)" :class="['nav-link', dropdownToggle]" href="" v-on:click.prevent="" aria-haspopup="true" :aria-expanded="show" :disabled="disabled" v-if="text" v-html="text">
+    <a @click.stop="toggle($event)" :class="['nav-link', dropdownToggle]" href="" v-on:click.prevent="" aria-haspopup="true" :aria-expanded="show" :disabled="disabled">
+      <slot>Slot</slot>
     </a>
-    <slot name="nav-link" v-if="!text">Slot "nav-link"</slot>
     <div :class="{'dropdown-menu': true, 'dropdown-menu-right': rightAlignment}">
-      <slot></slot>
+      <slot name="dropdown-menu">Slot "items"</slot>
     </div>
   </li>
 </template>
