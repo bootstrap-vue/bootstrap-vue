06e5ae93f6ef177667fe4631258517fd1d2da2b6
pi0 <pyapar@gmail.com>
2016-12-27 20:49:40 +0330

    Support ELA

--- docs/plugins/ga.js
+++ docs/plugins/ga.js
@@ -7,4 +7,9 @@ if (process.BROWSER_BUILD) {
         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
     ga('create', 'UA-89526435-1', 'auto');
+    ga('require', 'linkid', {
+        'cookieName': '_ela',
+        'duration': 300,
+        'levels': 10
+    });
 }
