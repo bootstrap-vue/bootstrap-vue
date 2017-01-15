457b47989b9263bb404f7c9e43c9c5f96c7a403d
Pooya Parsa <pooya@pi0.ir>
2017-01-15 14:27:42 +0330

    0.4.2

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,10 +2,10 @@ var inBrowser = typeof window !== 'undefined';
 
 // pulled from http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
 export function uniqueId() {
-  let text = '';
+  var text = '';
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-  for (let i = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) {
     text += possible.charAt(Math.floor(Math.random() * possible.length))
   }
   return text
