b793338b60edb8003efe86958098a1a5f67c5745
Pooya Parsa <pooya@pi0.ir>
2017-01-21 14:52:46 +0330

    Badge pill

--- components/badge.vue
+++ components/badge.vue
@@ -1,29 +1,36 @@
 <template>
-<span :class="['badge',labelVariant,labelType]">
+<span :class="['badge',badgeVariant,badgeType,badgePill]">
 	<slot></slot>
 </span>
 </template>
 
 <script>
-  export default {
-    replace: true,
-    computed: {
-      labelVariant() {
-        return !this.variant || this.variant === `default` ? `badge-default` : `badge-${this.variant}`
-      },
-      labelType() {
-        return !this.type ? `` : `tag-${this.type}`
-      }
-    },
-    props: {
-      variant: {
-        type: String,
-        default: 'default'
-      },
-      type: {
-        type: String,
-        default: ''
-      }
+    export default {
+        replace: true,
+        computed: {
+            badgeVariant() {
+                return !this.variant || this.variant === `default` ? `badge-default` : `badge-${this.variant}`;
+            },
+            badgeType() {
+                return !this.type ? `` : `badge-${this.type}`;
+            },
+            badgePill(){
+                return this.pill ? 'badge-pill' : '';
+            }
+        },
+        props: {
+            variant: {
+                type: String,
+                default: 'default'
+            },
+            type: {
+                type: String,
+                default: ''
+            },
+            pill: {
+                type: Boolean,
+                default: false
+            }
+        }
     }
-  }
 </script>
