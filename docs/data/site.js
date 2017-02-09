fd48658158012a3dc12501c857f7e566bcec2c3c
Greg Peden <greg.j.peden@gmail.com>
2017-02-09 12:18:40 -0700

    Correct and publish Popover documentation

--- docs/data/site.js
+++ docs/data/site.js
@@ -22,6 +22,7 @@ export default {
                 {"title": "Nav"},
                 {"title": "NavBar"},
                 {"title": "Pagination"},
+                {"title": "Popover"},
                 {"title": "Tables"},
             ]
         },
