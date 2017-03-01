bf369fc3a15e0620af71e19069bcebbccbd083d9
David Lee <dlee@salestreamsoft.com>
2017-03-01 11:06:03 -0800

    [Tabs component] made the tabs be able to be set using :value and it will emit a input that can be used for vuex and vue-router

--- components/tabs.vue
+++ components/tabs.vue
@@ -24,7 +24,9 @@
   export default {
       replace: true,
       data() {
+          let currentTab = this.value || 0
           return {
+              currentTab,
               items: []
           };
       },
@@ -41,6 +43,18 @@
           size: {
               type: String,
               default: 'md'
+          },
+          value: {
+              type: Number,
+              default: 0
+          }
+      },
+      watch: {
+          currentTab (val) {
+              this.$emit('input', val)
+          },
+          value (val) {
+             this.setActive(val);
           }
       },
       methods: {
@@ -85,12 +99,15 @@
                   this.$set(this.$children[index], 'animate', true);
                   this.$root.$emit('changed::tab', this.items[index].id);
               }, this.fade ? TRANSITION_DURATION : 0);
+
+          // store currentActive
+              this.currentTab = index
           }
       },
       mounted() {
       // if no active tab, set the first one by default
           if (this.getActive() === -1) {
-              this.setActive(0);
+              this.setActive(this.currentTab);
           }
       },
       destroyed() {
