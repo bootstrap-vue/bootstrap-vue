8bb926db64ee06055bddd6cbbdfbc99be85b8948
Pooya Parsa <pooya@pi0.ir>
2017-03-17 23:02:12 +0330

    Work on tooltip

--- components/tooltip.vue
+++ components/tooltip.vue
@@ -1,23 +1,33 @@
 <template>
-  <div>
-    <span class="tooltip-trigger" ref="trigger"><slot></slot></span>
-    <div class="['tooltip','fade','in',tooltipAlignment]" ref="tooltip">
-      <div class="tooltip-inner">
-        <div class="tooltip-arrow"></div>
-        <div class="tooltip-content">
-          <span v-html="text" v-if="text"></span>
-          <div class="tooltip-content-wrapper">
-            <slot name="content" v-if="!text"></slot>
-          </div>
+    <div>
+        <div :class="classObject" role="tooltip" style="opacity: 1">
+            <div class="tooltip-inner">
+                <slot name="text">{{text}}</slot>
+            </div>
         </div>
-      </div>
+        <slot></slot>
     </div>
-  </div>
 </template>
 
 <script>
-  export default {
-      replace: true,
-      computed: {}
-  };
+    export default {
+        props: {
+            placement: {
+                type: String,
+                default: 'top',
+            },
+            text: {
+                type: String,
+                default: null
+            }
+        },
+        computed: {
+            classObject() {
+                return [
+                    'tooltip',
+                    'tooltip-' + this.placement
+                ];
+            }
+        }
+    };
 </script>
