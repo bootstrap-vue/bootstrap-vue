b7fd8174a4d87f6204bb1acf7a4da0cd48f16aa8
MosinVE <mosinve@gmail.com>
2017-03-20 11:42:17 +0300

    updated "fixed-" styles according actual Bootstrap stylesheet  added sticky prop to toggle "sticky-top" class  removed full prop as "navbar-full" class does not exist in Bootstrap 4 stylesheet

--- lib/components/navbar.vue
+++ lib/components/navbar.vue
@@ -6,40 +6,38 @@
 
 
 <script>
-export default {
-    replace: true,
-    computed: {
-        classObject() {
-            return [
-                'navbar',
-                this.type ? `navbar-${this.type}` : '',
-                this.variant ? `bg-${this.variant}` : '',
-                this.fixed ? `navbar-fixed-${this.fixed}` : '',
-                this.full ? 'navbar-full' : '',
-                this.toggleable ? 'navbar-toggleable-md' : ''
-            ];
+    export default {
+        replace: true,
+        computed: {
+            classObject() {
+                return [
+                    'navbar',
+                    this.type ? `navbar-${this.type}` : '',
+                    this.variant ? `bg-${this.variant}` : '',
+                    this.fixed ? `fixed-${this.fixed}` : '',
+                    this.sticky ? 'sticky-top' : '',
+                    this.toggleable ? 'navbar-toggleable-md' : ''
+                ];
+            }
+        },
+        props: {
+            type: {
+                type: String,
+                default: 'light'
+            },
+            variant: {
+                type: String
+            },
+            toggleable: {
+                type: Boolean,
+                default: false
+            },
+            fixed: {
+                type: String
+            },
+            sticky: {
+                type: String
+            }
         }
-    },
-    props: {
-        type: {
-            type: String,
-            default: 'light'
-        },
-        variant: {
-            type: String
-        },
-        toggleable: {
-            type: Boolean,
-            default: false
-        },
-        fixed: {
-            type: String
-        },
-        full: {
-            type: Boolean,
-            default: false
-        }
-    }
-};
-
+    };
 </script>
