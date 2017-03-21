54a4727a2039c1355939b8dbbb6054eb34494cf9
Pooya Parsa <pooya@pi0.ir>
2017-03-21 13:00:46 +0330

    [playground] local storage implemented #157 :D

--- docs/nuxt/pages/play.vue
+++ docs/nuxt/pages/play.vue
@@ -1,100 +1,100 @@
 <template>
-        <div class="container mt-3">
+    <div class="container mt-3">
 
-            <div class="mb-3 row">
-                <div class="col-md-10">
-                    <span>Here you can interactively play and test components with a fresh vue instance.</span>
-                    <br>
-                    <Strong>TIP: </Strong>
-                    <span>You can clone docs repo, to hack and develop components.</span>
-                    <span> changes will be reflected and hot-reloaded instantly.</span>
-                    <br>
-                    <span>Please refer to</span>
-                    <router-link to="/docs"> Docs </router-link>
-                    <span>for more info about available tags and usage.</span>
-                </div>
-                <div class="col-md-1">
-                    <form method='post' action='https://jsfiddle.net/api/post/library/pure/'
-                          target='_blank' v-if="vm">
-                        <input type="hidden" :value="html_fiddle" name="html">
-                        <input type="hidden" :value="js_fiddle" name="js">
-                        <input type="hidden" value="l" name="js_wrap">
-                        <input name="resources" type="hidden" :value="fiddle_dependencies.join(',')">
-                        <b-btn size="sm" type="submit">
-                            <span>Export to JSFiddle</span>
-                        </b-btn>
-                    </form>
-                </div>
+        <div class="mb-3 row">
+            <div class="col-md-10">
+                <span>Here you can interactively play and test components with a fresh vue instance.</span>
+                <br>
+                <Strong>TIP: </Strong>
+                <span>You can clone docs repo, to hack and develop components.</span>
+                <span> changes will be reflected and hot-reloaded instantly.</span>
+                <br>
+                <span>Please refer to</span>
+                <router-link to="/docs"> Docs </router-link>
+                <span>for more info about available tags and usage.</span>
+            </div>
+            <div class="col-md-1">
+                <form method='post' action='https://jsfiddle.net/api/post/library/pure/'
+                      target='_blank' v-if="vm">
+                    <input type="hidden" :value="html_fiddle" name="html">
+                    <input type="hidden" :value="js_fiddle" name="js">
+                    <input type="hidden" value="l" name="js_wrap">
+                    <input name="resources" type="hidden" :value="fiddle_dependencies.join(',')">
+                    <b-btn size="sm" type="submit">
+                        <span>Export to JSFiddle</span>
+                    </b-btn>
+                </form>
+            </div>
+        </div>
+
+        <transition-group class="row" tag="div" name="flip">
+            <div key="A" :class="full?'col-12':'col'">
+                <transition-group class="row" tag="div" name="flip">
+                    <div :class="`col-md-${(vertical&&!full)?6:12} col-sm-12`" key="A1">
+                        <!--Template-->
+                        <div class="card mt-2">
+                            <div class="card-header card-outline-info">
+                                <span>Template</span>
+                                <b-btn size="sm" @click="toggleFull" variant="outline-info" class="float-right">
+                                    <span>{{full ? 'Split' : 'Full'}}</span>
+                                </b-btn>
+                            </div>
+                            <codemirror v-model="html" mode="htmlmixed"></codemirror>
+                        </div>
+                    </div>
+                    <div :class="`col-md-${(vertical&&!full)?6:12} col-sm-12`" key="A2">
+                        <!--JS-->
+                        <div class="card mt-2">
+                            <div class="card-header card-outline-warning">
+                                <span>JS</span>
+                                <b-btn size="sm" @click="toggleFull" variant="outline-info" class="float-right">
+                                    <span>{{full ? 'Split' : 'Full'}}</span>
+                                </b-btn>
+                            </div>
+                            <codemirror v-model="js" mode="javascript"></codemirror>
+                        </div>
+                    </div>
+                </transition-group>
             </div>
 
-            <transition-group class="row" tag="div" name="flip">
-                <div key="A" :class="full?'col-12':'col'">
-                    <transition-group class="row" tag="div" name="flip">
-                        <div :class="`col-md-${(vertical&&!full)?6:12} col-sm-12`" key="A1">
-                            <!--Template-->
-                            <div class="card mt-2">
-                                <div class="card-header card-outline-info">
-                                    <span>Template</span>
-                                    <b-btn size="sm" @click="toggleFull" variant="outline-info" class="float-right">
-                                        <span>{{full ? 'Split' : 'Full'}}</span>
-                                    </b-btn>
-                                </div>
-                                <codemirror v-model="html" mode="htmlmixed"></codemirror>
-                            </div>
-                        </div>
-                        <div :class="`col-md-${(vertical&&!full)?6:12} col-sm-12`" key="A2">
-                            <!--JS-->
-                            <div class="card mt-2">
-                                <div class="card-header card-outline-warning">
-                                    <span>JS</span>
-                                    <b-btn size="sm" @click="toggleFull" variant="outline-info" class="float-right">
-                                        <span>{{full ? 'Split' : 'Full'}}</span>
-                                    </b-btn>
-                                </div>
-                                <codemirror v-model="js" mode="javascript"></codemirror>
-                            </div>
-                        </div>
-                    </transition-group>
+            <div key="B" :class="`col-md-${(vertical || full)?12:6} col-sm-12`">
+                <!--Result-->
+                <div class="card mt-2">
+                    <div class="card-header card-outline-success">
+                        <span>Result</span>
+                        <b-btn size="sm" @click="toggleVertical" variant="outline-info" class="float-right"
+                               v-if="!full">
+                            <span>{{vertical ? 'Horizontal' : 'Vertical'}}</span>
+                        </b-btn>
+                    </div>
+                    <div class="card-block">
+                        <div id="result-container" ref="result"></div>
+                    </div>
                 </div>
 
-                <div key="B" :class="`col-md-${(vertical || full)?12:6} col-sm-12`">
-                    <!--Result-->
+                <!--Console-->
+                <div class="">
                     <div class="card mt-2">
-                        <div class="card-header card-outline-success">
-                            <span>Result</span>
-                            <b-btn size="sm" @click="toggleVertical" variant="outline-info" class="float-right"
-                                   v-if="!full">
-                                <span>{{vertical ? 'Horizontal' : 'Vertical'}}</span>
+                        <div class="card-header card-outline-secondary">
+                            <span>Console</span>
+                            <b-btn size="sm" @click="clear" variant="outline-danger" class="float-right"
+                                   v-if="messages.length">
+                                <span>Clear</span>
                             </b-btn>
                         </div>
                         <div class="card-block">
-                            <div id="result-container" ref="result"></div>
-                        </div>
-                    </div>
-
-                    <!--Console-->
-                    <div class="">
-                        <div class="card mt-2">
-                            <div class="card-header card-outline-secondary">
-                                <span>Console</span>
-                                <b-btn size="sm" @click="clear" variant="outline-danger" class="float-right"
-                                       v-if="messages.length">
-                                    <span>Clear</span>
-                                </b-btn>
-                            </div>
-                            <div class="card-block">
-                                <div v-for="message in messages">
-                                    <b-badge :variant="message[0]">{{message[0]}}</b-badge>
-                                    <span class="text-muted"> {{message[1]}}</span>
-                                    <br>
-                                </div>
+                            <div v-for="message in messages">
+                                <b-badge :variant="message[0]">{{message[0]}}</b-badge>
+                                <span class="text-muted"> {{message[1]}}</span>
+                                <br>
                             </div>
                         </div>
                     </div>
                 </div>
-            </transition-group>
+            </div>
+        </transition-group>
 
-        </div>
+    </div>
 </template>
 
 <style>
@@ -160,9 +160,7 @@ methods: {
             }
         },
         mounted() {
-            this.html = exampleHTML.trim();
-            this.js = exampleJS.trim();
-
+            this.load();
             this.run();
 
             if (typeof window !== 'undefined') {
@@ -247,6 +245,9 @@ methods: {
                 this.messages.unshift([tag, text]);
             },
             run() {
+                // commit latest changes
+                this.commit();
+
                 // Destroy old VM if exists
                 if (this.vm) {
                     try {
@@ -287,6 +288,20 @@ methods: {
             },
             clear() {
                 this.messages.splice(0);
+            },
+            load() {
+                if (typeof window === 'undefined' || !window.localStorage) {
+                    return;
+                }
+                this.js = window.localStorage.getItem('playground_js') || exampleJS;
+                this.html = window.localStorage.getItem('playground_html') || exampleHTML;
+            },
+            commit() {
+                if (typeof window === 'undefined' || !window.localStorage) {
+                    return;
+                }
+                window.localStorage.setItem('playground_js', this.js);
+                window.localStorage.setItem('playground_html', this.html);
             }
         }
     };
