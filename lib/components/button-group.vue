e98e6f5ca3f7fdd8b99aeec3e0f6b6a4668f2b5a
Pooya Parsa <pooya@pi0.ir>
2017-04-07 14:58:54 +0430

    Revert back ButtonGroup
    Was accidentally deleted during cleanup

---
+++ lib/components/button-group.vue
@@ -0,0 +1,33 @@
+<template>
+    <div :class="classObject" role="group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            classObject() {
+                return [
+                    'btn-' + (this.toolbar ? 'toolbar' : 'group'),
+                    this.vertical ? 'btn-group-vertical' : '',
+                    this.size ? ('btn-group-' + this.size) : ''
+                ];
+            }
+        },
+        props: {
+            vertical: {
+                type: Boolean,
+                default: false
+            },
+            toolbar: {
+                type: Boolean,
+                default: false
+            },
+            size: {
+                type: String,
+                default: null
+            }
+        }
+    };
+</script>
