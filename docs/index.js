88821dfa01bf888ccdbd52e9d520b9798e5aeaf2
Pooya Parsa <pooya@pi0.ir>
2017-04-08 00:09:21 +0430

    ESLint

--- docs/index.js
+++ docs/index.js
@@ -15,7 +15,7 @@ export default {
                 {
                     title: 'Contributing',
                     slug: 'contributing'
-                },
+                }
             ]
         },
         {
