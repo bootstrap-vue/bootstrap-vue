4368e760b6f0ca8f7d83b0fc33ac311447f1fe96
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-19 12:03:32 -0300

    dropdown item Keyboard navigation for ARIA
    More closely aligns with proposed Bootstrap V4 dropdown keyboard navigation. (similar to how native select elements work)
    
    When dropdown opens, the dropdown menu is focused.  Keyboard up/down will navigate the dropdown list. tabbing out of the dropdown will close the dropdown (and focus the next element based on normal tab flow).  hitting ESC will also close the dropdown and refocus either the button or toggle button (depending on if the button is split or not).
    
    Also made the .sr-only "Toggle Dropdown" text a prop (toggleText) to allow for different languages.

--- lib/components/dropdown.vue
+++ lib/components/dropdown.vue
@@ -2,6 +2,7 @@
     <div :class="['dropdown','btn-group',visible?'show':'',dropup?'dropup':'']">
 
         <b-button :class="[split?'':'dropdown-toggle']"
+                  ref="button"
                   @click="click"
                   aria-haspopup="true"
                   :aria-expanded="visible"
@@ -13,15 +14,23 @@
 
         <b-button class="dropdown-toggle dropdown-toggle-split"
                   v-if="split"
+                  ref="toggle"
                   @click="toggle"
                   :variant="variant"
                   :size="size"
                   :disabled="disabled"
         >
-            <span class="sr-only">Toggle Dropdown</span>
+            <span class="sr-only">{{toggleText}}</span>
         </b-button>
 
-        <div :class="['dropdown-menu',right?'dropdown-menu-right':'']">
+        <div :class="['dropdown-menu',right?'dropdown-menu-right':'']"
+             ref="menu"
+             tabindex="-1"
+		     @keydown.esc="onEsc($event)"
+		     @keydown.up="onKeyMove($event,true)"
+		     @keydown.down="onKeyMove($event,false)"
+		     @keydown.tab="toggle"
+        >
             <slot></slot>
         </div>
 
@@ -72,6 +81,14 @@
             right: {
                 type: Boolean,
                 default: false
+            },
+            closeOnEsc: {
+                type: Boolean,
+                default: true
+            },
+            toggletext: {
+                type: String,
+                default: 'Toggle Dropdown'
             }
         },
         created() {
@@ -97,6 +114,10 @@
         methods: {
             toggle() {
                 this.visible = !this.visible;
+				if (this.visible) {
+					// focus the dropdown-menu
+					this.$refs.menu.focus();
+				}
             },
             clickOutListener() {
                 this.visible = false;
@@ -109,6 +130,41 @@
                     this.toggle();
                 }
             }
+			onEsc(e) {
+				if (!this.visible || !this.closeOnEsc) {
+					return;
+				}
+				this.visble = false;
+				// Return focus to button/toggle
+				(this.split ? this.$refs.toggle : this.$refs.button).focus();
+				// In case dropdown inside Modal
+				e.stopPropagation();
+			},
+			onKeyMove(e, up) {
+				if (!this.visible || this.disabled) {
+					return;
+				}
+				// get current list of enabled dropdown-items
+				// if ES6 then items = [...this.$refs.menu.querySelectorAll('.dropdown-item:not(.disabled)')]
+				var items = [].slice.call(this.$refs.menu.querySelectorAll('.dropdown-item:not(.disabled)'));
+				if (!items.length) {
+					return;
+				}
+				items.forEach(function(i) {
+					// ensure dropdown-items are not in tab sequence, but still focusable
+					i.setAttribute('tabindex','-1');
+				}
+				var index = items.indexOf(e.target);
+				if (up) {
+					index--;
+				} else if (index < items.length) {
+					index++;
+				}
+				if (index < 0) {
+					index = 0;
+				}
+				items[index].focus();
+			}
         }
     };
 
