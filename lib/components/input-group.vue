793ed0fa02216ef509bccfe2f1d481a7a87bef68
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-21 15:17:06 -0300

    Create input-group.vue

---
+++ lib/components/input-group.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :class="classObject">
+        <slot name="left"></slot>
+        <slot></slot>
+        <slot name="right"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        computed: {
+            classObject() {
+                return [
+                    'input-group',
+                    this.size ? ('input-group-' + this.size) : ''
+                ];
+            }
+        },
+        props: {
+            size: {
+                type: String,
+                default: null
+            }
+        }
+    };
+</script>
