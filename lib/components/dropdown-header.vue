2e3d66a16e6b68750f925aae8ec3e9447a91a511
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-21 12:39:38 -0300

    Create dropdown-header.vue
    Dropdown header component
    
    HTML tag type can be set via the `tag` prop. Defaults to `h6`

---
+++ lib/components/dropdown-header.vue
@@ -0,0 +1,16 @@
+<template>
+    <component :is="tag" class="dropdown-header">
+        <slot></slot>
+    </component>
+</template>
+
+<script>
+    export default {
+        props: {
+            tag: {
+                type: String,
+                default: 'h6'
+            }
+        }
+    };
+</script>
