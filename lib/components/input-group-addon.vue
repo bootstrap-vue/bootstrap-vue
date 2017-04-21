1a7d407b53e8476351a733404d8316836035a0cb
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-21 16:40:35 -0300

    computed property for thisId

--- lib/components/input-group-addon.vue
+++ lib/components/input-group-addon.vue
@@ -1,16 +1,21 @@
 
 <template>
-    <span class="input-group-addon" :id="id">
+    <span class="input-group-addon" :id="thisId">
         <slot></slot>
     </span>
 </template>
 
 <script>
     export default {
+        computed: {
+            thisId() {
+                return: 'b_input_group_addon_' + this._uid;
+            }
+        }
         props: {
             id: {
                 type: String,
-                default: 'b_input_geoup_addon_' + this._uid
+                default: null
             }
         }
     };
