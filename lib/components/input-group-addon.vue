2153b28305861a41111497642b87466826c0bde0
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-21 15:24:11 -0300

    Create input-group-addon.vue

---
+++ lib/components/input-group-addon.vue
@@ -0,0 +1,17 @@
+
+<template>
+    <span class="input-group-addon" :id="id">
+        <slot></slot>
+    </span>
+</template>
+
+<script>
+    export default {
+        props: {
+            id: {
+                type: String,
+                default: 'b_input_geoup_addon_' + this._uid
+            }
+        }
+    };
+</script>
