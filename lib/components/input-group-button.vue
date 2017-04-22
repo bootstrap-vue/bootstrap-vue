3f32bcee340756e7ff0957285753535bdfe33ef0
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-22 06:19:34 -0300

    Support for input-group-button for input-group's (#267)

---
+++ lib/components/input-group-button.vue
@@ -0,0 +1,27 @@
+<template>
+    <span class="input-group-button">
+        <b-button :disabled="disabled" :variant="variant" href="">
+            <slot></slot>
+        </b-button>
+    </span>
+</template>
+
+<script>
+    import bButton from './button.vue';
+
+    export default {
+        components: {
+            bButton
+        },
+        props: {
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            variant: {
+                type: String,
+                default: null
+            }
+        }
+    };
+</script>
