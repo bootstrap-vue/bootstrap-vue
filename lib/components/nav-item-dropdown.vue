354911a4c614810f8e0a191b547cb768796feda8
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-23 23:54:37 -0300

    Fixed typo

--- lib/components/nav-item-dropdown.vue
+++ lib/components/nav-item-dropdown.vue
@@ -95,7 +95,7 @@
                     this.visible = !this.visible;
                     if (this.visible) {
                         // Focus first item
-                        const items = getItems();
+                        const items = this.getItems();
                         if (items.length > 0) {
                             items[0].focus();
                         }
