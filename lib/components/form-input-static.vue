769c7d8f10b0fedd3632f873fe39fe408e19a106
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-27 10:29:33 -0300

    Create form-input-static.vue
    New form-static input

---
+++ lib/components/form-input-static.vue
@@ -0,0 +1,32 @@
+<template>
+    <p :id="_id"
+       :class="['form-control-static',inputClass]"
+       v-html="staticValue"
+    ></p>
+</template>
+
+<script>
+    import formMixin from '../mixins/form';
+    import generateId from '../mixins/generate-id';
+
+    export default {
+        mixins: [formMixin, generateId],
+        computed: {
+            staticValue() {
+                return this.formatter ? this.formatter(value) : value;
+            }
+        },
+        props: {
+            value: {
+                default: null
+            },
+            formatter: {
+                type: Function
+            },
+            lazyFormatter: {
+                type: Boolean,
+                default: false
+            }
+        }
+    };
+</script>
