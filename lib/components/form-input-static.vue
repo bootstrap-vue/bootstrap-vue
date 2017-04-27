9b2e83e9b3a1d09e46e3b15de0108558f87ceac1
Troy Morehouse <troymore@nbnet.nb.ca>
2017-04-27 12:06:13 -0300

    new b-form-input-static component (#292)
    * Create form-input-static.vue
    
    New form-static input
    
    * Added form-input-static compoinent
    
    * Refactored static input
    
    Refactored to use the new `<b-form-input-static>` component
    
    * Switch to bFormInputStatic
    
    Updated child component var to bFormInputStatic to follow proper naming conventions
    
    * Removed lazyFormatter from static-input
    
    * Added trailing semi-colon
    
    To make CircleCI happy
    
    * Added missing 'this'
    
    * Added <slot> for robustness

---
+++ lib/components/form-input-static.vue
@@ -0,0 +1,31 @@
+<template>
+    <p :id="_id"
+       :class="['form-control-static',inputClass]"
+       v-html="staticValue"
+    >
+        <slot></slot>
+    </p>
+</template>
+
+<script>
+    import formMixin from '../mixins/form';
+    import generateId from '../mixins/generate-id';
+
+    export default {
+        mixins: [formMixin, generateId],
+        computed: {
+            staticValue() {
+                return this.formatter ? this.formatter(this.value) : this.value;
+            }
+        },
+        props: {
+            value: {
+                default: null
+            },
+            formatter: {
+                type: Function
+            }
+        }
+    };
+
+</script>
