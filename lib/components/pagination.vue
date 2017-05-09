aa9426c9b5212ed059ede1c3154623ef7a7bb4e0
Troy Morehouse <troymore@nbnet.nb.ca>
2017-05-09 02:26:28 -0300

    [pagination] ARIA attributes + Keyboard navigation (#364)
    * [pagination] ARIA attributes
    
    * Update pagination.vue
    
    * Update pagination.vue
    
    * [pagination] Minor fix
    
    * [pagination] minor fix for ARIA
    
    * [pagination] minor ARIA fixes
    
    * [pagination] Add keyboard navigation
    
    * [pagination] Keyboard navigation
    
    * [pagination] ESLint

--- lib/components/pagination.vue
+++ lib/components/pagination.vue
@@ -1,43 +1,74 @@
 <template>
-    <div class="btn-group pagination" role="group" aria-label="Pagination">
+    <div :class="['btn-group','pagination',btnSize]" 
+         role="group"
+         tabindex="0"
+         :aria-label="ariaLabel ? ariaLabel : null"
+         @focusin="focusCurrent"
+         @keydown.left.prevent="focusPrev"
+         @keydown.right.prevent="focusNext"
+         @keydown.shift.left.prevent="focusFirst"
+         @keydown.shift.right.prevent="focusLast"
+    >
 
         <button type="button"
-                :class="['btn','btn-'+secondaryVariant,btnSize]"
-                :disabled="currentPage == 1 "
-                @click.prevent="(currentPage == 1) ? _return : currentPage--">
+                :class="['btn','btn-'+secondaryVariant]"
+                :disabled="isActive(1)"
+                :aria-label="labelPrev"
+                tabindex="-1"
+                ref="buttons"
+                @click.prevent="isActive(1) ? _return : currentPage--"
+        >
             <span aria-hidden="true">&laquo;</span>
         </button>
 
         <button type="button"
-                :class="['btn','btn-'+secondaryVariant,btnSize,currentPage === 1 ?  'active' : '']"
+                :class="['btn','btn-'+secondaryVariant,isActive(1)?'active':'']"
+                :aria-label="labelPage + ' 1'"
+                :aria-current="isActive(1) ? 'true' : 'false'"
+                :aria-setsize="numberOfPages"
+                :aria-posinset="1"
+                tabindex="-1"
+                ref="buttons"
+                v-if="showPrev"
                 @click.prevent="currentPage = 1"
-                v-show="showPrev">1
-        </button>
+        >1</button>
 
-        <span :class="['btn','btn-'+secondaryVariant,btnSize]" v-show="showPrev">...</span>
+        <span :class="['btn','btn-'+secondaryVariant]" v-show="showPrev">...</span>
 
         <button type="button"
-                :class="['btn',
-                btnSize,
-                btnVariant(index),
-                index + diff === currentPage ? 'active' : '',
-                index + diff !== currentPage ? 'hidden-xs-down' : '']"
+                :class="['btn',btnVariant(index),isActive(index + diff)?'active':'',isActive(index + diff)?'':'hidden-xs-down']"
+                :aria-label="labelPage + ' ' + (index + diff)"
+                :aria-current="isActive(index + diff) ? 'true' : 'false'"
+                :aria-setsize="numberOfPages"
+                :aria-posinset="index + diff"
+                tabindex="-1"
+                ref="buttons"
                 v-for="_,index in pageLinks"
-                @click.prevent="currentPage = index + diff">{{index + diff}}
-        </button>
+                @click.prevent="currentPage = index + diff"
+        >{{index + diff}}</button>
 
-        <span :class="['btn','btn-'+secondaryVariant,btnSize]" v-show="showNext">...</span>
-
-        <button type="button"
-                :class="['btn','btn-'+secondaryVariant,btnSize,numberOfPages === currentPage ? 'active' : '']"
-                v-show="showNext"
-                @click.prevent="currentPage = numberOfPages">{{numberOfPages}}
-        </button>
+        <span :class="['btn','btn-'+secondaryVariant]" v-show="showNext">...</span>
 
         <button type="button"
-                :class="['btn','btn-'+secondaryVariant,btnSize]"
-                :disabled="currentPage == numberOfPages"
-                @click.prevent="(currentPage == numberOfPages) ? _return : currentPage++">
+                :class="['btn','btn-'+secondaryVariant,isActive(numberOfPages) ? 'active' : '']"
+                :aria-label="labelPage + ' ' + numberOfPages"
+                :aria-current="isActive(numberOfPages) ? 'true' : 'false'"
+                :aria-setsize="numberOfPages"
+                :aria-posinset="numberOfPages"
+                tabindex="-1"
+                ref="buttons"
+                v-if="showNext"
+                @click.prevent="currentPage = numberOfPages"
+        >{{numberOfPages}}</button>
+
+        <button type="button"
+                :class="['btn','btn-'+secondaryVariant]"
+                :disabled="isActive(numberOfPages)"
+                :aria-label="labelNext"
+                tabindex="-1"
+                ref="buttons"
+                @click.prevent="isActive(numberOfPages) ? _return : currentPage++"
+        >
             <span aria-hidden="true">&raquo;</span>
         </button>
 
@@ -60,7 +91,10 @@
                 return (result < 1) ? 1 : result;
             },
             btnSize() {
-                return !this.size || this.size === `default` ? `` : `btn-${this.size}`;
+                return !this.size || this.size === `default` ? `` : `pagination-${this.size}`;
+            },
+            isActive(page) {
+                return page === this.currentPage;
             },
             pageLinks() {
                 if (this.currentPage > this.numberOfPages) {
@@ -101,6 +135,40 @@
         methods: {
             btnVariant(index) {
                 return (index + this.diff === this.currentPage) ? `btn-${this.variant}` : `btn-${this.secondaryVariant}`;
+            },
+            focusFirst() {
+                const btn = this.$refs.buttons.find(el => !el.disabled);
+                if (btn && btn.focus && btn !== document.activeElement) {
+                    btn.focus();
+                }
+            },
+            focusLast() {
+                const btn = Array.prototype.slice.call(this.$refs.buttons).reverse().find(el => !el.disabled);
+                if (btn && btn.focus && btn !== document.activeElement) {
+                    btn.focus();
+                }
+            },
+            focusCurrent() {
+                const btn = this.$refs.buttons.find(el => parseInt(el.getAttribute('aria-posinset'), 10) === this.currentPage);
+                if (btn && btn.focus) {
+                    btn.focus();
+                } else {
+                    // Fallback if current page is not in button list
+                    this.focusFirst();
+                }
+            },
+            focusPrev() {
+                const idx = this.$refs.buttons.indexOf(document.activeElement);
+                if (idx > 0 && !this.$refs.buttons[idx - 1].disabled && this.$refs.buttons[idx - 1].focus) {
+                    this.$refs.buttons[idx - 1].focus();
+                }
+            },
+            focusNext() {
+                const idx = this.$refs.buttons.indexOf(document.activeElement);
+                const cnt = this.$refs.buttons.length - 1;
+                if (idx < cnt && !this.$refs.buttons[idx + 1].disabled && this.$refs.buttons[idx + 1].focus) {
+                    this.$refs.buttons[idx + 1].focus();
+                }
             },
             _return() {
 
@@ -148,6 +216,22 @@
             secondaryVariant: {
                 type: String,
                 default: 'secondary'
+            },
+            ariaLabel: {
+                type: String,
+                default: 'Pagination'
+            },
+            labelPrevPage: {
+                type: String,
+                default: 'Previous Page'
+            },
+            labelNextPage: {
+                type: String,
+                default: 'Next Page'
+            },
+            labelPage: {
+                type: String,
+                default: 'Page'
             }
         }
     };
