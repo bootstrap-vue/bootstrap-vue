4dc7fb137f04a9f4e0310661e500d83832b05eaa
Troy Morehouse <troymore@nbnet.nb.ca>
2017-05-09 02:40:02 -0300

    [link] Allow both router links and regular links to co-exist in same document (#365)
    * [link] Allow router routes & regular href links
    
    Allow both router links and regular links to coexist in same document
    
    If `to` is not provided, then assume a regular local HREF
    
    * Update link.vue
    
    * [link] minor fix
    
    * [link] minor condition update
    
    * [link] router and to testing

--- lib/components/link.vue
+++ lib/components/link.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- When VueRouter is available -->
-    <a v-if="routerAvailable"
+    <a v-if="isRouterLink"
        is="router-link"
        :active-class="activeClass"
        :exact-active-class="exactActiveClass"
@@ -32,8 +32,8 @@
 
     export default {
         computed: {
-            routerAvailable() {
-                return Boolean(this.$router);
+            isRouterLink() {
+                return this.$router && !this.to;
             },
             _href() {
                 if (this.disabled) {
@@ -51,16 +51,12 @@
                 }
             },
             _to() {
-                if (!this.routerAvailable || this.disabled) {
-                    return '#';
+                if (this.disabled) {
+                    return null;
                 }
 
                 if (this.to) {
                     return this.to;
-                }
-
-                if (this.href) {
-                    return this.href;
                 }
             }
         },
