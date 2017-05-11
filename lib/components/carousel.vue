62c304c51df7a760cbea7c26701ef25060d4fcf9
Troy Morehouse <troymore@nbnet.nb.ca>
2017-05-10 22:54:14 -0300

    [carousel] ARIA attributes and keyboard navigation

--- lib/components/carousel.vue
+++ lib/components/carousel.vue
@@ -1,27 +1,77 @@
 <template>
-    <div class="carousel slide" @mouseenter="pause" @mouseleave="start" :style="{background,height}">
+    <div class="carousel slide"
+         role="region"
+         :id="id || null"
+         :style="{background,height}"
+         tabindex="-1"
+         @mouseenter="pause"
+         @mouseleave="start"
+         @focusin="pause"
+         @focusout="restart($event)"
+         @keydown.left="prev"
+         @keydown.right="next"
+    >
         <!-- Indicators -->
-        <ol class="carousel-indicators" v-show="indicators">
+        <ol role="menu" 
+            class="carousel-indicators"
+            v-show="indicators"
+            :aria-hidden="indicators ? 'false' : 'true'"
+            :aria-owns="indictors && id ? (id + '__BV_inner_') : null"
+            :aria-activedescendant="slides[index].id || null"
+            :tabindex="indicators ? '0' : '-1"
+            @focusin.self="focusActiveIndicator"
+            @keydown.left.stop.prevent="focusPrevIndicator"
+            @keydown.up.stop.prevent="focusPrevIndicator"
+            @keydown.right.stop.prevent="focusNextIndicator"
+            @keydown.down.stop.prevent="focusNextIndicator"
+        >
             <li v-for="n in slides.length"
+                role="menuitem"
+                tabindex="-1"
+                ref="indcators"
+                :id="id ? (id + '__BV_indicator_' + n + '_') : null"
                 :class="{active:n-1 === index}"
+                :aria-current="n-1 === index ? 'true' : 'false'"
+                :aria-posinset="n"
+                :aria-setsize="slides.length"
+                :aria-label="labelGoto + ' ' + n"
+                :aria-describedby="slides[n-1].id || null"
+                :aria-controls="id ? (id + '__BV_inner_') : null"
                 @click="index=n-1"
+                @keydown.enter.stop.prevent="index=n-1"
+                @keydown.space.stop.prevent="index=n-1"
             ></li>
         </ol>
 
         <!-- Wrapper for slides -->
-        <div class="carousel-inner" role="listbox">
+        <div class="carousel-inner"
+             role="listbox"
+            :id="id ? (id + '__BV_inner_') : null"
+        >
             <slot></slot>
         </div>
 
         <!-- Controls -->
         <template v-if="controls">
-            <a class="carousel-control-prev" href="#" role="button" data-slide="prev" @click.stop.prevent="prev">
+            <a class="carousel-control-prev"
+               href="#"
+               role="button"
+               data-slide="prev"
+               :aria-controls="id ? (id + '__BV_inner_') : null"
+               @click.stop.prevent="prev"
+            >
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
+                <span class="sr-only">{{labelPrev}}</span>
             </a>
-            <a class="carousel-control-next" href="#" role="button" data-slide="next" @click.stop.prevent="next">
+            <a class="carousel-control-next"
+               href="#"
+               role="button"
+               data-slide="next"
+               :aria-controls="id ? (id + '__BV_inner_') : null"
+               @click.stop.prevent="next"
+            >
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
+                <span class="sr-only">{{labelNext}}</span>
             </a>
         </template>
     </div>
@@ -50,6 +100,21 @@
             };
         },
         props: {
+            id: {
+                type: String
+            },
+            labelPrev: {
+                type: String,
+                default: 'Previous Slide'
+            },
+            labelNext: {
+                type: String,
+                default: 'Next Slide'
+            },
+            labelGoto: {
+                type: String,
+                default: 'Goto Slide'
+            },
             interval: {
                 type: Number,
                 default: 5000
@@ -104,6 +169,47 @@
                 this._intervalId = setInterval(() => {
                     this.next();
                 }, this.interval);
+            },
+            
+            // Re-Start auto rotate slides when focus leaves the carousel
+            restart(e) {
+                if (!e.relatedTarget || !this.$el.contains(e.relatedTarget) {
+                    this.start();
+                }
+            },
+            
+            // Focus first indicator
+            focusActiveIndicator() {
+                if (indicators & this.$refs.indicators.length > 0) {
+                    thjis.$nextTick(() => {
+                        this.$refs.indicators[this.index].focus();
+                    });
+                }
+            },
+
+            // Focus prev indicator
+            focusPrevIndicator() {
+                if (indicators & this.$refs.indicators.length > 0) {
+                    const idx = this.$refs.indicators.indexOf(el => Boolean(el == document.activeElement)
+                    if (idx > 0) {
+                        idx--;
+                        this.$nextTick(() => {
+                            this.$refs.indicators[idx].focus();
+                        });
+                    }
+                }
+            },
+
+            focusNextIndicator() {
+                if (indicators & this.$refs.indicators.length > 0) {
+                    const idx = this.$refs.indicators.indexOf(el => Boolean(el === document.activeElement);
+                    if (idx > 0 && idx < this.$refs.indicators - 1) {
+                        idx++;
+                        this.$nextTick(() => {
+                            this.$refs.indicators[idx].focus();
+                        });
+                    }
+                }
             }
         },
         mounted() {
@@ -112,6 +218,15 @@
 
             // Set first slide as active
             this.slides[0].classList.add('active');
+            this.slides.forEach((slide, idx) => {
+                const n = idx + 1;
+                slide.setAttribute('aria-current', idx === 0 ? 'true', 'flase');
+                slide.setAttribute('aria-posinset', String(n));
+                slide.setAttribute('aria-setsize', String(this.slides.length));
+                if (this.id) {
+                    slide.setAttribute('aria-controlledby', id + '__BV_indicator_' + n + '_');
+                }
+            })
 
             // Auto rotate slides
             this.start();
@@ -156,7 +271,9 @@
                     this.$emit('slide', val);
 
                     currentSlide.classList.remove('active');
+                    currentSlide.setAttribute('aria-current','false');
                     nextSlide.classList.add('active');
+                    nextSlide.setAttribute('aria-current','true');
 
                     currentSlide.classList.remove(direction.current);
                     nextSlide.classList.remove(direction.next, direction.overlay);
