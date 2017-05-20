96a3bfb41a1ba942ecbe9dcde65a5f00172c82f3
Alex Regan <alex.regan@artisancolour.com>
2017-05-20 07:56:34 -0700

    update class test

--- __tests__/components/card.spec.js
+++ __tests__/components/card.spec.js
@@ -1,43 +1,50 @@
-import { loadFixture, testVM } from '../helpers';
+import { loadFixture, testVM } from '../helpers'
 
 describe('card', async() => {
-    beforeEach(loadFixture('card'));
-    testVM();
+    beforeEach(loadFixture('card'))
+    testVM()
 
-    it('should contain class names', async() => {
-        const { app: { $refs, $el } } = window;
-
-        expect($refs.simple_card).toHaveClass('card card-success card-inverse');
-        expect($refs.standard_card).toHaveClass('card');
-        expect($refs.img_card).toHaveClass('card');
-        expect($refs.img_overlay_card).toHaveClass('card card-inverse');
-    });
-
-    it('should contain text content', async() => {
-        const { app: { $refs, $el } } = window;
-
-        expect($refs.simple_card.$el.textContent).toContain('Simple Card')
-        expect($refs.standard_card.$el.textContent.trim()).toContain('Last updated 3 mins ago')
-        expect($refs.img_card.$el.textContent.trim()).toContain('This is my opinion :)')
-        expect($refs.img_overlay_card.$el.textContent.trim()).toContain('Overlay cards are cute!')
-    });
-
+    // since our class test depends on the card block,
+    // run this test first
     it('all examples should contain card block', async() => {
-        const { app: { $refs, $el } } = window;
+        const { app: { $refs, $el } } = window
 
         const refs = ['simple_card', 'standard_card', 'img_card', 'img_overlay_card']
 
         refs.forEach((ref) => {
             const childNodes = [...$refs[ref].$el.childNodes]
-            const cardBlock = childNodes.find(el => el.classList && el.classList.contains(
-                'card-block'))
+            const cardBlock = childNodes
+                .find(el => el.classList && el.classList.contains('card-block'))
 
             expect(cardBlock).toBeDefined()
         })
-    });
+    })
+
+    it('should contain class names', async() => {
+        const { app: { $refs, $el } } = window
+
+        expect($refs.simple_card).toHaveClass('card card-success card-inverse')
+        expect($refs.standard_card).toHaveClass('card')
+        expect($refs.img_card).toHaveClass('card')
+        expect($refs.img_overlay_card).toHaveClass('card card-inverse')
+
+        const blockEl = [...$refs.img_overlay_card.$el.childNodes]
+            .find(el => el.classList && el.classList.contains('card-block'))
+
+        expect(blockEl.classList.contains('card-img-overlay')).toBe(true)
+    })
+
+    it('should contain text content', async() => {
+        const { app: { $refs, $el } } = window
+
+        expect($refs.simple_card.$el.textContent).toContain('Simple Card')
+        expect($refs.standard_card.$el.textContent.trim()).toContain('Last updated 3 mins ago')
+        expect($refs.img_card.$el.textContent.trim()).toContain('This is my opinion :)')
+        expect($refs.img_overlay_card.$el.textContent.trim()).toContain('Overlay cards are cute!')
+    })
 
     it('standard_card should display card header', async() => {
-        const { app: { $refs, $el } } = window;
+        const { app: { $refs, $el } } = window
 
         const childNodes = [...$refs.standard_card.$el.childNodes]
         const headerEl = childNodes.find(el => el.classList && el.classList.contains('card-header'))
@@ -45,10 +52,10 @@ describe('card', async() => {
 
         expect(headerEl).toBeDefined()
         expect(headerEl.textContent).toContain(headerText)
-    });
+    })
 
     it('standard_card should display card footer', async() => {
-        const { app: { $refs, $el } } = window;
+        const { app: { $refs, $el } } = window
 
         const childNodes = [...$refs.standard_card.$el.childNodes]
         const footerEl = childNodes.find(el => el.classList && el.classList.contains('card-footer'))
@@ -56,10 +63,10 @@ describe('card', async() => {
 
         expect(footerEl).toBeDefined()
         expect(footerEl.textContent).toContain(footerText)
-    });
+    })
 
     it('should contain <img> with matching src', async() => {
-        const { app: { $refs, $el } } = window;
+        const { app: { $refs, $el } } = window
 
         const vmsWithImg = ['img_card', 'img_overlay_card']
 
@@ -72,5 +79,5 @@ describe('card', async() => {
             expect(imgEl).toBeDefined()
             expect(imgEl.src).toEqual(src)
         })
-    });
-});
+    })
+})
