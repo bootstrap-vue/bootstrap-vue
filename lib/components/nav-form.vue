4590c5c15dde773a44f6b302ed72b734c031f0da
Troy Morehouse <troymore@nbnet.nb.ca>
2017-05-22 22:17:20 -0300

    Create nav-form.vue

---
+++ lib/components/nav-form.vue
@@ -0,0 +1,18 @@
+<template>
+    <b-form :id="id || null" inline>
+        <slot></slot>
+    </b-form>
+</template>
+
+<script>
+    import bForm from './form.vue';
+    
+    export default {
+        components: [bForm],
+        props: {
+            id: {
+                type: String,
+            }
+        }
+    };
+</script>
