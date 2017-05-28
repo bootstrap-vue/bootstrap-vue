b1d78e546f36fe20eb6ed4374e2000fbabb06500
Alex Regan <alex.joseph.regan@gmail.com>
2017-05-28 08:53:21 -0700

    Tests & Breadcrumb improvements (#466)
    * Add bootstrap -> bootstrap-vue migration notes
    
    * add bFormFieldset label text alignment options
    
    * fixes per @pi0
    
    * add focus method proxied to element ref
    
    * Popover dom node leak fix
    
    * badge: add refs and generate variants
    
    * improving error messages when matcher fn called improperly
    
    * badge test suite
    
    * propagate click event and emit click with event obj
    
    * fix for dynamically setting the active link
    
    Before the link would always set the last item of the array as the active one. However, the active state was read from the user submitted items.active prop, rather than the normalized item.__active.
    
    Some more semantic improvements to make the code clearer
    
    * formatting
    
    * use only one lookup of array length
    
    * two components and items lists for tests
    
    * breadcrumb testing
    
    * stop scroll to top behavior

--- examples/breadcrumb/demo.js
+++ examples/breadcrumb/demo.js
@@ -2,14 +2,30 @@ window.app = new Vue({
     el: '#app',
     data: {
         items: [{
+            text: 'Home',
+            link: 'https://bootstrap-vue.github.io'
+        }, {
             text: 'Admin',
-            link: '#'
+            to: '#',
+            active: false
         }, {
             text: 'Manage',
             link: '#'
         }, {
             text: 'Library',
+        }],
+        items2: [{
+            text: 'Home',
+            link: 'https://bootstrap-vue.github.io'
+        }, {
+            text: 'Admin',
+            link: '#',
             active: true
+        }, {
+            text: 'Manage',
+            link: '#'
+        }, {
+            text: 'Library',
         }]
-    }
+    },
 });
