74639c5b541d3010dd9195d0ac91f5281c8bccfc
Troy Morehouse <troymore@nbnet.nb.ca>
2017-05-29 12:36:23 -0300

    [tabs] fix issue 472
    Ensures that there is only ever one active tab
    
    Fix for issue #472

--- lib/components/tabs.vue
+++ lib/components/tabs.vue
@@ -183,10 +183,12 @@
                     return;
                 }
 
-                // Deactivate previous active tab
-                if (this.tabs[this.currentTab]) {
-                    this.$set(this.tabs[this.currentTab], 'localActive', false);
-                }
+                // Deactivate any previous active tab(s)
+                this.tabs.forEach( t => {
+                    if (t !=== tab && t.active) {
+                        this.$set(t, 'localActive', false);
+                    }
+                });
 
                 // Set new tab as active
                 this.$set(tab, 'localActive', true);
@@ -207,15 +209,16 @@
                     this.tabs = [];
                 }
 
+                // Get initial active tab
+                let tabIndex = this.currentTab;
+
                 this.tabs.forEach(tab => {
                     this.$set(tab, 'fade', this.fade);
                     this.$set(tab, 'lazy', this.lazy);
                 });
 
-                // Set initial active tab
-                let tabIndex = this.currentTab;
-
-                if (this.currentTab === null || this.currentTab === undefined) {
+                if (tabIndex === null || tabIndex === undefined) {
+                    // Find last active tab in current tabs
                     this.tabs.forEach((tab, index) => {
                         if (tab.active) {
                             tabIndex = index;
