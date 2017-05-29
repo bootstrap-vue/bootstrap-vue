217666c84b78558854042bb3bfbff3acfa3be659
Troy Morehouse <troymore@nbnet.nb.ca>
2017-05-29 12:37:55 -0300

    [tabs] ESLint

--- lib/components/tabs.vue
+++ lib/components/tabs.vue
@@ -185,7 +185,7 @@
 
                 // Deactivate any previous active tab(s)
                 this.tabs.forEach( t => {
-                    if (t !=== tab && t.active) {
+                    if (t !== tab && t.active) {
                         this.$set(t, 'localActive', false);
                     }
                 });
