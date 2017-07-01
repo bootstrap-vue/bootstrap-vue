9c185b77587457e9f1e0461cbd63d1b1fa6b2b53
Troy Morehouse <troymore@nbnet.nb.ca>
2017-07-01 02:00:18 -0300

    feat(docs): Show one reference to native per page
    Move native event card to component main page

--- docs/nuxt/pages/docs/components/_component.vue
+++ docs/nuxt/pages/docs/components/_component.vue
@@ -5,6 +5,16 @@
 
         <componentdoc :component="meta.component" :events="meta.events" :slots="meta.slots"></componentdoc>
 
+        <b-card class="my-4">
+            <small class="font-italic">
+                Trying to get native browser events working on your component? Use the <code>.native</code>
+                modifier to capture browser native events such as: <code>@click.native="..."</code>,
+                <code>@mouseover.native="..."</code>, etc. See the the official
+                <a href="https://vuejs.org/v2/guide/components.html#Binding-Native-Events-to-Components">Vue.js docuementation</a>
+                for more information.
+            </small>
+        </b-card>
+
         <componentdoc :component="component" :key="component" v-for="component in meta.components"></componentdoc>
     </div>
 </template>
