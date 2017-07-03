63fcaa18dbe3f74cb22f2593754ffdc4199c5e9d
Troy Morehouse <troymore@nbnet.nb.ca>
2017-07-03 12:57:53 -0300

    [button-group-dropdown] Code fixes

--- lib/components/button-group-dropdown.vue
+++ lib/components/button-group-dropdown.vue
@@ -1,18 +1,34 @@
 <template>
     <div :id="id || null" :class="['btn-group','dropdown', {dropup, show: visible}]">
 
-        <b-button :class="{'dropdown-toggle': !split, 'btn-link': link}"
+        <b-button :class="{'dropdown-toggle': !split}"
                   ref="button"
                   :id="id ? (id + '__BV_button_') : null"
+                  :aria-haspopup="split ? null : 'true'"
                   :aria-expanded="visible ? 'true' : 'false'"
                   :variant="variant"
-                  :size="size"
                   :disabled="disabled"
                   @click.stop.prevent="click"
                   @kedown.space.stop.prevent="click"
                   @kedown.enter.stop.prevent="click"
         >
             <slot name="button-content"><slot name="text">{{text}}</slot></slot>
+        </b-button>
+
+        <b-button :class="['dropdown-toggle','dropdown-toggle-split']"
+                  v-if="split"
+                  ref="toggle"
+                  :id="id ? (id + '__BV_toggle_') : null"
+                  :aria-haspopup="split ? 'true' : null"
+                  :aria-expanded="split ? (visible ? 'true' : 'false') : null"
+                  :variant="variant"
+                  :size="size"
+                  :disabled="disabled"
+                  @click.stop.prevent="toggle"
+                  @kedown.space.stop.prevent="toggle"
+                  @kedown.enter.stop.prevent="toggle"
+        >
+            <span class="sr-only">{{toggleText}}</span>
         </b-button>
 
         <div :class="['dropdown-menu',{'dropdown-menu-right': right}]"
@@ -34,34 +50,44 @@
 <script>
     import clickOut from '../mixins/clickout';
     import dropdown from '../mixins/dropdown';
+    import bButton from './button.vue';
 
     export default {
-        mixins: [
-            clickOut,
-            dropdown
-        ],
+        mixins: [clickOut, dropdown],
+        components: {bButton},
         data() {
             return {
                 visible: false
             };
         },
-        computed: {
-            dropdownToggle() {
-                return this.caret ? 'dropdown-toggle' : '';
-            }
-        },
         props: {
             id: {
                 type: String
             },
-            caret: {
-                type: Boolean,
-                default: true
+            toggleText: {
+                type: String,
+                default: 'Toggle Dropdown'
+            },
+            variant: {
+                type: String,
+                default: null
             }
         },
         methods: {
             clickOutListener() {
                 this.visible = false;
+            },
+            click(e) {
+                if (this.disabled) {
+                    this.visible = false;
+                    return;
+                }
+                if (this.split) {
+                    this.$emit('click', e);
+                    this.$root.$emit('shown::dropdown', this);
+                } else {
+                    this.toggle();
+                }
             }
         }
     };
