1b25e2e960e008d23fe828f4289c892feeb49a2a
Troy Morehouse <troymore@nbnet.nb.ca>
2017-07-03 13:19:58 -0300

    [dropdown] move split prop to component

--- lib/components/dropdown.vue
+++ lib/components/dropdown.vue
@@ -61,6 +61,10 @@
             id: {
                 type: String
             },
+            split: {
+                type: Boolean,
+                default: false
+            },
             toggleText: {
                 type: String,
                 default: 'Toggle Dropdown'
