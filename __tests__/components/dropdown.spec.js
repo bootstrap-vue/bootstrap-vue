f31af040f691b0bdf9ec3d93175af993aafcb34d
Alex Regan <alex.regan@artisancolour.com>
2017-07-15 09:30:15 -0700

    feat(tests): add dropdown testing

--- __tests__/components/dropdown.spec.js
+++ __tests__/components/dropdown.spec.js
@@ -1,6 +1,40 @@
-import { loadFixture, testVM } from "../helpers";
+import { loadFixture, testVM, nextTick } from "../helpers";
+import { keys } from "../../lib/utils/object";
 
 describe("dropdown", async () => {
     beforeEach(loadFixture("dropdown"));
     testVM();
+
+    it("should work", async () => {
+        const { app: { $refs } } = window;
+        const dds = keys($refs).map(ref => $refs[ref]);
+
+        dds.forEach(dd => {
+            expect(dd._isVue).toBe(true);
+            expect(dd).toHaveClass("dropdown");
+        });
+    });
+
+    it("should work with shorthand component tag names", async () => {
+        const { app: { $refs } } = window;
+        const { dd_5 } = $refs;
+
+        expect(dd_5).toBeComponent("b-dd");
+    });
+
+    it("should open only one dropdown at a time", async () => {
+        const { app: { $refs } } = window;
+        const dds = keys($refs).map(ref => $refs[ref].$el);
+
+        // Without async iterators, just use a for loop.
+        for (let i = 0; i < dds.length; i++) {
+            Array.from(dds[i].children)
+                .find(node => node.tagName === "BUTTON" && node.classList.contains("dropdown-toggle"))
+                .click();
+            // Await the next render after click triggers dropdown.
+            await nextTick();
+            const openDds = dds.filter(dd => dd.classList.contains("show"));
+            expect(openDds.length).toBe(1);
+        }
+    });
 });
