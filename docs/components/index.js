e7bfe6f4f497fb12a82addc239c94d2df018a199
MosinVE <mosinve@gmail.com>
2017-07-28 04:45:00 +0300

    Add pagination-nav doc page into doc tree

--- docs/components/index.js
+++ docs/components/index.js
@@ -26,6 +26,7 @@ export default {
     'nav': require('./nav').default,
     'navbar': require('./navbar').default,
     'pagination': require('./pagination').default,
+    'pagination-nav': require('./pagination-nav').default,
     'popover': require('./popover').default,
     'progress': require('./progress').default,
     'table': require('./table').default,
