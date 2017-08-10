d0782a8ad9b5fc1d6f1dc9d62176fa3141754747
Pooya Parsa <pooya@pi0.ir>
2017-08-10 20:20:13 +0430

    fix(v-play): disable global error page

--- docs/nuxt/plugins/docs.js
+++ docs/nuxt/plugins/docs.js
@@ -1,3 +1,10 @@
 // Extract docs into main chunk
 import '../../components'
 import '../../directives'
+
+// Disable vue global error handler
+import Vue from 'vue'
+
+export default function () {
+    Vue.config.errorHandler = console.error
+}
