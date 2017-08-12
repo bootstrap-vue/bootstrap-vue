f4566e50ef8e7152768171514602fbfdb06f3489
Alex Regan <alex.regan@artisancolour.com>
2017-08-12 08:45:03 -0700

    feat(badge): functional component

--- tests/components/badge.spec.js
+++ tests/components/badge.spec.js
@@ -1,42 +1,35 @@
-import { loadFixture, testVM } from '../helpers'
+import { loadFixture, testVM } from "../helpers";
 
-const variantList = [
-    'default',
-    'primary',
-    'success',
-    'info',
-    'warning',
-    'danger',
-].map(variant => {
-    return { ref: `badge_${variant}`, variant }
-})
+const variantList = ["default", "primary", "success", "info", "warning", "danger"].map(variant => {
+    return { ref: `badge_${variant}`, variant };
+});
 
-describe('badge', async() => {
-    beforeEach(loadFixture('badge'))
-    testVM()
+describe("badge", async () => {
+    beforeEach(loadFixture("badge"));
+    testVM();
 
-    it('should apply variant classes', async() => {
-        const { app: { $refs, $el } } = window
+    it("should apply variant classes", async () => {
+        const { app: { $refs } } = window;
 
-        expect($refs.badge_pill).toHaveAllClasses(['badge', 'badge-pill'])
+        expect($refs.badge_pill).toHaveAllClasses(["badge", "badge-pill"]);
 
         variantList.forEach(({ ref, variant }) => {
-            const vm = $refs[ref][0]
-            expect(vm).toHaveAllClasses(['badge', `badge-${variant}`])
-        })
-    })
+            const vm = $refs[ref][0];
+            expect(vm).toHaveAllClasses(["badge", `badge-${variant}`]);
+        });
+    });
 
-    it('should apply secondary pill class when not passed variant', async() => {
-        const { app: { $refs, $el } } = window
+    it("should apply default pill class when not passed variant", async () => {
+        const { app: { $refs } } = window;
 
-        const vm = $refs.no_props
-        expect(vm).toHaveClass('badge-secondary')
-    })
+        const vm = $refs.no_props;
+        expect(vm).toHaveClass("badge-default");
+    });
 
-    it('should not apply pill class when not passed pill boolean prop', async() => {
-        const { app: { $refs, $el } } = window
+    it("should not apply pill class when not passed pill boolean prop", async () => {
+        const { app: { $refs } } = window;
 
-        const vm = $refs.no_props
-        expect(vm).not.toHaveClass('badge-pill')
-    })
+        const vm = $refs.no_props;
+        expect(vm).not.toHaveClass("badge-pill");
+    });
 });
