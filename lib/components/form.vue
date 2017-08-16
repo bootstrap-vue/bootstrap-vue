e7f9e8bf2d8caa739c50570a3964b55d502b827a
Troy Morehouse <troymore@nbnet.nb.ca>
2017-08-15 21:03:29 -0300

    [form] Update novalidate

--- lib/components/form.vue
+++ lib/components/form.vue
@@ -1,35 +1,40 @@
 <template>
     <form :class="formClasses"
-          :novalidation="!browserValidation"
-          @submit="$emit('submit',$event)">
+          :novalidate="novalidate"
+          @submit="onSubmit>
         <slot></slot>
     </form>
 </template>
 
 <script>
-export default {
-    computed: {
-        formClasses() {
-            return [
-                this.inline ? 'form-inline' : '',
-                this.validated ? 'was-validated' : ''
-            ];
-        }
-    },
-    props: {
-        inline: {
-            type: Boolean,
-            default: false
+    export default {
+        computed: {
+            formClasses() {
+                return [
+                    this.inline ? 'form-inline' : '',
+                    this.validated ? 'was-validated' : ''
+                ];
+            }
         },
-        browserValidation: {
-            type: Boolean,
-            default: false
+        props: {
+            inline: {
+                type: Boolean,
+                default: false
+            },
+            novalidate: {
+                type: Boolean,
+                default: false
+            },
+            validated: {
+                type: Boolean,
+                default: false
+            }
         },
-        validated: {
-            type: Boolean,
-            default: false
+        methods: {
+            onSubmit(e) {
+                // TODO: Add native validation helper routine here
+                this.$emit('submit', e);
+            }
         }
-    }
-};
-
+    };
 </script>
