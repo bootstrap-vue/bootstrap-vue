12bd1ef814f8655357d9e62e635ab7a8b6bfe362
Troy Morehouse <troymore@nbnet.nb.ca>
2017-08-18 13:14:30 -0300

    feat(input-group): Use input-group-addon functional component

--- lib/components/input-group.vue
+++ lib/components/input-group.vue
@@ -1,19 +1,25 @@
 <template>
-    <component :is="tag" :class="classObject" role="group">
+    <component :is="tag" :id="id || null" :class="classObject" role="group">
         <slot name="left">
-            <b-input-group-addon v-if="left" v-html="left"></b-input-group-addon>
+            <b-input-group-addon v-if="left"
+                                 :id="id ? `${id}_BV_addon_left_` : null"
+                                 v-html="left"
+            ></b-input-group-addon>
         </slot>
 
         <slot></slot>
 
         <slot name="right">
-            <b-input-group-addon v-if="right" v-html="right"></b-input-group-addon>
+            <b-input-group-addon v-if="right"
+                                 :id="id ? `${id}_BV_addon_right_` : null"
+                                 v-html="right"
+            ></b-input-group-addon>
         </slot>
     </component>
 </template>
 
 <script>
-    import bInputGroupAddon from './input-group-addon.vue';
+    import bInputGroupAddon from './input-group-addon';
 
     export default {
         components: { bInputGroupAddon },
@@ -27,6 +33,10 @@
             }
         },
         props: {
+            id: {
+                type: String,
+                defualt: null
+            },
             size: {
                 type: String,
                 default: null
