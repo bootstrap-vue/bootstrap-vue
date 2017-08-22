224e8c3c1baacbd788fa857023b3ab31d2313810
Troy Morehouse <troymore@nbnet.nb.ca>
2017-08-22 12:13:03 -0300

    chore(docs): Use tag instead of release to show version number

--- docs/nuxt/layouts/docs.vue
+++ docs/nuxt/layouts/docs.vue
@@ -6,8 +6,8 @@
             <div class="row">
                 <div class="col-12 col-md-9">
                     <div class="float-right">
-                        <a href="https://github.com/bootstrap-vue/bootstrap-vue/releases" target="_blank">
-                            <img src="https://img.shields.io/github/release/bootstrap-vue/bootstrap-vue.svg?style=flat-square" alt="GitHub release">
+                        <a href="https://github.com/bootstrap-vue/bootstrap-vue/tags" target="_blank">
+                            <img src="https://img.shields.io/github/tag/bootstrap-vue/bootstrap-vue.svg?style=flat-square" alt="GitHub release">
                         </a>
                     </div>
     
