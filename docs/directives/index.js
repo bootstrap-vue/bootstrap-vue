52a3513c84cf146f9c897f3b51b60fb1d942ce10
Troy Morehouse <troymore@nbnet.nb.ca>
2017-08-25 04:39:59 -0300

    Update index.js

--- docs/directives/index.js
+++ docs/directives/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable quote-props */
 
 export default {
+    'popover': require('./popover').default,
     'scrollspy': require('./scrollspy').default,
     'tooltip': require('./tooltip').default
 };
