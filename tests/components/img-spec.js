1af39af0037f40096340a591fa40b6ae19752060
Troy Morehouse <troymore@nbnet.nb.ca>
2017-08-28 00:51:57 -0300

    Create img-spec.js

---
+++ tests/components/img-spec.js
@@ -0,0 +1,55 @@
+import {loadFixture, testVM} from '../helpers';
+
+describe('image', async() => {
+    beforeEach(loadFixture('img'));
+    testVM();
+    
+    it("Should be rednered with an 'img' tag", async() => {
+      const { app: { $refs, $el } } = window;
+      const img = $refs.default;
+      expect(img).toBeDefined();
+      expect(img).toBeElement('img');
+    });
+
+    it("fluid shpould have class 'img-fluid`", async() => {
+      const { app: { $refs, $el } } = window;
+      const img = $refs.fluid;
+      expect(img).toBeDefined();
+      expect(img).toBeElement('img');
+      expect(img).toHaveClass('img-fluid');
+    });
+
+    it("thumbnail should have class 'img-thumbnail`", async() => {
+      const { app: { $refs, $el } } = window;
+      const img = $refs.thumbnail;
+      expect(img).toBeDefined();
+      expect(img).toBeElement('img');
+      expect(img).toHaveClass('img-thumbnail');
+    });
+
+    it("left should have class 'float-left`", async() => {
+      const { app: { $refs, $el } } = window;
+      const img = $refs.left;
+      expect(img).toBeDefined();
+      expect(img).toBeElement('img');
+      expect(img).toHaveClass('float-left');
+    });
+
+    it("right should have class 'float-right`", async() => {
+      const { app: { $refs, $el } } = window;
+      const img = $refs.right;
+      expect(img).toBeDefined();
+      expect(img).toBeElement('img');
+      expect(img).toHaveClass('float-right');
+    });
+
+    it("center should have classes 'mx-auto` and 'd-block'", async() => {
+      const { app: { $refs, $el } } = window;
+      const img = $refs.center;
+      expect(img).toBeDefined();
+      expect(img).toBeElement('img');
+      expect(img).toHaveClass('mx-auto');
+      expect(img).toHaveClass('d-block');
+    });
+
+});
