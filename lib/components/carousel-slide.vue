d95321bbb11759c1467ca18ced22af849da52ea9
Troy Morehouse <troymore@nbnet.nb.ca>
2017-08-29 16:55:37 -0300

    feat(carousel): Use b-img component and id Mixin (#945)
    * [carousel]: update docs to use b-img for blank image
    
    * [carousel-slide] use b-img component
    
    * [img] Add block prop for adding class d-block
    
    * [carousel-slide]: Allow auto-generation of blank images
    
    * [carousel] add global img-width & img-height props for sniffing by slides
    
    * [carousel-slide] use id mixin
    
    * [carousel] incorporate id mixin
    
    * Update README.md
    
    * Update img.js
    
    * [carousel-slide]: Enable local image if img-blank set
    
    * Rename events: slide => sliding-start, slid => sliding-end
    
    * Update README.md
    
    * Update meta.json
    
    * Update README.md

--- lib/components/carousel-slide.vue
+++ lib/components/carousel-slide.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="carousel-item"
          role="listitem"
-         :id="id || null"
-         :style="{background,height}"
+         :id="safeId()"
+         :style="{background}"
     >
         <slot name="img">
-            <img class="d-block img-fluid" v-if="img" :src="img" :alt="imgAlt">
+            <b-img v-if="imgSrc || imgBlank"
+                   fluid-grow
+                   block
+                   :blank="imgBlank"
+                   :blank-color="imgBlankColor"
+                   :src="imgSrc"
+                   :width="computedWidth"
+                   :height="computedHeight"
+                   :alt="imgAlt"></b-img>
         </slot>
         <div :is="contentTag" :class="contentClasses">
             <h3 v-if="caption" :is="captionTag" v-html="caption"></h3>
@@ -16,16 +24,45 @@
 </template>
 
 <script>
+    import bImg from './img';
+    import { warn } from '../utils';
+    import { idMixin } from '../mixins';
+
     export default {
+        components: { bImg },
+        mixins: [ idMixin ],
         props: {
-            id: {
-                type: String
+            imgSrc: {
+                type: String,
+                default() {
+                    if (this && this.src) {
+                        // Deprecate src
+                        warn("b-carousel-slide: prop 'src' has been deprecated. Use 'img-src' instead");
+                        return this.src;
+                    }
+                    return null;
+                }
             },
-            img: {
+            src: {
+                // Deprecated: use img-src instead
                 type: String
             },
             imgAlt: {
                 type: String
+            },
+            imgWidth: {
+                type: [Number, String]
+            },
+            imgHeight: {
+                type: [Number, String]
+            },
+            imgBlank: {
+                type: Boolean,
+                default: false
+            },
+            imgBlankColor: {
+                type: String,
+                default: 'transparent'
             },
             contentVisibleUp: {
                 type: String
@@ -50,9 +87,6 @@
             },
             background: {
                 type: String
-            },
-            height: {
-                type: String
             }
         },
         computed: {
@@ -62,6 +96,14 @@
                     this.contentVisibleUp ? 'd-none' : '',
                     this.contentVisibleUp ? `d-${this.contentVisibleUp}-block` : ''
                 ];
+            },
+            computedWidth() {
+                // Use local width, or try parent width
+                return this.imgWidth || this.$parent.imgWidth;
+            },
+            computedHeight() {
+                // Use local height, or try parent height
+                return this.imgHeight || this.$parent.imgHeight;
             }
         }
     };
