f770f962e5e5cdd6d169605df650136971776c89
Troy Morehouse <troymore@nbnet.nb.ca>
2017-09-05 16:37:14 -0300

    Create form-option.vue

---
+++ lib/components/form-option.vue
@@ -0,0 +1,17 @@
+<template>
+    <option :value="value" :disabled="diabled"><slot></slot></option>
+</template>
+
+<script>
+    export default {
+        props: {
+            value: {
+                // We use a component here to be able to bind objects as value
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        }
+    };
+</script>
