0b0793ea5ba81ba26900b93529b7e44776210845
Troy Morehouse <troymore@nbnet.nb.ca>
2017-09-13 19:47:04 -0300

    Create _reference.vue

---
+++ docs/nuxt/pages/docs/reference/_reference.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="bd-content" v-html="readme" v-play></div>
+    </div>
+</template>
+
+
+<script>
+import reference from '../../../../reference';
+
+export default {
+    layout: 'docs',
+
+    fetch({ params, redirect }) {
+        if (!reference[params.reference]) {
+            redirect('/docs/reference/' + Object.keys(reference)[0])
+        }
+    },
+
+    data() {
+        return Object.assign({ meta: {}, readme: '' }, reference[this.$route.params.reference])
+    },
+
+    head() {
+        return {
+            title: `${this.meta.title} - BootstrapVue`
+        };
+    }
+};
+</script>
