14b6021584fa93e0320c925b1634c21f1e7e1e00
Troy Morehouse <troymore@nbnet.nb.ca>
2017-09-13 22:15:04 -0300

    [img-lazy] Use new dom utils methods

--- lib/components/img-lazy.vue
+++ lib/components/img-lazy.vue
@@ -17,6 +17,7 @@
 
 <script>
     import bImg from './img';
+    import { isVisible, getBCR, eventOn, eventOff } from './uutils/dom';
 
     const THROTTLE = 100;
 
@@ -119,38 +120,39 @@
             this.checkView();
         },
         beforeDdestroy() {
-            this.setListeners(flase);
+            this.setListeners(false);
         },
         methods: {
             setListeners(on) {
                 const root = window;
                 if (on) {
-                    root.addEventListener('scroll', this.onScroll);
-                    root.addEventListener('resize', this.onScroll);
-                    root.addEventListener('orientationchange', this.onScroll);
+                    eventOn(root, 'scroll', this.onScroll);
+                    eventOn(root, 'resize', this.onScroll);
+                    eventOn(root, 'orientationchange', this.onScroll);
                 } else {
-                    root.removeEventListener('scroll', this.onScroll);
-                    root.removeEventListener('resize', this.onScroll);
-                    root.removeEventListener('orientationchange', this.onScroll);
+                    eventOff(root, 'scroll', this.onScroll);
+                    eventOff(root, 'resize', this.onScroll);
+                    eventOff(root, 'orientationchange', this.onScroll);
                     clearTimeout(this.scrollTimer);
                     this.scrollTimout = null;
                 }
             },
             checkView() {
                 // check bounding box + offset to see if we should show 
-                if (this.$el.offsetParent === null || !(this.$el.offsetWidth > 0 || this.$el.offsetHeight > 0)) {
+                if (!isVisible(this.$el)) {
                     // Element is hidden, so skip for now
                     return;
                 }
                 const offset = parseInt(this.offset,10) || 0;
+                const doElement = document.documentElement;
                 const view = {
-                    left: 0 - offset,
-                    top: 0 - offset,
-                    bottom: document.documentElement.clientHeight + offset,
-                    right: document.documentElement.clientWidth + offset
+                    l: 0 - offset,
+                    t: 0 - offset,
+                    b: docElement.clientHeight + offset,
+                    r: docElement.clientWidth + offset
                 };
-                const box = this.$el.getBoundingClientRect();
-                if (box.right >= view.left && box.bottom >= view.top && box.left <= view.right && box.top <= view.bottom) {
+                const box = getBCR(this.$el);
+                if (box.right >= view.l && box.bottom >= view.t && box.left <= view.r && box.top <= view.b) {
                      // image is in view (or about to be in view)
                      this.isShown = true;
                      this.setListeners(false);
