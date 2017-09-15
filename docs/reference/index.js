576b6e70f6709d5bdd240fa8c410df01555a4933
Troy Morehouse <troymore@nbnet.nb.ca>
2017-09-15 12:13:31 -0300

    Update index.js

--- docs/reference/index.js
+++ docs/reference/index.js
@@ -4,5 +4,6 @@ export default {
     'variants': require('./variants').default,
     'sizes': require('./sizes').default,
     'spacing': require('./spacing').default,
-    'router-links': require('./router-links').default
+    'router-links': require('./router-links').default,
+    'examples': require('./examples').default
 };
