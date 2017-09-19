0123bbddaf8fb0f05a2f78770ae290a838723517
Troy Morehouse <troymore@nbnet.nb.ca>
2017-09-18 22:27:34 -0300

    Update changelog.vue

--- docs/nuxt/pages/docs/misc/changelog.vue
+++ docs/nuxt/pages/docs/misc/changelog.vue
@@ -16,7 +16,7 @@
         },
         created() {
             // We remove the h3 tags because the all have the same generated ID
-            this.$root.$emit('bv-docs::update::toc', this.readme.replace(/<h3.*?</h3>/g, '') || '');
+            this.$root.$emit('bv-docs::update::toc', this.readme.replace(/<h3.*?<\/h3>/g, '') || '');
         }
     };
 </script>
