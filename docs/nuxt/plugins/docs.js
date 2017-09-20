9866022a0d2fb28b276a62fc1095f4e16a61a194
Troy Morehouse <troymore@nbnet.nb.ca>
2017-09-20 00:25:43 -0300

    chore(docs): Import layout and renference into docs.js

--- docs/nuxt/plugins/docs.js
+++ docs/nuxt/plugins/docs.js
@@ -1,6 +1,8 @@
 // Extract docs into main chunk
 import '../../components'
 import '../../directives'
+import '../../reference'
+import '../../layout'
 
 // Disable vue global error handler
 import Vue from 'vue'
