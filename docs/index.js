cd29b7a83ab18d8fbe2ea5fbec47ce8a6ddd364c
Troy Morehouse <troymore@nbnet.nb.ca>
2017-09-20 17:01:37 -0300

    docs(toc): Move toc object to site nav object (#1084)
    * docs(toc): Move toc object to site nav object
    
    * Update toc.vue

--- docs/index.js
+++ docs/index.js
@@ -4,9 +4,63 @@ import directives from './directives'
 import reference from './reference'
 import layout from './layout'
 import misc from './misc'
+import setup from '~/../README.md';
 
+// Process an hTML readme and create a page TOC array
+function processHeadings(readme) {
+    if (!readme) {
+        return [];
+    }
+    const toc = [];
+    // Grab all the H2 and H3 tags with ID's from readme
+    const headings = readme.match(/<h[23].*? id="[^"]+".+?<\/h\d>/g) || [];
+    let h2Idx = 0;
+    headings.forEach(heading => {
+        // Pass the link, label and heading level
+        const matches = heading.match(/^<(h[23]).*? id="([^"]+)"[^>]*>(.+?)<\/h\d>$/);
+        const tag = matches[1];
+        const href = `#${matches[2]}`;
+        // Remove any HTML markup in the label
+        const label = matches[3].replace(/<[^>]+>/g, '');
+        if (tag === 'h2') {
+            toc.push({ href, label });
+            h2Idx = toc.length;
+        } else if (tag === 'h3') {
+            toc[h2Idx] = toc[h2Idx] || [];
+            toc[h2Idx].push({ href, label });
+        }
+    });
+    return toc;
+}
+
+// Our documentation sections
+const SECTIONS = {
+    components,
+    directives,
+    reference,
+    misc
+};
+
+// TOC object
+const TOC = {};
+
+// Special case for /docs
+TOC['/docs/'] = processHeadings(setup);
+
+// Special case for /layout
+TOC['/docs/layout/'] = processHeadings(layout.readme);
+
+// Process the rest of the sections
+Object.keys(SECTIONS).forEach(section => {
+    Object.keys(SECTIONS[section]).forEach(page => {
+      TOC[`/docs/${section}/${page}/`] = processHeadings(SECTIONS[section][page].readme);
+    });
+});
+
+// Out site object
 export default {
     package_info,
+    toc: TOC,
     nav: [
         {
             title: 'Getting started',
