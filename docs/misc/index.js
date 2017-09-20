227d9a1d740c241d459e09794c4330c47db62869
Troy Morehouse <troymore@nbnet.nb.ca>
2017-09-20 14:48:06 -0300

    docs: Create _misc.vue handler page (#1082)
    * Create index.js
    
    * Update index.js
    
    * Create _misc.vue
    
    * Delete index.vue
    
    * Delete changelog.vue
    
    * Delete contributing.vue
    
    * Update docs.js

---
+++ docs/misc/index.js
@@ -0,0 +1,8 @@
+/* eslint-disable quote-props */
+import changelog from '../../CHANGELOG.md';
+import contributing from '../../CONTRIBUTING.md';
+
+export default {
+    'changelog': { meta: { title: 'Release Notes' }, readme: changelog },
+    'contributing': { meta: { title: 'Contributing' }, readme: contributing }
+};
