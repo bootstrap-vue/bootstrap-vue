d4f63f514a1e16f268d05aa485f87f61ba325c75
Troy Morehouse <troymore@nbnet.nb.ca>
2017-09-20 04:32:55 -0300

    Create _misc.vue

---
+++ docs/nuxt/pages/docs/misc/_misc.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <div class="bd-content" v-html="readme"></div>
+    </div>
+</template>
+
+<script>
+import docs from '~/../misc';
+
+export default {
+    layout: 'docs',
+
+    fetch({ params, redirect }) {
+        if (!docs[params.misc]) {
+            redirect('/docs/misc/' + Object.keys(docs)[0])
+        }
+    },
+
+    data() {
+        return Object.assign({ meta: {}, readme: '' }, docs[this.$route.params.misc])
+    },
+
+    head() {
+        return {
+            title: `${this.meta.title} - BootstrapVue`
+        };
+    }
+};
+</script>
