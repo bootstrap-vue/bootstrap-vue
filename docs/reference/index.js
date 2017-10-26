0a56caad90c808870a4909d129643d3c0b901a66
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-26 14:47:19 -0300

    docs(reference): fix images path URL

--- docs/reference/index.js
+++ docs/reference/index.js
@@ -4,7 +4,7 @@ export default {
     'variants': require('./variants').default,
     'sizes': require('./sizes').default,
     'spacing': require('./spacing').default,
-    'imgages': require('./images').default,
+    'images': require('./images').default,
     'router-links': require('./router-links').default,
     'examples': require('./examples').default
 };
