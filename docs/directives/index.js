607e39159e55a933e7e0e6c8f5aedd944dc52808
Pooya Parsa <pooya@pi0.ir>
2017-10-27 11:38:43 +0330

    refactor structure
    combine src/docs/tests

--- docs/directives/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable quote-props */
-
-export default {
-    'tooltip': require('./tooltip').default,
-    'popover': require('./popover').default,
-    'scrollspy': require('./scrollspy').default
-};
