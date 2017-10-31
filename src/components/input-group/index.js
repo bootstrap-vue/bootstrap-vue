772a27d323a501fab2e42bce3fe3e344ed2d3feb
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-30 22:43:36 -0300

    Update index.js

--- src/components/input-group/index.js
+++ src/components/input-group/index.js
@@ -1,6 +1,6 @@
-import bInputGroup `./input-group.vue`;
-import bInputGroupAddon `./input-group-addon`;
-import bInputGroupButton `./input-group-button`;
+import bInputGroup from './input-group.vue';
+import bInputGroupAddon from './input-group-addon';
+import bInputGroupButton from './input-group-button';
 
 const VuePlugin = {
   install(Vue) {
