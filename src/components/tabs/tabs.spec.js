b29256d5d77e1856cfa63389a8d423dfe3b73ac4
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 01:27:56 -0300

    Update and rename tab.spec.js to tabs.spec.js

--- src/components/tabs/tabs.spec.js
+++ src/components/tabs/tabs.spec.js
@@ -2,6 +2,6 @@ import {loadFixture, testVM} from '../../../tests/utils';
 
 
 describe('tab', async() => {
-    beforeEach(loadFixture(__dirname, 'tab'));
+    beforeEach(loadFixture(__dirname, 'tabs'));
     testVM();
 });
