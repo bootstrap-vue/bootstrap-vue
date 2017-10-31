059f78f7723e82515e90aa0edd61c6fa1dbd8b57
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-30 21:07:20 -0300

    Modal component Plugin

---
+++ src/components/modal/index.js
@@ -0,0 +1,15 @@
+import bModal `./modal.vue`;
+import modalPlugin from '../../directives/modal';
+
+const VuePlugin = {
+  install(Vue) {
+    Vue.component(bModal);
+    Vue.use(modalPlugin);
+  }
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(VuePlugin);
+};
+
+export default VuePlugin
