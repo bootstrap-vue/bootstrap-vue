84078e8321db3d4832dd8ec4512618f5a4229f82
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 17:50:19 -0300

    Update index.js

--- src/directives/tooltip/index.js
+++ src/directives/tooltip/index.js
@@ -9,8 +9,8 @@ const directives = {
 
 const VuePlugin = {
   install(Vue) {
-    if (!registerDirective(Vue, 'tooltip')) {
-      for (var directive in directives) {
+    for (var directive in directives) {
+      if (!registerDirective(Vue, directive)) {
         Vue.directive(directive, directives[directive]);
       }
     }
