717f8eb589ee0d9f5b032c12803218692376f9bc
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 16:48:27 -0300

    Update index.js

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -3,6 +3,7 @@ import bDropdownItem from './dropdown-item';
 import bDropdownItemButton from './dropdown-item-button';
 import bDropdownHeader from './dropdown-header';
 import bDropdownDivider from './dropdown-divider';
+import { registerComponent } from '../../utils';
 
 /* eslint-disable no-var, no-undef, guard-for-in, object-shorthand */
 
@@ -23,8 +24,10 @@ const components = {
 
 const VuePlugin = {
   install(Vue) {
-    for (var component in components) {
-      Vue.component(component, components[component]);
+    if (!registerComponent(Vue, 'dropdown')) {
+      for (var component in components) {
+        Vue.component(component, components[component]);
+      }
     }
   }
 };
