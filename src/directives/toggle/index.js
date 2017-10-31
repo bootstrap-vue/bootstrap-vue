e2cf62d58f7c2d18e147312af45d2d87254f2d17
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-30 21:35:04 -0300

    Toggle plugin

---
+++ src/directives/toggle/index.js
@@ -0,0 +1,13 @@
+import bToggle `./toggle`;
+
+const VuePlugin = {
+  install(Vue) {
+    Vue.directive(bToggle);
+  }
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(VuePlugin);
+};
+
+export default VuePlugin;
