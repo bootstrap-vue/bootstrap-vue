7ea18519c32aefa95bbc56020d5b1511d5dbe8f0
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 01:10:50 -0300

    Update index.js

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,9 +1,17 @@
 import bModal from './modal.vue';
 import modalPlugin from '../../directives/modal';
 
+/* eslint-disable no-var, no-undef, guard-for-in, object-shorthand */
+
+const components = {
+  bModal
+};
+
 const VuePlugin = {
   install(Vue) {
-    Vue.component(bModal);
+    for (var component in components) {
+      Vue.component(component, components[component]);
+    }
     Vue.use(modalPlugin);
   }
 };
