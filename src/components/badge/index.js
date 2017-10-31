39a2a47fc8a113eb707e58755582b64156549aee
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 16:14:54 -0300

    Update index.js

--- src/components/badge/index.js
+++ src/components/badge/index.js
@@ -1,4 +1,5 @@
 import bBadge from './badge';
+import registerComponent from '../../utils';
 
 /* eslint-disable no-var, no-undef, guard-for-in, object-shorthand */
 
@@ -9,7 +10,9 @@ const components = {
 const VuePlugin = {
   install(Vue) {
     for (var component in components) {
-      Vue.component(component, components[component]);
+      if (!registerComponent(Vue, 'badge')) {
+        Vue.component(component, components[component]);
+      }
     }
   }
 };
