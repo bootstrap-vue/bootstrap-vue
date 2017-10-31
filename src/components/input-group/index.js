8b17295d35e16730c295eaadf0fbebb37b48fa2a
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 01:04:43 -0300

    Update index.js

--- src/components/input-group/index.js
+++ src/components/input-group/index.js
@@ -2,12 +2,20 @@ import bInputGroup from './input-group.vue';
 import bInputGroupAddon from './input-group-addon';
 import bInputGroupButton from './input-group-button';
 
+/* eslint-disable no-var, no-undef, guard-for-in, object-shorthand */
+
+const components = {
+  bInputGroup,
+  bInputGroupAddon,
+  bInputGroupButton,
+  bInputGroupBtn: bInputGroupButton,
+};
+
 const VuePlugin = {
   install(Vue) {
-    Vue.component(bInputGroup);
-    Vue.component(bInputGroupAddon);
-    Vue.component(bInputGroupButton);
-    Vue.component(bInputGroupButton as bInputGroupBtn);
+    for (var component in components) {
+      Vue.component(component, components[component]);
+    }
   }
 };
 
