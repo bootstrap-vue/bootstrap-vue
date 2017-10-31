915431890e322e609b8fefc3e4b3745acfcf12d2
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-30 21:59:06 -0300

    form group plugin

---
+++ src/components/form-group/index.js
@@ -0,0 +1,14 @@
+import bFormGRoup `./form-group.vue`;
+
+const VuePlugin = {
+  install(Vue) {
+    Vue.component(bFormGroup);
+    Vue.component(bFormGroup as bFormFieldset);
+  }
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(VuePlugin);
+};
+
+export default VuePlugin;
