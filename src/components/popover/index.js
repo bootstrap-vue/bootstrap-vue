5948fe16a4fedfd967253302ea8dd0b9d595f269
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 17:15:10 -0300

    Update index.js

--- src/components/popover/index.js
+++ src/components/popover/index.js
@@ -1,4 +1,5 @@
 import bPopover from './popover.vue';
+import { registerComponent } from '../../utils';
 
 /* eslint-disable no-var, no-undef, guard-for-in, object-shorthand */
 
@@ -9,7 +10,9 @@ const components = {
 const VuePlugin = {
   install(Vue) {
     for (var component in components) {
-      Vue.component(component, components[component]);
+      if (!registerComponent(Vue, component)) {
+        Vue.component(component, components[component]);
+      }
     }
   }
 };
