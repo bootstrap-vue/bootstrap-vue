290443fbee863959f5747e23ddd4011ee0e51a1a
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 17:18:47 -0300

    Update index.js

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -9,8 +9,8 @@ const components = {
 
 const VuePlugin = {
   install(Vue) {
-    if (!registerComponent(Vue, 'jumbotron')) {
-      for (var component in components) {
+    for (var component in components) {
+      if (!registerComponent(Vue, component)) {
         Vue.component(component, components[component]);
       }
     }
