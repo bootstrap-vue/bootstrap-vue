2bc03ec45e0385ea90ce373fcacbf87f854d9d6b
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-30 21:25:52 -0300

    Button-group plugin

---
+++ src/components/button-group/index.js
@@ -0,0 +1,14 @@
+import bButtonGroup `./button-group`;
+
+const VuePlugin = {
+  install(Vue) {
+    Vue.component(bButtonGroup);
+    Vue.component(bButtonGroup as bBtnGroup);
+  }
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(VuePlugin);
+};
+
+export default VuePlugin;
