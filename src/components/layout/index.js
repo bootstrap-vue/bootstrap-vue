53988d8ecd932c14bf5960915fc98ab77c659bc3
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-30 22:45:10 -0300

    Update index.js

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,7 @@
-import bContainer `./container`;
-import bRow `./row`;
-import bCol `./col`;
-import bFormRow `../form/form-row`;
+import bContainer from './container';
+import bRow from './row';
+import bCol from './col';
+import bFormRow from '../form/form-row';
 
 const VuePlugin = {
   install(Vue) {
