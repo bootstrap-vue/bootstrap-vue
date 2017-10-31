c37b949ce5ab4018a092abd1b8fcf1946cc1f63d
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 17:37:52 -0300

    Update index.js

--- src/components/form-group/index.js
+++ src/components/form-group/index.js
@@ -10,8 +10,8 @@ const components = {
 
 const VuePlugin = {
   install(Vue) {
-    if (!registerComponent(Vue, 'form-group')) {
-      for (var component in components) {
+    for (var component in components) {
+      if (!registerComponent(Vue, component)) {
         Vue.component(component, components[component]);
       }
     }
