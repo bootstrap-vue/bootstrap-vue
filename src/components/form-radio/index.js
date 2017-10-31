fbf361f9920676364c1c260af1bea6a5a8f2d8ac
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 00:39:24 -0300

    Update index.js

--- src/components/form-radio/index.js
+++ src/components/form-radio/index.js
@@ -1,12 +1,20 @@
 import bFormRadio from './form-radio.vue';
 import bFormRadioGroup from './form-radio-group.vue';
 
+/* eslint-disable no-var, no-undef, guard-for-in, object-shorthand */
+
+const components = {
+  bFormRadio,
+  bFormRadio as bRadio,
+  bFormRadioGroup,
+  bFormRadioGroup as bRadioGroup
+};
+
 const VuePlugin = {
   install(Vue) {
-    Vue.component(bFormRadio);
-    Vue.component(bFormRadio as bRadio);
-    Vue.component(bFormRadioGroup);
-    Vue.component(bFormRadioGroup as bRadioGroup);
+    for (var component in components) {
+      Vue.component(component, components[component]);
+    }
   }
 };
 
