42fc0e29d84f51601e6b109b109542c579a6234c
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 17:39:30 -0300

    Update index.js

--- src/components/form-radio/index.js
+++ src/components/form-radio/index.js
@@ -13,8 +13,8 @@ const components = {
 
 const VuePlugin = {
   install(Vue) {
-    if (!registerComponent(Vue, 'form-radio')) {
-      for (var component in components) {
+    for (var component in components) {
+      if (!registerComponent(Vue, component)) {
         Vue.component(component, components[component]);
       }
     }
