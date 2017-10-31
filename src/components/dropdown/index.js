98b8b90938f6a307579098f0289e5549051350cb
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 17:34:34 -0300

    Update index.js

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -24,8 +24,8 @@ const components = {
 
 const VuePlugin = {
   install(Vue) {
-    if (!registerComponent(Vue, 'dropdown')) {
-      for (var component in components) {
+    for (var component in components) {
+      if (!registerComponent(Vue, component)) {
         Vue.component(component, components[component]);
       }
     }
