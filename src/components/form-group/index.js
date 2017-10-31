e3977c53ee76ee821dbb2c1fa4ead52976f6ea91
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 16:53:35 -0300

    Update index.js

--- src/components/form-group/index.js
+++ src/components/form-group/index.js
@@ -1,4 +1,5 @@
 import bFormGroup from './form-group.vue';
+import { registerComponent } from '../../utils';
 
 /* eslint-disable no-var, no-undef, guard-for-in, object-shorthand */
 
@@ -9,8 +10,10 @@ const components = {
 
 const VuePlugin = {
   install(Vue) {
-    for (var component in components) {
-      Vue.component(component, components[component]);
+    if (!registerComponent(Vue, 'form-group')) {
+      for (var component in components) {
+        Vue.component(component, components[component]);
+      }
     }
   }
 };
