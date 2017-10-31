5ef99646830af79e58fda11425f5be232ca2e169
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-31 17:36:12 -0300

    Update index.js

--- src/components/form-checkbox/index.js
+++ src/components/form-checkbox/index.js
@@ -15,8 +15,8 @@ const components = {
 
 const VuePlugin = {
   install(Vue) {
-    if (!registerComponent(Vue, 'form-checkbox')) {
-      for (var component in components) {
+    for (var component in components) {
+      if (!registerComponent(Vue, component)) {
         Vue.component(component, components[component]);
       }
     }
