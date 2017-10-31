a869bf07bface523e877c901e40d6650f197c027
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-30 22:04:37 -0300

    Form select plugin

---
+++ src/components/form-select/index.js
@@ -0,0 +1,14 @@
+import bFormSelect `./form-select.vue`;
+
+const VuePlugin = {
+  install(Vue) {
+    Vue.component(bFormSelect);
+    Vue.component(bFormSelect as bSelect);
+  }
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(VuePlugin);
+};
+
+export default VuePlugin;
