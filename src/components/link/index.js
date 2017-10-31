10d0f1b778a36180b4dc87ade3b10f32d28df4a3
Troy Morehouse <troymore@nbnet.nb.ca>
2017-10-30 22:17:36 -0300

    Link plugin

---
+++ src/components/link/index.js
@@ -0,0 +1,13 @@
+import bLink `./link`;
+
+const VuePlugin = {
+  install(Vue) {
+    Vue.component(bLink);
+  }
+};
+
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(VuePlugin);
+};
+
+export default VuePlugin;
