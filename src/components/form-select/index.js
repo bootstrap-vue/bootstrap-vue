ac3d350482feb106bf4f6df05e6722a889f70b0c
Troy Morehouse <troymore@nbnet.nb.ca>
2017-11-01 01:10:45 -0300

    Update index.js

--- src/components/form-select/index.js
+++ src/components/form-select/index.js
@@ -11,9 +11,7 @@ const components = {
 const VuePlugin = {
   install(Vue) {
     for (var component in components) {
-      if (!registerComponent(Vue, component)) {
-        Vue.component(component, components[component]);
-      }
+      registerComponent(Vue, component, components[component]);
     }
   }
 };
