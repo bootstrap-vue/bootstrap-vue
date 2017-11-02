f788b8d8d6b024fbceb1bf2ffb98c3cc183d2270
Troy Morehouse <troymore@nbnet.nb.ca>
2017-11-02 17:16:46 -0300

    Update index.js

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -1,4 +1,4 @@
-import bAlert from './alert.vue';
+import bAlert from './alert';
 import { registerComponent } from '../../utils';
 
 /* eslint-disable no-var, no-undef, guard-for-in, object-shorthand */
