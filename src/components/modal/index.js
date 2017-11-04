0277e1f7a6b0bdc7ac876c895c24092785be49f0
Troy Morehouse <troymore@nbnet.nb.ca>
2017-11-04 18:43:22 -0300

    Update index.js

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,6 +1,6 @@
 import bModal from './modal';
 import modalPlugin from '../../directives/modal';
-import { registerComponent } from '../../utils';
+import { registerComponents, vueUse } from '../../utils';
 
 /* eslint-disable no-var, no-undef, guard-for-in, object-shorthand */
 
@@ -10,15 +10,11 @@ const components = {
 
 const VuePlugin = {
   install(Vue) {
-    for (var component in components) {
-      registerComponent(Vue, component, components[component]);
-    }
+    registerComponents(Vue, components);
     Vue.use(modalPlugin);
   }
 };
 
-if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(VuePlugin);
-};
+vueUse(VuePlugin);
 
 export default VuePlugin
