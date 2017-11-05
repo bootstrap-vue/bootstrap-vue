bd4c3c38ce0c4317b8a95e1ab4bae3ef06bd61ea
Troy Morehouse <troymore@nbnet.nb.ca>
2017-11-05 00:16:38 -0300

    perf(input-group): convert to functional component (#1288)
    * Temp comment out test
    
    * perf(input-gropup): Convert to functional component
    
    * re-Enable tests
    
    * Delete input-group.vue
    
    * Update input-group.html

--- src/components/input-group/input-group.js
+++ src/components/input-group/input-group.js
@@ -1,3 +1,65 @@
-import bInputGroup from './input-group.vue';
+import { mergeData } from "../../utils";
+import InputGroupAddon from './input-group-addon';
 
-export default bInputGroup;
+export const props = {
+    id: {
+        type: String,
+        default: null
+    },
+    size: {
+        type: String,
+        default: null
+    },
+    left: {
+        type: String,
+        default: null
+    },
+    right: {
+        type: String,
+        default: null
+    },
+    tag: {
+        type: String,
+        default: 'div'
+    }
+};
+
+export default {
+    functional: true,
+    props: props,
+    render(h, { props, data, slots }) {
+        let childNodes = [];
+
+        // Left Slot / Prop
+        if (slots().left) {
+            childNodes.push(slots().left);
+        } else if (props.left) {
+            childNodes.push(h(InputGroupAddon, { domProps: { innerHTML: props.left } }))
+        }
+
+        // Default slot
+        childNodes.push(slots().default);
+
+        // Right slot / prop
+        if (slots().right) {
+            childNodes.push(slots().right);
+        } else if (props.left) {
+            childNodes.push(h(InputGroupAddon, { domProps: { innerHTML: props.right } }))
+        }
+      
+        return h(
+            props.tag,
+            mergeData(data, {
+                staticClass: 'input-group',
+                class: {
+                    [`input-group-${props.size}`]: Boolean(props.size)
+                },
+                attrs: {
+                    id: props.id || null,
+                    role: "group"
+                }
+            }),
+            childNodes
+        );
+    }
+};
