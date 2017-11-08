f5828374bfd6ed99e9f292712954503baf5ec47b
Troy Morehouse <troymore@nbnet.nb.ca>
2017-11-07 20:57:18 -0400

    [navbar-toggle] convert to render function

--- src/components/navbar/navbar-toggle.js
+++ src/components/navbar/navbar-toggle.js
@@ -1,3 +1,50 @@
-import bNavbarToggle from './navbar-toggle.vue';
+import { listenOnRootMixin } from '../../mixins';
 
-export default bNavbarToggle;
+export default {
+    mixins: [listenOnRootMixin],
+    render(h) {
+        const t = this;
+        return h(
+            'button',
+            {
+                class: [ 'navbar-toggler' ],
+                attrs: {
+                    type: 'button',
+                    'aria-label': t.label,
+                    'aria-controls': t.target,
+                    'aria-expanded': t.toggleState ? 'true' : 'false'
+                },
+                on: { click: t.onClick }
+            },
+            [ t.$slots.default || h('span', { class: [ 'navbar-toggler-icon' ] }) ]
+        );
+    },
+    data() {
+        return {
+            toggleState: false
+        };
+    },
+    props: {
+        label: {
+            type: String,
+            default: 'Toggle navigation'
+        },
+        target: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        onClick() {
+            this.$root.$emit('bv::toggle::collapse', this.target);
+        },
+        handleStateEvt(id, state) {
+            if (id === this.target) {
+                this.toggleState = state;
+            }
+        }
+    },
+    created() {
+        this.listenOnRoot('bv::collapse::state', this.handleStateEvt);
+    }
+};
