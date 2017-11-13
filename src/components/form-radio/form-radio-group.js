3a2aa0a19024ce1454e5d62f513fd8bc9a6a4707
Troy Morehouse <troymore@nbnet.nb.ca>
2017-11-12 21:49:01 -0400

    perf(form-radios): convert templates to render functions (#1336)
    * perf(form-radio): convert templates to render functions
    
    * Delete form-radio.vue
    
    * [form-radio-group] convert to render functions
    
    * Update form-radio-group.js
    
    * Delete form-radio-group.vue
    
    * Update form-radio.js
    
    * Update form-radio.js

--- src/components/form-radio/form-radio-group.js
+++ src/components/form-radio/form-radio-group.js
@@ -1,3 +1,117 @@
-import bFormRadioGroup from './form-radio-group.vue';
+import { idMixin, formMixin, formOptionsMixin, formSizeMixin, formStateMixin, formCustomMixin } from '../../mixins';
+import bFormRadio from './form-radio';
 
-export default bFormRadioGroup;
+export default {
+    mixins: [idMixin, formMixin, formSizeMixin, formStateMixin, formCustomMixin, formOptionsMixin],
+    components: { bFormRadio },
+    render(h) {
+        const t = this;
+        const $slots = t.$slots;
+        
+        const radios = t.formOptions.map((option, idx) => {
+            return h(
+                'b-form-radio',
+                {
+                    key: `radio_${idx}_opt`,
+                    props: {
+                        id: t.safeId(`_BV_radio_${idx}_opt_`),
+                        name: t.name,
+                        value: option.value,
+                        required: Boolean(t.name && t.required),
+                        disabled: option.disabled,
+                    }
+                },
+                [ h('span', { domProps: { innerHTML: option.text } }) ]
+            );
+        });
+        return h(
+            'div',
+            {
+                class: t.groupClasses,
+                attrs: {
+                    id: t.safeId(),
+                    role: 'radiogroup',
+                    tabindex: '-1',
+                    'data-toggle': t.buttons ? 'buttons' : null, // Needed for styling only!
+                    'aria-required': t.required ? 'true' : null,
+                    'aria-invalid': t.computedAriaInvalid
+                }
+            },
+            [ $slots.first, radios, $slots.default ]
+        );
+    },
+    data() {
+        return {
+            localChecked: this.checked,
+            // Flag for children
+            is_RadioCheckGroup: true
+          };
+    },
+    model: {
+        prop: 'checked',
+        event: 'input'
+    },
+    props: {
+        checked: {
+            type: [String, Object, Number, Boolean],
+            default: null
+        },
+        validated: {
+            // Used for applying hte `was-validated` class to the group
+            type: Boolean,
+            default: false
+        },
+        ariaInvalid: {
+            type: [Boolean, String],
+            default: false
+        },
+        stacked: {
+            type: Boolean,
+            default: false
+        },
+        buttons: {
+            // Render as button style
+            type: Boolean,
+            default: false
+        },
+        buttonVariant: {
+            // Only applicable when rendered with button style
+            type: String,
+            default: 'secondary'
+        }
+    },
+    watch: {
+        checked(newVal, oldVal) {
+            this.localChecked = this.checked;
+        },
+        localChecked(newVal, oldVal) {
+            this.$emit('input', newVal);
+        }
+    },
+    computed: {
+        groupClasses() {
+            if (this.buttons) {
+                return [
+                    this.stacked ? 'btn-group-vertical' : 'btn-group',
+                    this.size ? `btn-group-${this.size}` : '',
+                    this.validated ? `was-validated` : ''
+                ];
+            }
+            return [
+                this.sizeFormClass,
+                (this.stacked && this.custom) ? 'custom-controls-stacked' : '',
+                this.validated ? `was-validated` : ''
+           ];
+        },
+        computedAriaInvalid() {
+            if (this.ariaInvalid === true || this.ariaInvalid === 'true' || this.ariaInvalid === '') {
+                return 'true';
+            }
+            return this.get_State === false ? 'true' : null;
+        },
+        get_State() {
+            // Required by child radios
+            return this.computedState;
+        }
+    }
+};
--- src/components/form-radio/form-radio.js
+++ src/components/form-radio/form-radio.js
@@ -1,3 +1,92 @@
-import bFormRadio from './form-radio.vue';
+import { idMixin, formRadioCheckMixin, formMixin, formStateMixin } from '../../mixins';
+import { looseEqual } from '../../utils';
+import { isArray } from '../../utils/array';
 
-export default bFormRadio;
+export default {
+    mixins: [idMixin, formRadioCheckMixin, formMixin, formStateMixin],
+    render(h) {
+        const t = this;
+        
+        const input = h(
+            'input',
+            {
+                ref: 'radio',
+                class: [ t.is_ButtonMode ? '' : (t.is_Plain ? 'form-check-input' : 'custom-control-input'), t.get_StateClass ],
+                directives: [
+                    { name: 'model', rawName: 'v-model', value: t.computedLocalChecked, expression: 'computedLocalChecked' }
+                ],
+                attrs: {
+                    id: t.safeId(),
+                    type: 'radio',
+                    name: t.get_Name,
+                    required: t.get_Name && t.is_Required,
+                    disabled: t.is_Disabled,
+                    autocomplete: 'off'
+                },
+                domProps: { value: t.value, checked: looseEqual(t.computedLocalChecked, t.value) },
+                on: {
+                    focus: t.handleFocus,
+                    blur: t.handleFocus,
+                    change: t.emitChange,
+                    __c: (evt) => { t.computedLocalChecked = t.value; }
+                }
+            }
+        );
+        
+        let indicator = h(false);
+        if (!t.is_ButtonMode && !t.is_Plain) {
+            indicator = h('span', { class: 'custom-control-indicator', attrs: { 'aria-hidden': 'true' } });
+        }
+
+        const description = h(
+            'span',
+            { class: t.is_ButtonMode ? null : (t.is_Plain ? 'form-check-description' : 'custom-control-description') },
+            [ t.$slots.default ]
+        );
+        
+        const label = h(
+            'label',
+            { class: [ t.is_ButtonMode ? t.buttonClasses : t.is_Plain ? 'form-check-label' : t.labelClasses ] },
+            [ input, indicator, description ]
+        );
+        
+        if (t.is_Plain && !t.is_ButtonMode) {
+            return h('div', { class: ['form-check', { 'form-check-inline': !t.is_Stacked } ] }, [ label ]);
+        } else {
+            return label;
+        }
+    },
+    watch: {
+        // Radio Groups can only have a single value, so our watchers are simple
+        checked(newVal, oldVal) {
+            this.computedLocalChecked = newVal;
+        },
+        computedLocalChceked(newVal, oldVal) {
+            this.$emit('input', this.computedLocalChceked);
+        }
+    },
+    computed: {
+        is_Checked() {
+            return looseEqual(this.value, this.computedLocalChecked);
+        },
+        labelClasses() {
+            // Specific to radio
+            return [
+                Boolean(this.get_Size) ? `form-control-${this.get_Size}` : '',
+                'custom-control',
+                'custom-radio',
+                this.get_StateClass
+            ];
+        }
+    },
+    methods: {
+        emitChange({ target: { checked } }) {
+            // Change is only emitted on user interaction
+            this.$emit('change', checked ? this.value : null);
+            // If this is a child of form-radio-group, we emit a change event on it as well
+            if (this.is_Child) {
+                this.$parent.$emit('change', this.computedLocalChecked);
+            }
+        }
+    }
+};
