1bdb13d4a7e9cd7f58ea3f48da455c69614f5c9a
Troy Morehouse <troymore@nbnet.nb.ca>
2017-11-14 22:05:09 -0400

    [pagination] Use mixin render function

--- src/components/pagination/pagination.vue
+++ src/components/pagination/pagination.vue
@@ -1,133 +1,15 @@
-<template>
-    <ul :class="['pagination',btnSize,alignment]"
-        :aria-disabled="disabled ? 'true' : 'false'"
-        :aria-label="ariaLabel ? ariaLabel : null"
-        role="menubar"
-        @keydown.left.prevent="focusPrev"
-        @keydown.right.prevent="focusNext"
-        @keydown.shift.left.prevent="focusFirst"
-        @keydown.shift.right.prevent="focusLast"
-    >
-
-        <!-- Goto First Page button -->
-        <template v-if="!hideGotoEndButtons">
-            <li v-if="isActive(1) || disabled" class="page-item disabled" role="none presentation" aria-hidden="true">
-                <span class="page-link" v-html="firstText"></span>
-            </li>
-            <li v-else class="page-item" role="none presentation">
-                <a class="page-link"
-                   :aria-label="labelFirstPage"
-                   :aria-controls="ariaControls || null"
-                   role="menuitem"
-                   href="#"
-                   tabindex="-1"
-                   @click.prevent="setPage($event, 1)"
-                   @keydown.enter.prevent="setPage($event, 1)"
-                   @keydown.space.prevent="setPage($event, 1)"
-                ><span aria-hidden="true" v-html="firstText"></span></a>
-            </li>
-        </template>
-
-        <!-- Goto Previous page button -->
-        <li v-if="isActive(1) || disabled" class="page-item disabled" role="none presentation" aria-hidden="true">
-            <span class="page-link" v-html="prevText"></span>
-        </li>
-        <li v-else class="page-item" role="none presentation">
-            <a class="page-link"
-               :aria-label="labelPrevPage"
-               :aria-controls="ariaControls || null"
-               role="menuitem"
-               href="#"
-               tabindex="-1"
-               @click.prevent="setPage($event, currentPage - 1)"
-               @keydown.enter.prevent="setPage($event, currentPage - 1)"
-               @keydown.space.prevent="setPage($event, currentPage - 1)"
-            ><span aria-hidden="true" v-html="prevText"></span></a>
-        </li>
-
-        <!-- First Ellipsis Bookend -->
-        <li v-if="showFirstDots" class="page-item disabled d-none d-sm-flex" role="separator">
-            <span class="page-link" v-html="ellipsisText"></span>
-        </li>
-
-        <!-- Pages links -->
-        <li v-for="page in pageList" role="none presentation" :class="pageItemClasses(page)" :key="page.number">
-            <a :class="pageLinkClasses(page)"
-               :disabled="disabled"
-               :aria-disabled="disabled ? 'true' : null"
-               :aria-label="labelPage + ' ' + page.number"
-               :aria-checked="isActive(page.number) ? 'true' : 'false'"
-               :aria-controls="ariaControls || null"
-               :aria-posinset="page.number"
-               :aria-setsize="numberOfPages"
-               role="menuitemradio"
-               href="#"
-               :tabindex="isActive(page.number) ? '0' : '-1'"
-               @click.prevent="setPage($event, page.number)"
-               @keydown.enter.prevent="setPage($event, page.number)"
-               @keydown.space.prevent="setPage($event, page.number)"
-            >{{ page.number }}</a>
-        </li>
-
-        <!-- Last Ellipsis Bookend -->
-        <li v-if="showLastDots" class="page-item disabled d-none d-sm-flex" role="separator">
-            <span class="page-link" v-html="ellipsisText"></span>
-        </li>
-
-        <!-- Goto Next page -->
-        <li v-if="isActive(numberOfPages) || disabled" class="page-item disabled" role="none presentation" aria-hidden="true">
-            <span class="page-link" v-html="nextText"></span>
-        </li>
-        <li v-else class="page-item" role="none presentation">
-            <a class="page-link"
-               :aria-label="labelNextPage"
-               :aria-controls="ariaControls || null"
-               role="menuitem"
-               href="#"
-               tabindex="-1"
-               @click.prevent="setPage($event, currentPage + 1)"
-               @keydown.enter.prevent="setPage($event, currentPage + 1)"
-               @keydown.space.prevent="setPage($event, currentPage + 1)"
-            ><span aria-hidden="true" v-html="nextText"></span></a>
-        </li>
-
-        <!-- Goto Last page -->
-        <template v-if="!hideGotoEndButtons">
-            <li v-if="isActive(numberOfPages) || disabled"
-                class="page-item disabled"
-                role="none presentation"
-                aria-hidden="true"
-            >
-                <span class="page-link" v-html="lastText"></span>
-            </li>
-            <li v-else class="page-item" role="none presentation">
-                <a class="page-link"
-                   :aria-label="labelLastPage"
-                   :aria-controls="ariaControls || null"
-                   role="menuitem"
-                   href="#"
-                   tabindex="-1"
-                   @click.prevent="setPage($event, numberOfPages)"
-                   @keydown.enter.prevent="setPage($event, numberOfPages)"
-                   @keydown.space.prevent="setPage($event, numberOfPages)"
-                ><span aria-hidden="true" v-html="lastText"></span></a>
-            </li>
-        </template>
-    </ul>
-</template>
-
-<style scoped>
-    .page-item {
+<style>
+    .b-pagination .page-item {
         -webkit-user-select: none;
         -moz-user-select: none;
         -ms-user-select: none;
         user-select: none;
     }
-    .page-item.disabled {
+    .b-pagination .page-item.disabled {
         cursor: not-allowed;
         opacity: .65;
     }
-    .page-item .page-link.active:focus {
+    .b-pagination .page-item .page-link.active:focus {
         box-shadow: 0 0 0 3px rgba(0,123,255,.5);
         z-index: 1;
     }
@@ -136,6 +18,7 @@
 <script>
 import { paginationMixin } from '../../mixins';
 import { isVisible } from '../../utils/dom';
+import { KeyCodes } from '../../utils';
 
 const props = {
     perPage: {
@@ -152,6 +35,7 @@ const props = {
     }
 };
 
+// Our render function is brought in from the pagination mixin
 export default {
     mixins: [ paginationMixin ],
     props,
@@ -162,28 +46,31 @@ export default {
         }
     },
     methods: {
-        setPage(e, num) {
-            if (this.disabled) {
-                e.preventDefault();
-                e.stopPropagation();
-                return;
-            }
+        // These methods are used by the render function
+        onClick(num, evt) {
+            // Handle edge cases where number of pages has changed (i.e. if perPage changes)
             if (num > this.numberOfPages) {
-                this.currentPage = this.numberOfPages;
+                num = this.numberOfPages;
             } else if (num < 1) {
-                this.currentpage = 1;
-            } else {
-                this.currentPage = num;
+                num = 1;
             }
+            this.currentPage = num;
             this.$nextTick(() => {
                 // Keep the current button focused if possible
-                if (isVisible(e.target) && e.target.focus) {
-                    e.target.focus();
+                const target = evt.target;
+                if (isVisible(target) && this.$el.contains(target) && target.focus) {
+                    target.focus();
                 } else {
                     this.focusCurrent();
                 }
             });
             this.$emit('change', this.currentPage);
+        },
+        makePage(pagenum) {
+            return pagenum;
+        },
+        linkProps(pagenum) {
+            return { href: '#' };
         }
     }
 };
