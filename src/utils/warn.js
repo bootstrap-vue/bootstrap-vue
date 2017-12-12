9615ac415d8ac2da035928f40f39845a93c58d2d
Troy Morehouse <troymore@nbnet.nb.ca>
2017-12-11 21:45:53 -0400

    chore: tests/coverage

--- src/utils/warn.js
+++ src/utils/warn.js
@@ -2,6 +2,7 @@
  * Log a warning message to the console with bootstrap-vue formatting sugar.
  * @param {string} message
  */
+/* istanbul ignore next */
 function warn (message) {
   console.warn(`[Bootstrap-Vue warn]: ${message}`)
 }
