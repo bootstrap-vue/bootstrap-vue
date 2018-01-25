873b0e7164a834cb5cc4f62696e453c37dcf0ce0
Pooya Parsa <pooya@pi0.ir>
2018-01-25 20:40:25 +0330

    perf(id mixin): make localId_ a computed field

--- src/mixins/id.js
+++ src/mixins/id.js
@@ -10,16 +10,6 @@ export default {
       default: null
     }
   },
-  data () {
-    return {
-      localId_: null
-    }
-  },
-  mounted () {
-    if (!this.$isServer && !this.id && typeof this._uid !== 'undefined') {
-      this.localId_ = `__BVID__${this._uid}_`
-    }
-  },
   methods: {
     safeId (suffix = '') {
       const id = this.id || this.localId_ || null
@@ -27,7 +17,14 @@ export default {
         return null
       }
       suffix = String(suffix).replace(/\s+/g, '_')
-      return suffix ? `${id}_${suffix}` : id
+      return suffix ? id + '_' + suffix : id
+    }
+  },
+  computed: {
+    localId_ () {
+      if (!this.$isServer && !this.id && typeof this._uid !== 'undefined') {
+        return '__BVID__' + this._uid
+      }
     }
   }
 }
