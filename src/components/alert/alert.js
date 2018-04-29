0999b4cc4b4943741aef71367dc354af45decbc0
Emanuel Mutschlechner <emanuelmutschlechner@users.noreply.github.com>
2018-04-29 16:26:46 +0200

    feat(alert): Add fade prop (#1785)
    * feat(alert): Add fade prop
    
    * docs(alert): Add example for fading alerts and fix some errors

--- src/components/alert/alert.js
+++ src/components/alert/alert.js
@@ -1,5 +1,7 @@
 import bButtonClose from '../button/button-close'
 
+import './alert.css'
+
 export default {
   components: {bButtonClose},
   render (h) {
@@ -16,10 +18,15 @@ export default {
         [ this.$slots.dismiss ]
       )
     }
-    return h(
+    const alert = h(
       'div',
-      { class: this.classObject, attrs: { role: 'alert', 'aria-live': 'polite', 'aria-atomic': true } },
-      [ dismissBtn, this.$slots.default ]
+      {class: this.classObject, attrs: {role: 'alert', 'aria-live': 'polite', 'aria-atomic': true}},
+      [dismissBtn, this.$slots.default]
+    )
+    return !this.fade ? alert : h(
+      'transition',
+      { props: { name: 'fade', appear: true } },
+      [ alert ]
     )
   },
   model: {
@@ -59,6 +66,10 @@ export default {
     },
     show: {
       type: [Boolean, Number],
+      default: false
+    },
+    fade: {
+      type: Boolean,
       default: false
     }
   },
