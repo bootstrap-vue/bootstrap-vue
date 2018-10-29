5cd6623e070be67d8c290d98e94f343c616012b1
Jacob Müller <jacob.mueller.elz@gmail.com>
2018-10-29 21:44:51 +0100

    fix(tooltip): Add missing directive to plugin
    This commit adds the missing tooltip directive to the plugin.

--- src/components/tooltip/index.js
+++ src/components/tooltip/index.js
@@ -1,4 +1,5 @@
 import bTooltip from './tooltip'
+import tooltipPlugin from '../../directives/tooltip'
 import { registerComponents, vueUse } from '../../utils/plugins'
 
 const components = {
@@ -8,6 +9,7 @@ const components = {
 const VuePlugin = {
   install (Vue) {
     registerComponents(Vue, components)
+    Vue.use(tooltipPlugin)
   }
 }
 
