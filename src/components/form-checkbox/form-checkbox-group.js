dd7fad42e1340fd05d3ff80e8ef597beeb78631c
Troy Morehouse <troymore@nbnet.nb.ca>
2018-10-30 15:48:43 -0300

    form-checkbox-group import input-group.css

--- src/components/form-checkbox/form-checkbox-group.js
+++ src/components/form-checkbox/form-checkbox-group.js
@@ -7,6 +7,9 @@ import formCustomMixin from '../../mixins/form-custom'
 
 import bFormCheckbox from './form-checkbox'
 
+// Needed when checknox-groups are inside an input group
+import '../input-group/input-group.css'
+
 export default {
   mixins: [
     idMixin,
