77797143163e75f15e4f30b7b837782db08c7aff
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-03 19:10:18 -0300

    prepend lint

--- src/components/input-group/input-group-prepend.js
+++ src/components/input-group/input-group-prepend.js
@@ -7,8 +7,8 @@ export default {
   render (h, { data, children }) {
     // pass all our props/attrs down to child, and set`append` to false
     return h(InputGroupAddon,
-             mergeData(data, { props: {append: false} }),
-             children
+      mergeData(data, { props: {append: false} }),
+      children
     )
   }
 }
