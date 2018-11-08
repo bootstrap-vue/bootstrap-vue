f4f5dfbc6072d938668be7276be38095be602c8a
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-08 03:14:28 -0400

    fix(button): ensure the appropriate role and tab index set on non links or buttons
    Ensure that the nonstandard tag is available in tab index and has an appropriate role, and that it is in tab order when not disabled.
    
    Fixes for ARIA compliance

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -54,24 +54,97 @@ function handleFocus (evt) {
   }
 }
 
+// Helper functons to minimize runtime memory footprint when lots of buttons on page
+
+// Is the requested button a link?
+function isLink (props) {
+  // If tag prop is set to `a`, we use a b-link to get proper disabled handling
+  return Boolean(props.href || props.to || props.tag.toLowerCase() === 'a') 
+}
+
+// Is the button to be a toggle button?
+function isToggle (props) {
+  return typeof props.pressed === 'boolean'
+}
+
+// Is the button "really" a button?
+function isButton (props) {
+  return props.tag.tolowerCase() === 'button' && !isLink(props)
+}
+
+// Is the requested tag not a button or link?
+function isNonStandardTag(props) {
+   return !isLink(props) && !isButtonTag(props)
+}
+
+// Compute required classes (non static classes)
+function computeClass (props) {
+  return [
+    props.variant ? `btn-${props.variant}` : `btn-secondary`,
+    {
+      [`btn-${props.size}`]: Boolean(props.size),
+      'btn-block': props.block,
+      disabled: props.disabled,
+      active: props.pressed
+    }
+  ]
+}
+
+// Compute the link props to pass to b-link (if required)
+function computeLinkProps (props) {
+  return isLink(props) ? pluckProps(linkPropKeys, props) : null
+}
+
+// Compute the attributes for a button
+function computeAttrs (props, data) {
+  const button = isButton(props)
+  const link = isLink(props)
+  const toggle = isToggle(props)
+  const nonStdTag = isNonStandardTag(props)
+  const role = data.attrs && data.attrs['role'] ? data.attrs['role'] : null
+  let tabindex = data.attrs ? data.attrs['tabindex'] : null
+  if (nonStdTag) {
+    tabindex = '0'
+  }
+  return {
+    // Type only used for "real" buttons
+    type: button ? props.type : null,
+    // Disabled only set on "real" buttons
+    disabled: button ? props.disabled : null,
+    // We add a role of button when the tag is not a link or button for ARIA.
+    // Don't bork any role provided in data.attrs when isLink or isButton
+    role: nonStdTag ? 'button' : role,
+    // We set the aria-disabled state for non-standard tags
+    'aria-disabled': nonStdTag ? String(props.disabled) : null,
+    // For toggles, we need to set the pressed state for ARIA
+    'aria-pressed': toggle ? String(props.pressed) : null,
+    // autocomplete off is needed in toggle mode to prevent some browsers from
+    // remembering the previous setting when using the back button.
+    autocomplete: toggle ? 'off' : null,
+    // Tab index is used when the component is not a button.
+    // Links are tabable, but don't allow disabled, while non buttons or links
+    // are not tabable, so we mimic that functionality by disabling tabbing
+    // when disabled, and adding a tabindex of '0' to non buttons or non links.
+    tabindex: props.disabled && !button ? '-1' : tabindex
+  }
+}
+
 export default {
   functional: true,
   props,
-  render (h, { props, attrs, data, listeners, children }) {
-    // If tag prop is set to `a`, we use a b-link to get proper disabled handling
-    const isLink = Boolean(props.href || props.to || props.tag.toLowerCase() === 'a') 
-    const isToggle = typeof props.pressed === 'boolean'
-    const isButtonTag = props.tag.tolowerCase() === 'button'
-    const nonStandardTag = !isLink && !isButtonTag
+  render (h, { props, data, listeners, children }) {
+    const toggle = isToggle(props)
+    const link = isLink(props)
     const on = {
       click (e) {
         if (props.disabled && e instanceof Event) {
           e.stopPropagation()
           e.preventDefault()
-        } else if (isToggle) {
+        } else if (isToggle(props) && listeners && listeners['update:pressed']) {
+          // Send .sync updates to any "pressed" prop (if .sync listeners)
           // Concat will normalize the value to an array
           // without double wrapping an array value in an array.
-          concat(listeners['update:pressed'] || []).forEach(fn => {
+          concat(listeners['update:pressed']).forEach(fn => {
             if (typeof fn === 'function') {
               fn(!props.pressed)
             }
@@ -80,43 +153,19 @@ export default {
       }
     }
 
-    if (isToggle) {
+    if (isToggle(props)) {
       on.focusin = handleFocus
       on.focusout = handleFocus
     }
 
     const componentData = {
       staticClass: 'btn',
-      class: [
-        props.variant ? `btn-${props.variant}` : `btn-secondary`,
-        {
-          [`btn-${props.size}`]: Boolean(props.size),
-          'btn-block': props.block,
-          disabled: props.disabled,
-          active: props.pressed
-        }
-      ],
-      props: isLink ? pluckProps(linkPropKeys, props) : null,
-      attrs: {
-        // Type only used for tag button
-        type: isButtonTag && !isLink ? props.type : null,
-        disabled: isButtonTag && !isLink ? props.disabled : null,
-        // We add a role of button when the tag is not a link or button for ARIA.
-        // Don't bork the role provided in attrs when not a nonStandardTag
-        role: nonStandardTag ? 'button' : ((attrs && attrs['role']) ? attrs['role'] : null)
-        'aria-pressed': isToggle ? String(props.pressed) : null,
-        // Tab index is used when the component becomes a link or not a button.
-        // Links are tabable, but don't allow disabled, while non buttons or links
-        // are not tabable, so we mimic that functionality by disabling tabbing
-        // when disabled, and adding a tabindex of '0' to non buttons or non links.
-        tabindex:
-          props.disabled && (isLink || nonStandardTag)
-            ? '-1'
-            : data.attrs ? (data.attrs['tabindex'] || (nonStandardTag ? '0' : null)) : null 
-      },
+      class: computeClass(props),
+      props: computeLinkProps(props),
+      attrs: computedAttrs(props, data),
       on
     }
 
-    return h(isLink ? Link : props.tag, mergeData(data, componentData), children)
+    return h(link ? Link : props.tag, mergeData(data, componentData), children)
   }
 }
