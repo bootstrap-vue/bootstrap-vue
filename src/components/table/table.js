bootstrap-vue/bootstrap-vue

commit 5ee0923501e01d63881c2e0dfe3df4b7b3513123

fix(Table) allow empty labels

src/components/table/table.js
@@ -1,5 +1,4 @@
 import startCase from 'lodash.startcase'
-
 import looseEqual from '../../utils/loose-equal'
 import stableSort from '../../utils/stable-sort'
 import KeyCodes from '../../utils/key-codes'
@@ -763,12 +762,12 @@ export default {
           fields.push({ key: k, label: startCase(k) })
         })
       }
-      // Ensure we have a unique array of fields and that they have labels
+      // Ensure we have a unique array of fields and that they have String labels
       const memo = {}
       return fields.filter(f => {
         if (!memo[f.key]) {
           memo[f.key] = true
-          f.label = f.label || startCase(f.key)
+          f.label = typeof f.label === 'string' ? f.label : startCase(f.key)
           return true
         }
         return false

commit 6aea6681c232290db5b7212a50fb6a320a27047a

Add field key to header column aria-label when label is blank string
When the fields's label was set to a blank string, this left non-sighted users at a disadvantage as to what the column is about.

This adds the field's key (humanized) to the `aria-label` to provides hint to non-sighted users.

Alternatively if a field title is provided, then the aria-label is not altered.

Addresses ARIA issues introduced by PR #1587

src/components/table/table.js
@@ -126,6 +126,22 @@ export default {
     // factory function for thead and tfoot cells (th's)
     const makeHeadCells = (isFoot = false) => {
       return fields.map((field, colIndex) => {
+        let ariaLabel = ''
+        if (!(field.label.trim()) && !field.headerTitle) {
+          // In case field's label and title are empty/balnk
+          // We need to add a hint about what the column is about for non-dighted users
+          ariaLabel = _startCase(field.key)
+        }
+        const ariaLabelSorting = field.sortable
+          ? this.localSortDesc && this.localSortBy === field.key
+            ? this.labelSortAsc
+            : this.labelSortDesc
+          : null
+        // Assemble the aria-label
+        ariaLabel = [ariaLabel, ariaLabelSorting].filter(a => a).join(': ') || null
+        const ariaSort = field.sortable && this.localSortBy === field.key
+                ? (this.localSortDesc ? 'descending' : 'ascending')
+                : null
         const data = {
           key: field.key,
           class: this.fieldClasses(field),
@@ -135,15 +151,8 @@ export default {
             abbr: field.headerAbbr || null,
             title: field.headerTitle || null,
             'aria-colindex': String(colIndex + 1),
-            'aria-label': field.sortable
-              ? this.localSortDesc && this.localSortBy === field.key
-                ? this.labelSortAsc
-                : this.labelSortDesc
-              : null,
-            'aria-sort':
-              field.sortable && this.localSortBy === field.key
-                ? this.localSortDesc ? 'descending' : 'ascending'
-                : null
+            'aria-label': ariaLabel,
+            'aria-sort': ariaSort
           },
           on: {
             click: evt => {