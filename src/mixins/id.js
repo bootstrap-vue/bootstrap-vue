da51f97a676e96527fcb7952a6c32f8add063e70
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-12 02:35:48 -0400

    Update id.js

--- src/mixins/id.js
+++ src/mixins/id.js
@@ -18,16 +18,28 @@ export default {
   },
   mounted () {
     // mounted only occurs client side
-    this.localId_ = `__BVID__${this._uid}`
+    this.$nextTick(() => {
+      // Update dom with auto ID after dom loaded to prevent
+      // SSR hydration errors.
+      this.localId_ = `__BVID__${this._uid}`
+    })
   },
-  methods: {
-    safeId (suffix = '') {
-      const id = this.id || this.localId_ || null
-      if (!id) {
-        return null
+  computed: {
+    safeId () {
+      // Computed property that returns a dynamic function for creating the ID.
+      // Reacts to changes in both .id and .localId_ And regens a new function  
+      const id = this.id || this.localId_
+
+      // We return a function that accepts an optional suffix string
+      // So this computed prop looks and works like a method!!!
+      const fn = (suffix) => {
+        if (!id) {
+          return null
+        }
+        suffix = String(suffix || '').replace(\/s+/g, '_')
+        return suffix ? id + '_' + suffix : id
       }
-      suffix = String(suffix).replace(/\s+/g, '_')
-      return suffix ? id + '_' + suffix : id
+      return fn
     }
   }
 }
