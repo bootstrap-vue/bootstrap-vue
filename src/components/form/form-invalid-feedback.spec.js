c4f59a361619b37b693246ddddbdcd02541dc7b9
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-16 13:43:43 -0400

    Update form-invalid-feedback.spec.js

--- src/components/form/form-invalid-feedback.spec.js
+++ src/components/form/form-invalid-feedback.spec.js
@@ -3,7 +3,7 @@ import { mount } from '@vue/test-utils'
 
 describe('form-invalid-feedback', async () => {
   it('default should have tag div', async () => {
-    const feedback = mount(Fedback)
+    const feedback = mount(Feedback)
     expect(feedback.is('div')).toBe(true)
   })
 
@@ -13,17 +13,17 @@ describe('form-invalid-feedback', async () => {
   })
 
   it('default should not have class d-block', async () => {
-    const feedback = mount(Feedback) 
+    const feedback = mount(Feedback)
     expect(feedback.classes()).not.toContain('d-block')
   })
 
   it('default should not have class invalid-tooltip', async () => {
-    const feedback = mount(Feedback) 
+    const feedback = mount(Feedback)
     expect(feedback.classes()).not.toContain('invalid-tooltip')
   })
 
   it('default should not have id', async () => {
-    const feedback = mount(Feedback) 
+    const feedback = mount(Feedback)
     expect(feedback.attributes('id')).not.toBeDefined()
   })
 
@@ -32,7 +32,7 @@ describe('form-invalid-feedback', async () => {
       propsData: {
         id: 'foobar'
       }
-    }) 
+    })
     expect(feedback.attributes('id')).toBe('foobar')
   })
 
@@ -46,29 +46,29 @@ describe('form-invalid-feedback', async () => {
   })
 
   it('should contain class d-block when force-show is set', async () => {
-    const feedback = mount(Feedback,  {
+    const feedback = mount(Feedback, {
       propsData: {
         forceShow: true
       }
     })
-    expect(input.classes()).toContain('d-block')
+    expect(feedback.classes()).toContain('d-block')
   })
 
   it('should contain class invalid-tooltip when tooltip is set', async () => {
-    const feedback = mount(Feedback,  {
+    const feedback = mount(Feedback, {
       propsData: {
         tooltip: true
       }
     })
-    expect(input.classes()).toContain('invalid-tooltip')
+    expect(feedback.classes()).toContain('invalid-tooltip')
   })
 
   it('should not contain class invalid-feedback when tooltip is set', async () => {
-    const feedback = mount(Feedback,  {
+    const feedback = mount(Feedback, {
       propsData: {
         tooltip: true
       }
     })
-    expect(input.classes()).not.toContain('invalid-feedbac88')
+    expect(feedback.classes()).not.toContain('invalid-feedbac88')
   })
 })
