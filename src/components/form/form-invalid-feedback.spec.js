3ccfd69e8498394e77073496e9a410b366646e8b
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-16 17:43:38 -0400

    Update form-invalid-feedback.spec.js

--- src/components/form/form-invalid-feedback.spec.js
+++ src/components/form/form-invalid-feedback.spec.js
@@ -90,8 +90,5 @@ describe('form-invalid-feedback', async () => {
     })
     expect(feedback.text()).toContain('foo')
     expect(feedback.text()).toContain('bar')
-    // contains and find appear to only work on non-functional components
-    // expect(feedback.contains('span')).toBe(true)
-    expect(feedback.html()).toContain('foo<span>bar</span>')
   })
 })
