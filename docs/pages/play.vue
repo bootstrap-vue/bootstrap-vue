93a4b8da76e4ae6c25b2f26002f3d48c14dad0e0
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-16 04:51:09 -0400

    Update play.vue

--- docs/pages/play.vue
+++ docs/pages/play.vue
@@ -118,7 +118,11 @@
               <span>Clear</span>
             </b-btn>
           </div>
-          <transition-group tag="ul" name="flip-list" v-if="messages.length" class="list-group list-group-flush">
+          <transition-group
+            tag="ul"
+            name="flip-list"
+            v-if="messages.length"
+            class="list-group list-group-flush play-log">
             <li
               v-for="(message, idx) in messages"
               :class="['list-group-item','list-group-item-${message[0]}']"
@@ -141,7 +145,7 @@
 .flip-move {
   transition: all .3s;
 }
-.list-group-item {
+.play-log .list-group-item {
  transition: all .3s;
 }
 .flip-list-enter, .flip-list-leave-to {
