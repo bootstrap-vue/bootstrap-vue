f3f27a108d63a84681c986126c47f50ad400dbb5
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-18 19:01:08 -0400

    Update table-busy.spec.js

--- src/components/table/table-busy.spec.js
+++ src/components/table/table-busy.spec.js
@@ -40,25 +40,24 @@ describe('b-table busy state', async () => {
     expect(wrapper.attributes('aria-busy')).toEqual('true')
   })
 
-  it('should render table-busy slot when busy=true and slot provided', async () => {
+  it('should have attribute aria-busy=true when data localBusy=true', async () => {
     const wrapper = mount(Table, {
       propsData: {
-        busy: true,
         items: testItems
-      },
-      slots: {
-        'table-busy': 'busy slot content'
       }
     })
     expect(wrapper.attributes('aria-busy')).toBeDefined()
+    expect(wrapper.attributes('aria-busy')).toEqual('false')
+
+    wrapper.setData({
+      localBusy: true
+    })
+
+    expect(wrapper.attributes('aria-busy')).toBeDefined()
     expect(wrapper.attributes('aria-busy')).toEqual('true')
-    expect(wrapper.find('tbody').exists()).toBe(true)
-    expect(wrapper.find('tbody').findAll('tr').exists()).toBe(true)
-    expect(wrapper.find('tbody').findAll('tr').length).toBe(1)
-    expect(wrapper.find('tbody').text()).toContain('busy slot content')
   })
 
-  it('should not render table-busy slot when busy=false and slot provided', async () => {
+  it('should render table-busy slot when busy=true and slot provided', async () => {
     const wrapper = mount(Table, {
       propsData: {
         busy: false,
@@ -69,27 +68,21 @@ describe('b-table busy state', async () => {
       }
     })
     expect(wrapper.attributes('aria-busy')).toBeDefined()
-    expect(wrapper.attributes('aria-busy')).toEqual('true')
+    expect(wrapper.attributes('aria-busy')).toEqual('false')
     expect(wrapper.find('tbody').exists()).toBe(true)
     expect(wrapper.find('tbody').findAll('tr').exists()).toBe(true)
     expect(wrapper.find('tbody').findAll('tr').length).toBe(testItems.length)
-    expect(wrapper.find('tbody').text()).not.toContain('busy slot content')
-  })
 
-  it('should set class b-table-busy-slot on tbody>tr when busy=true and slot provided', async () => {
-    const wrapper = mount(Table, {
-      propsData: {
-        busy: true,
-        items: testItems
-      },
-      slots: {
-        'table-busy': 'busy slot content'
-      }
+    wrapper.setProps({
+      busy: true
     })
+
     expect(wrapper.attributes('aria-busy')).toBeDefined()
     expect(wrapper.attributes('aria-busy')).toEqual('true')
     expect(wrapper.find('tbody').exists()).toBe(true)
-    expect(wrapper.find('tbody').find('tr').exists()).toBe(true)
+    expect(wrapper.find('tbody').findAll('tr').exists()).toBe(true)
+    expect(wrapper.find('tbody').findAll('tr').length).toBe(1)
+    expect(wrapper.find('tbody').text()).toContain('busy slot content')
     expect(wrapper.find('tbody').find('tr').classes()).toContain('b-table-busy-slot')
   })
 })
