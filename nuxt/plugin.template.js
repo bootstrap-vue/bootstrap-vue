c47d30a65ece02d6eaf3c55577c8d2821fb39cd5
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-25 18:34:49 -0400

    Update plugin.template.js
    use minified CSS

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
-<% if (options.bootstrapVueCss) { %>include 'bootstrap-vue/dist/bootstrap-vue.css'<% } %>
+<% if (options.bootstrapVueCss) { %>include 'bootstrap-vue/dist/bootstrap-vue.min.css'<% } %>
 
 Vue.use(BootstrapVue)
