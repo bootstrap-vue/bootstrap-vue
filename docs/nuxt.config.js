ad5244d9a91d8328b3b255212f766a7808fb80ce
Jacob Müller <jacob.mueller.elz@gmail.com>
2018-11-25 19:27:49 +0100

    Move import to plugin

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -87,7 +87,6 @@ module.exports = {
 
   css: [
     'bootstrap/dist/css/bootstrap.css',
-    '../src/index.scss',
     'highlightjs/styles/atom-one-light.css',
     'codemirror/lib/codemirror.css',
     '~assets/css/docs.min.css',
