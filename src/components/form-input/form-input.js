d9d638d80e3cb735075bb992abac936d60279ca0
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-25 06:09:02 -0400

    Update form-input.js

--- src/components/form-input/form-input.js
+++ src/components/form-input/form-input.js
@@ -8,10 +8,7 @@ import formValidityMixin from '../../mixins/form-validity'
 import { arrayIncludes } from '../../utils/array'
 import { eventOn, eventOff } from '../../utils/dom'
 
-// Import styles for input type=color
-import './form-input-type-color.css'
-// Import temp styles and fixes for input type=range (custom-range)
-import './form-input-type-range.css'
+import './form-input.css'
 
 // Valid supported input types
 const TYPES = [
