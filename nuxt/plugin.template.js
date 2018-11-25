177352c39834f5ac6435a96233d072d644d58380
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-25 17:45:07 -0400

    Update plugin.template.js

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
-<% if (options.bootstrapVueCSS) { %>include 'bootstrap-vue/dist/bootstrap-vue.css'<% } %>
+<% if (options.bootstrapVueCss) { %>include 'bootstrap-vue/dist/bootstrap-vue.css'<% } %>
 
 Vue.use(BootstrapVue)
