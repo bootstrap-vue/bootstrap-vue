95cc76c6eed78d765d6d95435ec94b5dc3b9640a
Troy Morehouse <troymore@nbnet.nb.ca>
2018-11-25 23:50:01 -0400

    Update nuxt.config.js

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -86,9 +86,6 @@ module.exports = {
   },
 
   head: {
-    meta: [
-      { 'http-equiv': 'X-UA-COmpatible', content: 'IE-edge' }
-    ],
     script: [
       { type: 'javascript', src: '//unpkg.com/babel-polyfill@latest/dist/polyfill.min.js' }
     ]
