e02c1e85e1416ed0be308ca8a16ee6b60cc84cc1
Troy Morehouse <troymore@nbnet.nb.ca>
2018-12-06 22:07:44 -0400

    tests: for-checkbox add standalone button styling tests

--- src/components/form-checkbox/form-checkbox.spec.js
+++ src/components/form-checkbox/form-checkbox.spec.js
@@ -4,6 +4,24 @@ import { mount } from '@vue/test-utils'
 describe('form-checkbox', async () => {
   /* Custom checkbox structure, class and attributes tests */
 
+  it('default has structure <div><input/><label></label></div>', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        checked: '',
+        value: 'a'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+    expect(wrapper).toBeDefined()
+    expect(wrapper.is('div')).toBe(true)
+    const children = wrapper.element.children
+    expect(children.length).toEqual(2)
+    expect(children[0].tagName).toEqual('INPUT')
+    expect(children[1].tagName).toEqual('LABEL')
+  })
+
   it('default has wrapper class custom-control and custom-checkbox', async () => {
     const wrapper = mount(Input, {
       propsData: {
@@ -230,6 +248,26 @@ describe('form-checkbox', async () => {
   })
 
   /* plain styling */
+
+  it('plain has structure <div><input/><label></label></div>', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        plain: true,
+        checked: '',
+        value: 'a'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+    expect(wrapper).toBeDefined()
+    expect(wrapper.is('div')).toBe(true)
+    const children = wrapper.element.children
+    expect(children.length).toEqual(2)
+    expect(children[0].tagName).toEqual('INPUT')
+    expect(children[1].tagName).toEqual('LABEL')
+  })
+
   it('plain has wrapper class form-check', async () => {
     const wrapper = mount(Input, {
       propsData: {
@@ -303,6 +341,158 @@ describe('form-checkbox', async () => {
     })
     const label = wrapper.find('label')
     expect(label.text()).toEqual('foobar')
+  })
+
+  /* button styling - stand-alone mode*/
+
+  it('stand-alone button has structure <div><label><input/></label></div>', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        button: true,
+        checked: '',
+        value: 'a'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+    expect(wrapper).toBeDefined()
+    expect(wrapper.is('div')).toBe(true)
+    const label = wrapper.element.children
+    expect(label.length).toEqual(1)
+    expect(label[0].tagName).toEqual('LABEL')
+    const input = label[0].children
+    expect(input.length).toEqual(1)
+    expect(input[0].tagName).toEqual('INPUT')
+  })
+
+  it('stand-alone button has wrapper class btn-group-toggle', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        button: true,
+        checked: '',
+        value: 'a'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+    expect(wrapper.classes().length).toEqual(1)
+    expect(wrapper.classes()).toContain('btn-group-toggle')
+  })
+
+  it('stand-alone button has label classes btn and btn-secondary when uchecked', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        button: true,
+        checked: '',
+        value: 'a'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+    const label = wrapper.find('label')
+    expect(label).toBeDefined()
+    expect(label.classes().length).toEqual(2)
+    expect(label.classes()).not.toContain('active')
+    expect(label.classes()).not.toContain('focus')
+    expect(label.classes()).toContain('btn')
+    expect(label.classes()).toContain('btn-secondary')
+  })
+
+  it('stand-alone button has label classes btn, btn-secondary and active when checked by default', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        button: true,
+        checked: 'a',
+        value: 'a'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+    const label = wrapper.find('label')
+    expect(label).toBeDefined()
+    expect(label.classes().length).toEqual(3)
+    expect(label.classes()).not.toContain('focus')
+    expect(label.classes()).toContain('btn')
+    expect(label.classes()).toContain('btn-secondary')
+    expect(label.classes()).toContain('active')
+  })
+
+  it('stand-alone button has label class active when clicked (checked)', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        button: true,
+        checked: '',
+        value: 'a'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+    const label = wrapper.find('label')
+    expect(label).toBeDefined()
+    const input = wrapper.find('input')
+    expect(input).toBeDefined()
+    expect(label.classes().length).toEqual(2)
+    expect(label.classes()).not.toContain('focus')
+    expect(label.classes()).not.toContain('active')
+    expect(label.classes()).toContain('btn')
+    expect(label.classes()).toContain('btn-secondary')
+    input.setChecked(true)
+    expect(label.classes().length).toEqual(3)
+    expect(label.classes()).toContain('active')
+    expect(label.classes()).toContain('btn')
+    expect(label.classes()).toContain('btn-secondary')
+  })
+
+  it('stand-alone button has label class focus when input focused', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        button: true,
+        checked: '',
+        value: 'a'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+    const label = wrapper.find('label')
+    expect(label).toBeDefined()
+    const input = wrapper.find('input')
+    expect(label.classes().length).toEqual(2)
+    expect(label.classes()).not.toContain('focus')
+    expect(label.classes()).not.toContain('active')
+    expect(label.classes()).toContain('btn')
+    expect(label.classes()).toContain('btn-secondary')
+    expect(input).toBeDefined()
+    input.trigger('focus')
+    expect(label.classes().length).toEqual(3)
+    expect(label.classes()).toContain('focus')
+  })
+
+  it('stand-alone button has label btn-primary when prop btn-variant set to primary', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        button: true,
+        btnVariant: 'primary',
+        checked: '',
+        value: 'a'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+    const label = wrapper.find('label')
+    expect(label).toBeDefined()
+    expect(label.classes().length).toEqual(2)
+    expect(label.classes()).not.toContain('focus')
+    expect(label.classes()).not.toContain('active')
+    expect(label.classes()).not.toContain('btn-secondary')
+    expect(label.classes()).toContain('btn')
+    expect(label.classes()).toContain('btn-primary')
   })
 
   /* functionality testing */
