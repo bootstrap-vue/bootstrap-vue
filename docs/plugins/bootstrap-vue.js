33d948ae8cb3de95f8861f71de0f2aa692bbdca1
Troy Morehouse <troymore@nbnet.nb.ca>
2018-12-11 19:57:25 -0400

    Update bootstrap-vue.js

--- docs/plugins/bootstrap-vue.js
+++ docs/plugins/bootstrap-vue.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import BootstrapVue from '../../src'
-import 'bootstrap/scss/bootstrap.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../../src/index.scss'
 import 'highlightjs/styles/atom-one-light.css'
 import 'codemirror/lib/codemirror.css'
