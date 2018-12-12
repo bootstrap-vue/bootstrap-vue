9fc24d2679699db7a166a388deebf7da2dac5215
Troy Morehouse <troymore@nbnet.nb.ca>
2018-12-11 20:47:09 -0400

    Update nuxt.config.js

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -102,11 +102,6 @@ module.exports = {
   },
 
   css: [
-    'bootstrap/dist/css/bootstrap.css',
-    '../src/index.scss',
-    'highlightjs/styles/atom-one-light.css',
-    'codemirror/lib/codemirror.css',
-    '~/assets/css/docs.min.css',
-    '~/assets/css/styles.css'
+    '~/assets/css/main.scss'
   ]
 }
