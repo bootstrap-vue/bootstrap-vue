021e94596dde89fbd29e337069b45d30d84dfc20
Troy Morehouse <troymore@nbnet.nb.ca>
2018-12-15 00:50:49 -0400

    chore(docs): minor updates to docs home page

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -17,18 +17,22 @@
           <div class="col-md-6 order-md-1 text-center text-md-left pr-md-5">
             <h1 class="mb-3 bd-text-purple-bright">Bootstrap + Vue</h1>
             <p class="lead">
-              Build responsive, mobile-first projects on the web using Vue.js and the world's most popular front-end CSS library &mdash; Bootstrap V4.
+              Build responsive, mobile-first projects on the web using Vue.js and the world's most
+              popular front-end CSS library &mdash; Bootstrap V4.1.
             </p>
             <p class="lead mb-3">
-              <span class="bd-text-purple-bright">Bootstrap-Vue</span> provides one of the most comprehensive implementations
-              of <span class="bd-text-purple-bright">Bootstrap V4</span> components and grid system available for Vue.js 2.4+,
-              complete with extensive and automated WAI-ARIA accessibility markup.
+              <span class="bd-text-purple-bright">Bootstrap-Vue</span> provides one of the most
+              comprehensive implementations of <span class="bd-text-purple-bright">Bootstrap V4</span>
+              components and grid system available for Vue.js 2.5+, complete with extensive and
+              automated WAI-ARIA accessibility markup.
             </p>
             <p class="mb-2 text-muted">
-              <a href="https://getbootstrap.com/docs/4.0">Bootstrap 4</a> is the world's most popular framework for building responsive, mobile-first sites.
+              <a href="https://getbootstrap.com/docs/4.0">Bootstrap 4</a> is the world's most popular
+              framework for building responsive, mobile-first sites.
             </p>
             <p class="mb-4 text-muted">
-              <a href="https://vuejs.org"> Vue.js</a> (pronounced /vjuË, like view) is a progressive framework for building user interfaces.
+              <a href="https://vuejs.org"> Vue.js</a> (pronounced /vjuË, like view) is a progressive
+              framework for building user interfaces.
             </p>
             <div class="d-flex flex-column flex-md-row lead mb-3">
               <b-btn
