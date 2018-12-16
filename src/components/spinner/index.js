34e77fa3742061cc34dc3ffbc927b4580eaa6076
Troy Morehouse <troymore@nbnet.nb.ca>
2018-12-16 19:46:46 -0400

    Create index.js

---
+++ src/components/spinner/index.js
@@ -0,0 +1,16 @@
+import BSpinner from './spinner'
+import { registerComponents, vueUse } from '../../utils/plugins'
+
+const components = {
+  BSpinner
+}
+
+const VuePlugin = {
+  install (Vue) {
+    registerComponents(Vue, components)
+  }
+}
+
+vueUse(VuePlugin)
+
+export default VuePlugin
