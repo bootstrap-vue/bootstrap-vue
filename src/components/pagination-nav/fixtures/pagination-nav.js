78c6fca7e2e24a127b1048bbae98d561d1562caa
Troy Morehouse <troymore@nbnet.nb.ca>
2018-12-16 01:47:11 -0400

    update pagination-nav fixture code

--- src/components/pagination-nav/fixtures/pagination-nav.js
+++ src/components/pagination-nav/fixtures/pagination-nav.js
@@ -2,5 +2,13 @@ window.app = new Vue({
   el: '#app',
   data: {
     currentPage: 1
+  },
+  methods: {
+    pageGen (num) {
+      return `Page ${num})`
+    },
+    linkGen (num) {
+      return `#page/${num})`
+    }
   }
 })
