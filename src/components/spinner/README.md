bf3994ffa37c513535c3c6d724f86acca6bf4b4f
Troy Morehouse <troymore@nbnet.nb.ca>
2018-12-17 15:16:35 -0400

    feat(spinner): Pre-Release Bootstrap V4.2 spinners (#2306)

---
+++ src/components/spinner/README.md
@@ -0,0 +1,207 @@
+# Spinners
+
+> The `<b-spinner>` component can be used to show the loading state in your projects. They're rendered
+only with basical HTML and CSS as a lightweight Vue functional component. Their appearance, alignment,
+and sizing can be easily customized with a few built-in props and/or Bootstrap V4 utility classes.
+
+Spinners can be placed just about anywhere, including inside buttons, alerts, and even `<b-table>`'s busy slot.
+
+```html
+<div class="text-center">
+  <b-spinner label="Spinning"></b-spinner>
+  <b-spinner type="grow" label="Spinning"></b-spinner>
+  <b-spinner variant="primary" label="Spinning"></b-spinner>
+  <b-spinner variant="primary" type="grow" label="Spinning"></b-spinner>
+  <b-spinner variant="success" label="Spinning"></b-spinner>
+  <b-spinner variant="success" type="grow" label="Spinning"></b-spinner>
+</div>
+
+<!-- spinners.vue -->
+```
+
+## Spinner types
+
+Bootstrap V4.2 includes two types of spinners. The default spinner type is called `border`
+(spinning circle border), and the optional type `grow` (a throbber style indicator).
+
+### Border spinner
+Use the default `border` type spinners for a lightweight loading indicator.
+
+```html
+<div>
+  <b-spinner label="Loading..."></b-spinner>
+</div>
+
+<!-- spinner-border.vue -->
+```
+
+### Grow spinner
+If you don't fancy a `border` spinner, switch to the `grow` spinner by setting the prop `type` to `'grow'`.
+While it doesn't technically spin, it does repeatedly grow!
+
+```html
+<div>
+  <b-spinner type="grow" label="Loading..."></b-spinner>
+</div>
+
+<!-- spinner-grow.vue -->
+```
+
+## Spinner color variants
+Spinners use `currentColor` for their color, meaning it inherits the current font color.
+You can customize the color using the standard text color variants using the `variant` prop,
+or place classes or styles on the component to change it's color.
+
+The `variant` prop translates the variant name to the bootstrap V4 class `.text-{variant}`, so if
+you have custom defined text color variants, feel free to use them via the `variant` prop.
+
+
+```html
+<template>
+  <div>
+    <div class="text-center mb-2 d-flex justify-content-between">
+      <b-spinner v-for="variant in variants" :variant="variant" :key="variant"></b-spinner>
+    </div>
+    <div class="text-center d-flex justify-content-between">
+      <b-spinner v-for="variant in variants" :variant="variant" type="grow" :key="variant"></b-spinner>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      variants: [
+        'primary', 'secondary', 'danger', 'warning', 'success', 'info', 'light', 'dark'
+      ]
+    }
+  }
+}
+</script>
+
+<!-- spinner-variants.vue -->
+```
+
+**Why not use `border-color` utilities?** Each `border` spinner specifies a `transparent`
+border for at least one side, so `.border-{color}` utilities would override that.
+
+## Size
+Set the prop `small` to `true` to make a smaller spinner that can quickly be used within other components.
+
+```html
+<div>
+  <b-spinner small label="Small Spinner"></b-spinner>
+  <b-spinner small type="grow" label="Small Spinner"></b-spinner>
+</div>
+
+<!-- spinner-sizes.vue -->
+```
+
+Or, use custom CSS or inline styles to change the dimensions as needed.
+
+```html
+<div>
+  <b-spinner style="width: 3rem; height: 3rem;" label="Large Spinner"></b-spinner>
+  <b-spinner style="width: 3rem; height: 3rem;" type="grow" label="Large Spinner"></b-spinner>
+</div>
+
+<!-- spinner-sizes-custom.vue -->
+```
+
+## Alignment
+
+Spinners in Bootstrap are built with `rem`s, `currentColor`, and `display: inline-flex`. This means they
+can easily be resized, recolored, and quickly aligned.
+
+### Margin
+
+Use margin utilities like `.m-5` for easy spacing.
+
+```html
+<div>
+  <b-spinner class="m-5" label="Busy"></b-spinner>
+</div>
+
+<!-- spinner-margin.vue -->
+```
+
+## Placement
+
+Use flexbox utilities, float utilities, or text alignment utility classes to place spinners exactly
+where you need them in any situation.
+
+### Flex
+Using flex utility classes:
+
+```html
+<div>
+  <div class="d-flex justify-content-center mb-3">
+    <b-spinner label="Loading..."></b-spinner>
+  </div>
+  <div class="d-flex align-items-center">
+    <strong>Loading...</strong>
+    <b-spinner class="ml-auto"></b-spinner>
+  </div>
+</div>
+
+<!-- spinner-flex.vue -->
+```
+
+### Floats
+Using float utility classes:
+
+```html
+<div>
+  <div class="clearfix">
+    <b-spinner class="float-right" label="Floated Right"></b-spinner>
+  </div>
+</div>
+
+<!-- spinner-floats.vue -->
+```
+
+### Text align
+Using text alignment utility classes:
+
+```html
+<div>
+  <div class="text-center">
+    <b-spinner variant="primary" label="Text Centered"></b-spinner>
+  </div>
+</div>
+
+<!-- spinner-text-align.vue -->
+```
+
+## Spinners in buttons
+Use spinners within buttons to indicate an action is currently processing or taking place. You
+may also swap the label text out of the spinner element and utilize button text as needed.
+
+```html
+<div>
+  <b-button variant="primary" disabled>
+    <b-spinner small></b-spinner>
+    <span class="sr-only">Loading...</span>
+  </b-button>
+  <b-button variant="primary" disabled>
+    <b-spinner small type="grow"></b-spinner>
+    Loading...
+  </b-button>
+</div>
+
+<!-- spinner-buttons.vue -->
+```
+
+## Spinner accessibility
+Place a hidden label text inside the spinner for screen reader users, via the `label` prop or `label` slot.
+The content will be placed _inside_ the spinner wrapped in a `<span>` element that has the class `sr-only`,
+which will make the label available to screen reader users.
+
+For accessibility purposes, each spinner will automatically have a `role="status"` attribute when
+a label is provided. You can easily customize the role if required via prop `role`.  The specified `role`
+will not be applied when no label is provided.
+
+As well, when no label is provided, the spinner will automatically have the attribute `aria-hidden="true"` to hide
+the spinner from screen reader users.
+
---
+++ src/components/spinner/spinner.spec.js
@@ -0,0 +1,239 @@
+import Spinner from './spinner'
+import { mount } from '@vue/test-utils'
+
+describe('spinner', async () => {
+  it('default has root element of span, and no children', async () => {
+    const spinner = mount(Spinner)
+    expect(spinner).toBeDefined()
+    expect(spinner.is('span')).toBe(true)
+    expect(spinner.find('span.sr-only').exists()).toBe(false)
+  })
+
+  it('renders custom root element when tag prop is set', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: { tag: 'aside' }
+      }
+    })
+    expect(spinner.is('aside')).toBe(true)
+  })
+
+  it('default has inner span when label is set', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: {
+          tag: 'div',
+          label: 'Loading...'
+        }
+      }
+    })
+    expect(spinner).toBeDefined()
+    expect(spinner.is('div')).toBe(true)
+    expect(spinner.find('span').exists()).toBe(true)
+    expect(spinner.text()).toBe('Loading...')
+  })
+
+  it('accepts custom label text via label slot', async () => {
+    const spinner = mount(Spinner, {
+      slots: { label: 'foobar' },
+      context: {}
+    })
+    expect(spinner.text()).toBe('foobar')
+  })
+
+  it('has inner span class "sr-only" when label is set', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: {
+          tag: 'div',
+          label: 'Loading...'
+        }
+      }
+    })
+    const span = spinner.find('span')
+    expect(span).toBeDefined()
+    expect(span.classes().length).toBe(1)
+    expect(span.classes()).toContain('sr-only')
+  })
+
+  it('default has class "spinner-border"', async () => {
+    const spinner = mount(Spinner)
+    expect(spinner.classes().length).toBe(1)
+    expect(spinner.classes()).toContain('spinner-border')
+  })
+
+  it('default has class "spinner-border-sm" when prop small=true', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: { small: true }
+      }
+    })
+    expect(spinner.classes().length).toBe(2)
+    expect(spinner.classes()).toContain('spinner-border')
+    expect(spinner.classes()).toContain('spinner-border-sm')
+  })
+
+  it('default has classes "spinner-border" and "text-danger" when prop variant="danger"', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: { variant: 'danger' }
+      }
+    })
+    expect(spinner.classes().length).toBe(2)
+    expect(spinner.classes()).toContain('spinner-border')
+    expect(spinner.classes()).toContain('text-danger')
+  })
+
+  it('default has class "text-danger" and "spinner-border-sm" when prop variant="danger" and small=true', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: {
+          variant: 'danger',
+          small: true
+        }
+      }
+    })
+    expect(spinner.classes().length).toBe(3)
+    expect(spinner.classes()).toContain('spinner-border')
+    expect(spinner.classes()).toContain('spinner-border-sm')
+    expect(spinner.classes()).toContain('text-danger')
+  })
+
+  it('does not have role "status" when no label provided', async () => {
+    const spinner = mount(Spinner)
+    expect(spinner.attributes('role')).not.toBeDefined()
+  })
+
+  it('has role "status" when label provided', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: { label: 'Loading' }
+      }
+    })
+    expect(spinner.attributes('role')).toBeDefined()
+    expect(spinner.attributes('role')).toEqual('status')
+  })
+
+  it('does not add custom role when role prop is set and no label', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: {
+          role: 'foobar'
+        }
+      }
+    })
+    expect(spinner.attributes('role')).not.toBeDefined()
+  })
+
+  it('adds custom role when role prop is set and label provided', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: {
+          role: 'foobar',
+          label: 'loading'
+        }
+      }
+    })
+    expect(spinner.attributes('role')).toBeDefined()
+    expect(spinner.attributes('role')).toEqual('foobar')
+  })
+
+  it('has attribute "aria-hidden" when no label provided', async () => {
+    const spinner = mount(Spinner)
+    expect(spinner.attributes('aria-hidden')).toBeDefined()
+    expect(spinner.attributes('aria-hidden')).toEqual('true')
+  })
+
+  it('does not have attribute "aria-hidden" when label provided', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: { label: 'loading' }
+      }
+    })
+    expect(spinner.attributes('aria-hidden')).not.toBeDefined()
+  })
+
+  it('does not have attribute "aria-hidden" when label slot provided', async () => {
+    const spinner = mount(Spinner, {
+      slots: {
+        label: 'loading'
+      }
+    })
+    expect(spinner.attributes('aria-hidden')).not.toBeDefined()
+  })
+
+  it('places user supplied attributes on root element', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        attrs: { id: 'foobar' }
+      }
+    })
+    expect(spinner.attributes('id')).toBeDefined()
+    expect(spinner.attributes('id')).toEqual('foobar')
+  })
+
+  it('places user supplied class on root element', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        class: ['foo', 'bar']
+      }
+    })
+    expect(spinner.classes()).toContain('spinner-border')
+    expect(spinner.classes()).toContain('foo')
+    expect(spinner.classes()).toContain('bar')
+  })
+
+  it('has class "spinner-grow" when prop type="grow"', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: { type: 'grow' }
+      }
+    })
+    expect(spinner.classes().length).toBe(1)
+    expect(spinner.classes()).toContain('spinner-grow')
+  })
+
+  it('has class "spinner-grow-sm" when props small=true and type="grow"', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: {
+          small: true,
+          type: 'grow'
+        }
+      }
+    })
+    expect(spinner.classes().length).toBe(2)
+    expect(spinner.classes()).toContain('spinner-grow')
+    expect(spinner.classes()).toContain('spinner-grow-sm')
+  })
+
+  it('has classes "spinner-grow" and "text-danger" when props type="grow" and variant="danger"', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: {
+          type: 'grow',
+          variant: 'danger'
+        }
+      }
+    })
+    expect(spinner.classes().length).toBe(2)
+    expect(spinner.classes()).toContain('spinner-grow')
+    expect(spinner.classes()).toContain('text-danger')
+  })
+
+  it('has classes "text-info", "spinner-grow" and "spinner-grow-sm" when props type="grow", variant="info" and small=true', async () => {
+    const spinner = mount(Spinner, {
+      context: {
+        props: {
+          type: 'grow',
+          variant: 'info',
+          small: true
+        }
+      }
+    })
+    expect(spinner.classes().length).toBe(3)
+    expect(spinner.classes()).toContain('spinner-grow')
+    expect(spinner.classes()).toContain('spinner-grow-sm')
+    expect(spinner.classes()).toContain('text-info')
+  })
+})
