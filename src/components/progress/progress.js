026be7c8a550bbac37230430d8cc67662d8d4051
Troy Morehouse <troymore@nbnet.nb.ca>
2018-12-18 00:59:16 -0400

    Update progress.js

--- src/components/progress/progress.js
+++ src/components/progress/progress.js
@@ -1,9 +1,9 @@
-import bProgressBar from './progress-bar'
+import BProgressBar from './progress-bar'
 
 // @vue/component
 export default {
   name: 'BProgress',
-  components: { bProgressBar },
+  components: { BProgressBar },
   props: {
     // These props can be inherited via the child b-progress-bar(s)
     variant: {
