a91fbb2b6891382529a11c84b71097ff5ffa193c
Troy Morehouse <troymore@nbnet.nb.ca>
2018-12-18 00:21:27 -0400

    Update breadcrumb-item.js

--- src/components/breadcrumb/breadcrumb-item.js
+++ src/components/breadcrumb/breadcrumb-item.js
@@ -1,6 +1,6 @@
 import { mergeData } from 'vue-functional-data-merge'
 import { assign } from '../../utils/object'
-import BreadcrumbLink, { props as crumbLinks } from './breadcrumb-link'
+import BBreadcrumbLink, { props as crumbLinks } from './breadcrumb-link'
 
 export const props = assign({}, crumbLinks, {
   text: {
@@ -26,7 +26,7 @@ export default {
         class: { active: props.active },
         attrs: { role: 'presentation' }
       }),
-      [h(BreadcrumbLink, { props }, children)]
+      [h(BBreadcrumbLink, { props }, children)]
     )
   }
 }
