974b47278a5aaaa371d81f4a47bd20888074473c
Troy Morehouse <troymore@nbnet.nb.ca>
2019-01-02 19:13:59 -0400

    lint

--- docs/utils/marked-loader.js
+++ docs/utils/marked-loader.js
@@ -1,15 +1,12 @@
 // Based on https://github.com/peerigon/markdown-loader/
 
-const marked = require('marked');
-const loaderUtils = require('loader-utils');
+const marked = require('marked')
+const loaderUtils = require('loader-utils')
 
 module.exports = function (markdown) {
   // merge params and default config
   const options = loaderUtils.getOptions(this);
-
-  this.cacheable();
-
-  marked.setOptions(options);
-
-  return marked(markdown);
-};
+  this.cacheable()
+  marked.setOptions(options)
+  return marked(markdown)
+}
