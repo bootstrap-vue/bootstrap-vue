5504e5ef8f66db37e19a623c1c9e2f24664f99a5
Troy Morehouse <troymore@nbnet.nb.ca>
2019-01-02 22:13:36 -0400

    Update marked-loader.js

--- docs/utils/marked-loader.js
+++ docs/utils/marked-loader.js
@@ -1,4 +1,5 @@
 // Based on https://github.com/peerigon/markdown-loader/
+// Converts markdown files into HTML format
 
 const marked = require('marked')
 const { getOptions } = require('loader-utils')
@@ -6,7 +7,10 @@ const { getOptions } = require('loader-utils')
 module.exports = function (markdown) {
   // merge params and default config
   const options = getOptions(this)
+  // Make results cacheable
   this.cacheable()
+  // Pass our options
   marked.setOptions(options)
+  // Return the converted file as HTML
   return marked(markdown)
 }
