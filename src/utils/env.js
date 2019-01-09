564b5cf8d177cab1e8db0988fd03a04d4b8da742
Troy Morehouse <troymore@nbnet.nb.ca>
2019-01-09 01:54:43 -0400

    Update env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -2,6 +2,8 @@
 
 export const inBrowser = typeof document !== 'undefined' && typeof window !== 'undefined'
 
+export const isServer = !inBrowser
+
 export const hasTouchSupport = inBrowser && ('ontouchstart' in document.documentElement || navigator.maxTouchPoints > 0)
 
 export const hasPointerEvent = inBrowser && Boolean(window.PointerEvent || window.MSPointerEvent)
