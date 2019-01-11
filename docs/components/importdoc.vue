35dcb74ed43a5f2a2a483c5768dada3c37834ee7
Jacob Müller <jacob.mueller.elz@gmail.com>
2019-01-11 01:26:41 +0100

    chore(docs): Improve import docs

--- docs/components/importdoc.vue
+++ docs/components/importdoc.vue
@@ -3,108 +3,103 @@
     class="bd-content"
     v-if="components.length > 0 || directives.length > 0">
 
-    <article v-if="components.length > 0">
-      <h3 id="importing-individual-components">Importing Individual  {{ pluginTitle }} Components</h3>
+    <template v-if="components.length > 0">
+      <h3 id="importing-individual-components">
+        Importing individual {{ pluginTitle }} Components
+      </h3>
+
       <b-table
         :items="componentImports"
         small
         head-variant="default"
         striped>
+
         <template
           slot="component"
           slot-scope="field">
           <code>{{ field.value }}</code>
         </template>
+
         <template
-          slot="import_path"
+          slot="importPath"
           slot-scope="field">
           <code>{{ field.value }}</code>
         </template>
       </b-table>
-      <p><strong>Example:</strong></p>
-      <div class="bd-example">
-        <code>import {{ components[0] }} from '{{ componentImports[0].import_path }}';</code>
-        <br>
-        <code>Vue.component('{{ componentName(components[0]) }}', {{ components[0] }});</code>
-      </div>
-    </article>
 
-    <article v-if="directives.length > 0">
-      <h3 id="importing-individual-directives">Importing Individual  {{ pluginTitle }} Directives</h3>
+      <p><strong>Example:</strong></p>
+
+      <pre class="hljs js text-monospace p-2"><code v-html="componentImportCode" /></pre>
+    </template>
+
+    <template v-if="directives.length > 0">
+      <h3 id="importing-individual-directives">
+        Importing individual {{ pluginTitle }} Directives
+      </h3>
+
       <b-table
         :items="directiveImports"
         small
         head-variant="default"
         striped>
+
         <template
           slot="directive"
           slot-scope="field">
           <code>{{ field.value }}</code>
         </template>
+
         <template
-          slot="import_path"
+          slot="importPath"
           slot-scope="field">
           <code>{{ field.value }}</code>
         </template>
       </b-table>
+
       <p><strong>Example:</strong></p>
-      <div class="bd-example">
-        <code>import {{ directives[0] }} from '{{ directiveImports[0].import_path }}';</code>
-        <br>
-        <code>Vue.directive('{{ directiveName(directives[0]) }}', {{ directives[0] }});</code>
-        <br>
-        <code>// <b>Note:</b> Vue automatically prefixes the directive name with 'v-'</code>
-      </div>
-    </article>
 
-    <article class="pb-5">
-      <h3 id="importing-as-a-plugin">Importing {{ pluginTitle }} as a Vue plugin</h3>
-      <p v-if="$route.name === 'docs-components-slug'">
-        This plugin includes all of the above listed individual
-        components<span v-if="directives.length"> and directives</span>.
-        Plugins also include any component aliases.
-      </p>
-      <p v-else>
-        This plugin includes all of the above listed individual directives.
-      </p>
-      <div class="bd-example">
-        <code v-if="$route.name === 'docs-components-slug'">
-          import { {{ pluginName }} } from 'bootstrap-vue/es/components';
-        </code>
-        <code v-else>
-          import { {{ pluginName }} } from 'bootstrap-vue/es/directives';
-        </code>
-        <br>
-        <code>Vue.use({{ pluginName }});</code>
-      </div>
-      <template v-if="meta.plugins && meta.plugins.length > 0">
-        <p>This plugin also automatically includes the following plugins:</p>
-        <ul>
-          <li
-            v-for="plugin in meta.plugins"
-            :key="plugin"><code>{{ plugin }}</code></li>
-        </ul>
-      </template>
-    </article>
+      <pre class="hljs js text-monospace p-2"><code v-html="directiveImportCode" /></pre>
+    </template>
 
+    <h3 id="importing-as-a-plugin">
+      Importing {{ pluginTitle }} as a Vue plugin
+    </h3>
+
+    <p v-if="isComponentRoute">
+      This plugin includes all of the above listed individual
+      components<span v-if="directives.length"> and directives</span>.
+      Plugins also include any component aliases.
+    </p>
+    <p v-else>
+      This plugin includes all of the above listed individual directives.
+    </p>
+
+    <pre class="hljs js text-monospace p-2"><code v-html="pluginImportCode" /></pre>
+
+    <template v-if="meta.plugins && meta.plugins.length > 0">
+      <p>This plugin also automatically includes the following plugins:</p>
+      <ul>
+        <li
+          v-for="plugin in meta.plugins"
+          :key="plugin"><code>{{ plugin }}</code></li>
+      </ul>
+    </template>
   </section>
 </template>
-
-<style scoped>
-    h3 {
-        padding: 20px 0;
-    }
-</style>
 
 <script>
 import kebabCase from 'lodash/kebabCase'
 import startCase from 'lodash/startCase'
+import hljs from 'highlightjs'
 
 export default {
   props: {
     meta: {}
   },
   computed: {
+    isComponentRoute () {
+      return this.$route.name === 'docs-components-slug'
+    },
     pluginName () {
       return startCase(this.$route.params.slug).replace(/\s+/g, '')
     },
@@ -115,7 +110,7 @@ export default {
       return this.components.map(c => {
         return {
           component: this.componentTag(c),
-          import_path: this.componentPath(c)
+          importPath: this.componentPath(c)
         }
       })
     },
@@ -123,7 +118,7 @@ export default {
       return this.directives.map(d => {
         return {
           directive: this.directiveAttr(d),
-          import_path: this.directivePath(d)
+          importPath: this.directivePath(d)
         }
       })
     },
@@ -137,27 +132,58 @@ export default {
     },
     directives () {
       return [].concat(this.meta.directive, this.meta.directives).filter(d => d)
+    },
+    componentImportCode () {
+      const firstComponent = this.components[0]
+      const firstComponentImport = this.componentImports[0]
+      return [
+        `import ${firstComponent} from '${firstComponentImport.importPath}'`,
+        `Vue.component('${this.componentName(firstComponent)}', ${firstComponent })`
+      ].join('\n')
+    },
+    directiveImportCode () {
+      const firstDirective = this.directives[0]
+      const firstDirectiveImport = this.directiveImports[0]
+      return [
+        "// <b>Note:</b> Vue automatically prefixes the directive name with 'v-'",
+        `import ${firstDirective} from '${firstDirectiveImport.importPath}'`,
+        `Vue.directive('${this.directiveName(firstDirective)}', ${firstDirective })`
+      ].join('\n')
+    },
+    pluginImportCode () {
+      const pluginLocation = this.isComponentRoute ? 'components' : 'directives'
+      return [
+        `import { ${this.pluginName} } from 'bootstrap-vue/es/${pluginLocation}'`,
+        `Vue.use(${this.pluginName})`
+      ].join('\n')
     }
+  },
+  mounted () {
+    // Highlight code blocks
+    [...this.$el.querySelectorAll('pre.hljs')].forEach(pre => {
+      hljs.highlightBlock(pre)
+    })
   },
   methods: {
     componentName (component) {
       return kebabCase(component)
     },
     componentTag (component) {
-      return `<${kebabCase(component)}>`
+      return `<${this.componentName(component)}>`
     },
     componentPath (component) {
-      return `bootstrap-vue/es/components/${this.$route.params.slug}/${kebabCase(component).replace(/^b-/, '')}`
+      const componentName = this.componentName(component).replace(/^b-/, '')
+      return `bootstrap-vue/es/components/${componentName}/${componentName}`
     },
     directiveName (directive) {
       return kebabCase(directive).replace(/^v-/, '')
     },
     directiveAttr (directive) {
-      return kebabCase(directive)
+      return this.directiveName(directive)
     },
     directivePath (directive) {
-      const slug = kebabCase(directive).replace(/^v-b-/, '')
-      return `bootstrap-vue/es/directives/${slug}/${slug}`
+      const directiveName = this.directiveName(directive)
+      return `bootstrap-vue/es/directives/${directiveName}/${directiveName}`
     }
   }
 }
