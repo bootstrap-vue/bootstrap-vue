b4495687799fc2d5030a49fa9741fc99c0c1c98a
Troy Morehouse <troymore@nbnet.nb.ca>
2019-01-11 21:59:58 -0400

    Update tab.js

--- src/components/tabs/tab.js
+++ src/components/tabs/tab.js
@@ -5,7 +5,11 @@ export default {
   name: 'BTab',
   mixins: [idMixin],
   inject: {
-    tabs: { default: null }
+    bTabs: {
+      default: function () {
+        return {}
+      }
+    }
   },
   props: {
     active: {
@@ -25,12 +29,12 @@ export default {
       default: ''
     },
     titleItemClass: {
-      // Sniffed by tabs.vue and added to nav 'li.nav-item'
+      // Sniffed by tabs.js and added to nav 'li.nav-item'
       type: [String, Array, Object],
       default: null
     },
     titleLinkClass: {
-      // Sniffed by tabs.vue and added to nav 'a.nav-link'
+      // Sniffed by tabs.js and added to nav 'a.nav-link'
       type: [String, Array, Object],
       default: null
     },
@@ -48,8 +52,15 @@ export default {
       default: false
     },
     href: {
+      // This should be deprecated, as tabs are not navigation (URL) based
+      // <b-nav> + <b-card> + <router-view>/<nuxt-child> should be used instead
+      // And we dont support router-links here
       type: String,
       default: '#'
+    },
+    lazy: {
+      type: Boolean,
+      default: false
     },
     order: {
       type: [Number, String],
@@ -65,7 +76,7 @@ export default {
   computed: {
     tabClasses () {
       return [
-        this.tabs && this.tabs.card && !this.noBody ? 'card-body' : '',
+        this.bTabs.card && !this.noBody ? 'card-body' : '',
         this.show ? 'show' : '',
         this.computedFade ? 'fade' : '',
         this.disabled ? 'disabled' : '',
@@ -76,10 +87,10 @@ export default {
       return this.buttonId || this.safeId('__BV_tab_button__')
     },
     computedFade () {
-      return this.tabs ? this.tabs.fade : false
+      return this.bTabs.fade || false
     },
     computedLazy () {
-      return this.tabs ? this.tabs.lazy : false
+      return this.bTabs.lazy || this.lazy
     },
     computedOrder () {
       return parseInt(this.order, 10) || 0
@@ -96,6 +107,7 @@ export default {
     beforeEnter () {
       // change opacity (add 'show' class) 1 frame after display
       // otherwise css transition won't happen
+      // TODO: Move raf method into utils/dom.js
       const raf = window.requestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame ||
@@ -111,26 +123,24 @@ export default {
     }
   },
   render (h) {
-    let content = h(false)
-    if (this.localActive || !this.computedLazy) {
-      content = h(
-        this.tag,
-        {
-          ref: 'panel',
-          staticClass: 'tab-pane',
-          class: this.tabClasses,
-          directives: [{ name: 'show', value: this.localActive }],
-          attrs: {
-            role: 'tabpanel',
-            id: this.safeId(),
-            'aria-hidden': this.localActive ? 'false' : 'true',
-            'aria-expanded': this.localActive ? 'true' : 'false',
-            'aria-labelledby': this.controlledBy || null
-          }
-        },
-        [this.$slots.default]
-      )
-    }
+    let content = h(
+      this.tag,
+      {
+        ref: 'panel',
+        staticClass: 'tab-pane',
+        class: this.tabClasses,
+        directives: [{ name: 'show', value: this.localActive }],
+        attrs: {
+          role: 'tabpanel',
+          id: this.safeId(),
+          'aria-hidden': this.localActive ? 'false' : 'true',
+          'aria-expanded': this.localActive ? 'true' : 'false',
+          'aria-labelledby': this.controlledBy || null
+        }
+      },
+      // Render content lazily if requested
+      [(this.localActive || !this.computedLazy) ? this.$slots.default : h(false)]
+    )
     return h(
       'transition',
       {
