bootstrap-vue/bootstrap-vue

commit de11a8f5e9999cbd98909b9ae3a9d58b556ee587

fix(tabs): various fixes and improvements (Fixes: #2327, #2148. Closes: #2403, #2180) (#2442)

src/components/tabs/tabs.js
@@ -1,3 +1,4 @@
+import BLink from '../link/link'
 import KeyCodes from '../../utils/key-codes'
 import observeDom from '../../utils/observe-dom'
 import idMixin from '../../mixins/id'
@@ -6,76 +7,113 @@ import idMixin from '../../mixins/id'
 const BTabButtonHelper = {
   name: 'BTabButtonHelper',
   props: {
-    content: { type: [String, Array], default: '' },
-    href: { type: String, default: '#' },
+    // Reference to the child b-tab instance
+    tab: { default: null, required: true },
+    id: { type: String, default: null },
+    controls: { type: String, default: null },
+    tabIndex: { type: Boolean, default: null },
     posInSet: { type: Number, default: null },
     setSize: { type: Number, default: null },
-    controls: { type: String, default: null },
-    id: { type: String, default: null },
-    active: { type: Boolean, default: false },
-    disabled: { type: Boolean, default: false },
-    linkClass: { default: null },
-    itemClass: { default: null },
     noKeyNav: { type: Boolean, default: false }
   },
-  render (h) {
-    const link = h('a', {
-      staticClass: 'nav-link',
-      class: [
-        { active: this.active, disabled: this.disabled },
-        this.linkClass
-      ],
-      attrs: {
-        role: 'tab',
-        tabindex: this.noKeyNav ? null : '-1',
-        href: this.href,
-        id: this.id,
-        disabled: this.disabled,
-        'aria-selected': this.active ? 'true' : 'false',
-        'aria-setsize': this.setSize,
-        'aria-posinset': this.posInSet,
-        'aria-controls': this.controls
-      },
-      on: {
-        click: this.handleClick,
-        keydown: this.handleClick
-      }
-    }, this.content)
-    return h(
-      'li',
-      { class: ['nav-item', this.itemClass], attrs: { role: 'presentation' } },
-      [link]
-    )
-  },
   methods: {
-    handleClick (evt) {
+    focus () {
+      if (this.$refs && this.$refs.link && this.$refs.link.focus) {
+        this.$refs.link.focus()
+      }
+    },
+    handleEvt (evt) {
       function stop () {
         evt.preventDefault()
         evt.stopPropagation()
       }
-      if (evt.type !== 'click' && this.noKeyNav) {
+      if (this.tab.disabled) {
         return
       }
-      if (this.disabled) {
+      const type = evt.type
+      const key = evt.keyCode
+      const shift = evt.shiftKey
+      if (type === 'click') {
         stop()
-        return
-      }
-      if (
-        evt.type === 'click' ||
-        evt.keyCode === KeyCodes.ENTER ||
-        evt.keyCode === KeyCodes.SPACE
-      ) {
+        this.$emit('click', evt) // Could call this.tab.activate() instead
+      } else if (type === 'keydown' && !this.noKeyNav && key === KeyCodes.SPACE) {
+        // In keyNav mode, SAPCE press will also trigger a click/select
         stop()
-        this.$emit('click', evt)
+        this.$emit('click', evt) // Could call this.tab.activate() instead
+      } else if (type === 'keydown' && !this.noKeyNav) {
+        // For keyboard navigation
+        if (key === KeyCodes.UP || key === KeyCodes.LEFT || key === KeyCodes.HOME) {
+          stop()
+          if (shift || key === KeyCodes.HOME) {
+            this.$emit('first', evt)
+          } else {
+            this.$emit('prev', evt)
+          }
+        } else if (key === KeyCodes.DOWN || key === KeyCodes.RIGHT || key === KeyCodes.END) {
+          stop()
+          if (shift || key === KeyCodes.END) {
+            this.$emit('last', evt)
+          } else {
+            this.$emit('next', evt)
+          }
+        }
       }
     }
+  },
+  render (h) {
+    const link = h(
+      BLink,
+      {
+        ref: 'link',
+        staticClass: 'nav-link',
+        class: [
+          {
+            active: this.tab.localActive && !this.tab.disabled,
+            disabled: this.tab.disabled
+          },
+          this.tab.linkClass
+        ],
+        props: {
+          href: this.tab.href, // To be deprecated to always be '#'
+          disabled: this.tab.disabled
+        },
+        attrs: {
+          role: 'tab',
+          id: this.id,
+          // Roving tab index when keynav enabled
+          tabindex: this.tabIndex,
+          'aria-selected': this.tab.localActive && !this.tab.disabled ? 'true' : 'false',
+          'aria-setsize': this.setSize,
+          'aria-posinset': this.posInSet,
+          'aria-controls': this.controls
+        },
+        on: {
+          click: this.handleEvt,
+          keydown: this.handleEvt
+        }
+      },
+      [this.tab.$slots.title || this.tab.title]
+    )
+    return h(
+      'li',
+      { class: ['nav-item', this.tab.itemClass], attrs: { role: 'presentation' } },
+      [link]
+    )
   }
+}
+
+// Filter function to filter out disabled tabs
+function notDisabled (tab) {
+  return !tab.disabled
 }
 
 // @vue/component
 export default {
   name: 'BTabs',
   mixins: [idMixin],
+  provide () {
+    return { bTabs: this }
+  },
   props: {
     tag: {
       type: String,
@@ -88,10 +126,6 @@ export default {
     small: {
       type: Boolean,
       default: false
-    },
-    value: {
-      type: Number,
-      default: null
     },
     pills: {
       type: Boolean,
@@ -138,11 +172,18 @@ export default {
     navWrapperClass: {
       type: [String, Array, Object],
       default: null
+    },
+    value: {
+      // v-model
+      type: Number,
+      default: null
     }
   },
   data () {
     return {
-      currentTab: this.value,
+      // Index of current tab
+      currentTab: parseInt(this.value, 10) || -1,
+      // Array of direct child b-tab instances
       tabs: []
     }
   },
@@ -157,185 +198,213 @@ export default {
   },
   watch: {
     currentTab (val, old) {
-      if (val === old) {
-        return
-      }
-      this.$root.$emit('changed::tab', this, val, this.tabs[val])
-      this.$emit('input', val)
-      this.tabs[val].$emit('click')
+      let index = -1
+      // Ensure only one tab is active at most
+      this.tabs.forEach((tab, idx) => {
+        if (val === idx && !tab.disabled) {
+          tab.localActive = true
+          index = idx
+        } else {
+          tab.localActive = false
+        }
+      })
+      // update the v-model
+      this.$emit('input', index)
     },
     value (val, old) {
-      if (val === old) {
-        return
+      if (val !== old) {
+        val = parseInt(val, 10)
+        old = parseInt(old, 10) || 0
+        const tabs = this.tabs
+        if (tabs[val] && !tabs[val].disabled) {
+          this.currentTab = val
+        } else {
+          // Try next or prev tabs
+          if (val < old) {
+            this.previousTab()
+          } else {
+            this.nextTab()
+          }
+        }
       }
-      if (typeof old !== 'number') {
-        old = 0
-      }
-      // Moving left or right?
-      const direction = val < old ? -1 : 1
-      this.setTab(val, false, direction)
     }
   },
-  mounted () {
+  created () {
+    // For SSR and to make sure only a single tab is shown on mount
     this.updateTabs()
-    // Observe Child changes so we can notify tabs change
+  },
+  mounted () {
+    // In case tabs have changed before mount
+    this.updateTabs()
+    // Observe Child changes so we can update list of tabs
     observeDom(this.$refs.tabsContainer, this.updateTabs.bind(this), {
       subtree: false
     })
   },
   methods: {
-    /**
-     * Util: Return the sign of a number (as -1, 0, or 1)
-     */
-    sign (x) {
-      return x === 0 ? 0 : x > 0 ? 1 : -1
-    },
-    /*
-         * handle keyboard navigation
-         */
-    onKeynav (evt) {
-      if (this.noKeyNav) {
-        return
-      }
-      const key = evt.keyCode
-      const shift = evt.shiftKey
-      function stop () {
-        evt.preventDefault()
-        evt.stopPropagation()
-      }
-      if (key === KeyCodes.UP || key === KeyCodes.LEFT) {
-        stop()
-        if (shift) {
-          this.setTab(0, false, 1)
-        } else {
-          this.previousTab()
-        }
-      } else if (key === KeyCodes.DOWN || key === KeyCodes.RIGHT) {
-        stop()
-        if (shift) {
-          this.setTab(this.tabs.length - 1, false, -1)
-        } else {
-          this.nextTab()
-        }
-      }
-    },
-    /**
-     * Move to next tab
-     */
-    nextTab () {
-      this.setTab(this.currentTab + 1, false, 1)
-    },
-    /**
-     * Move to previous tab
-     */
-    previousTab () {
-      this.setTab(this.currentTab - 1, false, -1)
-    },
-    /**
-     * Set active tab on the tabs collection and the child 'tab' component
-     * Index is the tab we want to activate. Direction is the direction we are moving
-     * so if the tab we requested is disabled, we can skip over it.
-     * Force is used by updateTabs to ensure we have cleared any previous active tabs.
-     */
-    setTab (index, force, direction) {
-      direction = this.sign(direction || 0)
-      index = index || 0
-      // Prevent setting same tab and infinite loops!
-      if (!force && index === this.currentTab) {
-        return
-      }
-      const tab = this.tabs[index]
-      // Don't go beyond indexes!
-      if (!tab) {
-        // Reset the v-model to the current Tab
-        this.$emit('input', this.currentTab)
-        return
-      }
-      // Ignore or Skip disabled
-      if (tab.disabled) {
-        if (direction) {
-          // Skip to next non disabled tab in specified direction (recursive)
-          this.setTab(index + direction, force, direction)
-        }
-        return
-      }
-      // Activate requested current tab, and deactivte any old tabs
-      this.tabs.forEach(t => {
-        if (t === tab) {
-          // Set new tab as active
-          this.$set(t, 'localActive', true)
-        } else {
-          // Ensure non current tabs are not active
-          this.$set(t, 'localActive', false)
-        }
-      })
-      // Update currentTab
-      this.currentTab = index
-    },
-    /**
-     * Dynamically update tabs list
-     */
+    // Update list of b-tab children
     updateTabs () {
       // Probe tabs
-      this.tabs = this.$children.filter(child => child._isTab)
-      // Set initial active tab
-      let tabIndex = null
-      // Find *last* active non-dsabled tab in current tabs
-      // We trust tab state over currentTab
-      this.tabs.forEach((tab, index) => {
-        if (tab.localActive && !tab.disabled) {
-          tabIndex = index
+      const tabs = (this.$slots.default || [])
+        .map(vnode => vnode.componentInstance)
+        .filter(tab => tab && tab._isTab)
+
+      // Find *last* active non-disabled tab in current tabs
+      // We trust tab state over currentTab, in case tabs were added/removed/re-ordered
+      let tabIndex = tabs.indexOf(tabs.slice().reverse().find(tab => tab.localActive && !tab.disabled))
+
+      // Else try setting to currentTab
+      if (tabIndex < 0) {
+        const currentTab = this.currentTab
+        if (currentTab >= tabs.length) {
+          // Handle last tab being removed, so find the last non-disabled tab
+          tabIndex = tabs.indexOf(tabs.slice().reverse().find(notDisabled))
+        } else if (tabs[currentTab] && !tabs[currentTab].disabled) {
+          // current tab is not disabled
+          tabIndex = currentTab
+        }
+      }
+
+      // Else find *first* non-disabled tab in current tabs
+      if (tabIndex < 0) {
+        tabIndex = tabs.indexOf(tabs.find(notDisabled))
+      }
+
+      // Set the current tab state to active
+      tabs.forEach((tab, idx) => {
+        tab.localActive = idx === tabIndex && !tab.disabled
+      })
+
+      // Update the array of tab children
+      this.tabs = tabs
+      // Set the currentTab index (can be -1 if no non-disabled tabs)
+      this.currentTab = tabIndex
+    },
+    // Find a button taht controls a tab, given the tab reference
+    // Returns the button vm instance
+    getButtonForTab (tab) {
+      return (this.$refs.buttons || []).find(btn => btn.tab === tab)
+    },
+    // Force a button to re-render it's content, given a b-tab instance
+    // Called by b-tab on update()
+    updateButton (tab) {
+      const button = this.getButtonForTab(tab)
+      if (button && button.$forceUpdate) {
+        button.$forceUpdate()
+      }
+    },
+    // Activate a tab given a b-tab instance
+    // Also accessed by b-tab
+    activateTab (tab) {
+      let result = false
+      if (tab) {
+        const index = this.tabs.indexOf(tab)
+        if (!tab.disabled && index > -1) {
+          result = true
+          this.currentTab = index
+        }
+      }
+      this.$emit('input', this.currentTab)
+      return result
+    },
+    // Deactivate a tab given a b-tab instance
+    // Accessed by b-tab
+    deactivateTab (tab) {
+      if (tab) {
+        // Find first non-disabled tab that isn't the one being deactivated
+        // If no available tabs, then don't deactivate current tab
+        return this.activateTab(this.tabs.filter(t => t !== tab).find(notDisabled))
+      } else {
+        // No tab specified
+        return false
+      }
+    },
+    // Focus a tab button given it's b-tab instance
+    focusButton (tab) {
+      // Wrap in nextTick to ensure DOM has completed rendering/updating before focusing
+      this.$nextTick(() => {
+        const button = this.getButtonForTab(tab)
+        if (button && button.focus) {
+          button.focus()
         }
       })
-      // Else try setting to currentTab
-      if (tabIndex === null) {
-        if (this.currentTab >= this.tabs.length) {
-          // Handle last tab being removed
-          this.setTab(this.tabs.length - 1, true, -1)
-          return
-        } else if (
-          this.tabs[this.currentTab] &&
-          !this.tabs[this.currentTab].disabled
-        ) {
-          tabIndex = this.currentTab
-        }
+    },
+    // Move to first non-disabled tab
+    firstTab (focus) {
+      const tab = this.tabs.find(notDisabled)
+      if (this.activateTab(tab) && focus) {
+        this.focusButton(tab)
       }
-      // Else find *first* non-disabled tab in current tabs
-      if (tabIndex === null) {
-        this.tabs.forEach((tab, index) => {
-          if (!tab.disabled && tabIndex === null) {
-            tabIndex = index
-          }
-        })
+    },
+    // Move to previous non-disabled tab
+    previousTab (focus) {
+      const currentIndex = Math.max(this.currentTab, 0)
+      const tab = this.tabs.slice(0, currentIndex).reverse().find(notDisabled)
+      if (this.activateTab(tab) && focus) {
+        this.focusButton(tab)
       }
-      this.setTab(tabIndex || 0, true, 0)
+    },
+    // Move to next non-disabled tab
+    nextTab (focus) {
+      const currentIndex = Math.max(this.currentTab, -1)
+      const tab = this.tabs.slice(currentIndex + 1).find(notDisabled)
+      if (this.activateTab(tab) && focus) {
+        this.focusButton(tab)
+      }
+    },
+    // Move to last non-disabled tab
+    lastTab (focus) {
+      const tab = this.tabs.slice().reverse().find(notDisabled)
+      if (this.activateTab(tab) && focus) {
+        this.focusButton(tab)
+      }
     }
   },
   render (h) {
     const tabs = this.tabs
-    // Navigation 'buttons'
+    // Currently active tab
+    let activeTab = tabs.find(tab => tab.localActive && !tab.disabled)
+    // Tab button to allow focusing when no actgive tab found (keynav only)
+    const fallbackTab = tabs.find(tab => !tab.disabled)
+
+    // For each b-tab found create teh tab buttons
     const buttons = tabs.map((tab, index) => {
-      return h(BTabButtonHelper, {
-        key: index,
-        props: {
-          content: tab.$slots.title || tab.title,
-          href: tab.href,
-          id: tab.controlledBy || this.safeId(`_BV_tab_${index + 1}_`),
-          active: tab.localActive,
-          disabled: tab.disabled,
-          setSize: tabs.length,
-          posInSet: index + 1,
-          controls: this.safeId('_BV_tab_container_'),
-          linkClass: tab.titleLinkClass,
-          itemClass: tab.titleItemClass,
-          noKeyNav: this.noKeyNav
-        },
-        on: {
-          click: evt => {
-            this.setTab(index)
+      const buttonId = tab.controlledBy || this.safeId(`_BV_tab_${index + 1}_`)
+      let tabindex = null
+      // Ensure at least one tab button is focusable when keynav enabled (if possible)
+      if (!this.noKeyNav) {
+        // Buttons are not in tab index unless active, or a fallback tab
+        tabindex = '-1'
+        if (activeTab === tab || (!activeTab && fallbackTab === tab)) {
+          // Place tab button in tab sequence
+          tabindex = null
+        }
+      }
+      return h(
+        BTabButtonHelper,
+        {
+          key: tab._uid || buttonId || index,
+          ref: 'buttons',
+          // Needed to make this.$refs.buttons an array
+          refInFor: true,
+          props: {
+            tab: tab,
+            id: buttonId,
+            controls: this.safeId('_BV_tab_container_'),
+            tabIndex: tabindex,
+            setSize: tabs.length,
+            posInSet: index + 1,
+            noKeyNav: this.noKeyNav
+          },
+          on: {
+            click: evt => { this.activateTab(tab) },
+            first: this.firstTab,
+            prev: this.previousTab,
+            next: this.nextTab,
+            last: this.lastTab
           }
         }
-      })
+      )
     })
 
     // Nav 'button' wrapper
@@ -358,10 +427,8 @@ export default {
         ],
         attrs: {
           role: 'tablist',
-          tabindex: this.noKeyNav ? null : '0',
           id: this.safeId('_BV_tab_controls_')
-        },
-        on: { keydown: this.onKeynav }
+        }
       },
       [buttons, this.$slots.tabs]
     )
@@ -386,7 +453,7 @@ export default {
     } else {
       empty = h(
         'div',
-        { class: ['tab-pane', 'active', { 'card-body': this.card }] },
+        { key: 'empty-tab', class: ['tab-pane', 'active', { 'card-body': this.card }] },
         this.$slots.empty
       )
     }
@@ -396,7 +463,8 @@ export default {
       'div',
       {
         ref: 'tabsContainer',
-        class: ['tab-content', { col: this.vertical }, this.contentClass],
+        staticClass: 'tab-content',
+        class: [{ col: this.vertical }, this.contentClass],
         attrs: { id: this.safeId('_BV_tab_container_') }
       },
       [this.$slots.default, empty]
@@ -406,10 +474,11 @@ export default {
     return h(
       this.tag,
       {
-        class: [
-          'tabs',
-          { row: this.vertical, 'no-gutters': this.vertical && this.card }
-        ],
+        staticClass: 'tabs',
+        class: {
+          row: this.vertical,
+          'no-gutters': this.vertical && this.card
+        },
         attrs: { id: this.safeId() }
       },
       [

src/components/tabs/tab.js
@@ -4,6 +4,16 @@ import idMixin from '../../mixins/id'
 export default {
   name: 'BTab',
   mixins: [idMixin],
+  inject: {
+    bTabs: {
+      default: function () {
+        return {
+          // Dont set a tab index if not rendered inside b-tabs
+          noKeyNav: true
+        }
+      }
+    }
+  },
   props: {
     active: {
       type: Boolean,
@@ -22,12 +32,12 @@ export default {
       default: ''
     },
     titleItemClass: {
-      // Sniffed by tabs.vue and added to nav 'li.nav-item'
+      // Sniffed by tabs.js and added to nav 'li.nav-item'
       type: [String, Array, Object],
       default: null
     },
     titleLinkClass: {
-      // Sniffed by tabs.vue and added to nav 'a.nav-link'
+      // Sniffed by tabs.js and added to nav 'a.nav-link'
       type: [String, Array, Object],
       default: null
     },
@@ -45,8 +55,15 @@ export default {
       default: false
     },
     href: {
+      // This should be deprecated, as tabs are not navigation (URL) based
+      // <b-nav> + <b-card> + <router-view>/<nuxt-child> should be used instead
+      // And we dont support router-links here
       type: String,
       default: '#'
+    },
+    lazy: {
+      type: Boolean,
+      default: false
     }
   },
   data () {
@@ -58,8 +75,7 @@ export default {
   computed: {
     tabClasses () {
       return [
-        'tab-pane',
-        this.$parent && this.$parent.card && !this.noBody ? 'card-body' : '',
+        this.bTabs.card && !this.noBody ? 'card-body' : '',
         this.show ? 'show' : '',
         this.computedFade ? 'fade' : '',
         this.disabled ? 'disabled' : '',
@@ -70,49 +86,113 @@ export default {
       return this.buttonId || this.safeId('__BV_tab_button__')
     },
     computedFade () {
-      return this.$parent.fade
+      return this.bTabs.fade || false
     },
     computedLazy () {
-      return this.$parent.lazy
+      return this.bTabs.lazy || this.lazy
     },
     _isTab () {
       // For parent sniffing of child
       return true
     }
   },
+  watch: {
+    localActive (newVal, oldVal) {
+      // Make 'active' prop work with `.sync` modifier
+      this.$emit('update:active', newVal)
+    },
+    active (newVal, oldVal) {
+      if (newVal !== oldVal) {
+        if (newVal) {
+          // If activated post mount
+          this.activate()
+        } else {
+          if (!this.deactivate()) {
+            // Tab couldn't be deactivated, so we reset the synced active prop
+            // Deactivation will fail if no other tabs to activate.
+            this.$emit('update:active', this.localActive)
+          }
+        }
+      }
+    },
+    disabled (newVal, oldVal) {
+      if (newVal !== oldVal) {
+        if (newVal && this.localActive && this.bTabs.firstTab) {
+          this.localActive = false
+          this.bTabs.firstTab()
+        }
+      }
+    }
+  },
   mounted () {
+    // Initially show on mount if active and not disabled
     this.show = this.localActive
   },
+  updated () {
+    // Force the tab button content to update (since slots are not reactive)
+    // Only done if we have a title slot, as the title prop is reactive
+    if (this.$slots.title && this.bTabs.updateButton) {
+      this.bTabs.updateButton(this)
+    }
+  },
   methods: {
-    beforeEnter () {
-      // change opacity 1 frame after display
+    // Transition handlers
+    beforeEnter () /* instanbul ignore next: difficult to test rAF in JSDOM */ {
+      // change opacity (add 'show' class) 1 frame after display
       // otherwise css transition won't happen
-      window.requestAnimationFrame(() => { this.show = true })
+      // TODO: Move raf method into utils/dom.js
+      const raf = window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            /* istanbul ignore next */
+            function (cb) { setTimeout(cb, 16) }
+
+      raf(() => { this.show = true })
     },
-    beforeLeave () {
+    beforeLeave () /* instanbul ignore next: difficult to test rAF in JSDOM */ {
+      // Remove the 'show' class
       this.show = false
+    },
+    // Public methods
+    activate () {
+      if (this.bTabs.activateTab && !this.disabled) {
+        return this.bTabs.activateTab(this)
+      } else {
+        // Not inside a b-tabs component or tab is disabled
+        return false
+      }
+    },
+    deactivate () {
+      if (this.bTabs.deactivateTab && this.localActive) {
+        return this.bTabs.deactivateTab(this)
+      } else {
+        // Not inside a b-tabs component or not active to begin with
+        return false
+      }
     }
   },
   render (h) {
-    let content = h(false)
-    if (this.localActive || !this.computedLazy) {
-      content = h(
-        this.tag,
-        {
-          ref: 'panel',
-          class: this.tabClasses,
-          directives: [{ name: 'show', value: this.localActive }],
-          attrs: {
-            role: 'tabpanel',
-            id: this.safeId(),
-            'aria-hidden': this.localActive ? 'false' : 'true',
-            'aria-expanded': this.localActive ? 'true' : 'false',
-            'aria-labelledby': this.controlledBy || null
-          }
-        },
-        [this.$slots.default]
-      )
-    }
+    let content = h(
+      this.tag,
+      {
+        ref: 'panel',
+        staticClass: 'tab-pane',
+        class: this.tabClasses,
+        directives: [{ name: 'show', value: this.localActive }],
+        attrs: {
+          role: 'tabpanel',
+          id: this.safeId(),
+          tabindex: (this.localActive && !this.bTabs.noKeyNav) ? '0' : null,
+          'aria-hidden': this.localActive ? 'false' : 'true',
+          'aria-expanded': this.localActive ? 'true' : 'false',
+          'aria-labelledby': this.controlledBy || null
+        }
+      },
+      // Render content lazily if requested
+      [(this.localActive || !this.computedLazy) ? this.$slots.default : h(false)]
+    )
     return h(
       'transition',
       {

commit 9547564e348d00a84a4eab7c8cb7fbcf5fc54721

fix(tabs): Fix tab titleLinkClass and titleItemClass handling
Fixes an issue introduced by PR #2442 where the classes were not being applied to the correct items.

src/components/tabs/tabs.js
@@ -71,7 +71,7 @@ const BTabButtonHelper = {
             active: this.tab.localActive && !this.tab.disabled,
             disabled: this.tab.disabled
           },
-          this.tab.linkClass
+          this.tab.titleLinkClass
         ],
         props: {
           href: this.tab.href, // To be deprecated to always be '#'
@@ -96,7 +96,11 @@ const BTabButtonHelper = {
     )
     return h(
       'li',
-      { class: ['nav-item', this.tab.itemClass], attrs: { role: 'presentation' } },
+      {
+        staticClass: 'nav-item',
+        class: [this.tab.titleItemClass],
+        attrs: { role: 'presentation' }
+      },
       [link]
     )
   }