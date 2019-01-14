302cef47d6ad1e477a3056c948d4dc4ce155aa07
Troy Morehouse <troymore@nbnet.nb.ca>
2019-01-14 00:17:08 -0400

    Update nav.vue

--- docs/components/nav.vue
+++ docs/components/nav.vue
@@ -1,15 +1,8 @@
 <template>
-  <b-navbar
-    toggleable="md"
-    class="navbar-dark bd-navbar"
-    sticky
-  >
+  <b-navbar toggleable="md" class="navbar-dark bd-navbar" sticky>
     <b-navbar-toggle target="bd-main-nav" />
 
-    <b-navbar-brand
-      to="/"
-      exact
-    >
+    <b-navbar-brand to="/" exact>
       <svg
         class="d-block"
         width="36"
@@ -23,40 +16,17 @@
           d="M510,8 C561.846401,8.16468012 603.83532,50.1535995 604,102 L604,510 C603.83532,561.846401 561.846401,603.83532 510,604 L102,604 C50.1535995,603.83532 8.16468012,561.846401 8,510 L8,102 C8.16468012,50.1535995 50.1535995,8.16468012 102,8 L510,8 L510,8 Z M510,0 L102,0 C45.9,6.21724894e-15 0,45.9 0,102 L0,510 C0,566.1 45.9,612 102,612 L510,612 C566.1,612 612,566.1 612,510 L612,102 C612,45.9 566.1,6.21724894e-15 510,0 Z"
           fill-rule="nonzero"
         />
-        <text
-          id="BV"
-          font-family="Arial"
-          font-size="350"
-          font-weight="light"
-          letter-spacing="2"
-        >
-          <tspan
-            x="72.0527344"
-            y="446"
-          >
-            B
-          </tspan>
-          <tspan
-            x="307.5"
-            y="446"
-          >
-            V
-          </tspan>
+        <text id="BV" font-family="Arial" font-size="350" font-weight="light" letter-spacing="2">
+          <tspan x="72.0527344" y="446">B</tspan>
+          <tspan x="307.5" y="446">V</tspan>
         </text>
       </svg>
       <span class="sr-only">Home</span>
     </b-navbar-brand>
 
-    <b-collapse
-      id="bd-main-nav"
-      is-nav
-      class="justify-content-between"
-    >
+    <b-collapse id="bd-main-nav" is-nav class="justify-content-between">
       <b-navbar-nav>
-        <b-nav-item
-          to="/docs"
-          exact
-        >
+        <b-nav-item to="/docs" exact>
           Docs
         </b-nav-item>
         <b-nav-item to="/docs/components" active-class="active">Components</b-nav-item>
@@ -67,18 +37,12 @@
       </b-navbar-nav>
 
       <b-navbar-nav>
-        <b-nav-item-dropdown
-          :text="`v${version}`"
-          right
-        >
+        <b-nav-item-dropdown :text="`v${version}`" right>
           <b-dropdown-item href="http://dev.bootstrap-vue.netlify.com">Development</b-dropdown-item>
           <b-dropdown-item href="https://bootstrap-vue.js.org">Latest (v{{ version }})</b-dropdown-item>
         </b-nav-item-dropdown>
 
-        <b-nav-item
-          href="https://github.com/bootstrap-vue/bootstrap-vue"
-          target="_blank"
-        >
+        <b-nav-item href="https://github.com/bootstrap-vue/bootstrap-vue" target="_blank">
           <svg
             class="navbar-nav-svg"
             xmlns="http://www.w3.org/2000/svg"
@@ -95,10 +59,7 @@
           <span class="sr-only">GitHub</span>
         </b-nav-item>
 
-        <b-nav-item
-          href="https://twitter.com/BootstrapVue"
-          target="_blank"
-        >
+        <b-nav-item href="https://twitter.com/BootstrapVue" target="_blank">
           <svg
             class="navbar-nav-svg"
             xmlns="http://www.w3.org/2000/svg"
@@ -114,10 +75,7 @@
           <span class="sr-only">Twitter</span>
         </b-nav-item>
 
-        <b-nav-item
-          href="https://discord.gg/j2Mtcny"
-          target="_blank"
-        >
+        <b-nav-item href="https://discord.gg/j2Mtcny" target="_blank">
           <svg
             class="navbar-nav-svg"
             xmlns="http://www.w3.org/2000/svg"
@@ -125,8 +83,14 @@
             viewBox="0 0 245 240"
           >
             <title>Discord Chat</title>
-            <path d="M104.4 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1.1-6.1-4.5-11.1-10.2-11.1zM140.9 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1s-4.5-11.1-10.2-11.1z" fill="currentColor" />
-            <path d="M189.5 20h-134C44.2 20 35 29.2 35 40.6v135.2c0 11.4 9.2 20.6 20.5 20.6h113.4l-5.3-18.5 12.8 11.9 12.1 11.2 21.5 19V40.6c0-11.4-9.2-20.6-20.5-20.6zm-38.6 130.6s-3.6-4.3-6.6-8.1c13.1-3.7 18.1-11.9 18.1-11.9-4.1 2.7-8 4.6-11.5 5.9-5 2.1-9.8 3.5-14.5 4.3-9.6 1.8-18.4 1.3-25.9-.1-5.7-1.1-10.6-2.7-14.7-4.3-2.3-.9-4.8-2-7.3-3.4-.3-.2-.6-.3-.9-.5-.2-.1-.3-.2-.4-.3-1.8-1-2.8-1.7-2.8-1.7s4.8 8 17.5 11.8c-3 3.8-6.7 8.3-6.7 8.3-22.1-.7-30.5-15.2-30.5-15.2 0-32.2 14.4-58.3 14.4-58.3 14.4-10.8 28.1-10.5 28.1-10.5l1 1.2c-18 5.2-26.3 13.1-26.3 13.1s2.2-1.2 5.9-2.9c10.7-4.7 19.2-6 22.7-6.3.6-.1 1.1-.2 1.7-.2 6.1-.8 13-1 20.2-.2 9.5 1.1 19.7 3.9 30.1 9.6 0 0-7.9-7.5-24.9-12.7l1.4-1.6s13.7-.3 28.1 10.5c0 0 14.4 26.1 14.4 58.3 0 0-8.5 14.5-30.6 15.2z" fill="currentColor" />
+            <path
+              d="M104.4 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1.1-6.1-4.5-11.1-10.2-11.1zM140.9 103.9c-5.7 0-10.2 5-10.2 11.1s4.6 11.1 10.2 11.1c5.7 0 10.2-5 10.2-11.1s-4.5-11.1-10.2-11.1z"
+              fill="currentColor"
+            />
+            <path
+              d="M189.5 20h-134C44.2 20 35 29.2 35 40.6v135.2c0 11.4 9.2 20.6 20.5 20.6h113.4l-5.3-18.5 12.8 11.9 12.1 11.2 21.5 19V40.6c0-11.4-9.2-20.6-20.5-20.6zm-38.6 130.6s-3.6-4.3-6.6-8.1c13.1-3.7 18.1-11.9 18.1-11.9-4.1 2.7-8 4.6-11.5 5.9-5 2.1-9.8 3.5-14.5 4.3-9.6 1.8-18.4 1.3-25.9-.1-5.7-1.1-10.6-2.7-14.7-4.3-2.3-.9-4.8-2-7.3-3.4-.3-.2-.6-.3-.9-.5-.2-.1-.3-.2-.4-.3-1.8-1-2.8-1.7-2.8-1.7s4.8 8 17.5 11.8c-3 3.8-6.7 8.3-6.7 8.3-22.1-.7-30.5-15.2-30.5-15.2 0-32.2 14.4-58.3 14.4-58.3 14.4-10.8 28.1-10.5 28.1-10.5l1 1.2c-18 5.2-26.3 13.1-26.3 13.1s2.2-1.2 5.9-2.9c10.7-4.7 19.2-6 22.7-6.3.6-.1 1.1-.2 1.7-.2 6.1-.8 13-1 20.2-.2 9.5 1.1 19.7 3.9 30.1 9.6 0 0-7.9-7.5-24.9-12.7l1.4-1.6s13.7-.3 28.1 10.5c0 0 14.4 26.1 14.4 58.3 0 0-8.5 14.5-30.6 15.2z"
+              fill="currentColor"
+            />
           </svg>
           <span class="sr-only">Discord Chat</span>
         </b-nav-item>
