b3345e4e235042acca9f2c700c9b6d37b0460c64
Troy Morehouse <troymore@nbnet.nb.ca>
2019-01-14 00:29:39 -0400

    Update _slug.vue

--- docs/pages/docs/misc/_slug.vue
+++ docs/pages/docs/misc/_slug.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="container">
-    <div class="bd-content" v-html="readme" />
-  </div>
+  <div class="container"><div class="bd-content" v-html="readme" /></div>
 </template>
 
 <script>
