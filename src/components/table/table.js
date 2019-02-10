bd1026b4bac7b653400d8c61016e791c68c56536
pooya parsa <pooya@pi0.ir>
2019-02-11 01:20:48 +0330

    remove lodash dependency from table

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,5 +1,5 @@
-import _startCase from 'lodash.startcase'
-import _get from 'lodash.get'
+import startCase from '../../utils/startcase'
+import get from '../../utils/get'
 import looseEqual from '../../utils/loose-equal'
 import stableSort from '../../utils/stable-sort'
 import KeyCodes from '../../utils/key-codes'
@@ -62,8 +62,8 @@ function recToString(row) {
 //  where sprtBy could be an array of objects [ {key: 'foo', sortDir: 'asc'}, {key:'bar', sortDir: 'desc'} ...]
 //  or an array of arrays [ ['foo','asc'], ['bar','desc'] ]
 function defaultSortCompare(a, b, sortBy) {
-  a = _get(a, sortBy, '')
-  b = _get(b, sortBy, '')
+  a = get(a, sortBy, '')
+  b = get(b, sortBy, '')
   if (typeof a === 'number' && typeof b === 'number') {
     return (a < b && -1) || (a > b && 1) || 0
   }
@@ -488,7 +488,7 @@ export default {
         // Normalize array Form
         this.fields.filter(f => f).forEach(f => {
           if (typeof f === 'string') {
-            fields.push({ key: f, label: _startCase(f) })
+            fields.push({ key: f, label: startCase(f) })
           } else if (typeof f === 'object' && f.key && typeof f.key === 'string') {
             // Full object definition. We use assign so that we don't mutate the original
             fields.push(assign({}, f))
@@ -515,7 +515,7 @@ export default {
         const sample = this.localItems[0]
         keys(sample).forEach(k => {
           if (!IGNORED_FIELD_KEYS[k]) {
-            fields.push({ key: k, label: _startCase(k) })
+            fields.push({ key: k, label: startCase(k) })
           }
         })
       }
@@ -524,7 +524,7 @@ export default {
       return fields.filter(f => {
         if (!memo[f.key]) {
           memo[f.key] = true
-          f.label = typeof f.label === 'string' ? f.label : _startCase(f.key)
+          f.label = typeof f.label === 'string' ? f.label : startCase(f.key)
           return true
         }
         return false
@@ -808,7 +808,7 @@ export default {
     getTdValues(item, key, tdValue, defValue) {
       const parent = this.$parent
       if (tdValue) {
-        const value = _get(item, key, '')
+        const value = get(item, key, '')
         if (typeof tdValue === 'function') {
           return tdValue(value, key, item)
         } else if (typeof tdValue === 'string' && typeof parent[tdValue] === 'function') {
@@ -823,7 +823,7 @@ export default {
       const key = field.key
       const formatter = field.formatter
       const parent = this.$parent
-      let value = _get(item, key, null)
+      let value = get(item, key, null)
       if (formatter) {
         if (typeof formatter === 'function') {
           value = formatter(value, key, item)
@@ -1156,7 +1156,7 @@ export default {
         if (!field.label.trim() && !field.headerTitle) {
           // In case field's label and title are empty/blank
           // We need to add a hint about what the column is about for non-dighted users
-          ariaLabel = _startCase(field.key)
+          ariaLabel = startCase(field.key)
         }
         const ariaLabelSorting = field.sortable
           ? this.localSortDesc && this.localSortBy === field.key
@@ -1310,7 +1310,7 @@ export default {
                 item: item,
                 index: rowIndex,
                 field: field,
-                unformatted: _get(item, field.key, ''),
+                unformatted: get(item, field.key, ''),
                 value: formatted,
                 toggleDetails: toggleDetailsFn,
                 detailsShowing: Boolean(item._showDetails),
