88e566e9e7e56f7e2bf54ed09256d714b6945000
pooya parsa <pooya@pi0.ir>
2019-02-15 21:56:41 +0330

    fix: bootstrap should be fisrt

--- nuxt/index.js
+++ nuxt/index.js
@@ -11,16 +11,16 @@ function pickFirst(...args) {
 module.exports = function nuxtBootstrapVue(moduleOptions = {}) {
   const options = Object.assign({}, this.options.bootstrapVue, moduleOptions)
 
-  const bootstrapCSS = pickFirst(options.bootstrapCSS, options.bootstrapCss, options.css)
-  if (bootstrapCSS) {
-    // Add Bootstrap CSS
-    this.options.css.unshift('bootstrap/dist/css/bootstrap.css')
-  }
-
   const bootstrapVueCSS = pickFirst(options.bootstrapVueCSS, options.bootstrapCss, options.bvCSS)
   if (bootstrapVueCSS) {
     // Add BootstrapVue CSS
     this.options.css.unshift('bootstrap-vue/dist/bootstrap-vue.css')
+  }
+
+  const bootstrapCSS = pickFirst(options.bootstrapCSS, options.bootstrapCss, options.css)
+  if (bootstrapCSS) {
+    // Add Bootstrap CSS
+    this.options.css.unshift('bootstrap/dist/css/bootstrap.css')
   }
 
   // Register plugin, pasing options to plugin template
