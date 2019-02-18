01db202ee937eff50f7d476a7fabfdba1d1c3dfa
Jacob Müller <jacob.mueller.elz@gmail.com>
2019-02-18 17:12:37 +0100

    fix(utils): Make `looseEqual()` util handle File's comparison correctly

--- src/utils/loose-equal.js
+++ src/utils/loose-equal.js
@@ -1,5 +1,8 @@
 import { isArray } from './array'
-import { keys } from './object'
+
+function isDate(obj) {
+  return obj instanceof Date
+}
 
 /**
  * Quick object check - this is primarily used to tell
@@ -7,7 +10,7 @@ import { keys } from './object'
  * is a JSON-compliant type.
  */
 function isObject(obj) {
-  return obj !== null && typeof obj === 'object'
+  return obj !== null && typeof obj === 'object' && !isArray(obj) && !isDate(obj)
 }
 
 /**
@@ -16,44 +19,27 @@ function isObject(obj) {
  * Returns boolean true or false
  */
 function looseEqual(a, b) {
-  if (a === b) return true
-  const isObjectA = isObject(a)
-  const isObjectB = isObject(b)
-  if (isObjectA && isObjectB) {
-    try {
-      const isArrayA = isArray(a)
-      const isArrayB = isArray(b)
-      if (isArrayA && isArrayB) {
-        return (
-          a.length === b.length &&
-          a.every((e, i) => {
-            return looseEqual(e, b[i])
-          })
-        )
-      } else if (a instanceof Date && b instanceof Date) {
-        return a.getTime() === b.getTime()
-      } else if (!isArrayA && !isArrayB) {
-        const keysA = keys(a)
-        const keysB = keys(b)
-        return (
-          keysA.length === keysB.length &&
-          keysA.every(key => {
-            return looseEqual(a[key], b[key])
-          })
-        )
-      } else {
-        /* istanbul ignore next */
+  if (a === b) {
+    return true
+  }
+  if (isObject(a) && isObject(b)) {
+    // Using for loop over `Object.keys()` here since some class
+    // keys are not handled correctly otherwise
+    for (const key in a) {
+      const aHasKey = a.hasOwnProperty(key)
+      const bHasKey = b.hasOwnProperty(key)
+      if ((aHasKey && !bHasKey) || (!aHasKey && bHasKey) || !looseEqual(a[key], b[key])) {
         return false
       }
-    } catch (e) {
-      /* istanbul ignore next */
-      return false
     }
-  } else if (!isObjectA && !isObjectB) {
-    return String(a) === String(b)
-  } else {
-    return false
   }
+  if (isArray(a) && isArray(b)) {
+    return a.length === b.length && a.every((e, i) => looseEqual(e, b[i]))
+  }
+  if (isDate(a) && isDate(b)) {
+    return a.getTime() === b.getTime()
+  }
+  return false
 }
 
 export default looseEqual
