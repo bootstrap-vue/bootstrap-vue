07ad3ecc93dd5ed20b7b079752a246ff5a05afeb
Troy Morehouse <troymore@nbnet.nb.ca>
2019-02-18 14:53:24 -0400

    Tweak checks and break out of for loop if falsey equality detected

--- src/utils/loose-equal.js
+++ src/utils/loose-equal.js
@@ -5,6 +5,11 @@ function isDate(obj) {
   return obj instanceof Date
 }
 
+function isFile(obj) {
+  return obj instanceof File
+}
+
+
 /**
  * Quick object check - this is primarily used to tell
  * Objects from primitive values when we know the value
@@ -23,6 +28,15 @@ function looseEqual(a, b) {
   if (a === b) {
     return true
   }
+  if (isDate(a) && isDate(b)) {
+    return a.getTime() === b.getTime()
+  }
+  if (isFile(a) && isFile(b)) {
+    return a === b
+  }
+  if (isArray(a) && isArray(b)) {
+    return a.length === b.length && a.every((e, i) => looseEqual(e, b[i]))
+  }
   if (isObject(a) && isObject(b)) {
     if (keys(a).length !== keys(b).length) {
       return false
@@ -30,21 +44,19 @@ function looseEqual(a, b) {
 
     // Using for loop over `Object.keys()` here since some class
     // keys are not handled correctly otherwise
+    let result = true
     for (const key in a) {
       const aHasKey = a.hasOwnProperty(key)
       const bHasKey = b.hasOwnProperty(key)
       if ((aHasKey && !bHasKey) || (!aHasKey && bHasKey) || !looseEqual(a[key], b[key])) {
-        return false
+        result = false
+        break
       }
     }
+    return result
   }
-  if (isArray(a) && isArray(b)) {
-    return a.length === b.length && a.every((e, i) => looseEqual(e, b[i]))
-  }
-  if (isDate(a) && isDate(b)) {
-    return a.getTime() === b.getTime()
-  }
-  return false
+  // Handle other cases
+  return String(a) === String(b)
 }
 
 export default looseEqual
