d157dac6c1386701fde44f00ea0fa8bf4159532c
Jacob Müller <jacob.mueller.elz@gmail.com>
2019-02-18 22:03:38 +0100

    Save on variables

--- src/utils/loose-equal.js
+++ src/utils/loose-equal.js
@@ -31,30 +31,32 @@ function looseEqual(a, b) {
   if (typeof a !== typeof b) {
     return false
   }
-  const validDatesCount = [isDate(a), isDate(b)].filter(Boolean).length
-  if (validDatesCount > 0) {
-    return validDatesCount === 2 ? a.getTime() === b.getTime() : false
+  let validTypesCount = [isDate(a), isDate(b)].filter(Boolean).length
+  if (validTypesCount > 0) {
+    return validTypesCount === 2 ? a.getTime() === b.getTime() : false
   }
-  const validFilesCount = [isFile(a), isFile(b)].filter(Boolean).length
-  if (validFilesCount > 0) {
-    return validFilesCount === 2 ? a === b : false
+  validTypesCount = [isFile(a), isFile(b)].filter(Boolean).length
+  if (validTypesCount > 0) {
+    return validTypesCount === 2 ? a === b : false
   }
-  const validArraysCount = [isArray(a), isArray(b)].filter(Boolean).length
-  if (validArraysCount > 0) {
-    return validArraysCount === 2
+  validTypesCount = [isArray(a), isArray(b)].filter(Boolean).length
+  if (validTypesCount > 0) {
+    return validTypesCount === 2
       ? a.length === b.length && a.every((e, i) => looseEqual(e, b[i]))
       : false
   }
-  const validObjectsCount = [isObject(a), isObject(b)].filter(Boolean).length
-  if (validObjectsCount > 0) {
-    if (validObjectsCount === 1 || keys(a).length !== keys(b).length) {
+  validTypesCount = [isObject(a), isObject(b)].filter(Boolean).length
+  if (validTypesCount > 0) {
+    if (validTypesCount === 1 || keys(a).length !== keys(b).length) {
       return false
     }
     // Using for loop over `Object.keys()` here since some class
     // keys are not handled correctly otherwise
     for (const key in a) {
-      const validKeysCount = [a.hasOwnProperty(key), b.hasOwnProperty(key)].filter(Boolean).length
-      if (validKeysCount === 1 || !looseEqual(a[key], b[key])) {
+      if (
+        [a.hasOwnProperty(key), b.hasOwnProperty(key)].filter(Boolean).length === 1 ||
+        !looseEqual(a[key], b[key])
+      ) {
         return false
       }
     }
