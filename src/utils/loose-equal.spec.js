65d95227c63bde5406f82244a949857c34555fa6
Troy Morehouse <troymore@nbnet.nb.ca>
2019-02-18 14:25:34 -0400

    date and array tests

--- src/utils/loose-equal.spec.js
+++ src/utils/loose-equal.spec.js
@@ -34,6 +34,42 @@ describe('looseEqual', async () => {
     expect(looseEqual(multiplier, multiplier)).toBe(true)
   })
 
+  it('compares dates correctly', async () => {
+    const date1 = new Date(2019, 1, 2, 3, 4, 5, 6)
+    const date2 = new Date(2019, 1, 2, 3, 4, 5, 6)
+    const date3 = new Date(2019, 1, 2, 3, 4, 5, 7)
+    const date4 = new Date(2219, 1, 2, 3, 4, 5, 6)
+
+    // Identical date object references
+    expect(looseEqual(date1, date1)).toBe(true)
+    // Different date references with identical values
+    expect(looseEqual(date1, date2)).toBe(true)
+    // Dates with slightly different time (ms)
+    expect(looseEqual(date1, date3)).toBe(false)
+    // Dates with different year
+    expect(looseEqual(date1, date4)).toBe(false)
+  })
+
+  it('compares arrays correctly', async () => {
+    const arr1 = [1, 2, 3, 4]
+    const arr2 = [1, 2, 3, '4']
+    const arr3 = [1, 2, 3, 4, 5]
+    const arr4 = [1, 2, 3, 4, { a: 5 }]
+
+    // Identical array references
+    expect(looseEqual(arr1, arr1)).toBe(true)
+    // Different array references with identical values
+    expect(looseEqual(arr1, arr1.slice())).toBe(true)
+    expect(looseEqual(arr4, arr4.slice())).toBe(true)
+    // Array with one value different
+    expect(looseEqual(arr1, arr2)).toBe(false)
+    expect(looseEqual(arr3, arr4)).toBe(false)
+    // Arrays with different lengths
+    expect(looseEqual(arr1, arr3)).toBe(false)
+    // Arrays with values in different order
+    expect(looseEqual(arr1, arr1.slice().reverse())).toBe(false)
+  })
+
   it('compares objects correctly', async () => {
     const obj1 = { foo: 'bar' }
     const obj2 = { foo: 'bar1' }
@@ -42,16 +78,22 @@ describe('looseEqual', async () => {
     const obj5 = { ...obj4, z: 999 }
     const nestedObj1 = { ...obj1, bar: [{ ...obj1 }, { ...obj1 }] }
     const nestedObj2 = { ...obj1, bar: [{ ...obj1 }, { ...obj2 }] }
-    const nestedObj3 = { ...nestedObj2 }
 
+    // identical object references
     expect(looseEqual(obj1, obj1)).toBe(true)
+    // two objects with identical keys/values
     expect(looseEqual(obj1, { ...obj1 })).toBe(false)
+    // different key values
     expect(looseEqual(obj1, obj2)).toBe(false)
+    // keys in different orders
     expect(looseEqual(obj3, obj4)).toBe(true)
+    // one object has additional key
     expect(looseEqual(obj4, obj5)).toBe(false)
+    // identical object references with nested array
     expect(looseEqual(nestedObj1, nestedObj1)).toBe(true)
+    // identical object definitions with nested array
     expect(looseEqual(nestedObj1, { ...nestedObj1 })).toBe(false)
+    // Object definitions with nested array in different order
     expect(looseEqual(nestedObj1, nestedObj2)).toBe(false)
-    expect(looseEqual(nestedObj2, nestedObj3)).toBe(true)
   })
 })
