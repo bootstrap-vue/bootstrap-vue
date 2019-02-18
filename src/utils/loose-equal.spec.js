c493c9ce2546e17dcadc3d4d1aef30cac6c04458
Troy Morehouse <troymore@nbnet.nb.ca>
2019-02-18 14:05:43 -0400

    additional object checks

--- src/utils/loose-equal.spec.js
+++ src/utils/loose-equal.spec.js
@@ -14,6 +14,7 @@ describe('looseEqual', async () => {
     const text = 'Lorem ipsum'
     const number = 1
     const bool = true
+
     expect(looseEqual(text, text)).toBe(true)
     expect(looseEqual(text, text.slice(0, -1))).toBe(false)
     expect(looseEqual(String(number), number)).toBe(false)
@@ -24,6 +25,7 @@ describe('looseEqual', async () => {
     const number = 100
     const decimal = 2.5
     const multiplier = 1.0000001
+
     expect(looseEqual(number, number)).toBe(true)
     expect(looseEqual(number, number - 1)).toBe(false)
     expect(looseEqual(decimal, decimal)).toBe(true)
@@ -35,13 +37,21 @@ describe('looseEqual', async () => {
   it('compares objects correctly', async () => {
     const obj1 = { foo: 'bar' }
     const obj2 = { foo: 'bar1' }
+    const obj3 = { a: 1, b: 2, c: 3 }
+    const obj4 = { b: 2, c: 3, a: 1 }
+    const obj5 = { ...obj4, z: 999 }
     const nestedObj1 = { ...obj1, bar: [{ ...obj1 }, { ...obj1 }] }
     const nestedObj2 = { ...obj1, bar: [{ ...obj1 }, { ...obj2 }] }
+    const nestedObj3 = { ...nestedObj2 }
+
     expect(looseEqual(obj1, obj1)).toBe(true)
     expect(looseEqual(obj1, { ...obj1 })).toBe(false)
     expect(looseEqual(obj1, obj2)).toBe(false)
+    expect(looseEqual(obj3, obj4)).toBe(true)
+    expect(looseEqual(obj4, obj5)).toBe(false)
     expect(looseEqual(nestedObj1, nestedObj1)).toBe(true)
     expect(looseEqual(nestedObj1, { ...nestedObj1 })).toBe(false)
     expect(looseEqual(nestedObj1, nestedObj2)).toBe(false)
+    expect(looseEqual(nestedObj2, nestedObj3)).toBe(true)
   })
 })
