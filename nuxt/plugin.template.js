d86b45317190713b1ab263569ce6c37a4a8864b6
pimlie <pimlie@hotmail.com>
2019-02-19 16:50:27 +0100

    feat: add tree-shaking support to Nuxt plugin

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
+<% if (options.plugins) { %>
+<%= options.plugins.reduce((acc, p) => (acc += `import ${p[0]} from 'bootstrap-vue/es/components/${p[1]}'\n` ), '') %>
+
+<%= options.plugins.reduce((acc, p) => (acc += `Vue.use(${p[0]})\n` ), '') %>
+<% } else { %>
 import BootstrapVue from 'bootstrap-vue/es'
 
 Vue.use(BootstrapVue)
+<% } %>
