ff5cac01cc93c960e070704118c453eef07f3a8d
Troy Morehouse <troymore@nbnet.nb.ca>
2019-02-23 18:09:00 -0400

    Update tabs.spec.js

--- src/components/tabs/tabs.spec.js
+++ src/components/tabs/tabs.spec.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import Tab from './tab'
 import Tabs from './tabs'
 import { mount } from '@vue/test-utils'
@@ -36,5 +37,40 @@ describe('tabs', async () => {
     expect(wrapper.vm.currentTab).toBe(tabIndex)
     expect(wrapper.vm.tabs.length).toBe(3)
     expect(wrapper.vm.tabs[tabIndex].localActive).toBe(true)
+  })
+
+  it('sets correct tab active when first tab is disabled', async () => {
+    const App = Vue.extend({
+      render(h) {
+        return h(
+          Tabs,
+          {},
+          [
+            h(Tab, { props: { disabled: true } }, 'tab 0'),
+            h(Tab, { props: {} }, 'tab 0'),
+            h(Tab, { props: {} }, 'tab 0')
+          ]
+          )
+      }
+    })
+    const wrapper = mount(App)
+
+    expect(wrapper).toBeDefined()
+
+    await wrapper.vm.$nextTick()
+
+    const tabs = wrapper.find(Tabs)
+    expect(tabs).toBeDefined()
+
+    expect(tabs.findAll(Tab).length).toBe(3)
+
+    // Expect 2nd tab (index 1) to be active
+    expect(tabs.vm.currentTab).toBe(1)
+    expect(tabs.vm.tabs[1].localActive).toBe(true)
+
+    expect(tabs.emitted('input')).toBeDefined()
+    expect(tabs.emitted('input').length).toBe(1)
+    // Should emit index of 1 (2nd tab)
+    expect(tabs.emitted('input'][0][0]).toBe(1)
   })
 })
