0c2b2056899f99a0e42e5a78f6a94b8a90b0f210
Troy Morehouse <troymore@nbnet.nb.ca>
2019-02-22 23:36:13 -0400

    Update form-file.spec.js

--- src/components/form-file/form-file.spec.js
+++ src/components/form-file/form-file.spec.js
@@ -1,6 +1,120 @@
-import { loadFixture, testVM } from '../../../tests/utils'
+import Input from './form-file'
+import { mount } from '@vue/test-utils'
+import Vue from 'vue'
 
 describe('form-file', async () => {
-  beforeEach(loadFixture(__dirname, 'form-file'))
-  testVM()
+  it('default has expected structure, classes and attributes', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        id: 'foo'
+      }
+    })
+
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('custom-file')
+    expect(wrapper.attributes('id')).toBeDefined()
+    expect(wrapper.attributes('id')).toBe('foo__BV_file_outer_')
+
+    const input = wrapper.find('input')
+    expect(input).toBeDefined()
+    expect(input.classes()).toContain('custom-file-input')
+    expect(input.attributes('type')).toBeDefined()
+    expect(input.attributes('type')).toBe('file')
+    expect(input.attributes('id')).toBeDefined()
+    expect(input.attributes('id')).toBe('foo')
+    expect(input.attributes('multiple')).not.toBeDefined()
+
+    const label = wrapper.find('label')
+    expect(label).toBeDefined()
+    expect(label.classes()).toContain('custom-file-label')
+    expect(input.attributes('for')).toBeDefined()
+    expect(input.attributes('for')).toBe('foo')
+  })
+
+  it('default has input attribute multiple when multiple=true', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        id: 'foo',
+        multiple: true
+      }
+    })
+    const input = wrapper.find('input')
+    expect(input.attributes('multiple')).toBeDefined()
+  })
+
+  it('default has class focus when input focused', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        id: 'foo'
+      }
+    })
+    const input = wrapper.find('input')
+    expect(input).toBeDefined()
+
+    expect(input.classes()).not.toContain('focus')
+
+    input.element.focus()
+    await wrapper.vm.$nextTick()
+    expect(input.classes()).toContain('focus')
+
+    input.element.blur()
+    await wrapper.vm.$nextTick()
+    expect(input.classes()).not.toContain('focus')
+  })
+
+  it('has no wrapper div or label when plain=true', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        id: 'foo',
+        plain: true
+      }
+    })
+    expect(wrapper.is('input')).toBe(true)
+    expect(wrapper.attributes('type')).toBeDefined()
+    expect(wrapper.attributes('type')).toBe('file')
+    expect(wrapper.attributes('id')).toBeDefined()
+    expect(wrapper.attributes('id')).toBe('foo')
+    expect(wrapper.attributes('multiple')).not.toBeDefined()
+  })
+
+  it('emits input event when file changed', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        id: 'foo'
+      }
+    })
+    const file = new File(['foo'], 'foo.txt')
+
+    // Emulate the files array
+    wrapper.vm.setFiles([file])
+    expect(wrapper.emitted('input')).toBeDefined()
+    expect(wrapper.emitted('input').length).toEqual(1)
+    expect(wrapper.emitted('input')[0][0]).toEqual(file)
+  })
+
+  it('emits input event when files changed in multiple mode', async () => {
+    const wrapper = mount(Input, {
+      propsData: {
+        id: 'foo',
+        multiple: true
+      }
+    })
+    const file1 = new File(['foo'], 'foo.txt')
+    const file2 = new File(['bar'], 'bar.txt')
+    const files = [file1, file2]
+
+    // Emulate the files array
+    wrapper.vm.setFiles(files)
+    expect(wrapper.emitted('input')).toBeDefined()
+    expect(wrapper.emitted('input').length).toEqual(1)
+    expect(wrapper.emitted('input')[0][0]).toEqual(files)
+
+    // Setting to array of same files should not emit event
+    wrapper.vm.setFiles([file1, file2])
+    expect(wrapper.emitted('input').length).toEqual(1)
+
+    // Setting to array of new files should not emit event
+    wrapper.vm.setFiles(files.slice().reverse())
+    expect(wrapper.emitted('input').length).toEqual(2)
+  })
 })
