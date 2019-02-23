9e890223d0155d1f9893f49c6d82e8268553d1b3
Troy Morehouse <troymore@nbnet.nb.ca>
2019-02-23 11:31:58 -0400

    lint

--- nuxt/index.js
+++ nuxt/index.js
@@ -15,15 +15,25 @@ module.exports = function nuxtBootstrapVue(moduleOptions = {}) {
     ...moduleOptions
   }
 
-  // Defaults to `true` if no other options provided
-  const bootstrapVueCSS = pickFirst(options.bootstrapVueCSS, options.bootstrapVueCss, options.bvCSS, true)
+  const bootstrapVueCSS = pickFirst(
+    options.bootstrapVueCSS,
+    options.bootstrapVueCss,
+    options.bvCSS,
+    // Defaults to `true` if no other options provided
+    true
+  )
   if (bootstrapVueCSS) {
     // Add BootstrapVue CSS
     this.options.css.unshift('bootstrap-vue/dist/bootstrap-vue.css')
   }
 
-  // Defaults to `true` if no other options provided
-  const bootstrapCSS = pickFirst(options.bootstrapCSS, options.bootstrapCss, options.css, true)
+  const bootstrapCSS = pickFirst(
+    options.bootstrapCSS,
+    options.bootstrapCss,
+    options.css,
+    // Defaults to `true` if no other options provided
+    true
+  )
   if (bootstrapCSS) {
     // Add Bootstrap CSS before BootstrapVue CSS
     this.options.css.unshift('bootstrap/dist/css/bootstrap.css')
