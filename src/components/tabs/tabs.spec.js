1ffc52fc1eb6484e455e293fef8b8be7b76663f0
Troy Morehouse <troymore@nbnet.nb.ca>
2019-02-23 19:57:44 -0400

    Update tabs.spec.js

--- src/components/tabs/tabs.spec.js
+++ src/components/tabs/tabs.spec.js
@@ -189,24 +189,27 @@ describe('tabs', async () => {
     expect(tabs.vm.tabs[0].localActive).toBe(true)
 
     // Try to set 2nd Tab to be active via click
-    expect(tab2.emitted('click').not.toBeDefined()
+    expect(tab2.emitted('click')).not.toBeDefined()
     buttons[1].element.trigger('click')
     await wrapper.vm.$nextTick()
     expect(tabs.vm.currentTab).toBe(1)
-    expect(tab2.emitted('click').toBeDefined()
+    expect(tab2.vm.localActive).toBe(true)
+    expect(tab2.emitted('click')).toBeDefined()
 
     // Try to set 3rd Tab to be active via click
-    expect(tab3.emitted('click').not.toBeDefined()
+    expect(tab3.emitted('click')).not.toBeDefined()
     buttons[2].element.trigger('click')
     await wrapper.vm.$nextTick()
     expect(tabs.vm.currentTab).toBe(2)
-    expect(tab3.emitted('click').toBeDefined()
+    expect(tab3.vm.localActive).toBe(true)
+    expect(tab3.emitted('click')).toBeDefined()
 
     // Try to set 1st Tab to be active via click
-    expect(tab1.emitted('click').not.toBeDefined()
+    expect(tab1.emitted('click')).not.toBeDefined()
     buttons[1].element.trigger('click')
     await wrapper.vm.$nextTick()
     expect(tabs.vm.currentTab).toBe(0)
-    expect(tab1.emitted('click').toBeDefined()
+    expect(tab1.vm.localActive).toBe(true)
+    expect(tab1.emitted('click')).toBeDefined()
   })
 })
