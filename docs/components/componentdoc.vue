da8c6f9f7a591c63365f5d2d20c44c31565fa9c4
Jacob Müller <jacob.mueller.elz@gmail.com>
2019-02-25 00:27:39 +0100

    `rootEventEmitters` => `rootEventListeners`

--- docs/components/componentdoc.vue
+++ docs/components/componentdoc.vue
@@ -69,12 +69,12 @@
       </b-table>
     </article>
 
-    <article v-if="rootEventEmitters && rootEventEmitters.length > 0">
+    <article v-if="rootEventListeners && rootEventListeners.length > 0">
       <h4 :id="`comp-ref-${componentName}-rootEventListeners`">$root Event Listeners</h4>
       <p>You can control <code>{{ tag }}</code> by emitting the following events on <samp>$root</samp>:</p>
       <b-table
-        :items="rootEventEmitters"
-        :fields="rootEventEmittersFields"
+        :items="rootEventListeners"
+        :fields="rootEventListenersFields"
         small
         head-variant="default"
         striped
@@ -118,7 +118,7 @@ export default {
       type: Array,
       default: () => []
     },
-    rootEventEmitters: {
+    rootEventListeners: {
       type: Array,
       default: () => []
     },
@@ -160,7 +160,7 @@ export default {
         description: { label: 'Description' }
       }
     },
-    rootEventEmittersFields() {
+    rootEventListenersFields() {
       return {
         event: { label: 'Event' },
         args: { label: 'Arguments' },
