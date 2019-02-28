55ae31271cfa3e0c4a29ff6ed06cc441444296b7
Troy Morehouse <troymore@nbnet.nb.ca>
2019-02-27 23:57:19 -0400

    Create to-string.js

---
+++ src/utils/to-string.js
@@ -0,0 +1,13 @@
+import { isArray } from './array'
+import { isPlainObject } from './object'
+
+/**
+ * Convert a value to a string that can be rendered.
+ */
+export default function toString (val) {
+  return val === null || val === undefined
+    ? ''
+    : isArray(val) || (isPlainObject(val) && val.toString === Object.prototype.toString)
+      ? JSON.stringify(val, null, 2)
+      : String(val)
+}
