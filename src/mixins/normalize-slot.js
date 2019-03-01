a16fa24124606574a2de0f100b216447b54b4fc4
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-01 10:50:46 -0400

    mixin: add helper for checking if either a scopedSlot or slot exists
    Also always returns an array from `normalizeSlot` if vNode(s) exist.

--- src/mixins/normalize-slot.js
+++ src/mixins/normalize-slot.js
@@ -1,9 +1,17 @@
 import normalizeSlot from '../utils/normalize-slot'
+import concat from './array'
 
 export default {
   methods: {
-    normalizeSlot(name, scope = {}) {
-      return normalizeSlot(name, scope, this.$scopedSlots, this.$slots)
+    hasNormalizedSlot (name) {
+      // Returns true if the eitehr a $scopedSlot or $slot exists with the specified name
+      return Boolean(this.$scopedSlots[name] || this.$slots[name])
+    },
+    normalizeSlot (name, scope = {}) {
+      // Returns an array of rendered vNodes if slot found.
+      // Returns undefined if not found.
+      const vNodes = normalizeSlot(name, scope, this.$scopedSlots, this.$slots)
+      return vNodes ? concat(vNodes) : vNodes
     }
   }
 }
