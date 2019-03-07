de6b07a3d15333fc71fcda3380928548bfd09244
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-07 19:59:10 -0400

    Update form-selection.js

--- src/mixins/form-selection.js
+++ src/mixins/form-selection.js
@@ -1,53 +1,47 @@
 // @vue/component
 export default {
   computed: {
-    /* istanbul ignore next */
     selectionStart: {
       // Expose selectionStart for formatters, etc
       cache: false,
-      get() {
+      get() /* istanbul ignore next */ {
         return this.$refs.input.selectionStart
       },
-      set(val) {
+      set(val) /* istanbul ignore next */ {
         this.$refs.input.selectionStart = val
       }
     },
-    /* istanbul ignore next */
     selectionEnd: {
       // Expose selectionEnd for formatters, etc
       cache: false,
-      get() {
+      get() /* istanbul ignore next */ {
         return this.$refs.input.selectionEnd
       },
-      set(val) {
+      set(val) /* istanbul ignore next */ {
         this.$refs.input.selectionEnd = val
       }
     },
-    /* istanbul ignore next */
     selectionDirection: {
       // Expose selectionDirection for formatters, etc
       cache: false,
-      get() {
+      get() /* istanbul ignore next */ {
         return this.$refs.input.selectionDirection
       },
-      set(val) {
+      set(val) /* istanbul ignore next */ {
         this.$refs.input.selectionDirection = val
       }
     }
   },
   methods: {
-    /* istanbul ignore next */
-    select() {
+    select() /* istanbul ignore next */ {
       // For external handler that may want a select() method
       this.$refs.input.select(...arguments)
     },
-    /* istanbul ignore next */
-    setSelectionRange() {
+    setSelectionRange() /* istanbul ignore next */ {
       // For external handler that may want a setSelectionRange(a,b,c) method
       this.$refs.input.setSelectionRange(...arguments)
     },
-    /* istanbul ignore next */
-    setRangeText() {
+    setRangeText() /* istanbul ignore next */ {
       // For external handler that may want a setRangeText(a,b,c) method
       this.$refs.input.setRangeText(...arguments)
     }
