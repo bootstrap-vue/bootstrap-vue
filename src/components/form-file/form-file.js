5187a9394fa3a90f4dc85194140efb18ef58c1a8
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-07 21:47:01 -0400

    Update form-file.js

--- src/components/form-file/form-file.js
+++ src/components/form-file/form-file.js
@@ -137,6 +137,7 @@ export default {
       // Can be disabled by setting no-traverse
       const items = evt.dataTransfer && evt.dataTransfer.items
       if (items && !this.noTraverse) {
+        /* istanbul ignore next: not supported in JSDOM */
         const queue = []
         for (let i = 0; i < items.length; i++) {
           const item = items[i].webkitGetAsEntry()
