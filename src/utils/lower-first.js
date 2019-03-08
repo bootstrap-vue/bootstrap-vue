2037ef444e20d6a4eb257660ffa12aba3a48b3fd
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-07 20:02:32 -0400

    Update lower-first.js

--- src/utils/lower-first.js
+++ src/utils/lower-first.js
@@ -2,8 +2,6 @@
  * @param {string} str
  */
 export default function lowerFirst(str) {
-  if (typeof str !== 'string') {
-    str = String(str)
-  }
+  str = String(str)
   return str.charAt(0).toLowerCase() + str.slice(1)
 }
