0a8bbe0ae0b3bfa868e08f525bab8bd7169e2bd1
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-07 22:03:14 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,6 @@ import { vueUse } from './utils/plugins'
 
 const VuePlugin = {
   install: function(Vue) {
-    if (Vue._bootstrap_vue_installed) {
-      return
-    }
-
-    Vue._bootstrap_vue_installed = true
-
     // Register component plugins
     for (let plugin in componentPlugins) {
       Vue.use(componentPlugins[plugin])
