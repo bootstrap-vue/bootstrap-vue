994268b039125edcfeafda04fdaa97a5624ce478
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-09 01:33:04 -0400

    Create mixin-top-row.js

---
+++ src/components/table/helpers/mixin-top-row.js
@@ -0,0 +1,27 @@
+export default {
+  methods:
+    renderTopRow(h) {
+      // Add static Top Row slot (hidden in visibly stacked mode as we can't control the data-label)
+      // If in always stacked mode, we don't bother rendering the row
+      if (!this.hasNormalizedSlot('top-row') || this.isStacked === true) {
+        return h(false)
+      }
+
+      const fields = this.computedFields
+
+      return h(
+        'tr',
+        {
+          key: 'top-row',
+          staticClass: 'b-table-top-row',
+          class: [
+            typeof this.tbodyTrClass === 'function'
+              ? this.tbodyTrClass(null, 'row-top')
+              : this.tbodyTrClass
+          ]
+        },
+        [this.normalizeSlot('top-row', { columns: fields.length, fields: fields })]
+      )
+    )
+  }
+}
