0969bdd124fe0180f3e2a96be887a4a811f0c3e4
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-09 01:53:42 -0400

    Update mixin-bottom-row.js

--- src/components/table/helpers/mixin-bottom-row.js
+++ src/components/table/helpers/mixin-bottom-row.js
@@ -1,26 +1,27 @@
 export default {
   methods: {
     renderBottomRow(h) {
-    // Static bottom row slot (hidden in visibly stacked mode as we can't control the data-label)
-    // If in always stacked mode, we don't bother rendering the row
-    if (!this.hasNormalizedSlot('bottom-row') || this.isStacked === true) {
-      return h(false)
+      // Static bottom row slot (hidden in visibly stacked mode as we can't control the data-label)
+      // If in always stacked mode, we don't bother rendering the row
+      if (!this.hasNormalizedSlot('bottom-row') || this.isStacked === true) {
+        return h(false)
+      }
+
+      const fields = this.computedFields
+
+      return h(
+        'tr',
+        {
+          key: '__b-table-bottom-row__',
+          staticClass: 'b-table-bottom-row',
+          class: [
+            typeof this.tbodyTrClass === 'function'
+              ? this.tbodyTrClass(null, 'row-bottom')
+              : this.tbodyTrClass
+          ]
+        },
+        this.normalizeSlot('bottom-row', { columns: fields.length, fields: fields })
+      )
     }
-
-    const fields = this.computedFields
-
-    return h(
-      'tr',
-      {
-        key: '__b-table-bottom-row__',
-        staticClass: 'b-table-bottom-row',
-        class: [
-          typeof this.tbodyTrClass === 'function'
-            ? this.tbodyTrClass(null, 'row-bottom')
-            : this.tbodyTrClass
-        ]
-      },
-      this.normalizeSlot('bottom-row', { columns: fields.length, fields: fields })
-    )
   }
 }
