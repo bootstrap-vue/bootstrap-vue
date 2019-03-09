f00bd208ee7bf72092cd3fc9fb14053b061548c1
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-08 22:14:35 -0400

    Create mixin-colgroup.js

---
+++ src/components/table/helpers/mixin-colgroup.js
@@ -0,0 +1,19 @@
+export default {
+  methods: {
+    renderColgroup(h)
+      // Build the colgroup
+      const fields = this.computedFields
+      let $colgroup = h(false)
+
+      if (this.hasNormalizedSlot('table-colgroup')) {
+        $colgroup = h(
+          'colgroup',
+          { key: 'colgroup' },
+          this.normalizeSlot('table-colgroup', { columns: fields.length, fields: fields })
+        )
+      }
+
+      return $colgroup
+    }
+  }
+}
