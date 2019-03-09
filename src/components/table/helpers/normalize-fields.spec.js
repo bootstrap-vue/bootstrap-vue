523ed8c427e72604515fd6128f802cfdbbe8d49d
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-09 12:39:03 -0400

    Update normalize-fields.spec.js

--- src/components/table/helpers/normalize-fields.spec.js
+++ src/components/table/helpers/normalize-fields.spec.js
@@ -1,10 +1,10 @@
-import noramlizeFields from './normalize-fields'
+import normalizeFields from './normalize-fields'
 
 describe('table/helpers/normalize-fields', () => {
   it('uses first row of items when fields are not defined and items passed', async () => {
-    const fileds = normalizeFields(null, [
-     { foo: 1, bar: { a: 2}, 'foo bar': 3, 'baz_bar': 4},
-     { foo: 1, bar: { a: 2}, 'foo bar': 3, baz: 5 },
+    const fields = normalizeFields(null, [
+     { foo: 1, bar: { a: 2 }, 'foo bar': 3, 'baz_bar': 4 },
+     { foo: 1, bar: { a: 2 }, 'foo bar': 3, baz: 5 },
     ])
     expect(fields).toEqual([
       { key: 'foo', label: 'Foo' },
@@ -15,17 +15,17 @@ describe('table/helpers/normalize-fields', () => {
   })
 
   it('returns and empty array when no fields or items passed', async () => {
-     expect(normalizeFields(undefined, undefined)).toEqual([])
-     expect(normalizeFields(null, null)).toEqual([])
-     expect(normalizeFields([], undefined)).toEqual([])
-     expect(normalizeFields(undefined, [])).toEqual([])
-     expect(normalizeFields([], [])).toEqual([])
-     expect(normalizeFields({}, [])).toEqual([])
+    expect(normalizeFields(undefined, undefined)).toEqual([])
+    expect(normalizeFields(null, null)).toEqual([])
+    expect(normalizeFields([], undefined)).toEqual([])
+    expect(normalizeFields(undefined, [])).toEqual([])
+    expect(normalizeFields([], [])).toEqual([])
+    expect(normalizeFields({}, [])).toEqual([])
   })
 
   it('handles simple array as fields', async () => {
     const arr1 = ['foo', 'bar', 'foo_bar']
-    expect(normalizeFields(arr, [])).toEqual([
+    expect(normalizeFields(arr1, [])).toEqual([
       { key: 'foo', label: 'Foo' },
       { key: 'bar', label: 'Bar' },
       { key: 'foo_bar', label: 'Foo Bar' }
