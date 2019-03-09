f2ea4aedcc9345f5931465d3abac3ce2696f77f8
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-09 01:55:32 -0400

    Create mixin-busy.js

---
+++ src/components/table/helpers/mixin-busy.js
@@ -0,0 +1,35 @@
+export default {
+  methods: {
+    renderBusy(h) {
+      // Return a busy indicator row, or null if not busy
+      if ($slots['table-busy'] && this.computedBusy) {
+        // Show the busy slot
+        const trAttrs = {
+          role: this.isStacked ? 'row' : null
+        }
+        const tdAttrs = {
+          colspan: String(fields.length),
+          role: this.isStacked ? 'cell' : null
+        }
+        return h(
+          'tr',
+          {
+            key: 'table-busy-slot',
+            staticClass: 'b-table-busy-slot',
+            class: [
+              typeof this.tbodyTrClass === 'function'
+                ? this.tbodyTrClass(null, 'table-busy')
+                : this.tbodyTrClass
+            ],
+            attrs: trAttrs
+          },
+          [h('td', { attrs: tdAttrs }, [$slots['table-busy']])]
+        )
+      } else {
+        // We return null here so that we can determine if we need to
+        // render the table items rows or not.
+        return null
+      }
+    }
+  }
+}
