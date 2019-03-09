cf5e451e3974e6cdaf92cc055e89ac6914db7914
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-09 00:10:14 -0400

    Create mixn-tfoot.js

---
+++ src/components/table/helpers/mixn-tfoot.js
@@ -0,0 +1,32 @@
+export default {
+  props: {
+    footClone: {
+      type: Boolean,
+      default: false
+    },
+    footVariant: {
+      type: String,
+      default: ''
+    },
+    tfootClass: {
+      type: [String, Array, Object],
+      default: null
+    },
+    tfootTrClass: {
+      type: [String, Array, Object],
+      default: null
+    }
+  },
+  computed: {
+    footClasses() {
+      const variant = this.footVariant || this.headVariant || null
+      return [variant ? 'thead-' + variant : '', this.tfootClass]
+    }
+  },
+  methods: {
+    renderTfoot(h) {
+      // Passing true to renderThead will make it render a tfoot
+      this.renderThead(h, true)
+    }
+  }
+}
