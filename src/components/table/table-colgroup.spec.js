9bfc6a5786b1160583abf7cd19eb01823cb4e1f0
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-10 21:02:25 -0300

    Update table-colgroup.spec.js

--- src/components/table/table-colgroup.spec.js
+++ src/components/table/table-colgroup.spec.js
@@ -5,8 +5,8 @@ import { mount } from '@vue/test-utils'
 const testItems = [{ a: 1, b: 2, c: 3 }, { a: 5, b: 5, c: 6 }, { a: 7, b: 8, c: 9 }]
 const testFields = ['a', 'b', 'c']
 
-describe('table colgroup, caption, header, and top/bottom row slots', () => {
-  it('should not have colgroup or caption by default', async () => {
+describe('table colgroup', () => {
+  it('should not have colgroup by default', async () => {
     const wrapper = mount(Table, {
       propsData: {
         fields: testFields,
@@ -16,23 +16,6 @@ describe('table colgroup, caption, header, and top/bottom row slots', () => {
     expect(wrapper).toBeDefined()
     expect(wrapper.is('table')).toBe(true)
     expect(wrapper.find('colgroup').exists()).toBe(false)
-    expect(wrapper.find('caption').exists()).toBe(false)
-    expect(wrapper.find('tr.b-table-top-row').exists()).toBe(false)
-    expect(wrapper.find('tr.b-table-bottom-row').exists()).toBe(false)
-    expect(
-      wrapper
-        .find('tbody')
-        .find('tr')
-        .exists()
-    ).toBe(true)
-    expect(wrapper.find('tbody').findAll('tr').length).toBe(testItems.length)
-    expect(
-      wrapper
-        .find('thead')
-        .find('tr')
-        .exists()
-    ).toBe(true)
-    expect(wrapper.find('thead').findAll('tr').length).toBe(1)
   })
 
   it('should render named slot `table-colgroup`', async () => {
@@ -87,145 +70,5 @@ describe('table colgroup, caption, header, and top/bottom row slots', () => {
     ).toBe(true)
     expect(wrapper.findAll('col').length).toBe(1)
     expect(wrapper.find('col').attributes('span')).toBe('3')
-  })
-
-  it('should render named slot `table-caption`', async () => {
-    const wrapper = mount(Table, {
-      propsData: {
-        fields: testFields,
-        items: testItems
-      },
-      slots: {
-        'table-caption': 'foobar'
-      }
-    })
-    expect(wrapper).toBeDefined()
-    expect(wrapper.is('table')).toBe(true)
-    expect(wrapper.find('table > caption').exists()).toBe(true)
-    expect(wrapper.find('caption').text()).toBe('foobar')
-    expect(wrapper.find('caption').attributes('id')).not.toBeDefined()
-    expect(wrapper.find('caption').classes()).not.toContain('b-table-caption-top')
-  })
-
-  it('should render scoped slot `table-caption`', async () => {
-    let scope = null
-    const wrapper = mount(Table, {
-      propsData: {
-        fields: testFields,
-        items: testItems
-      },
-      scopedSlots: {
-        'table-caption': function(props) {
-          scope = props
-          return this.$createElement('b', {}, 'foobar')
-        }
-      }
-    })
-    expect(wrapper).toBeDefined()
-    expect(wrapper.is('table')).toBe(true)
-    expect(wrapper.find('table > caption').exists()).toBe(true)
-    expect(scope).toEqual({}) /* scoped is an empty object for caption */
-    expect(
-      wrapper
-        .find('caption')
-        .find('b')
-        .exists()
-    ).toBe(true)
-    expect(wrapper.find('caption').text()).toBe('foobar')
-  })
-
-  it('should render `caption` when prop caption is set', async () => {
-    const wrapper = mount(Table, {
-      propsData: {
-        fields: testFields,
-        items: testItems,
-        caption: 'foobar'
-      }
-    })
-    expect(wrapper).toBeDefined()
-    expect(wrapper.is('table')).toBe(true)
-    expect(wrapper.find('table > caption').exists()).toBe(true)
-    expect(wrapper.find('caption').text()).toBe('foobar')
-    expect(wrapper.find('caption').attributes('id')).not.toBeDefined()
-    expect(wrapper.find('caption').classes()).not.toContain('b-table-caption-top')
-  })
-
-  it('should render `caption` when prop caption-html is set', async () => {
-    const wrapper = mount(Table, {
-      propsData: {
-        fields: testFields,
-        items: testItems,
-        captionHtml: '<b>foobar</b>'
-      }
-    })
-    expect(wrapper).toBeDefined()
-    expect(wrapper.is('table')).toBe(true)
-    expect(wrapper.find('table > caption').exists()).toBe(true)
-    expect(
-      wrapper
-        .find('caption')
-        .find('b')
-        .exists()
-    ).toBe(true)
-    expect(wrapper.find('caption').text()).toBe('foobar')
-    expect(wrapper.find('caption').attributes('id')).not.toBeDefined()
-    expect(wrapper.find('caption').classes()).not.toContain('b-table-caption-top')
-  })
-
-  it('should render `caption` with class when prop caption-top is set', async () => {
-    const wrapper = mount(Table, {
-      propsData: {
-        fields: testFields,
-        items: testItems,
-        caption: 'foobar',
-        captionTop: true
-      }
-    })
-    expect(wrapper).toBeDefined()
-    expect(wrapper.is('table')).toBe(true)
-    expect(wrapper.find('table > caption').exists()).toBe(true)
-    expect(wrapper.find('caption').text()).toBe('foobar')
-    expect(wrapper.find('caption').attributes('id')).not.toBeDefined()
-    expect(wrapper.find('caption').classes()).toContain('b-table-caption-top')
-  })
-
-  it('should render `caption` with id attribute when prop stacked is true', async () => {
-    const wrapper = mount(Table, {
-      propsData: {
-        id: 'zzz',
-        fields: testFields,
-        items: testItems,
-        caption: 'foobar',
-        stacked: true
-      }
-    })
-    expect(wrapper).toBeDefined()
-    expect(wrapper.is('table')).toBe(true)
-    expect(wrapper.attributes('id')).toBe('zzz')
-    await wrapper.vm.$nextTick()
-    expect(wrapper.find('table > caption').exists()).toBe(true)
-    expect(wrapper.find('caption').text()).toBe('foobar')
-    expect(wrapper.find('caption').attributes('id')).toBeDefined()
-    expect(wrapper.find('caption').attributes('id')).toBe('zzz__caption_')
-  })
-
-  it('should render `caption` with id attribute when prop stacked is sm', async () => {
-    const wrapper = mount(Table, {
-      propsData: {
-        id: 'zzz',
-        fields: testFields,
-        items: testItems,
-        caption: 'foobar',
-        stacked: 'sm'
-      }
-    })
-    expect(wrapper).toBeDefined()
-    expect(wrapper.is('table')).toBe(true)
-    expect(wrapper.attributes('id')).toBe('zzz')
-    await wrapper.vm.$nextTick()
-    expect(wrapper.find('table > caption').exists()).toBe(true)
-    expect(wrapper.find('caption').text()).toBe('foobar')
-    expect(wrapper.find('caption').attributes('id')).toBeDefined()
-    expect(wrapper.find('caption').attributes('id')).toBe('zzz__caption_')
   })
 })
