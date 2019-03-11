957f4ffdb7df4abcccf41129520627684a7a08bc
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-10 23:37:26 -0300

    Update table-sort.spec.js

--- src/components/table/table-sort.spec.js
+++ src/components/table/table-sort.spec.js
@@ -1,15 +1,11 @@
 import Table from './table'
 import { mount } from '@vue/test-utils'
 
-const testItems = [
-  { a: 3, b: 'b', c: 'x' },
-  { a: 1, b: 'c', c: 'y' },
-  { a: 2, b: 'a', c: 'z' }
-]
+const testItems = [{ a: 3, b: 'b', c: 'x' }, { a: 1, b: 'c', c: 'y' }, { a: 2, b: 'a', c: 'z' }]
 const testFields = [
- { key: 'a', label: 'A', sortable: true },
- { key: 'b', label: 'B', sortable: true },
- { key: 'c', label: 'C', sortable: false }
+  { key: 'a', label: 'A', sortable: true },
+  { key: 'b', label: 'B', sortable: true },
+  { key: 'c', label: 'C', sortable: false }
 ]
 
 describe('table sorting', () => {
@@ -33,12 +29,29 @@ describe('table sorting', () => {
     await wrapper.$nextTick()
     expect(wrapper.emitted('input')).toBeDefined()
     expect(wrapper.emitted('input').lnegth).toBe(1)
-    expect(wrapper.emitted('input')[0][0]).toEqual(testRows)
-    expect(visibleRows).toEqual(testRows)
+    expect(wrapper.emitted('input')[0][0]).toEqual(testItems)
+    expect(visibleRows).toEqual(testItems)
     const $rows = wrapper.findAll('tbody > tr')
     expect($rows.length).toBe(3)
-    expect($rows.at(0).findAll('td').at(0).text()).toBe('3')
-    expect($rows.at(1).findAll('td').at(0).text()).toBe('1')
-    expect($rows.at(2).findAll('td').at(0).text()).toBe('2')
+    expect(
+      $rows
+        .at(0)
+        .findAll('td')
+        .at(0)
+        .text()
+    ).toBe('3')
+    expect(
+      $rows
+        .at(1)
+        .findAll('td')
+        .at(0)
+        .text()
+    ).toBe('1')
+    expect(
+      $rows
+        .at(2)
+        .findAll('td')
+        .at(0).text()
+    ).toBe('2')
   })
 })
