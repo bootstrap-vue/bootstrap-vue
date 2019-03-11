588daf3e0be29b7681c627eb6186a5c97127be26
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-11 01:50:13 -0300

    Update stringify-record-values.js

--- src/components/table/helpers/stringify-record-values.js
+++ src/components/table/helpers/stringify-record-values.js
@@ -4,9 +4,11 @@ import stringifyObjectValues from './stringify-object-values'
 // Stringifies the values of a record, ignoring any special top level field keys
 // TODO: add option to strigify formatted/scopedSlot items, and only specific fields
 export default function stringifyRecordValues(row) {
-  if (!(row instanceof Object)) {
+  /* istanbul ignore else */
+  if (row instanceof Object) {
+    return stringifyObjectValues(sanitizeRow(row))
+  } else {
     /* istanbul ignore next */
     return ''
   }
-  return stringifyObjectValues(sanitizeRow(row))
 }
