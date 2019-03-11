14a9cad5f90dc8de8cc094f379145dc68ce548b7
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-10 23:04:48 -0300

    Update default-sort-compare.spec.js

--- src/components/table/helpers/default-sort-compare.spec.js
+++ src/components/table/helpers/default-sort-compare.spec.js
@@ -2,37 +2,37 @@ import defaultSortCompare from './default-sort-compare'
 
 describe('table/helpers/default-sort-compare', () => {
   it('sorts numbers correctly', async () => {
-    expect(defaultSortCompare(1, 2)).toBe(-1)
-    expect(defaultSortCompare(2, 1)).toBe(1)
-    expect(defaultSortCompare(1, 1)).toBe(0)
-    expect(defaultSortCompare(-1, 1)).toBe(-1)
-    expect(defaultSortCompare(1, -1)).toBe(1)
-    expect(defaultSortCompare(0, 0)).toBe(0)
-    expect(defaultSortCompare(1.234, 1.567)).toBe(-1)
-    expect(defaultSortCompare(1.561, 1.234)).toBe(1)
+    expect(defaultSortCompare({ a: 1 }, { a: 2 }, 'a')).toBe(-1)
+    expect(defaultSortCompare({ a: 2 } , { a: 1 }, 'a')).toBe(1)
+    expect(defaultSortCompare({ a: 1 }, { a: 1 }, 'a')).toBe(0)
+    expect(defaultSortCompare({ a: -1 }, { a: 1 }, 'a')).toBe(-1)
+    expect(defaultSortCompare({ a: 1 }, { a: -1 }, 'a')).toBe(1)
+    expect(defaultSortCompare({ a: 0 }, { a: 0 }, 'a')).toBe(0)
+    expect(defaultSortCompare({ a: 1.234 }, { a: 1.567 }, 'a')).toBe(-1)
+    expect(defaultSortCompare({ a: 1.561 }, { a: 1.234 }, 'a')).toBe(1)
   })
 
   it('sorts dates correctly', async () => {
-    const date1 = new Date(2019, 1, 1)
-    const date2 = new Date(1999, 11, 31)
-    const date3 = new Date(1999, 1, 1)
-    const date4 = new Date(1999, 1, 1, 12, 12, 12, 12)
+    const date1 = { a: new Date(2019, 1, 1) }
+    const date2 = { a: new Date(1999, 11, 31) }
+    const date3 = { a: new Date(1999, 1, 1) }
+    const date4 = { a: new Date(1999, 1, 1, 12, 12, 12, 12) }
 
-    expect(defaultSortCompare(date1, date2)).toBe(1)
-    expect(defaultSortCompare(date1, date1)).toBe(0)
-    expect(defaultSortCompare(date2, date1)).toBe(-1)
-    expect(defaultSortCompare(date2, date3)).toBe(1)
-    expect(defaultSortCompare(date3, date2)).toBe(-1)
-    expect(defaultSortCompare(date4, date4)).toBe(0)
-    expect(defaultSortCompare(date3, date4)).toBe(-1)
-    expect(defaultSortCompare(date4, date3)).toBe(-1)
+    expect(defaultSortCompare(date1, date2, 'a')).toBe(1)
+    expect(defaultSortCompare(date1, date1, 'a')).toBe(0)
+    expect(defaultSortCompare(date2, date1, 'a')).toBe(-1)
+    expect(defaultSortCompare(date2, date3, 'a')).toBe(1)
+    expect(defaultSortCompare(date3, date2, 'a')).toBe(-1)
+    expect(defaultSortCompare(date4, date4, 'a')).toBe(0)
+    expect(defaultSortCompare(date3, date4, 'a')).toBe(-1)
+    expect(defaultSortCompare(date4, date3, 'a')).toBe(-1)
   })
 
-  it('sorts string correctly', async () => {
+  it('sorts strings correctly', async () => {
     // Note: string comparisons are locale based
-    expect(defaultSortCompare('a', 'b')).toBe(-1)
-    expect(defaultSortCompare('b', 'a')).toBe(1)
-    expect(defaultSortCompare('a', 'a')).toBe(0)
-    expect(defaultSortCompare('a', 'aaa')).toBe(-1)
+    expect(defaultSortCompare({ a: 'a' }, { a: 'b' }, 'a')).toBe(-1)
+    expect(defaultSortCompare({ a: 'b' }, { a: 'a' }, 'a')).toBe(1)
+    expect(defaultSortCompare({ a: 'a' }, { a: 'a' }, 'a')).toBe(0)
+    expect(defaultSortCompare({ a: 'a' }, { a: 'aaa' }, 'a')).toBe(-1)
   })
 })
