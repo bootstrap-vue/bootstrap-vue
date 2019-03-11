7dc9aa2a5c0e2175753a424711094b5cf80054eb
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-11 13:19:56 -0300

    Update table.js

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -1,7 +1,6 @@
 // Utilities
 import looseEqual from '../../utils/loose-equal'
 import stableSort from '../../utils/stable-sort'
-import warn from '../../utils/warn'
 import { arrayIncludes, isArray } from '../../utils/array'
 
 // Table helper functions
@@ -12,7 +11,6 @@ import defaultSortCompare from './helpers/default-sort-compare'
 
 // Mixins
 import idMixin from '../../mixins/id'
-import listenOnRootMixin from '../../mixins/listen-on-root'
 import normalizeSlotMixin from '../../mixins/normalize-slot'
 
 // Table helper mixins
@@ -26,6 +24,7 @@ import busyMixin from './helpers/mixin-busy'
 import emptyMixin from './helpers/mixin-empty'
 import topRowMixin from './helpers/mixin-top-row'
 import bottomRowMixin from './helpers/mixin-bottom-row'
+import providerMixin from './helpers/mixin-provider'
 
 // b-table component definition
 // @vue/component
@@ -33,7 +32,6 @@ export default {
   name: 'BTable',
   mixins: [
     idMixin,
-    listenOnRootMixin,
     normalizeSlotMixin,
     captionMixin,
     theadMixin,
@@ -44,7 +42,8 @@ export default {
     colgroupMixin,
     emptyMixin,
     topRowMixin,
-    bottomRowMixin
+    bottomRowMixin,
+    providerMixin
   ],
   // Don't place ATTRS on root element automatically, as table could be wrapped in responsive div
   inheritAttrs: false,
@@ -158,23 +157,6 @@ export default {
       type: Boolean,
       default: false
     },
-    noProviderPaging: {
-      type: Boolean,
-      default: false
-    },
-    noProviderSorting: {
-      type: Boolean,
-      default: false
-    },
-    noProviderFiltering: {
-      type: Boolean,
-      default: false
-    },
-    apiUrl: {
-      // Passthrough prop. Passed to the context object. Not used by b-table directly
-      type: String,
-      default: ''
-    },
     noFooterSorting: {
       type: Boolean,
       default: false
@@ -236,9 +218,6 @@ export default {
       return [this.tbodyClass]
     },
     // Items related computed props
-    hasProvider() {
-      return this.items instanceof Function
-    },
     localFiltering() {
       return this.hasProvider ? !!this.noProviderFiltering : true
     },
@@ -258,28 +237,6 @@ export default {
         currentPage: this.currentPage,
         apiUrl: this.apiUrl
       }
-    },
-    providerTriggerContext() {
-      // Used to trigger the provider function via a watcher. Only the fields that
-      // are needed for triggering a provider update are included. Note that the
-      // regular this.context is sent to the provider during fetches though, as they
-      // may neeed all the prop info.
-      const ctx = {
-        apiUrl: this.apiUrl
-      }
-      if (!this.noProviderFiltering) {
-        // Either a string, or could be an object or array.
-        ctx.filter = this.localFilter
-      }
-      if (!this.noProviderSorting) {
-        ctx.sortBy = this.localSortBy
-        ctx.sortDesc = this.localSortDesc
-      }
-      if (!this.noProviderPaging) {
-        ctx.perPage = this.perPage
-        ctx.currentPage = this.currentPage
-      }
-      return ctx
     },
     computedBusy() {
       return this.busy || this.localBusy
@@ -498,31 +455,11 @@ export default {
       if (!looseEqual(newVal, oldVal)) {
         this.$emit('context-changed', newVal)
       }
-    },
-    // Provider update triggering
-    providerTriggerContext(newVal, oldVal) {
-      // Trigger the provider to update as the relevant context values have changed.
-      if (!looseEqual(newVal, oldVal)) {
-        this.$nextTick(this._providerUpdate)
-      }
     }
   },
   mounted() {
-    // Call the items provider if necessary
-    if (this.hasProvider && (!this.localItems || this.localItems.length === 0)) {
-      // Fetch on mount if localItems is empty
-      this._providerUpdate()
-    }
-
     // Initially update the v-model of displayed items
     this.$emit('input', this.computedItems)
-
-    // Listen for global messages to tell us to force refresh the table
-    this.listenOnRoot('bv::refresh::table', id => {
-      if (id === this.id || id === this) {
-        this.refresh()
-      }
-    })
   },
   methods: {
     // Filter Function factories
@@ -596,85 +533,6 @@ export default {
         return true
       }
       return false
-    },
-    // Exposed method(s)
-    refresh() {
-      this.$off('refreshed', this.refresh)
-      if (this.computedBusy) {
-        // Can't force an update when forced busy by user (busy prop === true)
-        if (this.localBusy && this.hasProvider) {
-          // But if provider running (localBusy), re-schedule refresh once `refreshed` emitted
-          this.$on('refreshed', this.refresh)
-        }
-      } else {
-        this.clearSelected()
-        if (this.hasProvider) {
-          this.$nextTick(this._providerUpdate)
-        } else {
-          /* istanbul ignore next */
-          this.localItems = isArray(this.items) ? this.items.slice() : []
-        }
-      }
-    },
-    // Provider related methods
-    _providerSetLocal(items) {
-      this.localItems = isArray(items) ? items.slice() : []
-      this.localBusy = false
-      this.$emit('refreshed')
-      // New root emit
-      if (this.id) {
-        this.emitOnRoot('bv::table::refreshed', this.id)
-      }
-    },
-    _providerUpdate() {
-      // Refresh the provider function items.
-      if (!this.hasProvider) {
-        // Do nothing if no provider
-        return
-      }
-      // If table is busy, wait until refereshed before calling again
-      if (this.computedBusy) {
-        // Schedule a new refresh once `refreshed` is emitted
-        this.$nextTick(this.refresh)
-        return
-      }
-
-      // Set internal busy state
-      this.localBusy = true
-
-      // Call provider function with context and optional callback after DOM is fully updated
-      this.$nextTick(function() {
-        try {
-          // Call provider function passing it the context and optional callback
-          const data = this.items(this.context, this._providerSetLocal)
-          if (data && data.then && typeof data.then === 'function') {
-            // Provider returned Promise
-            data.then(items => {
-              // Provider resolved with items
-              this._providerSetLocal(items)
-            })
-          } else if (isArray(data)) {
-            // Provider returned Array data
-            this._providerSetLocal(data)
-          } else if (this.items.length !== 2) {
-            // Check number of arguments provider function requested
-            // Provider not using callback (didn't request second argument), so we clear
-            // busy state as most likely there was an error in the provider function
-            /* istanbul ignore next */
-            warn(
-              "b-table provider function didn't request calback and did not return a promise or data"
-            )
-            /* istanbul ignore next */
-            this.localBusy = false
-          }
-        } catch (e) /* istanbul ignore next */ {
-          // Provider function borked on us, so we spew out a warning
-          // and clear the busy state
-          warn(`b-table provider function error [${e.name}] ${e.message}`)
-          this.localBusy = false
-          this.$off('refreshed', this.refresh)
-        }
-      })
     }
   },
   render(h) {
