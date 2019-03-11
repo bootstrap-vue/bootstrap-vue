91c83206da4908b106a592e431ed28db0d0dea97
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-11 19:32:00 -0300

    Update table-selectable.spec.js

--- src/components/table/table-selectable.spec.js
+++ src/components/table/table-selectable.spec.js
@@ -17,6 +17,23 @@ describe('table row select', () => {
     expect(wrapper.emitted('row-selected')).not.toBeDefined()
   })
 
+  it('should not have aria-selected/tabindex attribute when not selctable and no row-clicked listener', async () => {
+    const wrapper = mount(Table, {
+      propsData: {
+        fields: testFields,
+        items: testItems
+      }
+    })
+    expect(wrapper).toBeDefined()
+    await wrapper.vm.$nextTick()
+    const $rows = wrapper.findAll('tbody > tr')
+    expect($rows.length).toBe(4)
+    // Doesn't have aria-selected attribute on all TRs
+    expect($rows.wrapper.is('tr[aria-selected]')).toBe(false)
+    // Doesn't have tabindex attribute on all TRs
+    expect($rows.wrapper.is('tr[tabindex]')).toBe(false)
+  })
+
   it('select mode single works', async () => {
     const wrapper = mount(Table, {
       propsData: {
@@ -29,6 +46,9 @@ describe('table row select', () => {
     expect(wrapper).toBeDefined()
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected')).not.toBeDefined()
+    expect(wrapper.findAll('tbody > tr').length).toBe(4)
+    expect(wrapper.findAll('tbody > tr').is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
 
     // Click first row
     wrapper
@@ -39,6 +59,11 @@ describe('table row select', () => {
     expect(wrapper.emitted('row-selected')).toBeDefined()
     expect(wrapper.emitted('row-selected').length).toBe(1)
     expect(wrapper.emitted('row-selected')[0][0]).toEqual([testItems[0]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Click third row
     wrapper
@@ -48,6 +73,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(2)
     expect(wrapper.emitted('row-selected')[1][0]).toEqual([testItems[2]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Click third row again
     wrapper
@@ -57,6 +87,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(3)
     expect(wrapper.emitted('row-selected')[2][0]).toEqual([])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
   })
 
   it('select mode multi works', async () => {
@@ -81,6 +116,11 @@ describe('table row select', () => {
     expect(wrapper.emitted('row-selected')).toBeDefined()
     expect(wrapper.emitted('row-selected').length).toBe(1)
     expect(wrapper.emitted('row-selected')[0][0]).toEqual([testItems[0]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Click third row
     wrapper
@@ -90,6 +130,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(2)
     expect(wrapper.emitted('row-selected')[1][0]).toEqual([testItems[0], testItems[2]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Click third row again
     wrapper
@@ -99,6 +144,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(3)
     expect(wrapper.emitted('row-selected')[2][0]).toEqual([testItems[0]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Click first row again
     wrapper
@@ -108,6 +158,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(4)
     expect(wrapper.emitted('row-selected')[3][0]).toEqual([])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
   })
 
   it('select mode range works', async () => {
@@ -122,6 +177,8 @@ describe('table row select', () => {
     expect(wrapper).toBeDefined()
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected')).not.toBeDefined()
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').is('[aria-selected="false"]')).toBe(true)
 
     // Click first row
     wrapper
@@ -132,6 +189,11 @@ describe('table row select', () => {
     expect(wrapper.emitted('row-selected')).toBeDefined()
     expect(wrapper.emitted('row-selected').length).toBe(1)
     expect(wrapper.emitted('row-selected')[0][0]).toEqual([testItems[0]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Shift-Click third row
     wrapper
@@ -145,6 +207,11 @@ describe('table row select', () => {
       testItems[1],
       testItems[2]
     ])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Click third row again
     wrapper
@@ -154,6 +221,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(3)
     expect(wrapper.emitted('row-selected')[2][0]).toEqual([testItems[2]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Click fourth row
     wrapper
@@ -163,6 +235,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(4)
     expect(wrapper.emitted('row-selected')[3][0]).toEqual([testItems[3]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="true"]')).toBe(true)
 
     // Click fourth row again
     wrapper
@@ -172,6 +249,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     // No change to selected rows
     expect(wrapper.emitted('row-selected').length).toBe(4)
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="true"]')).toBe(true)
 
     // Ctrl-Click second row
     wrapper
@@ -181,6 +263,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(5)
     expect(wrapper.emitted('row-selected')[4][0]).toEqual([testItems[1], testItems[3]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="true"]')).toBe(true)
 
     // Ctrl-Click second row
     wrapper
@@ -190,6 +277,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(6)
     expect(wrapper.emitted('row-selected')[5][0]).toEqual([testItems[3]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="true"]')).toBe(true)
 
     // Ctrl-Click fourth row
     wrapper
@@ -199,6 +291,11 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(7)
     expect(wrapper.emitted('row-selected')[6][0]).toEqual([])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
   })
 
   it('sort change clears selection', async () => {
@@ -223,6 +320,11 @@ describe('table row select', () => {
     expect(wrapper.emitted('row-selected')).toBeDefined()
     expect(wrapper.emitted('row-selected').length).toBe(1)
     expect(wrapper.emitted('row-selected')[0][0]).toEqual([testItems[0]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Click row header
     wrapper
@@ -234,6 +336,11 @@ describe('table row select', () => {
     expect(wrapper.emitted('sort-changed').length).toBe(1)
     expect(wrapper.emitted('row-selected').length).toBe(2)
     expect(wrapper.emitted('row-selected')[1][0]).toEqual([])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
   })
 
   it('filter change clears selection', async () => {
@@ -248,6 +355,8 @@ describe('table row select', () => {
     expect(wrapper).toBeDefined()
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected')).not.toBeDefined()
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').is('[aria-selected="false"]')).toBe(true)
 
     // Click first row
     wrapper
@@ -258,6 +367,11 @@ describe('table row select', () => {
     expect(wrapper.emitted('row-selected')).toBeDefined()
     expect(wrapper.emitted('row-selected').length).toBe(1)
     expect(wrapper.emitted('row-selected')[0][0]).toEqual([testItems[0]])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(3).is('[aria-selected="false"]')).toBe(true)
 
     // Change filter
     wrapper.setProps({
@@ -266,6 +380,8 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(2)
     expect(wrapper.emitted('row-selected')[1][0]).toEqual([])
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').is('[aria-selected="false"]')).toBe(true)
   })
 
   it('pagination change clears selection', async () => {
@@ -282,6 +398,9 @@ describe('table row select', () => {
     expect(wrapper).toBeDefined()
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected')).not.toBeDefined()
+    expect(wrapper.findAll('tbody > tr').length).toBe(3)
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').is('[aria-selected="false"]')).toBe(true)
 
     // Click first row
     wrapper
@@ -292,6 +411,12 @@ describe('table row select', () => {
     expect(wrapper.emitted('row-selected')).toBeDefined()
     expect(wrapper.emitted('row-selected').length).toBe(1)
     expect(wrapper.emitted('row-selected')[0][0]).toEqual([testItems[0]])
+    expect(wrapper.findAll('tbody > tr').length).toBe(3)
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    // We only have 3 rows max per page
+    expect(wrapper.findAll('tbody > tr').at(0).is('[aria-selected="true"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(1).is('[aria-selected="false"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').at(2).is('[aria-selected="false"]')).toBe(true)
 
     // Change page
     wrapper.setProps({
@@ -300,5 +425,8 @@ describe('table row select', () => {
     await wrapper.vm.$nextTick()
     expect(wrapper.emitted('row-selected').length).toBe(2)
     expect(wrapper.emitted('row-selected')[1][0]).toEqual([])
+    expect(wrapper.findAll('tbody > tr').length).toBe(1)
+    expect(wrapper.findAll('tbody > tr').is('[tabindex="0"]')).toBe(true)
+    expect(wrapper.findAll('tbody > tr').is('[aria-selected="false"]')).toBe(true)
   })
 })
