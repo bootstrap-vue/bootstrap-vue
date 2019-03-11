f923c58c486fd9a96ac6b7b03846716c684c8374
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-10 21:02:46 -0300

    Update table-caption.spec.js

--- src/components/table/table-caption.spec.js
+++ src/components/table/table-caption.spec.js
@@ -1,5 +1,4 @@
 import Table from './table'
-import normalizeFields from './helpers/normalize-fields'
 import { mount } from '@vue/test-utils'
 
 const testItems = [{ a: 1, b: 2, c: 3 }, { a: 5, b: 5, c: 6 }, { a: 7, b: 8, c: 9 }]
