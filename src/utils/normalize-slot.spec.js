976ed11a34f8d101bed6d9d30d4820dc38ae4b68
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-12 13:18:06 -0300

    Update normalize-slot.spec.js

--- src/utils/normalize-slot.spec.js
+++ src/utils/normalize-slot.spec.js
@@ -28,8 +28,16 @@ describe('util/normalize-slot', () => {
     result = normalizeSlot('default', { a: ' foo' }, {}, $slots)
     expect(result).toBe('bar')
 
+    // Works if only named slot found and scopedSlots is undef
+    result = normalizeSlot('default', { a: ' foo' }, undefined, $slots)
+    expect(result).toBe('bar')
+
     // Works if only scoped slot found
     result = normalizeSlot('default', { a: ' bar' }, $scoped, {})
+    expect(result).toBe('foo bar')
+
+    // Works if only scoped slot found and scoped is undef
+    result = normalizeSlot('default', { a: ' bar' }, $scoped, undefined)
     expect(result).toBe('foo bar')
 
     // Returns undefined if slot name not found
