007278035cda8777d157dfdad65ba0d40358d1e1
Winnie Hellmann <31919422+gitlab-winnie@users.noreply.github.com>
2019-03-13 00:11:54 +0100

    feat: add BOOTSTRAP_VUE_NO_WARN environment variable to hide warnings

--- src/utils/warn.js
+++ src/utils/warn.js
@@ -4,6 +4,10 @@
  */
 /* istanbul ignore next */
 function warn(message) {
+  if (process && process.env && process.env.BOOTSTRAP_VUE_NO_WARN) {
+    return
+  }
+
   console.warn(`[BootstrapVue warn]: ${message}`)
 }
 
