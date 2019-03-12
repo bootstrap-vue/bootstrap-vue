6b1fa0395105f7cb7e962b062c3367377c7ea57b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-12 20:51:30 -0300

    Update html.js

--- src/utils/html.js
+++ src/utils/html.js
@@ -1,5 +1,9 @@
 const stripTagsRegex = /(<([^>]+)>)/gi
 
+// Removes any thing that looks like an HTML tag from the supplied string
 export const stripTags = (text = '') => String(text).replace(stripTagsRegex, '')
 
-export const htmlOrText = (innerHTML, textContent) => (innerHTML ? { innerHTML } : { textContent })
+// Generate a domProps object for either innerHTML, textContent or nothing
+export const htmlOrText = (innerHTML, textContent) => {
+  return innerHTML ? { innerHTML } : textContent ? { textContent } : {}
+}
