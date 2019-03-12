6a46a700d09792c62e87fcb54daf0b9057a5ec9b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-11 21:18:59 -0300

    Create table-tbody-transition.spec.js

---
+++ src/components/table/table-tbody-transition.spec.js
@@ -0,0 +1,59 @@
+import Table from './table'
+import normalizeFields from './helpers/normalize-fields'
+import { mount } from '@vue/test-utils'
+
+const testItems = [{ a: 1, b: 2, c: 3 }, { a: 5, b: 5, c: 6 }, { a: 7, b: 8, c: 9 }]
+const testFields = ['a', 'b', 'c']
+
+describe('table body transition', () => {
+  it('tbody should not be a transition element by default', async () => {
+    const wrapper = mount(Table, {
+      propsData: {
+        fields: testFields,
+        items: testItems
+      }
+    })
+    expect(wrapper).toBeDefined()
+    expect(wrapper.is('table')).toBe(true)
+    expect(wrapper.find('tbody').exists()).toBe(false)
+    expect(wrapper.find('tbody').is('tbody')).toBe(true)
+    expect(wrapper.find('tbody').is('transition')).toBe(false)
+  })
+
+  it('tbody should be a transition component when tbody-transition-props set', async () => {
+    const wrapper = mount(Table, {
+      propsData: {
+        fields: testFields,
+        items: testItems,
+        tbodyTransitionProps: {
+          name: 'fade'
+        }
+      }
+    })
+    expect(wrapper).toBeDefined()
+    expect(wrapper.is('table')).toBe(true)
+    expect(wrapper.find('tbody').exists()).toBe(false)
+    expect(wrapper.find('tbody').is('tbody')).toBe(true)
+    expect(wrapper.find('tbody').is('transition')).toBe(true)
+  })
+
+  it('tbody should be a transition component when tbody-transition-handlers set', async () => {
+    const wrapper = mount(Table, {
+      propsData: {
+        fields: testFields,
+        items: testItems,
+        tbodyTransitionHanlders: {
+          onBeforeEnter: el => {},
+          onAfterEnter: el => {},
+          onBeforeLeave: el => {},
+          onAfterLeave: el => {}
+        }
+      }
+    })
+    expect(wrapper).toBeDefined()
+    expect(wrapper.is('table')).toBe(true)
+    expect(wrapper.find('tbody').exists()).toBe(false)
+    expect(wrapper.find('tbody').is('tbody')).toBe(true)
+    expect(wrapper.find('tbody').is('transition')).toBe(true)
+  })
+})
