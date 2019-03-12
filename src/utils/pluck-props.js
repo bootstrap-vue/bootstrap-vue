e9171b9c47bbd58b93f822fa8bb2f90ce2e40b3d
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-12 15:08:43 -0300

    Update pluck-props.js

--- src/utils/pluck-props.js
+++ src/utils/pluck-props.js
@@ -4,7 +4,9 @@ import identity from './identity'
 
 /**
  * Given an array of properties or an object of property keys,
- * plucks all the values off the target object, returning copies
+ * plucks all the values off the target object, returning a new object
+ * that has props that reference the original prop values
+ *
  * @param {{}|string[]} keysToPluck
  * @param {{}} objToPluck
  * @param {Function} transformFn
