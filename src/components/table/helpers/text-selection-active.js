d0e34368b4ad40a69e332eba18d8e6e1eaf85229
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-12 00:36:37 -0300

    Update text-selection-active.js

--- src/components/table/helpers/text-selection-active.js
+++ src/components/table/helpers/text-selection-active.js
@@ -1,17 +1,11 @@
 // Helper to determine if a there is an active text selection on the document page.
 // Used to filter out click events caused by the mouse up at end of selection
-//
-// Expects an element reference to check to see if the selection contains part
-// of or is contained within the element.
 
-import { isElement } from '../../../utils/dom'
-
-export default function textSelectionActive(el) {
+export default function textSelectionActive() {
   const win = window
   /* istanbul ignore if: JSDOM doesn't support getSelection */
-  if (win && win.getSelection && win.getSelection().toString() !== '' && isElement(el)) {
-    const sel = win.getSelection()
-    return sel.containsNode ? sel.containsNode(el, true) : false
+  if (win && win.getSelection && win.getSelection().toString() !== '') {
+    return true
   } else {
     return false
   }
