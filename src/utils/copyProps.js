cef7e35a8b04a7dac459bfd00776c1778c94e6b1
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-12 15:53:40 -0300

    Update copyProps.js

--- src/utils/copyProps.js
+++ src/utils/copyProps.js
@@ -1,10 +1,11 @@
 import { isArray } from './array'
+import { isPlainObject } from './object'
 import identity from './identity'
 
 /**
  * Copies props from one array/object to a new aray/object. Prop values
  * are also cloned as new references to prevent possible mution of original
- * prop object values.
+ * prop object values. Optionally accepts a function to transform the prop name.
  *
  * @param {[]|{}} props
  * @param {Function} transformFn
@@ -19,12 +20,7 @@ export default function copyProps(props, transformFn = identity) {
   for (const prop in props) {
     /* istanbul ignore else */
     if (props.hasOwnProperty(prop)) {
-      if (typeof props[prop] === 'object') {
-        // If the prop value is an object, do a shallow clone
-        copied[transformFn(prop)] = { ...props[prop] }
-      } else {
-        copied[transformFn(prop)] = props[prop]
-      }
+      copied[transformFn(prop)] = isPlainObject(props[prop]) ? { ...props[prop] } : props[prop]
     }
   }
 
