d996196417a1746b38499b81ee7669f907d23252
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-12 13:46:05 -0300

    lint

--- src/utils/copyProps.spec.js
+++ src/utils/copyProps.spec.js
@@ -3,6 +3,7 @@ import copyProps from './copyProps'
 describe('utils/copyProps', () => {
   it('works with array props', async () => {
     const props = ['a', 'b', 'c']
+
     expect(copyProps(props)).toEqual(props)
     // Should be a new array reference
     expect(copyProps(props)).not.toBe(props)
@@ -13,11 +14,12 @@ describe('utils/copyProps', () => {
       a: { type: String, default: '' },
       b: { type: [Object, Array], default: null },
       c: 'c'
-    ]
+    }
+
     expect(copyProps(props)).toEqual(props)
     // Should be a new object reference
     expect(copyProps(props)).not.toBe(props)
-    // Prop values should be a new object reference
+    // Prop values should be a new object references
     expect(copyProps(props).a).not.toBe(props.a)
     expect(copyProps(props).b).not.toBe(props.b)
     // Except for primatives
