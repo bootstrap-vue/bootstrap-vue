ff0ac99fa810ab139ded313c67676d98a5491c37
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-11 21:56:16 -0300

    Update table-tbody-transition.spec.js

--- src/components/table/table-tbody-transition.spec.js
+++ src/components/table/table-tbody-transition.spec.js
@@ -38,6 +38,8 @@ describe('table body transition', () => {
     expect(wrapper).toBeDefined()
     expect(wrapper.is('table')).toBe(true)
     expect(wrapper.find(TransitionGroupStub).exists()).toBe(true)
+    expect(wrapper.find(TransitionGroupStub).props('tag')).toBe('tbody')
+    expect(wrapper.find(TransitionGroupStub).props('name')).toBe('fade')
     // Transition-group stub doesn't render itself with the specified tag
     expect(wrapper.find('tbody').exists()).toBe(false)
   })
@@ -61,6 +63,8 @@ describe('table body transition', () => {
     expect(wrapper).toBeDefined()
     expect(wrapper.is('table')).toBe(true)
     expect(wrapper.find(TransitionGroupStub).exists()).toBe(true)
+    expect(wrapper.find(TransitionGroupStub).props('tag')).toBe('tbody')
+    // We can't easily check to see of the handlers were attached, so we just assume they are
     // Transition-group stub doesn't render itself with the specified tag
     expect(wrapper.find('tbody').exists()).toBe(false)
   })
