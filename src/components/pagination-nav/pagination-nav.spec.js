66e971f8ccc85a0a9e53358507028b1024b36752
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-13 20:58:06 -0300

    Update pagination-nav.spec.js

--- src/components/pagination-nav/pagination-nav.spec.js
+++ src/components/pagination-nav/pagination-nav.spec.js
@@ -1,6 +1,37 @@
-import { loadFixture, testVM } from '../../../tests/utils'
+import PaginationNav from './pagination-nav'
+import { requestAF } from '../../utils/dom'
+import { mount } from '@vue/test-utils'
+
+// The majority of tests for the core of pagination mixin are performed
+// in pagination.spec.js.  Here we just test the differences that
+// pagination-nav has
 
 describe('pagination-nav', () => {
-  beforeEach(loadFixture(__dirname, 'pagination-nav'))
-  testVM()
+  it('renders with correct basic structure for root elements', async () => {
+    const wrapper = mount(PaginationNav, {
+      propsData: {
+        numberOfPages: 1,
+        value: 1
+      }
+    })
+    // pagination-nav has an outer wrapper of nav
+    expect(wrapper.is('nav')).toBe(true)
+    const $ul = wrapper.find('ul.pagination')
+    expect($ul.exists()).toBe(true)
+
+    // NAV Attributes
+    expect(wrapper.attributes('aria-hidden')).toBe('false')
+
+    // UL Classes
+    expect($ul.classes()).toContain('pagination')
+    expect($ul.classes()).toContain('b-pagination')
+    expect($ul.classes()).not.toContain('pagination-sm')
+    expect($ul.classes()).not.toContain('pagination-lg')
+    expect($ul.classes()).not.toContain('justify-content-center')
+    expect($ul.classes()).not.toContain('justify-content-end')
+    // UL Attributes
+    expect($ul.attributes('role')).toBe('menubar')
+    expect($ul.attributes('aria-disabled')).toBe('false')
+    expect($ul.attributes('aria-label')).toBe('Pagination')
+  })
 })
