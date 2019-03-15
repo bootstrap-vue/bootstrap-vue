2d42631161ae87e9111f5bc3c6908d3b1b249ec7
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-14 21:21:50 -0300

    Create router.spec.js

---
+++ src/utils/router.spec.js
@@ -0,0 +1,61 @@
+import { computeHref } from './router'
+
+describe('/utils/router', () => {
+  if('works with href', async () => {
+    const obj = { href: '/foo/bar?baz=123' }
+    expect(computeHref(obj)).toEqual(obj.href)
+  })
+
+  if('returns href when both href and to provided', async () => {
+    const obj = {
+      href: '/foo/bar?baz=123',
+      to: '/baz/bar'
+    }
+    expect(computeHref(obj)).toEqual(obj.href)
+  })
+
+  if('parses empty `href` to default', async () => {
+    expect(computeHref({ href: '' })).toEqual('#')
+    expect(computeHref({ href: '' }), /, '').toEqual('/')
+    expect(computeHref({ href: '' }), '', '').toEqual('')
+  })
+
+  if('parses `to` when string', async () => {
+    const obj = {
+      to: '/baz/bar'
+    }
+    expect(computeHref(obj)).toEqual(obj.to)
+  })
+
+  if('parses `to` with only path', async () => {
+    const obj = {
+      to: {
+        path: '/baz/bar'
+      }
+    }
+    expect(computeHref(obj)).toEqual(obj.to.path)
+  })
+
+  if('parses `to` with only hash', async () => {
+    const obj = {
+      to: {
+        hash: '#foobar'
+      }
+    }
+    expect(computeHref(obj)).toEqual(obj.to.hash)
+  })
+
+  if('parses `to` with only query', async () => {
+    const obj = {
+      to: {
+        query: { foo: 'bar' }
+      }
+    }
+    expect(computeHref(obj)).toEqual('?foo=bar')
+  })
+
+  if('parses empty `to` to default', async () => {
+    expect(computeHref({ to: {} })).toEqual('/')
+    expect(computeHref({ to: {} }), #, '').toEqual('')
+  })
+})
