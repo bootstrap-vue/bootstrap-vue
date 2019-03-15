71dba69aa492018cb62ba14439b5ff6a6dcc0143
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-14 22:34:34 -0300

    Update router.spec.js

--- src/utils/router.spec.js
+++ src/utils/router.spec.js
@@ -1,8 +1,54 @@
 import { computeHref } from './router'
 
 describe('utils/router', () => {
-  // computeHref utility
-  describe('computeHref', () => {
+  // stringifyQueryObject utility method
+  describe('stringifyQueryObj()', async () => {
+    it('returns empty string when noting passed', async () => {
+      expect(stringifyQueryObj()).toEqual('')
+    })
+
+    it('handles special chars', async () => {
+      expect(stringifyQueryObj({ foo: ", !'()*" })).toEqual('foo=,%20%21%27%28%29%2a')
+      expect(stringifyQueryObj({ ", !'()*": 'foo' })).toEqual(',%20%21%27%28%29%2a=foo')
+    })
+
+    it('handles multiple keys', async () => {
+      const obj = {
+        foo: 1,
+        bar: 'baz'
+      }
+      expect(stringifyQueryObj(obj)).toEqual('foo=1&bar=baz')
+    })
+
+    it('handles array as values', async () => {
+      const obj = {
+        foo: 1,
+        bar: ['a', 'b', 'c']
+      }
+      expect(stringifyQueryObj(obj)).toEqual('foo=1&bar=a&bar=b&bar=c')
+    })
+
+    it('skips undefined vals', async () => {
+      const obj = {
+        foo: 1,
+        bar: undefined,
+        baz: 2
+      }
+      expect(stringifyQueryObj(obj)).toEqual('foo=1&baz=2')
+    })
+
+    it('leaves in null val keys', async () => {
+      const obj = {
+        foo: 1,
+        bar: null,
+        baz: 2
+      }
+      expect(stringifyQueryObj(obj)).toEqual('foo=1&bar&baz=2')
+    })
+  })
+
+  // computeHref utility metod
+  describe('computeHref()', () => {
     it('works with href', async () => {
       const obj = { href: '/foo/bar?baz=123' }
       expect(computeHref(obj)).toEqual(obj.href)
