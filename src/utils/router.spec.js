6d18f31e185b4f6e1ab3ab7dfb6db17f886db93a
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-14 21:47:05 -0300

    Update router.spec.js

--- src/utils/router.spec.js
+++ src/utils/router.spec.js
@@ -1,12 +1,12 @@
 import { computeHref } from './router'
 
 describe('/utils/router', () => {
-  if('works with href', async () => {
+  it('works with href', async () => {
     const obj = { href: '/foo/bar?baz=123' }
     expect(computeHref(obj)).toEqual(obj.href)
   })
 
-  if('returns href when both href and to provided', async () => {
+  it('returns href when both href and to provided', async () => {
     const obj = {
       href: '/foo/bar?baz=123',
       to: '/baz/bar'
@@ -14,20 +14,20 @@ describe('/utils/router', () => {
     expect(computeHref(obj)).toEqual(obj.href)
   })
 
-  if('parses empty `href` to default', async () => {
+  it('parses empty `href` to default', async () => {
     expect(computeHref({ href: '' })).toEqual('#')
     expect(computeHref({ href: '' }), '/', '').toEqual('/')
     expect(computeHref({ href: '' }), '', '').toEqual('')
   })
 
-  if('parses `to` when string', async () => {
+  it('parses `to` when string', async () => {
     const obj = {
       to: '/baz/bar'
     }
     expect(computeHref(obj)).toEqual(obj.to)
   })
 
-  if('parses `to` with only path', async () => {
+  it('parses `to` with only path', async () => {
     const obj = {
       to: {
         path: '/baz/bar'
@@ -36,7 +36,7 @@ describe('/utils/router', () => {
     expect(computeHref(obj)).toEqual(obj.to.path)
   })
 
-  if('parses `to` with only hash', async () => {
+  it('parses `to` with only hash', async () => {
     const obj = {
       to: {
         hash: '#foobar'
@@ -45,7 +45,7 @@ describe('/utils/router', () => {
     expect(computeHref(obj)).toEqual(obj.to.hash)
   })
 
-  if('parses `to` with only query', async () => {
+  it('parses `to` with only query', async () => {
     const obj = {
       to: {
         query: { foo: 'bar' }
@@ -54,7 +54,7 @@ describe('/utils/router', () => {
     expect(computeHref(obj)).toEqual('?foo=bar')
   })
 
-  if('parses empty `to` to default', async () => {
+  it('parses empty `to` to default', async () => {
     expect(computeHref({ to: {} })).toEqual('/')
     expect(computeHref({ to: {} }), '#', '').toEqual('')
   })
