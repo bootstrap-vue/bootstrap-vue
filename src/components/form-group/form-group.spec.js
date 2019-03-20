106f7a92443096f4fe10e96f4709e2ebf3e4960e
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-19 21:36:52 -0300

    Update form-group.spec.js

--- src/components/form-group/form-group.spec.js
+++ src/components/form-group/form-group.spec.js
@@ -1,6 +1,24 @@
-import { loadFixture, testVM } from '../../../tests/utils'
+import { loadFixture, testVM, setData, nextTick } from '../../../tests/utils'
 
 describe('form-group', () => {
   beforeEach(loadFixture(__dirname, 'form-group'))
   testVM()
+
+  it('app changes validation state when text supplied', async () => {
+    const { app } = window
+    const $group = app.$refs.group1
+
+    expect($group.$el.getAttribute('aria-invalid')).toBe('true')
+
+    const oldADB = $group.$el.getAttribute('aria-describedby')
+
+    await setData(app, 'text', 'foobar doodle')
+    await nextTick()
+
+    expect($group.$el.getAttribute('aria-invalid')).toBe(null)
+
+    const newADB = $group.$el.getAttribute('aria-describedby')
+
+    expect(oldADB).not.toBe(newADB)
+  })
 })
