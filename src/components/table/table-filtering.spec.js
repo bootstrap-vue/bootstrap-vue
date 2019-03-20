6d14f1bb870b8b4d8530e57e568edce17b0adb74
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-20 18:11:10 -0300

    Update table-filtering.spec.js

--- src/components/table/table-filtering.spec.js
+++ src/components/table/table-filtering.spec.js
@@ -60,4 +60,51 @@ describe('table > filtering', () => {
 
     wrapper.destroy()
   })
+
+  it('should emit filtered event when filter string is changed', async () => {
+    const wrapper = mount(Table, {
+      propsData: {
+        fields: testFields,
+        items: testItems,
+        filter: ''
+      }
+    })
+    expect(wrapper).toBeDefined()
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.findAll('tbody > tr').exists()).toBe(true)
+    expect(wrapper.findAll('tbody > tr').length).toBe(3)
+    expect(wrapper.emitted('filtered')).not.toBeDefined()
+
+    wrapper.setProps({
+      filter: 'z'
+    })
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.findAll('tbody > tr').exists()).toBe(true)
+    expect(wrapper.findAll('tbody > tr').length).toBe(1)
+
+    expect(wrapper.emitted('filtered')).toBeDefined()
+    expect(wrapper.emitted('filtered').length).toBe(1)
+    // Copy of items matching filter
+    expect(wrapper.emitted('filtered')[0][0]).toEqual([testItems[2]])
+    // Number of rows matching filter
+    expect(wrapper.emitted('filtered')[0][1]).toEqual(1)
+
+    wrapper.setProps({
+      filter: ''
+    })
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.findAll('tbody > tr').exists()).toBe(true)
+    expect(wrapper.findAll('tbody > tr').length).toBe(3)
+
+    expect(wrapper.emitted('filtered').length).toBe(2)
+    // Copy of items matching filter
+    expect(wrapper.emitted('filtered')[0][0]).toEqual(testItems)
+    // Number of rows matching filter
+    expect(wrapper.emitted('filtered')[0][1]).toEqual(3)
+
+    wrapper.destroy()
+  })
 })
