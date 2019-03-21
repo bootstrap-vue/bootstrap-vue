50ec49c035e183017db48c601c9a30f910bc2179
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-21 11:30:27 -0300

    Update table-primarykey.spec.js

--- src/components/table/table-primarykey.spec.js
+++ src/components/table/table-primarykey.spec.js
@@ -3,7 +3,7 @@ import { mount } from '@vue/test-utils'
 
 const testItems = [{ a: 1, b: 2, c: 3 }, { a: 5, b: 5, c: 6 }, { a: 7, b: 8, c: 9 }]
 
-describe('b-table primary key', () => {
+describe('table > primary key', () => {
   it('default should not have ids on table rows', async () => {
     const wrapper = mount(Table, {
       propsData: {
