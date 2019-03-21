e60afce6749c6c90ed061fe8932f74b28b65ac6b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-21 11:28:47 -0300

    Update table-busy.spec.js

--- src/components/table/table-busy.spec.js
+++ src/components/table/table-busy.spec.js
@@ -3,7 +3,7 @@ import { mount } from '@vue/test-utils'
 
 const testItems = [{ a: 1, b: 2, c: 3 }, { a: 5, b: 5, c: 6 }, { a: 7, b: 8, c: 9 }]
 
-describe('b-table > busy state', () => {
+describe('table > busy state', () => {
   it('default should have attribute aria-busy=false', async () => {
     const wrapper = mount(Table, {
       propsData: {
