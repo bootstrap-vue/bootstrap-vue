cc1a6a5ac76da8795171e23b81fb405d97e82aa1
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-23 20:11:07 -0300

    Update alert.js

--- src/components/alert/alert.js
+++ src/components/alert/alert.js
@@ -14,7 +14,7 @@ export default {
   props: {
     variant: {
       type: String,
-      default: getConfigComponent(NAME, 'variant')
+      default: () => getConfigComponent(NAME, 'variant')
     },
     dismissible: {
       type: Boolean,
