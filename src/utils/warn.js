a663dbdf4fb0e359253028c225d1c5e4dd371556
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-22 22:08:20 -0300

    Update warn.js

--- src/utils/warn.js
+++ src/utils/warn.js
@@ -1,13 +1,14 @@
+import { noWarn } from './env'
+
 /**
  * Log a warning message to the console with bootstrap-vue formatting sugar.
  * @param {string} message
  */
 /* istanbul ignore next */
 const warn = message => {
-  if (process && process.env && process.env.BOOTSTRAP_VUE_NO_WARN) {
-    return
+  if (!noWarn) {
+    console.warn(`[BootstrapVue warn]: ${message}`)
   }
-  console.warn(`[BootstrapVue warn]: ${message}`)
 }
 
 export default warn
