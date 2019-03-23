ae21764a1dcabc48c53154882c8d029378ec0fe2
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-23 16:37:07 -0300

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -11,19 +11,29 @@
 import get from './get'
 
 const BV_DEFAULTS = {
-  breakpoints: ['xs', 'sm', 'md', 'lg', 'xl']
+  breakpoints: ['xs', 'sm', 'md', 'lg', 'xl'],
+  // Component Specific defaults are keyed by the component name
+  BAlert: { variant: 'info' },
+  BBadge: { variant: 'secondary' },
+  BButton: { variant: 'secondary' },
+  BButtonCose: { textVariant: null },
+  BDropdown: { variant: null }, // defaults to button variant
+  BImg: { blankColor: 'transparent' }
 }
 
 // This will be the object that any future user defined
-// configuration will be in
+// configuration will be placed in the future
 const config = {}
 
-// Method to grab a config value based on a dotted/array notation key
+// Method to get a deep clone (immutable) copy of the defaults
+const getDefaults = () => JSON.parse(JSON.stringify(BV_DEFAULTS))
+
+// Method to grab a config value based on a dotted/array notation key.
+// Returns a deep clone (immutable) copy
 const getConfigParam = key => {
   // First we try the user config, and if key not found we
   // fall back to default value.
-  // Returns a deep cloned version
-  return JSON.parse(JSON.stringify(get(config, key, get(BV_DEFAULTS, key))))
+  return JSON.parse(JSON.stringify(get(config, key, get(getDefaults(), key))))
 }
 
 // Convenience method for getting all breakpoint names
@@ -51,4 +61,10 @@ const getBreakpointsDown = () => {
 }
 
 // Named Exports
-export { getConfigParam, getBreakpointsAll, getBreakpointsUp, getBreakpointsDown }
+export {
+  getDefaults, 
+  getConfigParam,
+  getBreakpointsAll,
+  getBreakpointsUp,
+  getBreakpointsDown
+}
