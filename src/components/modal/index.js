bc066472dcab8d4e6a1eb3e9d9e6476fca6dd861
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-24 18:57:12 -0300

    Update index.js

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,15 +1,15 @@
 import BModal from './modal'
-import BModalDirective from '../../directives/modal/modal'
+import BModalDirectivePlugin from '../../directives/modal'
 import { installFactory } from '../../utils/plugins'
 
 const components = {
   BModal
 }
 
-const directives = {
-  BModal: BModalDirective
+const plugins = {
+  BModalDirectivePlugin
 }
 
 export default {
-  install: installFactory({ components, directives })
+  install: installFactory({ components, plugins })
 }
