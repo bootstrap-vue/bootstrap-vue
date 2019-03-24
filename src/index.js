2e8ff5bc937955913ec6e7c9d105c612e9befd8d
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-24 00:57:14 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import * as componentPlugins from './components'
 import * as directivePlugins from './directives'
 import { vueUse } from './utils/plugins'
 
-const VuePlugin = {
-  install: function(Vue) {
-    // Register component plugins
-    for (let plugin in componentPlugins) {
-      Vue.use(componentPlugins[plugin])
-    }
+const VuePlugin = Vue => {
+  // Register component plugins
+  for (let plugin in componentPlugins) {
+    Vue.use(componentPlugins[plugin])
+  }
 
-    // Register directive plugins
-    for (let plugin in directivePlugins) {
-      Vue.use(directivePlugins[plugin])
-    }
+  // Register directive plugins
+  for (let plugin in directivePlugins) {
+    Vue.use(directivePlugins[plugin])
   }
 }
 
