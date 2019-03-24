7db0b49ff4f14fd68455a8b14526a4f4807a89c4
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-24 19:23:25 -0300

    Update index.js

--- docs/pages/docs/index.js
+++ docs/pages/docs/index.js
@@ -1,11 +1,11 @@
-import { bootstrapVersion, vueVersion } from '~/content'
+import { bootstrapVersion, vueVersion, nuxtVersion } from '~/content'
 import docsMixin from '~/plugins/docs-mixin'
 import readme from '~/markdown/intro/README.md'
 
 export default {
   mixins: [docsMixin],
   data() {
-    return { bootstrapVersion, vueVersion }
+    return { bootstrapVersion, vueVersion, nuxtVersion }
   },
   template: `<div class="container bd-content">${readme}</div>`,
   layout: 'docs'
