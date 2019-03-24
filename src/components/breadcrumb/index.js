fcc8e311b5604263669604d1217e9af6776fdd52
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-24 17:59:57 -0300

    Update index.js

--- src/components/breadcrumb/index.js
+++ src/components/breadcrumb/index.js
@@ -1,7 +1,7 @@
 import BBreadcrumb from './breadcrumb'
 import BBreadcrumbItem from './breadcrumb-item'
 import BBreadcrumbLink from './breadcrumb-link'
-import { registerComponents } from '../../utils/plugins'
+import { installFactory } from '../../utils/plugins'
 
 const components = {
   BBreadcrumb,
@@ -10,7 +10,5 @@ const components = {
 }
 
 export default {
-  install(Vue) {
-    registerComponents(Vue, components)
-  }
+  install: installFactory({ components })
 }
