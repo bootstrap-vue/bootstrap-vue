12ce242a60770c8913229d5beca41ce11139dc1c
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-24 17:58:21 -0300

    Update index.js

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -1,12 +1,10 @@
 import BAlert from './alert'
-import { registerComponents } from '../../utils/plugins'
+import { installFactory } from '../../utils/plugins'
 
 const components = {
   BAlert
 }
 
 export default {
-  install(Vue) {
-    registerComponents(Vue, components)
-  }
+  install: installFactory({ components })
 }
