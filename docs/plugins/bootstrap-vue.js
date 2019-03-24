1a0354016583ff51144cbe1705055ea7e82e1c94
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-24 02:09:03 -0300

    Update bootstrap-vue.js

--- docs/plugins/bootstrap-vue.js
+++ docs/plugins/bootstrap-vue.js
@@ -1,9 +1,4 @@
 import Vue from 'vue'
 import BootstrapVue from '../../src'
 
-if (window && !window.Vue) {
-  // We add Vue to window to help with debugging
-  window.Vue = Vue
-}
-
 Vue.use(BootstrapVue)
