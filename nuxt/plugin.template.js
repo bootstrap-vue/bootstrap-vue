423b43f6651941a0ecf5522771d4fe74dc7fb3cb
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-25 01:25:58 -0300

    Update plugin.template.js

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
+<% if (options.config) { %>
+import { setConfig } from 'bootstrap-vue/es/utils/config'
+
+setConfig(<%= JSON.stringify(options.config, undefined, 2) %>)
+<% } %>
+
 <% if (options.componentPlugins.length || options.directivePlugins.length) { %><%=
 options.componentPlugins.reduce((acc, p) => (acc += `import ${p[0]} from 'bootstrap-vue/es/components/${p[1]}'\n` ), '') %><%=
 options.directivePlugins.reduce((acc, p) => (acc += `import ${p[0]} from 'bootstrap-vue/es/directives/${p[1]}'\n` ), '') %>
