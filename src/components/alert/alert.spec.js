62afecf8dc95d3db2ae7d55e24077e241211ab47
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-26 15:26:03 -0300

    Update alert.spec.js

--- src/components/alert/alert.spec.js
+++ src/components/alert/alert.spec.js
@@ -178,7 +178,7 @@ describe('alert', () => {
     })
     expect(wrapper.isVueInstance()).toBe(true)
     expect(wrapper.html()).not.toBeDefined()
-    
+
     wrapper.setProps({
       show: true
     })
