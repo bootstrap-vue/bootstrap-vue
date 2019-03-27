43eda8cbe91ddf63040f01c96787db04822247ca
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-27 20:41:08 -0300

    Update button-group.spec.js

--- src/components/button-group/button-group.spec.js
+++ src/components/button-group/button-group.spec.js
@@ -9,6 +9,22 @@ describe('button-group', () => {
     expect(wrapper.classes().length).toBe(1)
     expect(wrapper.attributes('role')).toBeDefined()
     expect(wrapper.attributes('role')).toBe('group')
+    expect(wrapper.text()).toBe('')
+  })
+
+  it('should render default slot', async () => {
+    const wrapper = mount(ButtonGroup, {
+      slots: {
+        default: '<span>foobar</span>'
+      }
+    })
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('btn-group')
+    expect(wrapper.classes().length).toBe(1)
+    expect(wrapper.attributes('role')).toBeDefined()
+    expect(wrapper.attributes('role')).toBe('group')
+    expect(wrapper.find('span').exists()).toBe(true)
+    expect(wrapper.text()).toBe('foobar')
   })
 
   it('should apply vertical class', async () => {
