ca5a78347a6893669d3bcc5e529e56391f40d932
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-27 10:43:23 -0300

    fix(collapse/toggle): persist toggle state on element and prevent multiple state emits

--- src/directives/toggle/toggle.js
+++ src/directives/toggle/toggle.js
@@ -1,20 +1,36 @@
 import target from '../../utils/target'
 import { setAttr, addClass, removeClass } from '../../utils/dom'
-
-// Are we client side?
-const inBrowser = typeof window !== 'undefined'
+import { inBrowser } from '../../utils/env'
 
 // target listen types
 const listenTypes = { click: true }
 
 // Property key for handler storage
 const BVT = '__BV_toggle__'
+const BVTS = '__BV_toggle_STATE__'
+const BVTC = '__BV_toggle_CONTROLS__'
 
 // Emitted Control Event for collapse (emitted to collapse)
 const EVENT_TOGGLE = 'bv::toggle::collapse'
 
 // Listen to Event for toggle state update (Emited by collapse)
 const EVENT_STATE = 'bv::collapse::state'
+
+const handleUpdate(el, binding, vnode) /* istanbul ignore next */ {
+  // Ensure the collapse class and aria-* attributes persist
+  // after element is updated (eitehr by parent re-rendering
+  // or changes to this element or it's contents.
+  if (inBrowser) {
+    if (el[BVT_STATE] === true) {
+      addClass(el, 'collapsed')
+      setAttr(el, 'aria-expanded', 'true')
+    } else if (el[BVT_STATE] === false) {
+      removeClass(el, 'collapsed')
+      setAttr(el, 'aria-expanded', 'false')
+    }
+    setAttr(el, 'aria-controls', el[BVT_CONTROLS])
+  }
+}
 
 export default {
   bind(el, binding, vnode) {
@@ -26,7 +42,10 @@ export default {
 
     if (inBrowser && vnode.context && targets.length > 0) {
       // Add aria attributes to element
-      setAttr(el, 'aria-controls', targets.join(' '))
+      el[BVT_CONTROLS] = targets.join(' ')
+      // state is initialy collapsed until we receive a state event
+      el[BVT_STATE] = false
+      setAttr(el, 'aria-controls', el[BVT_CONTROLS])
       setAttr(el, 'aria-expanded', 'false')
       if (el.tagName !== 'BUTTON') {
         // If element is not a button, we add `role="button"` for accessibility
@@ -39,6 +58,7 @@ export default {
           // Set aria-expanded state
           setAttr(el, 'aria-expanded', state ? 'true' : 'false')
           // Set/Clear 'collapsed' class state
+          el[BVT_STATE] = state
           if (state) {
             removeClass(el, 'collapsed')
           } else {
@@ -51,11 +71,15 @@ export default {
       vnode.context.$root.$on(EVENT_STATE, el[BVT])
     }
   },
+  componentUpdated: handleUpdate,
+  updated: handleUpdate,
   unbind(el, binding, vnode) /* istanbul ignore next */ {
     if (el[BVT]) {
       // Remove our $root listener
       vnode.context.$root.$off(EVENT_STATE, el[BVT])
       el[BVT] = null
+      el[BVT_STATE] = null
+      el[BVT_CONTROLS] = null
     }
   }
 }
