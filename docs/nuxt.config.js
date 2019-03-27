bb0fb34fcb4b17c1334ffd2d6c156a550a8b91a3
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-27 14:37:35 -0300

    Update nuxt.config.js

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -117,7 +117,7 @@ module.exports = {
           .readdirSync(`${root}/${dir}`)
           .filter(c => c !== 'index.js' && c[0] !== '_')
           .filter(c => excludeDirs.indexOf(c) === -1)
-          .filter(c => !/\.scss/.test(c))
+          .filter(c => !/\.s?css$/.test(c))
           .map(page => `/docs/${dir}/${page}`)
 
       return []
