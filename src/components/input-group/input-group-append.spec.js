180145f195799608e4ec2b4697d814bc26fcdd31
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-31 04:20:16 -0300

    Create input-group-append.spec.js

---
+++ src/components/input-group/input-group-append.spec.js
@@ -0,0 +1,74 @@
+import InputGroupAppend from './input-group-append'
+import { mount } from '@vue/test-utils'
+
+describe('input-group-append', () => {
+  it('has expected default structure', async () => {
+    const wrapper = mount(InputGroupAppend)
+
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('input-group-append')
+    expect(wrapper.classes().length).toBe(1)
+    expect(wrapper.findAll('.input-group-append > *').length).toBe(0)
+    expect(wrapper.text()).toEqual('')
+  })
+
+  it('renders custom root element when tag prop is set', async () => {
+    const wrapper = mount(InputGroupAppend, {
+      propsData: {
+        tag: 'span'
+      }
+    })
+
+    expect(wrapper.is('span')).toBe(true)
+    expect(wrapper.classes()).toContain('input-group-append')
+    expect(wrapper.classes().length).toBe(1)
+    expect(wrapper.findAll('.input-group-append > *').length).toBe(0)
+    expect(wrapper.text()).toEqual('')
+  })
+
+  it('renders content of default slot', async () => {
+    const wrapper = mount(InputGroupAppend, {
+      slots: {
+        default: 'foobar'
+      }
+    })
+
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('input-group-append')
+    expect(wrapper.classes().length).toBe(1)
+    expect(wrapper.text()).toEqual('foobar')
+  })
+
+  it('renders child input-group-text when prop is-text set', async () => {
+    const wrapper = mount(InputGroupAppend, {
+      propsData: {
+        isText: true
+      }
+    })
+
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('input-group-append')
+    expect(wrapper.classes().length).toBe(1)
+    expect(wrapper.findAll('.input-group-text').length).toBe(1)
+    expect(wrapper.findAll('.input-group-append > .input-group-text').length).toBe(1)
+    expect(wrapper.text()).toEqual('')
+  })
+
+  it('renders default slot inside child input-group-text when prop is-text set', async () => {
+    const wrapper = mount(InputGroupAppend, {
+      propsData: {
+        isText: true
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('input-group-append')
+    expect(wrapper.classes().length).toBe(1)
+    expect(wrapper.findAll('.input-group-text').length).toBe(1)
+    expect(wrapper.text()).toEqual('foobar')
+    expect(wrapper.find('.input-group-text').text()).toEqual('foobar')
+  })
+})
