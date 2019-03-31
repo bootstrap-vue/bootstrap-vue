31978e2324b2278c0450b3ae0c71472b58fe65ee
Troy Morehouse <troymore@nbnet.nb.ca>
2019-03-31 01:09:59 -0300

    Create nav-item-dropdown.spec.js

---
+++ src/components/nav/nav-item-dropdown.spec.js
@@ -0,0 +1,22 @@
+import NavItemDropdown from './nav-item-dropdown'
+import { mount } from '@vue/test-utils'
+
+describe('nav-item-dropdown', () => {
+  it('should have custom toggle class in nav-item-dropdown', async () => {
+    const wrapper = mount(NavItemDropdown, {
+      propsData: {
+        text: 'toggle',
+        extraToggleClasses: 'nav-link-custom'
+      }
+    })
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.findAll('.dropdown-toggle').length).toBe(1)
+    const $toggle = wrapper.find('.dropdown-toggle')
+    
+    expect($toggle.classes()).toBeContain('nav-link')
+    expect($toggle.classes()).toBeContain('dropdown-toggle')
+    expect($toggle.classes()).toBeContain('nav-link-custom')
+
+    wrapper.destroy()
+  })
+})
