d633749ef4a646fc983b495222fd29699f5e23f4
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-01 11:59:41 -0300

    Create dropdown.spec.js

---
+++ src/components/dropdown/dropdown.spec.js
@@ -0,0 +1,49 @@
+import Dropdown from './dropdown'
+import { mount } from '@vue/test-utils'
+
+describe('dropdown', () => {
+  it('has expected default structure', async () => {
+    const wrapper = mount(Dropdown, {
+      attachToDocument: true
+    })
+
+    expect(wrapper.is('div')).toBe(true)
+
+    // Wait for auto ID to be generated
+    await wrapper.$nextTick()
+
+    expect(wrapper.classes()).toContain('dropdown')
+    expect(wrapper.classes()).toContain('btn-group')
+    expect(wrapper.classes()).toContain('b-dropdown')
+    expect(wrapper.classes().length).toBe(3)
+    expect(wrapper.attributes('id')).toBeDefined()
+    const wrapperId = wrapper.attributes('id')
+
+    expect(wrapper.findAll('button').length).toBe(1)
+    const $button = wrapper.find('button')
+    expect($button.classes()).toContain('btn')
+    expect($button.classes()).toContain('btn-secondary')
+    expect($button.classes()).toContain('dropdown-toggle')
+    expect($button.classes().length).toBe(3)
+    expect($button.attributes('aria-haspopup')).toBeDefined()
+    expect($button.attributes('aria-haspopup')).toEqal('true')
+    expect($button.attributes('aria-expanded')).toBeDefined()
+    expect($button.attributes('aria-expanded')).toEqal('false')
+    expect($button.attributes('id')).toBeDefined()
+    expect($button.attributes('id')).toEqual(`${wrapperId}_BV_toggle_`)
+    expect($button.text()).toEqual('')
+
+    expect(wrapper.findAll('.dropdown-menu').length).toBe(1)
+    const $menu = wrapper.find('.dropdown-menu')
+    expect($menu.classes().length).toBe(1)
+    expect($menu.attributes('role')).toBeDefined()
+    expect($menu.attributes('role')).toEqual('menu')
+    expect($menu.attributes('tabindex')).toBeDefined()
+    expect($menu.attributes('tabindex')).toEqual('-1')
+    expect($menu.attributes('aria-labelledby')).toBeDefined()
+    expect($menu.attributes('aria-labelledby')).toEqual(`${wrapperId}_BV_toggle_`)
+    expect($menu.text()).toEqual('')
+
+    wrapper.destroy()
+  })
+})
