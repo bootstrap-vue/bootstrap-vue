45550c8b633a6cb22548cb4e0dcfec62224c48db
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-01 12:07:49 -0300

    Update dropdown.spec.js

--- src/components/dropdown/dropdown.spec.js
+++ src/components/dropdown/dropdown.spec.js
@@ -27,12 +27,70 @@ describe('dropdown', () => {
     expect($button.classes()).toContain('dropdown-toggle')
     expect($button.classes().length).toBe(3)
     expect($button.attributes('aria-haspopup')).toBeDefined()
-    expect($button.attributes('aria-haspopup')).toEqal('true')
+    expect($button.attributes('aria-haspopup')).toEqual('true')
     expect($button.attributes('aria-expanded')).toBeDefined()
-    expect($button.attributes('aria-expanded')).toEqal('false')
+    expect($button.attributes('aria-expanded')).toEqual('false')
     expect($button.attributes('id')).toBeDefined()
     expect($button.attributes('id')).toEqual(`${wrapperId}_BV_toggle_`)
     expect($button.text()).toEqual('')
+
+    expect(wrapper.findAll('.dropdown-menu').length).toBe(1)
+    const $menu = wrapper.find('.dropdown-menu')
+    expect($menu.classes().length).toBe(1)
+    expect($menu.attributes('role')).toBeDefined()
+    expect($menu.attributes('role')).toEqual('menu')
+    expect($menu.attributes('tabindex')).toBeDefined()
+    expect($menu.attributes('tabindex')).toEqual('-1')
+    expect($menu.attributes('aria-labelledby')).toBeDefined()
+    expect($menu.attributes('aria-labelledby')).toEqual(`${wrapperId}_BV_toggle_`)
+    expect($menu.text()).toEqual('')
+
+    wrapper.destroy()
+  })
+
+  it('split mode has expected default structure', async () => {
+    const wrapper = mount(Dropdown, {
+      attachToDocument: true,
+      propsData: {
+        split: true
+      }
+    })
+
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.isVueInstance()).toBe(true)
+
+    // Wait for auto ID to be generated
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.classes()).toContain('dropdown')
+    expect(wrapper.classes()).toContain('btn-group')
+    expect(wrapper.classes()).toContain('b-dropdown')
+    expect(wrapper.classes().length).toBe(3)
+    expect(wrapper.attributes('id')).toBeDefined()
+    const wrapperId = wrapper.attributes('id')
+
+    expect(wrapper.findAll('button').length).toBe(2)
+    const $buttons = wrapper.findAll('button')
+    const $split = $buttons.at(0)
+    const $toggle = $buttons.at(1)
+
+    expect($split.classes()).toContain('btn')
+    expect($split.classes()).toContain('btn-secondary')
+    expect($split.attributes('id')).toBeDefined()
+    expect($split.attributes('id')).toEqual(`${wrapperId}_BV_button_`)
+    expect($split.text()).toEqual('')
+
+    expect($toggle.classes()).toContain('btn')
+    expect($toggle.classes()).toContain('btn-secondary')
+    expect($toggle.classes()).toContain('dropdown-toggle')
+    expect($toggle.classes().length).toBe(3)
+    expect($toggle.attributes('aria-haspopup')).toBeDefined()
+    expect($toggle.attributes('aria-haspopup')).toEqual('true')
+    expect($toggle.attributes('aria-expanded')).toBeDefined()
+    expect($toggle.attributes('aria-expanded')).toEqual('false')
+    expect($toggle.attributes('id')).toBeDefined()
+    expect($toggle.attributes('id')).toEqual(`${wrapperId}_BV_toggle_`)
+    expect($toggle.text()).toEqual('')
 
     expect(wrapper.findAll('.dropdown-menu').length).toBe(1)
     const $menu = wrapper.find('.dropdown-menu')
