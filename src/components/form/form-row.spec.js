03e60ec29f3d5b81c809692d17ee4fe30a04cdc7
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-01 01:28:31 -0300

    Update form-row.spec.js

--- src/components/form/form-row.spec.js
+++ src/components/form/form-row.spec.js
@@ -1,48 +1,10 @@
-import { loadFixture, testVM } from '../../../tests/utils'
+import FormRow from 'form-row'
+import { mount } from '@vue/test-utils'
 
-describe('form-row', () => {
-  beforeEach(loadFixture(__dirname, 'form-row'))
-  testVM()
-
-  it('default should contain base class', async () => {
-    const {
-      app: { $refs }
-    } = window
-    expect($refs.default).toHaveClass('form-row')
-  })
-
-  it('custom should contain base class', async () => {
-    const {
-      app: { $refs }
-    } = window
-    expect($refs.custom).toHaveClass('form-row')
-  })
-
-  it('default should have content', async () => {
-    const {
-      app: { $refs }
-    } = window
-    expect($refs.default.textContent).toContain('default')
-  })
-
-  it('custom should have content', async () => {
-    const {
-      app: { $refs }
-    } = window
-    expect($refs.custom.textContent).toContain('custom')
-  })
-
-  it('default should have tag div', async () => {
-    const {
-      app: { $refs }
-    } = window
-    expect($refs.default).toBeElement('div')
-  })
-
-  it('custom should have tag p', async () => {
-    const {
-      app: { $refs }
-    } = window
-    expect($refs.custom).toBeElement('p')
+describe('form > form-row', () => {
+  it('works', () => {
+    const wrapper = mount(FormRow)
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('form-row')
   })
 })
