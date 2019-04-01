7b5791ce073fb8dc1b212d4c38e6f6434ceb5ed3
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-01 16:48:44 -0300

    Update dropdown.legacy.spec.js

--- src/components/dropdown/dropdown.legacy.spec.js
+++ src/components/dropdown/dropdown.legacy.spec.js
@@ -4,30 +4,6 @@ describe('dropdown (legacy tests)', () => {
   beforeEach(loadFixture(__dirname, 'dropdown'))
   testVM()
 
-  it('should have "dropdown-toggle-no-caret" class when no-caret is true', async () => {
-    const {
-      app: { $refs }
-    } = window
-    const { dd_7 } = $refs // eslint-disable-line camelcase
-
-    const toggle = Array.from(dd_7.$el.children).find(
-      node => node.tagName === 'BUTTON' && node.id === `${dd_7.safeId('_BV_toggle_')}`
-    )
-    expect(toggle).toHaveClass('dropdown-toggle-no-caret')
-  })
-
-  it('should not have "dropdown-toggle-no-caret" class when no-caret and split are true', async () => {
-    const {
-      app: { $refs }
-    } = window
-    const { dd_8 } = $refs // eslint-disable-line camelcase
-
-    const toggle = Array.from(dd_8.$el.children).find(
-      node => node.tagName === 'BUTTON' && node.id === `${dd_8.safeId('_BV_toggle_')}`
-    )
-    expect(toggle).not.toHaveClass('dropdown-toggle-no-caret')
-  })
-
   /*
   it('boundary set to viewport should have class position-static', async () => {
     const {app: {$refs}} = window
@@ -43,14 +19,4 @@ describe('dropdown (legacy tests)', () => {
     expect(dd_1).not.toHaveClass('position-static')
   })
   */
-
-  it('should have a toggle with the given toggle tag', async () => {
-    const {
-      app: { $refs }
-    } = window
-    const { dd_10 } = $refs // eslint-disable-line camelcase
-
-    const toggle = dd_10.$el.querySelector('.dropdown-toggle')
-    expect(toggle).toBeElement('div')
-  })
 })
