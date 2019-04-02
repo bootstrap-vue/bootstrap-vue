c05419ae38008504a62681542868847e9c9b7cf4
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-02 04:16:25 -0300

    Update form-group.spec.js

--- src/components/form-group/form-group.spec.js
+++ src/components/form-group/form-group.spec.js
@@ -271,4 +271,55 @@ describe('form-group', () => {
     expect(wrapper.classes()).not.toContain('is-valid')
     expect(wrapper.classes()).toContain('is-invalid')
   })
+
+  /*
+  describe('form-group > legend click', () => {
+    // These tests are wrapped in a new describe to limit the scope of the getBCR Mock
+    const origGetBCR = Element.prototype.getBoundingClientRect
+
+    beforeEach(() => {
+      // Mock getBCR so that the isVisible(el) test returns true
+      // In our test below, all pagination buttons would normally be visible
+      Element.prototype.getBoundingClientRect = jest.fn(() => {
+        return {
+          width: 24,
+          height: 24,
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0
+        }
+      })
+    })
+
+    afterEach(() => {
+      // Restore prototype
+      Element.prototype.getBoundingClientRect = origGetBCR
+    })
+
+    it('clicking legend focuses input', async () => {
+      const { app } = window
+      const $group = app.$refs.group10
+
+      const legend = $group.$el.querySelector('legend')
+      expect(legend).toBeDefined()
+      expect(legend.tagName).toBe('LEGEND')
+      expect(legend.textContent).toContain('legend-click')
+      const input = $group.$el.querySelector('input')
+      expect(input).toBeDefined()
+
+      expect(document.activeElement).not.toBe(input)
+
+      // legend.click()
+      // legend.click() doesn't trigger the click event, since it is
+      // a non-interactive element
+      const clickEvt = new MouseEvent('click')
+      legend.dispatchEvent(clickEvt)
+      await nextTick()
+
+      // Can't get this to work in the test environment for some reason
+      // expect(document.activeElement).toBe(input)
+    })
+  })
+  */
 })
