56935ca945dfa2226238024671628243af53bc83
Jon Funkhouser <jonathanf@ladodgers.com>
2019-04-01 20:09:40 -0700

    added tests for top level properties

--- src/utils/get.spec.js
+++ src/utils/get.spec.js
@@ -20,6 +20,7 @@ describe('get', () => {
     expect(get({ a: 'b' }, 'b', {})).toEqual({})
     expect(get({ a: { c: 'd' } }, 'a.d', [])).toEqual([])
     expect(get({ a: { c: undefined } }, 'a.c')).toBe(null)
+    expect(get({ a: 0, b: false }, 'c')).toBe(null)
   })
 
   it('returns expected value', async () => {
@@ -28,6 +29,7 @@ describe('get', () => {
     const obj3 = { a: [{ b: 'c' }] }
     const obj4 = { a: [[{ b: 'c' }], [{ d: { e: ['f'] } }]] }
     const obj5 = { a: { b: 0, c: '', d: false } }
+    const obj6 = { a: 0, b: false }
 
     expect(get(obj1, 'a')).toBe('b')
     expect(get(obj1, ['a'])).toBe('b')
@@ -41,6 +43,8 @@ describe('get', () => {
     expect(get(obj5, 'a.b')).toBe(0)
     expect(get(obj5, 'a.c')).toBe('')
     expect(get(obj5, 'a.d')).toBe(false)
+    expect(get(obj6, 'a')).toBe(0)
+    expect(get(obj6, 'b')).toBe(false)
   })
 
   it('handles when field name has dot', async () => {
