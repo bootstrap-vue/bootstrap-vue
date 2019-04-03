2f3457b298e35d4f739fd41eca1d4ec3400e09fd
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-02 22:44:59 -0300

    Update modal.spec.js

--- src/components/modal/modal.spec.js
+++ src/components/modal/modal.spec.js
@@ -1,6 +1,8 @@
 import Modal from './modal'
 import { mount, createWrapper } from '@vue/test-utils'
 
+const waitAF = new Promise(resolve => requestAnimationFrame(resolve))
+
 describe('modal', () => {
   describe('structure', () => {
     it('has expected default structure', async () => {
@@ -65,6 +67,64 @@ describe('modal', () => {
       expect(wrapper.is('div')).toBe(true)
       expect(wrapper.classes().length).toBe(0)
       expect(wrapper.findAll('div > *').length).toBe(0) // no content
+
+      wrapper.destroy()
+    })
+
+    it('has expected structure when initially open', async () => {
+      const wrapper = mount(Modal, {
+        attachToDocument: true,
+        propsData: {
+          id: 'test',
+          visible: true
+        }
+      })
+
+      expect(wrapper.isVueInstance()).toBe(true)
+      await wrapper.vm.$nextTick()
+      await waitAF()
+      await wrapper.vm.$nextTick()
+      await waitAF()
+
+      // This outer DIV will go away once we migrate to Portal-Vue
+      // As all modals will be lazy
+      expect(wrapper.is('div')).toBe(true)
+      expect(wrapper.classes().length).toBe(0)
+
+      // Main outer wraper (has z-index, etc)... the stacker div
+      const $outer = createWrapper(wrapper.element.firstElementChild)
+      expect($outer.is('div')).toBe(true)
+      expect($outer.classes().length).toBe(0)
+      expect($outer.element.style.position).toEqual('absolute')
+      expect($outer.element.style.zIndex).toEqual('2000')
+
+      // Should have a backdrop
+      const $backdrop = $outer.find('div.modal-backdrop')
+      expect($backdrop.exists()).toBe(false)
+      expect($backdrop.classes()).toContain('fade')
+      expect($backdrop.classes()).toContain('show')
+
+      // Main modal wraper
+      const $modal = $outer.find('div.modal')
+      expect($modal.exists()).toBe(true)
+      expect($modal.attributes('id')).toBeDefined()
+      expect($modal.attributes('id')).toEqual('test')
+      expect($modal.attributes('role')).toBeDefined()
+      expect($modal.attributes('role')).toEqual('dialog')
+      expect($modal.attributes('tabindex')).toBeDefined()
+      expect($modal.attributes('tabindex')).toEqual('-1')
+      expect($modal.attributes('aria-hidden')).not.toBeDefined()
+      expect($modal.attributes('aria-modal')).toBeDefined()
+      expect($modal.attributes('aria-modal')).toEqual('true')
+      expect($modal.classes()).toContain('modal')
+      expect($modal.classes()).toContain('fade')
+      expect($modal.classes()).toContain('show')
+      expect($modal.classes()).toContain('d-block')
+      expect($modal.element.style.display).toEqual('')
+
+      // Modal dialog wrapper
+      const $dialog = $modal.find('div.modal-dialog')
+      expect($dialog.exists()).toBe(true)
 
       wrapper.destroy()
     })
