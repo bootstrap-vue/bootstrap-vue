bd87371da65eeea956a5e31f01fd23995b557e18
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-03 13:17:23 -0300

    Update tooltip.spec.js

--- src/components/tooltip/tooltip.spec.js
+++ src/components/tooltip/tooltip.spec.js
@@ -5,10 +5,14 @@ const localVue = new CreateLocalVue()
 
 // Our test application definition
 const appDef = {
-  props: ['trigger', 'show', 'disabed', 'title'],
+  props: ['trigger', 'show', 'disabed', 'title', 'titleAttr'],
   render(h) {
     return h('article', { attrs: { id: 'wrapper' } }, [
-      h('button', { attrs: { id: 'foo', type: 'button' } }, 'text'),
+      h(
+        'button',
+        { attrs: { id: 'foo', type: 'button', title: this.titleAttr || null } },
+        'text'
+      ),
       h(
         Tooltip,
         {
@@ -106,6 +110,60 @@ describe('tooltip', () => {
     // title placeholder (from default slot)
     expect($tipholder.findAll('div.d-none > div').length).toBe(1)
     expect($tipholder.find('div.d-none > div').text()).toBe('title')
+
+    wrapper.destroy()
+  })
+
+  it('initially open has expected structure', async () => {
+    const App = localVue.extend(appDef)
+    const wrapper = mount(App, {
+      attachToDocument: true,
+      localVue: localVue,
+      propsData: {
+        trigger: 'click',
+        show: true
+      },
+      slots: {
+        default: 'title'
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.is('article')).toBe(true)
+    expect(wrapper.attributes('id')).toBeDefined()
+    expect(wrapper.attributes('id')).toEqual('wrapper')
+
+    // The trigger button
+    const $button = wrapper.find('button')
+    expect($button.exists()).toBe(true)
+    expect($button.attributes('id')).toBeDefined()
+    expect($button.attributes('id')).toEqual('foo')
+    expect($button.attributes('title')).toBeDefined()
+    expect($button.attributes('title')).toEqual('')
+    expect($button.attributes('data-original-title')).toBeDefined()
+    expect($button.attributes('data-original-title')).toEqual('')
+    expect($button.attributes('aria-describedby')).toBeDefined()
+    // ID of the tooltip that will be in the body
+    const adb = $button.attributes('aria-describedby')
+
+    // b-tooltip wrapper
+    const $tipholder = wrapper.find('div#bar')
+    expect($tipholder.exists()).toBe(true)
+    expect($tipholder.classes()).toContain('d-none')
+    expect($tipholder.attributes('aria-hidden')).toBeDefined()
+    expect($tipholder.attributes('aria-hidden')).toEqual('true')
+    expect($tipholder.element.style.display).toEqual('none')
+
+    // title placeholder (from default slot) will ahve been moved to tooltip element
+    expect($tipholder.findAll('div.d-none > div').length).toBe(0)
+    // title text will be moved into the tooltip
+    expect($tipholder.text()).toBe('')
+
+    // Find the tooltip element in the document
+    const tip = document.querySelector(`#${adb}`)
+    expect(tip).not.toBe(null)
 
     wrapper.destroy()
   })
