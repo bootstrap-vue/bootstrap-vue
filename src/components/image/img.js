0f5c9ba05b230a97fb737d7b53542afe7eca52d1
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-04 20:07:26 -0300

    Update img.js

--- src/components/image/img.js
+++ src/components/image/img.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import { mergeData } from 'vue-functional-data-merge'
 import { getComponentConfig } from '../../utils/config'
 
@@ -90,7 +91,7 @@ export const props = {
 }
 
 // @vue/component
-export default {
+export default Vue.extend({
   name: 'BImg',
   functional: true,
   props,
@@ -142,4 +143,4 @@ export default {
       })
     )
   }
-}
+})
