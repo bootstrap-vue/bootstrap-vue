488eb992539343d50f19295f084233fb5b4d8e3e
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-04 19:02:19 -0300

    Update componentdoc.vue

--- docs/components/componentdoc.vue
+++ docs/components/componentdoc.vue
@@ -205,10 +205,14 @@ export default {
       }
 
       let options = {}
-      if (!component.options && typeof component === 'function') {
+      if (typeof component === 'function') {
         // Async component that hans't been resolved yet
-        component(opts => {
-          options = opts ? { ...opts } : {}
+        component(cmp => {
+          if (Object.prototype.toString.call(cmp) === '[object Object]') {
+            options = { ...cmp }
+          } else if (cmp && cmp.options) {
+            options = cmp.options
+          }
         })
       } else {
         // Regular component
