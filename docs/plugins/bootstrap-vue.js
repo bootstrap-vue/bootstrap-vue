b43f39d2d88b197a796c832fec602497ea6a6931
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-05 17:19:42 -0300

    Update bootstrap-vue.js

--- docs/plugins/bootstrap-vue.js
+++ docs/plugins/bootstrap-vue.js
@@ -1,7 +1,4 @@
 import Vue from 'vue'
 import BootstrapVue from '../../src'
 
-Vue.use(BootstrapVue, {
-  // Make sure modals in the docs are above anything else
-  BModal: { zIndexOffset: 2000 }
-})
+Vue.use(BootstrapVue, {})
