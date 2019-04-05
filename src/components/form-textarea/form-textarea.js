f5123c6538a87227ec90ab84ef8eab4b1003b9e4
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-04 23:52:12 -0300

    Update form-textarea.js

--- src/components/form-textarea/form-textarea.js
+++ src/components/form-textarea/form-textarea.js
@@ -54,7 +54,10 @@ export default {
       const styles = {
         // Setting `noResize` to true will disable the ability for the user to
         // manually resize the textarea. We also disable when in auto resize mode
-        resize: !this.computedRows || this.noResize ? 'none' : null
+        resize: !this.computedRows || this.noResize ? 'none' : null,
+        // We always add a vertical scrollbar to the textarea when auto-resize is
+        // enabled so that the computed height calcaultion returns a stable value.
+        'overflow-y': this.computedRows ? 'scroll' : null
       }
       if (!this.computedRows) {
         // The computed height for auto resize.
