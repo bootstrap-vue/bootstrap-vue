fdd7b2029dd8439389fa1371d8c9e6fccd08f726
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-05 14:32:01 -0300

    Update toggle.js

--- src/directives/toggle/toggle.js
+++ src/directives/toggle/toggle.js
@@ -9,12 +9,17 @@ const listenTypes = { click: true }
 const BV_TOGGLE = '__BV_toggle__'
 const BV_TOGGLE_STATE = '__BV_toggle_STATE__'
 const BV_TOGGLE_CONTROLS = '__BV_toggle_CONTROLS__'
+const BV_TOGGLE_TARGETS = '__BV_toggle_CONTROLS__'
 
 // Emitted control event for collapse (emitted to collapse)
 const EVENT_TOGGLE = 'bv::toggle::collapse'
 
 // Listen to event for toggle state update (emitted by collapse)
 const EVENT_STATE = 'bv::collapse::state'
+
+// Private event emitted on $root to ensure the toggle state is always synced.
+// Gets emitted even if the state of b-collapse has not changed
+const EVENT_STATE_SYNC = 'bv::collapse::sync::state'
 
 // Reset and remove a property from the provided element
 const resetProp = (el, prop) => {
@@ -53,6 +58,8 @@ export default {
     })
 
     if (inBrowser && vnode.context && targets.length > 0) {
+      // Add targets array to element
+      el[BV_TOGGLE_TARGETS] = targets
       // Add aria attributes to element
       el[BV_TOGGLE_CONTROLS] = targets.join(' ')
       // State is initially collapsed until we receive a state event
@@ -66,6 +73,7 @@ export default {
 
       // Toggle state handler, stored on element
       el[BV_TOGGLE] = function toggleDirectiveHandler(id, state) {
+        const targets = el[BV_TOGGLE_TARGETS] || []
         if (targets.indexOf(id) !== -1) {
           // Set aria-expanded state
           setAttr(el, 'aria-expanded', state ? 'true' : 'false')
@@ -79,8 +87,10 @@ export default {
         }
       }
 
-      // Listen for toggle state changes
+      // Listen for toggle state changes (public)
       vnode.context.$root.$on(EVENT_STATE, el[BV_TOGGLE])
+      // Listen for toggle state sync (private)
+      vnode.context.$root.$on(EVENT_STATE_SYNC, el[BV_TOGGLE])
     }
   },
   componentUpdated: handleUpdate,
@@ -90,11 +100,13 @@ export default {
     // Remove our $root listener
     if (el[BV_TOGGLE]) {
       vnode.context.$root.$off(EVENT_STATE, el[BV_TOGGLE])
+      vnode.context.$root.$off(EVENT_STATE_SYNC, el[BV_TOGGLE])
     }
     // Reset custom  props
     resetProp(el, BV_TOGGLE)
     resetProp(el, BV_TOGGLE_STATE)
     resetProp(el, BV_TOGGLE_CONTROLS)
+    resetProp(el, BV_TOGGLE_TARGETS)
     // Reset classes/attrs
     removeClass(el, 'collapsed')
     removeAttr(el, 'aria-expanded')
