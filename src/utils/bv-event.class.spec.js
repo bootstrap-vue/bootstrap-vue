43a7e00baa9531031c80f5eee991c229053984f7
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-05 21:04:12 -0300

    Update bv-event.class.spec.js

--- src/utils/bv-event.class.spec.js
+++ src/utils/bv-event.class.spec.js
@@ -1,4 +1,4 @@
-import BvEvent from './bv-event.class'
+import { BvEvent, BvModalEvent } from './bv-event.class'
 
 describe('utils/BvEvent', () => {
   it('works', async () => {
@@ -62,5 +62,83 @@ describe('utils/BvEvent', () => {
     expect(evt.type).toBe('foobar')
     expect(evt.cancelable).toBe(true)
     expect(evt.custom).toBe(123)
+  })
+
+  describe('BvModalEvent', () => {
+    it('works', async () => {
+      const evt = new BvModalEvent('foobar')
+      expect(evt).toBeInstanceOf(BvModalEvent)
+      expect(evt).toBeInstanceOf(BvEvent)
+      expect(evt.type).toBe('foobar')
+      expect(evt.isOK).toBe(false)
+    })
+
+    it('throws exception if no type given', async () => {
+      let evt = null
+      let failed = false
+      try {
+        evt = new BvModalEvent()
+      } catch (e) {
+        failed = true
+      }
+      expect(evt).not.toBeInstanceOf(BvModalEvent)
+      expect(evt).not.toBeInstanceOf(BvEvent)
+      expect(evt).toBe(null)
+      expect(failed).toBe(true)
+    })
+
+    it('supports cancelable events', async () => {
+      const evt = new BvModalEvent('foobar', {
+        cancelable: true
+      })
+      expect(evt).toBeInstanceOf(BvModalEvent)
+      expect(evt.type).toBe('foobar')
+      expect(evt.cancelable).toBe(true)
+      expect(evt.defaultPrevented).toBe(false)
+      evt.preventDefault()
+      expect(evt.defaultPrevented).toBe(true)
+    })
+
+    it('supports non cancelable events', async () => {
+      const evt = new BvModalEvent('foobar', {
+        cancelable: false
+      })
+      expect(evt).toBeInstanceOf(BvModalEvent)
+      expect(evt.type).toBe('foobar')
+      expect(evt.cancelable).toBe(false)
+      expect(evt.defaultPrevented).toBe(false)
+      evt.preventDefault()
+      expect(evt.defaultPrevented).toBe(false)
+    })
+
+    it('supports built in properties', async () => {
+      const evt = new BvModalEvent('foobar', {
+        target: 'baz',
+        trigger: 'ok'
+      })
+      expect(evt).toBeInstanceOf(BvModalEvent)
+      expect(evt.type).toBe('foobar')
+      expect(evt.cancelable).toBe(true)
+      expect(evt.target).toBe('baz')
+      expect(evt.trigger).toBe('ok')
+      expect(evt.isOK).toBe(true)
+      let failed = false
+      try {
+        evt.trigger = 'foobar'
+      } catch (e) {
+        failed = true
+      }
+      expect(failed).toBe(true)
+    })
+
+    it('supports custom properties', async () => {
+      const evt = new BvEvent('foobar', {
+        custom: 123
+      })
+      expect(evt).toBeInstanceOf(BvEvent)
+      expect(evt.type).toBe('foobar')
+      expect(evt.cancelable).toBe(true)
+      expect(evt.custom).toBe(123)
+    })
   })
 })
