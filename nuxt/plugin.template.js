8022481c302d77aede311c88707fd8742a152eed
Pooya Parsa <pyapar@gmail.com>
2019-04-08 23:21:45 +0430

    feat(nuxt): add `usePretranspiled` option (#3048)

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 <% if (options.componentPlugins.length || options.directivePlugins.length) { %><%=
-options.componentPlugins.reduce((acc, p) => (acc += `import ${p[0]} from 'bootstrap-vue/es/components/${p[1]}'\n` ), '') %><%=
-options.directivePlugins.reduce((acc, p) => (acc += `import ${p[0]} from 'bootstrap-vue/es/directives/${p[1]}'\n` ), '') %>
+options.componentPlugins.reduce((acc, p) => (acc += `import ${p[0]} from 'bootstrap-vue/${options.dist}/components/${p[1]}'\n` ), '') %><%=
+options.directivePlugins.reduce((acc, p) => (acc += `import ${p[0]} from 'bootstrap-vue/${options.dist}/directives/${p[1]}'\n` ), '') %>
 
 <% if (options.config) { %>
-import BVConfigPlugin from 'bootstrap-vue/es/bv-config'
+import BVConfigPlugin from 'bootstrap-vue/<%= options.dist %>/bv-config'
 
 Vue.use(BVConfigPlugin, <%= JSON.stringify(options.config, undefined, 2) %>)
 <% } %>
@@ -13,7 +13,7 @@ Vue.use(BVConfigPlugin, <%= JSON.stringify(options.config, undefined, 2) %>)
 options.componentPlugins.reduce((acc, p) => (acc += `Vue.use(${p[0]})\n` ), '') %><%=
 options.directivePlugins.reduce((acc, p) => (acc += `Vue.use(${p[0]})\n` ), '') %>
 <% } else { %>
-import BootstrapVue from 'bootstrap-vue/es'
+import BootstrapVue from 'bootstrap-vue/<%= options.dist %>'
 
 Vue.use(BootstrapVue, <%= JSON.stringify(options.config || {}, undefined, 2) %>)
 <% } %>
