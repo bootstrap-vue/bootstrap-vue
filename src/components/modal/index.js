c5bf2c78bccef8e3c69ca5d3313e94d13dc6f921
Troy Morehouse <troymore@nbnet.nb.ca>
2019-04-10 03:49:01 -0300

    Update index.js

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,5 +1,7 @@
 import BModal from './modal'
 import BModalDirective from '../../directives/modal/modal'
+import bvModalInstall from './helpers/bv-modal'
+
 import { installFactory } from '../../utils/plugins'
 
 const components = {
@@ -10,6 +12,13 @@ const directives = {
   BModal: BModalDirective
 }
 
+const install = installFactory({ components, directives })
+
 export default {
-  install: installFactory({ components, directives })
+  install: Vue => {
+    // Inject $bvModal into Vue prototype
+    bvModalInstall(Vue)
+    // Install component and directive
+    install(Vue)
+  }
 }
