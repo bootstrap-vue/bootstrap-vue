e36fd0f1d7e59d3b8a3eda681186f1dd9009dc16
T Morehouse <troymore@nbnet.nb.ca>
2019-04-14 04:20:54 -0300

    Create toaster.js

---
+++ src/components/toast/toaster.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import { PortalTarget } from 'portal-vue'
+
+/* istanbul ignore file: for now until ready for testing */
+
+const NAME = 'BToaster'
+
+export default Vue.extend({
+  name: NAME,
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    transition: {
+      type: [Boolean, String, Object]
+      default: false
+    }
+  },
+  render(h) {
+    render(h) {
+      return h(
+        Portaltarget,
+        {
+          class: [ this.name ],
+          props: {
+            name: this.name,
+            multiple: true,
+            tag: 'div',
+            slim: false,
+            transition: this.transition
+          }
+        }
+      )
+    }
+  }
+})
