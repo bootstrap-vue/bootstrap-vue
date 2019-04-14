b647830779954b0155c96a7396bef55872b495f0
T Morehouse <troymore@nbnet.nb.ca>
2019-04-14 10:50:43 -0300

    feat(modal): support for optionally scoped slots and new `Vue.prototype.$bvModal` helper (#3056)
    * feat(modal): optionally scoped slots
    
    * lint
    
    * Update modal.js
    
    * lint
    
    * Update modal.js
    
    * Update package.json
    
    * Update modal.js
    
    * Update package.json
    
    * Update package.json
    
    * Update env.js
    
    * lint
    
    * Create bv-modal.js
    
    * Update bv-modal.js
    
    * lint
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update index.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update env.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Create inspect.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update inspect.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update modal.js
    
    * Update bv-modal.js
    
    * lint
    
    * Update modal.js
    
    * Create inspect.spec.js
    
    * Update inspect.spec.js
    
    * Update inspect.spec.js
    
    * Update inspect.js
    
    * Update inspect.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Create bv-modal.spec.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.spec.js
    
    * Update inspect.spec.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.js
    
    * Update bv-modal.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.js
    
    * Update bv-modal.spec.js
    
    * Update bv-modal.js
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update package.json
    
    * Update README.md
    
    * Update modal.js
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Final tweaks

---
+++ src/utils/inspect.js
@@ -0,0 +1,34 @@
+/**
+ * Convenience inspection utilities
+ */
+
+import { isArray } from './array'
+import { isObject, isPlainObject } from './object'
+
+export const toType = val => typeof val
+
+export const toRawType = val => Object.prototype.toString.call(val).slice(8, -1)
+
+export const toRawTypeLC = val => toRawType(val).toLowerCase()
+
+export const isUndefined = val => val === undefined
+
+export const isNull = val => val === null
+
+export const isFunction = val => toType(val) === 'function'
+
+export const isBoolean = val => toType(val) === 'boolean'
+
+export const isString = val => toType(val) === 'string'
+
+export const isNumber = val => toType(val) === 'number'
+
+export const isPrimitive = val => isBoolean(val) || isString(val) || isNumber(val)
+
+export const isRegExp = val => toRawType(val) === 'RegExp'
+
+export const isPromise = val =>
+  !isUndefined(val) && !isNull(val) && isFunction(val.then) && isFunction(val.catch)
+
+// Extra convenience named re-exports
+export { isObject, isPlainObject, isArray }
