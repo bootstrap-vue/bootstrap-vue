16dcef7c618c76d7c0e992a5025877a7c9550c94
T Morehouse <troymore@nbnet.nb.ca>
2019-04-17 14:25:38 -0300

    Update col.js

--- src/components/layout/col.js
+++ src/components/layout/col.js
@@ -1,9 +1,10 @@
+import Vue from '../../utils/vue'
 import { mergeData } from 'vue-functional-data-merge'
 import memoize from '../../utils/memoize'
 import suffixPropName from '../../utils/suffix-prop-name'
 import { arrayIncludes } from '../../utils/array'
 import { isUndefined, isNull } from '../../utils/inspect'
-import { keys, assign, create, defineProperty } from '../../utils/object'
+import { keys, assign, create } from '../../utils/object'
 import { getBreakpointsUp } from '../../utils/config'
 
 // --- Constants ---
@@ -26,15 +27,7 @@ const strNum = () => ({
   default: null
 })
 
-// Get breakpoints for generating props (cached on first call)
-const getBreakpoints = () => {
-  if (isUndefined(BREAKPOINTS)) {
-    BREAKPOINTS = getBreakpointsUp().filter(Boolean)
-  }
-  // Grab the breakpoints from the config (exclude the '' (xs) breakpoint)
-  return BREAKPOINTS
-}
-
+// Generate a class name based on type, breakpoint name and value
 const computeBkPt = (type, breakpoint, val) => {
   let className = type
   if (isUndefined(val) || isNull(val) || val === false) {
@@ -58,9 +51,15 @@ const computeBkPt = (type, breakpoint, val) => {
 // Memoize `computeBkPt()` for better performance on generating class names
 const computeBkPtClass = memoize(computeBkPt)
 
-// Supports classes like: .col-sm, .col-md-6, .col-lg-auto
-const breakpointCol = () => {
-  return getBreakpoints().reduce((propMap, breakpoint) => {
+// Async Component:
+//   So that we can generate breakpoint specific
+//   props once the config has been updated.
+export default new Promise(resolve => {
+  // Get breakpoints for generating props (cached on first call)
+  const breakpoints = getBreakpointsUp().filter(Boolean)
+
+  // Supports classes like: .col-sm, .col-md-6, .col-lg-auto
+  const breakpointCol = breakpoints.reduce((propMap, breakpoint) => {
     if (breakpoint) {
       // We filter out the '' breakpoint (xs), as making a prop name ''
       // would not work. The `cols` prop is used for `xs`
@@ -68,98 +67,44 @@ const breakpointCol = () => {
     }
     return propMap
   }, create(null))
-}
 
-// Supports classes like: .offset-md-1, .offset-lg-12
-const breakpointOffset = () => {
-  return getBreakpoints().reduce((propMap, breakpoint) => {
-    propMap[suffixPropName(breakpoint, 'offset')] = strNum()
-    return propMap
-  }, create(null))
-}
-
-// Supports classes like: .order-md-1, .order-lg-12
-const breakpointOrder = () => {
-  return getBreakpoints().reduce((propMap, breakpoint) => {
+  // Supports classes like: .order-md-1, .order-lg-12
+  const breakpointOrder = breakpoints.reduce((propMap, breakpoint) => {
     propMap[suffixPropName(breakpoint, 'order')] = strNum()
     return propMap
   }, create(null))
-}
 
-// For loop doesn't need to check hasOwnProperty
-// when using an object created from `null`
-const breakpointPropMap = () => {
-  if (isUndefined(PROP_MAP)) {
-    PROP_MAP = assign(create(null), {
-      col: keys(breakpointCol()),
-      offset: keys(breakpointOffset()),
-      order: keys(breakpointOrder())
-    })
-  }
-  return PROP_MAP
-}
+  // For loop doesn't need to check hasOwnProperty
+  // when using an object created from `null`
+  const breakpointPropMap = assign(create(null), {
+    col: keys(breakpointCol),
+    offset: keys(breakpointOffset),
+    order: keys(breakpointOrder)
+  })
 
-// We do not use `Vue.extend()` here as we need the prop's getters
-// to be evaluated by vue on first use
-// @vue/component
-const BCol = {
-  name: 'BCol',
-  functional: true,
-  // We define the props as a getter further down
-  // props: {},
-  render(h, { props, data, children }) {
-    const classList = []
-    const propsMap = breakpointPropMap()
-    // Loop through `col`, `offset`, `order` breakpoint props
-    for (const type in propsMap) {
-      // Returns colSm, offset, offsetSm, orderMd, etc.
-      const keys = propsMap[type]
-      for (let i = 0; i < keys.length; i++) {
-        // computeBkPt(col, colSm => Sm, value=[String, Number, Boolean])
-        const c = computeBkPtClass(type, keys[i].replace(type, ''), props[keys[i]])
-        // If a class is returned, push it onto the array
-        if (c) {
-          classList.push(c)
-        }
-      }
-    }
-
-    const hasColClasses = classList.some(className => /^col-/.test(className))
-
-    classList.push({
-      // Default to .col if no other col-{bp}-* classes generated nor `cols` specified.
-      col: props.col || (!hasColClasses && !props.cols),
-      [`col-${props.cols}`]: props.cols,
-      [`offset-${props.offset}`]: props.offset,
-      [`order-${props.order}`]: props.order,
-      [`align-self-${props.alignSelf}`]: props.alignSelf
-    })
-
-    return h(props.tag, mergeData(data, { class: classList }), children)
-  }
-}
-
-// Add our props getter, for lazy loading breakpoint props based on config
-// When Vue extends this component, it will then evaluate the props, which
-// will trigger our getter to compute the props
-defineProperty(BCol, 'props', {
-  configurable: true,
-  enumerable: true,
-  get() {
-    return {
+  // @vue/component
+  const BCol = {
+    name: 'BCol',
+    fuctional: true,
+    props: {
       // Generic flexbox .col (xs)
       col: {
         type: Boolean,
         default: false
       },
+      // Breakpoint Specific props
       // .col-[1-12]|auto (xs)
       cols: strNum(),
-      // Breakpoint Specific props
-      ...breakpointCol(),
-      offset: strNum(), // (xs)
-      ...breakpointOffset(),
-      order: strNum(), // (xs)
-      ...breakpointOrder(),
+      // .col-[sm[xl]-[1-12]|auto
+      ...breakpointCol,
+      // .offet-[1-12] (xs)
+      offset: strNum(),
+      // .offset-[sm-xl]-[1-12]
+      ...breakpointOffset,
+      // .order-[1-12] (xs)
+      order: strNum(),
+      // .order-[sm-xl]-[1-12]
+      ...breakpointOrder,
       // Flex alignment
       alignSelf: {
         type: String,
@@ -171,8 +116,39 @@ defineProperty(BCol, 'props', {
         type: String,
         default: 'div'
       }
+    },
+    render(h, { props, data, children }) {
+      const classList = []
+      const propsMap = breakpointPropMap
+      // Loop through `col`, `offset`, `order` breakpoint props
+      for (const type in propsMap) {
+        // Returns colSm, offset, offsetSm, orderMd, etc.
+        const keys = propsMap[type]
+        for (let i = 0; i < keys.length; i++) {
+          // computeBkPt(col, colSm => Sm, value=[String, Number, Boolean])
+          const c = computeBkPtClass(type, keys[i].replace(type, ''), props[keys[i]])
+          // If a class is returned, push it onto the array
+          if (c) {
+            classList.push(c)
+          }
+        }
+      }
+
+      const hasColClasses = classList.some(className => /^col-/.test(className))
+
+      classList.push({
+        // Default to .col if no other col-{bp}-* classes generated nor `cols` specified.
+        col: props.col || (!hasColClasses && !props.cols),
+        [`col-${props.cols}`]: props.cols,
+        [`offset-${props.offset}`]: props.offset,
+        [`order-${props.order}`]: props.order,
+        [`align-self-${props.alignSelf}`]: props.alignSelf
+      })
+
+      return h(props.tag, mergeData(data, { class: classList }), children)
     }
   }
-})
 
-export default BCol
+  // Return a resolved promise
+  return Promise.resolve(Vue.extend(BCol))
+})
