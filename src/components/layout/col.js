1632375a8df85e7300b1c41bad0009af4f590476
T Morehouse <troymore@nbnet.nb.ca>
2019-04-17 15:35:34 -0300

    Update col.js

--- src/components/layout/col.js
+++ src/components/layout/col.js
@@ -1,4 +1,3 @@
-import Vue from '../../utils/vue'
 import { mergeData } from 'vue-functional-data-merge'
 import memoize from '../../utils/memoize'
 import suffixPropName from '../../utils/suffix-prop-name'
@@ -7,50 +6,56 @@ import { isUndefined, isNull } from '../../utils/inspect'
 import { keys, assign, create } from '../../utils/object'
 import { getBreakpointsUp } from '../../utils/config'
 
-// --- Helper methods ---
-
-// Generates a prop object with a type of [Boolean, String, Number]
-const boolStrNum = () => ({
-  type: [Boolean, String, Number],
-  default: false
-})
-
-// Generates a prop object with a type of [String, Number]
-const strNum = () => ({
-  type: [String, Number],
-  default: null
-})
-
-// Generate a class name based on type, breakpoint name and value
-const computeBkPt = (type, breakpoint, val) => {
-  let className = type
-  if (isUndefined(val) || isNull(val) || val === false) {
-    return undefined
+/**
+ * Generates a prop object with a type of
+ * [Boolean, String, Number]
+ */
+function boolStrNum() {
+  return {
+    type: [Boolean, String, Number],
+    default: false
   }
-  if (breakpoint) {
-    className += `-${breakpoint}`
-  }
-  // Handling the boolean style prop when accepting [Boolean, String, Number]
-  // means Vue will not convert <b-col sm></b-col> to sm: true for us.
-  // Since the default is false, an empty string indicates the prop's presence.
-  if (type === 'col' && (val === '' || val === true)) {
-    // .col-md
-    return className.toLowerCase()
-  }
-  // .order-md-6
-  className += `-${val}`
-  return className.toLowerCase()
 }
 
-// Memoize `computeBkPt()` for better performance on generating class names
-const computeBkPtClass = memoize(computeBkPt)
+/**
+ * Generates a prop object with a type of
+ * [String, Number]
+ */
+function strNum() {
+  return {
+    type: [String, Number],
+    default: null
+  }
+}
 
-// Async Component:
-//   So that we can generate breakpoint specific
-//   props once the config has been updated.
-export default new Promise(resolve => {
-  // Get breakpoints for generating props (cached on first call)
+// Async (lazy) component
+// So that we can generate breakpoint specific props once the config has been updated.
+//
+// See: https://vuejs.org/v2/guide/components-dynamic-async.html#Async-Components
+export default (resolve, reject) => {
+  // Grab the breakpoints from the config (exclude the '' (xs) breakpoint)
   const breakpoints = getBreakpointsUp().filter(Boolean)
+
+  // Memoized function for better performance on generating class names
+  const computeBkPtClass = memoize(function computeBkPt(type, breakpoint, val) {
+    let className = type
+    if (isUndefined(val) || isNull(val) || val === false) {
+      return undefined
+    }
+    if (breakpoint) {
+      className += `-${breakpoint}`
+    }
+    // Handling the boolean style prop when accepting [Boolean, String, Number]
+    // means Vue will not convert <b-col sm></b-col> to sm: true for us.
+    // Since the default is false, an empty string indicates the prop's presence.
+    if (type === 'col' && (val === '' || val === true)) {
+      // .col-md
+      return className.toLowerCase()
+    }
+    // .order-md-6
+    className += `-${val}`
+    return className.toLowerCase()
+  })
 
   // Supports classes like: .col-sm, .col-md-6, .col-lg-auto
   const breakpointCol = breakpoints.reduce((propMap, breakpoint) => {
@@ -62,20 +67,20 @@ export default new Promise(resolve => {
     return propMap
   }, create(null))
 
-  // Supports classes like: .order-md-1, .order-lg-12
-  const breakpointOrder = breakpoints.reduce((propMap, breakpoint) => {
-    propMap[suffixPropName(breakpoint, 'order')] = strNum()
-    return propMap
-  }, create(null))
-
   // Supports classes like: .offset-md-1, .offset-lg-12
   const breakpointOffset = breakpoints.reduce((propMap, breakpoint) => {
     propMap[suffixPropName(breakpoint, 'offset')] = strNum()
     return propMap
   }, create(null))
 
+  // Supports classes like: .order-md-1, .order-lg-12
+  const breakpointOrder = breakpoints.reduce((propMap, breakpoint) => {
+    propMap[suffixPropName(breakpoint, 'order')] = strNum()
+    return propMap
+  }, create(null))
+
   // For loop doesn't need to check hasOwnProperty
-  // when using an object created from `null`
+  // when using an object created from null
   const breakpointPropMap = assign(create(null), {
     col: keys(breakpointCol),
     offset: keys(breakpointOffset),
@@ -85,25 +90,20 @@ export default new Promise(resolve => {
   // @vue/component
   const BCol = {
     name: 'BCol',
-    fuctional: true,
+    functional: true,
     props: {
       // Generic flexbox .col (xs)
       col: {
         type: Boolean,
         default: false
       },
-      // Breakpoint Specific props
-      // .col-[1-12]|auto (xs)
+      // .col-[1-12]|auto  (xs)
       cols: strNum(),
-      // .col-[sm[xl]-[1-12]|auto
+      // Breakpoint Specific props
       ...breakpointCol,
-      // .offet-[1-12] (xs)
       offset: strNum(),
-      // .offset-[sm-xl]-[1-12]
       ...breakpointOffset,
-      // .order-[1-12] (xs)
       order: strNum(),
-      // .order-[sm-xl]-[1-12]
       ...breakpointOrder,
       // Flex alignment
       alignSelf: {
@@ -119,15 +119,14 @@ export default new Promise(resolve => {
     },
     render(h, { props, data, children }) {
       const classList = []
-      const propsMap = breakpointPropMap
       // Loop through `col`, `offset`, `order` breakpoint props
-      for (const type in propsMap) {
+      for (const type in breakpointPropMap) {
         // Returns colSm, offset, offsetSm, orderMd, etc.
-        const keys = propsMap[type]
+        const keys = breakpointPropMap[type]
         for (let i = 0; i < keys.length; i++) {
           // computeBkPt(col, colSm => Sm, value=[String, Number, Boolean])
           const c = computeBkPtClass(type, keys[i].replace(type, ''), props[keys[i]])
-          // If a class is returned, push it onto the array
+          // If a class is returned, push it onto the array.
           if (c) {
             classList.push(c)
           }
@@ -149,6 +148,9 @@ export default new Promise(resolve => {
     }
   }
 
-  // Return a resolved promise
-  resolve(Vue.extend(BCol))
-})
+  // Return the component options.
+  // We don't use Vue.extend on the component options, as Vue gets confused when
+  // resolving a factory function component that is lazily imported into a project.
+  // i.e. cmp = () => import('path/to/component')
+  resolve(BCol)
+}
