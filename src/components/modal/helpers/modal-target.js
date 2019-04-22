740fd5b8fc2a76484ec43379e2bfcde9ed4d31ae
T Morehouse <troymore@nbnet.nb.ca>
2019-04-22 12:47:04 -0300

    Create modal-target.js

---
+++ src/components/modal/helpers/modal-target.js
@@ -0,0 +1,60 @@
+import Vue from '../../../utils/vue'
+import { Portaltarget, Wormhole } from 'portal-vue'
+
+const NAME = 'BModalTarget'
+
+export const modalTargetName = `BV-${NAME}`
+
+export const BModalTarget = Vue.extend({
+  name: NAME,
+  data() {
+    return {
+      doRender: false
+    }
+  },
+  destroyed() {
+    // Ensure we get removed from DOM when destroyed
+    if (this.$el && this.$el.parentNode) {
+      this.$el.parentNode.removeChild(this.$el)
+    }
+  }, 
+  beforeMount() {
+    const self = this
+    // There can be only one Modal target in the document
+    /* istanbul ignore if */
+    if (Wormhole.hasTarget(modalTargetName)) {
+      this.$once('hook:mounted', () => {
+        self.$nextTick(() => {
+          requestAF(() => {
+            self.$destroy()
+          })
+        })
+      })
+    } else {
+      this.doRender = true
+      this.$once('hook:beforeDestroy', () => {
+        // Trigger any open modals to hide if we are destroyed
+        self.$root.$emit('bv::modaltarget::destroy')
+      })
+    }
+  },
+  render(h) {
+    let $target = h('div', {})
+    if (this.doRender) {
+      $target = h(
+        PortalTarget,
+        {
+          staticClass: 'b-modal-target',
+          props: {
+            tag: 'div',
+            name: modalTargetName,
+            multiple: true,
+            transition: false,
+            slim: false
+          }
+        }
+      )
+    }
+    return $target
+  }
+})
