7a43067e38a84a3188aba328312fdcf589d373c8
T Morehouse <troymore@nbnet.nb.ca>
2019-04-26 15:48:31 -0300

    Update normalize-slot.js

--- src/utils/normalize-slot.js
+++ src/utils/normalize-slot.js
@@ -1,5 +1,23 @@
 import { isFunction } from './inspect'
 
+// Note for functional components:
+// In functional components, `slots` is a function so it must be called
+// first before passing to the below methods. `scopedSlots` is always an
+// object and may be undefined (for Vue < 2.6.x)
+
+/**
+ * Returns true if either scoped or unscoped named slot eists
+ *
+ * @param {String} name
+ * @param {Object} scopedSlots
+ * @param {Object} slots
+ * @returns {Array|undefined} vNodes
+ */
+const hasNormalizedSlot = (name, $scopedSlots = {}, $slots = {}) => {
+  // Returns true if the either a $scopedSlot or $slot exists with the specified name
+  return Boolean(this.$scopedSlots[name] || this.$slots[name])
+}
+
 /**
  * Returns vNodes for named slot either scoped or unscoped
  *
@@ -13,6 +31,11 @@ const normalizeSlot = (name, scope = {}, $scopedSlots = {}, $slots = {}) => {
   // Note: in Vue 2.6.x, all names slots are also scoped slots
   const slot = $scopedSlots[name] || $slots[name]
   return isFunction(slot) ? slot(scope) : slot
+}
+
+export {
+  hasNormalizedSlot,
+  normalizeSlot
 }
 
 export default normalizeSlot
