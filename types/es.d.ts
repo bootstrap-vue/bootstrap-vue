3be3ab8af3a2682132359ea5057320d9fc0fff51
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 14:34:03 -0300

    Update es.d.ts

--- types/es.d.ts
+++ types/es.d.ts
@@ -21,8 +21,23 @@ declare module 'bootstrap-vue/es/components/*' {
   import { VueConstructor, Component, AsyncComponent, ComponentOptions } from 'vue'
   import { BvPlugin } from './bv-plugin'
 
+  interface BvComponentsObject {
+    [key: string]?: Component | AsyncComponent | ComponentOptions
+  }
+
+  interface BvDirectivesObject {
+    [key: string]?: DirectiveOptions | DirectiveFunction
+  }
+
+  interface BvPluginsObject {
+    [key: string]?: BvPlugin
+  }
+
   const BootstrapVueComponent: {
     default: BvPlugin
+    components?: BvComponentsObject
+    directives?: BvDirectivesObject
+    plugins?: BvPluginsObject
     [key: string]: VueConstructor | Component | AsyncComponent | ComponentOptions
   }
 
@@ -33,8 +48,13 @@ declare module 'bootstrap-vue/es/directives' {
   import { DirectiveOptions, DirectiveFunction, PluginFunction } from 'vue'
   import { BvPlugin } from './bv-plugin'
 
+  interface BvDirectivesObject {
+    [key: string]?: DirectiveOptions | DirectiveFunction
+  }
+
   const BootstrapVueDirective: {
     default: BvPlugin
+    directives?: BvDirectivesObject
     [key: string]: DirectiveOptions | DirectiveFunction
   }
 
