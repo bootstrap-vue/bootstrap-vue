230f2c212d94521d31af73a23103d5fc3b28fe4c
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 18:54:26 -0300

    Create link.d.ts

---
+++ types/components/link.d.ts
@@ -0,0 +1,13 @@
+//
+// Link
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Link: Link
+export default Link
+export interface Link extends BvPlugin {}
+
+// Component: b-link
+export interface BLink extends Vue {}
