6feb910f9dc2cb9bfb01c0f3a2b110c5476af933
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:37:04 -0300

    Update index.d.ts

--- types/components/index.d.ts
+++ types/components/index.d.ts
@@ -1,7 +1,11 @@
 export * from './alert'
 export * from './badge'
 export * from './breadcrumb'
+export * from './button'
 
 export * from './modal'
+
+export * from './spinner'
+
 export * from './table'
 export * from './toast'
