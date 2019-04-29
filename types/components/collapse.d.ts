7227d4dc904403ce748d206b181923d6be61752e
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:56:26 -0300

    Create collapse.d.ts

---
+++ types/components/collapse.d.ts
@@ -0,0 +1,13 @@
+//
+// Collapse
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Collapse: Collapse
+export default Collapse
+export interface Collapse extends BvPlugin {}
+
+// Component: b-collapse
+export interface BCollapse extends Vue {}
