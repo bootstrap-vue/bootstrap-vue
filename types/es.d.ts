06f01785c97be1340913e07bdfc26356506b691b
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 14:17:44 -0300

    Update es.d.ts

--- types/es.d.ts
+++ types/es.d.ts
@@ -1,25 +1,41 @@
 //
 // ES build directory
 //
+declare module 'bootstrap-vue/es' {
+  import { VueConstructor, Component, AsyncComponent, ComponentOptions } from 'vue'
+  import { BvPlugin } from './bv-plugin'
+
+  interface BvPluginsObject {
+    [key: string]?: BvPlugin
+  }
+
+  const BootstrapVueComponent: {
+    default: BvPlugin
+    [key: string]: BvPluginsObject
+  }
+
+  export = BootstrapVueComponent
+}
+
 declare module 'bootstrap-vue/es/components/*' {
-  import { VueConstructor } from 'vue'
+  import { VueConstructor, Component, AsyncComponent, ComponentOptions } from 'vue'
   import { BvPlugin } from './bv-plugin'
 
   const BootstrapVueComponent: {
     default: BvPlugin
-    [key: string]: VueConstructor
+    [key: string]: VueConstructor | Component | AsyncComponent | ComponentOptions
   }
 
   export = BootstrapVueComponent
 }
 
 declare module 'bootstrap-vue/es/directives' {
-  import { DirectiveOptions, PluginFunction } from 'vue'
+  import { DirectiveOptions, DirectiveFunction, PluginFunction } from 'vue'
   import { BvPlugin } from './bv-plugin'
 
   const BootstrapVueDirective: {
     default: BvPlugin
-    [key: string]: DirectiveOptions
+    [key: string]: DirectiveOptions | DirectiveFunction
   }
 
   export = BootstrapVueDirective
