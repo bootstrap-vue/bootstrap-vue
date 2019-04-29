5d417e0f6206a38c68948718e2dc8b98a5bb26ca
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:38:25 -0300

    Create button.d.ts

---
+++ types/components/button.d.ts
@@ -0,0 +1,16 @@
+//
+// Buttons
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Button: Button
+export default Button
+export interface Button extends BvPlugin {}
+
+// Component: b-button
+export interface BButton extends Vue {}
+
+// Component: b-button-close
+export interface BButtonClose extends Vue {}
