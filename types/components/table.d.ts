38eb5b59102b630920c8f7289452a9c8258a2aa2
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:22:53 -0300

    Update table.d.ts

--- types/components/table.d.ts
+++ types/components/table.d.ts
@@ -4,20 +4,7 @@
 import Vue, { VNode } from 'vue'
 import { BvPlugin } from '../bv-plugin'
 
-// Modal Plugin
-declare const TablePlugin: Table
-export default Table
-export interface TablePlugin extends BvPlugin {}
-
-// Component: b-modal
-export interface BTable extends Vue {
-  // Public methods
-  refresh: () => void
-  // Props
-  fields?: BTableFieldObject | BTableFieldArray
-}
-
-type BTableVariant =
+type BvTableVariant =
   | 'active'
   | 'success'
   | 'info'
@@ -28,28 +15,42 @@ type BTableVariant =
   | 'light'
   | 'dark'
 
-type BTableSortDirection = 'asc' | 'desc' | 'last'
+type BvTableSortDirection = 'asc' | 'desc' | 'last'
 
-type BTableFormatterCallback = ((value: any, key: string, item: any) => any)
+type BvTableFormatterCallback = ((value: any, key: string, item: any) => any)
 
-export interface BTableField {
+// Modal Plugin
+declare const Table: Table
+export default Table
+export interface Table extends BvPlugin {}
+export { Table }
+
+// Component: b-modal
+export interface BTable extends Vue {
+  // Public methods
+  refresh: () => void
+  // Props
+  fields?: BvTableFieldObject | BvTableFieldArray
+}
+
+export interface BvTableField {
   label?: string
   headerTitle?: string
   headerAbbr?: string
   class?: string | string[]
-  formatter?: string | BTableFormatterCallback
+  formatter?: string | BvTableFormatterCallback
   sortable?: boolean
   sortDirection?: SortDirection
-  tdClass?: string | string[] | BTableFormatterCallback
+  tdClass?: string | string[] | BvTableFormatterCallback
   thClass?: string | string[]
   thStyle?: any
-  variant?: BTableVariant | string
+  variant?: BvTableVariant | string
   tdAttr?: any | ((value: any, key: string, item: any) => any)
   isRowHeader?: boolean
 }
 
-export interface BTableFieldObject {
-  [key: string]: BTableField
+export interface BvTableFieldObject {
+  [key: string]: BvTableField
 }
 
-export type BTableFieldArray = Array<string | ({ key: string } & BTableField)>
+export type BvTableFieldArray = Array<string | ({ key: string } & BvTableField)>
