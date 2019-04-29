64da6cf63518746e105a1c7cbe69e2fa8253982e
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 15:57:13 -0300

    Update es.d.ts

--- types/es.d.ts
+++ types/es.d.ts
@@ -22,24 +22,21 @@ declare module 'bootstrap-vue/es/components/*' {
   import { VueConstructor, Component, AsyncComponent, ComponentOptions } from 'vue'
   import { BvPlugin } from './bv-plugin'
 
-  interface BvComponentsObject {
-    [key: string]?: Component | AsyncComponent | ComponentOptions
+  const BootstrapVueComponentPlugin: {
+    default: BvPlugin
+    [key: string]: any
   }
 
-  interface BvDirectivesObject {
-    [key: string]?: DirectiveOptions | DirectiveFunction
-  }
+  export = BootstrapVueComponentPlugin
+}
 
-  interface BvPluginsObject {
-    [key: string]?: BvPlugin
-  }
+declare module 'bootstrap-vue/es/components/*/*' {
+  import { VueConstructor, Component, AsyncComponent, ComponentOptions } from 'vue'
+  import { BvPlugin } from './bv-plugin'
 
   const BootstrapVueComponent: {
-    default: BvPlugin
-    components?: BvComponentsObject
-    directives?: BvDirectivesObject
-    plugins?: BvPluginsObject
-    [key: string]: VueConstructor | Component | AsyncComponent | ComponentOptions
+    default: VueConstructor | Component | AsyncComponent | ComponentOptions | BvPlugin
+    [key: string]: any
   }
 
   export = BootstrapVueComponent
@@ -49,14 +46,21 @@ declare module 'bootstrap-vue/es/directives/*' {
   import { DirectiveOptions, DirectiveFunction, PluginFunction } from 'vue'
   import { BvPlugin } from './bv-plugin'
 
-  interface BvDirectivesObject {
-    [key: string]?: DirectiveOptions | DirectiveFunction
+  const BootstrapVueDirectivePlugin: {
+    default: BvPlugin
+    [key: string]: any
   }
 
+  export = BootstrapVueDirectivePlugin
+}
+
+declare module 'bootstrap-vue/es/directives/*/*' {
+  import { DirectiveOptions, DirectiveFunction, PluginFunction } from 'vue'
+  import { BvPlugin } from './bv-plugin'
+
   const BootstrapVueDirective: {
-    default: BvPlugin
-    directives?: BvDirectivesObject
-    [key: string]: DirectiveOptions | DirectiveFunction
+    default: DirectiveOptions | DirectiveFunction | BvPlugin
+    [key: string]: any
   }
 
   export = BootstrapVueDirective
