5487f5e62cddbcd86e9bc7273e5d31c928d76c76
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 19:11:33 -0300

    Create tooltip.d.ts

---
+++ types/components/tooltip.d.ts
@@ -0,0 +1,13 @@
+//
+// Tooltip
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Tooltip: Tooltip
+export default Tooltip
+export interface Tooltip extends BvPlugin {}
+
+// Component: b-tooltip
+export interface BTooltip extends Vue {}
