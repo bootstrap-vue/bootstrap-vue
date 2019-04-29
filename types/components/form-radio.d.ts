1406f5c166d1034df603ce9b626a86772f854db0
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 18:18:16 -0300

    Create form-radio.d.ts

---
+++ types/components/form-radio.d.ts
@@ -0,0 +1,16 @@
+//
+// Form Radio
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const FormRadio: FormRadio
+export default FormRadio
+export interface FormRadio extends BvPlugin {}
+
+// Component: b-form-radio
+export interface BFormRadio extends Vue {}
+
+// Component: b-form-radio-group
+export interface BFormRadioGroup extends Vue {}
