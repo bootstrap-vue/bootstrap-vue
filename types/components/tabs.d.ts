2c268268be30fcdd558d217b98a4460a47b793de
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 19:15:54 -0300

    Create tabs.d.ts

---
+++ types/components/tabs.d.ts
@@ -0,0 +1,16 @@
+//
+// Tabs
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Tabs: Tabs
+export default Tabs
+export interface Tabs extends BvPlugin {}
+
+// Component: b-tabs
+export interface BTabs extends Vue {}
+
+// Component: b-tab
+export interface BTab extends Vue {}
