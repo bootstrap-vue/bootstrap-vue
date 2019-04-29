4d6bfc54bb8e742c6ac968673e487f5d3fe13e5f
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 19:04:56 -0300

    Create navbar.d.ts

---
+++ types/components/navbar.d.ts
@@ -0,0 +1,22 @@
+//
+// Navbar
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Navbar: Navbar
+export default Navbar
+export interface Navbar extends BvPlugin {}
+
+// Component: b-navbar
+export interface BNavbar extends Vue {}
+
+// Component: b-navbar-brand
+export interface BNavbarBrand extends Vue {}
+
+// Component: b-navbar-nav
+export interface BNavbarNav extends Vue {}
+
+// Component: b-navbar-toggle
+export interface BNavbarToggle extends Vue {}
