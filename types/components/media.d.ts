e90ca033582824c495185ed9f85666f9bc0ff0ef
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 18:58:22 -0300

    Create media.d.ts

---
+++ types/components/media.d.ts
@@ -0,0 +1,19 @@
+//
+// Media
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Media: Media
+export default Media
+export interface Media extends BvPlugin {}
+
+// Component: b-media
+export interface BMedia extends Vue {}
+
+// Component: b-media-aside
+export interface BMediaAside extends Vue {}
+
+// Component: b-media-body
+export interface BMediaBody extends Vue {}
