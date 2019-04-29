dbe1cb1f1e73d340453050ae8e7f369373fecf6f
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 18:49:52 -0300

    Create layout.d.ts

---
+++ types/components/layout.d.ts
@@ -0,0 +1,22 @@
+//
+// Layout
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Layout: Layout
+export default Layout
+export interface Layoutt extends BvPlugin {}
+
+// Component: b-container
+export interface BContainer extends Vue {}
+
+// Component: b-row
+export interface BRow extends Vue {}
+
+// Component: b-container
+export interface BCol extends Vue {}
+
+// Component: b-form-row
+export interface BFormRow extends Vue {}
