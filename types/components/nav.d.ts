d9886be44194534e97bc0da208df6b66a5dc353c
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 19:02:01 -0300

    Create nav.d.ts

---
+++ types/components/nav.d.ts
@@ -0,0 +1,25 @@
+//
+// Nav
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Nav: Alert
+export default Nav
+export interface Nav extends BvPlugin {}
+
+// Component: b-nav
+export interface BNav extends Vue {}
+
+// Component: b-nav-form
+export interface BNavForm extends Vue {}
+
+// Component: b-nav-item
+export interface BNavItem extends Vue {}
+
+// Component: b-nav-item-dropdown
+export interface BNavItemDropdown extends Vue {}
+
+// Component: b-nav-text
+export interface BNavText extends Vue {}
