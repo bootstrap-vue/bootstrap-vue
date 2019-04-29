13c9ec2c74bbdadfd1cd04f374fc83f841afe441
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 13:29:34 -0300

    Update bootstrap-vue.d.ts

--- types/bootstrap-vue.d.ts
+++ types/bootstrap-vue.d.ts
@@ -2,23 +2,11 @@ import Vue, { PluginFunction, PluginObject } from 'vue'
 
 import { BvEvent } from "./bv-event"
 import { BModalComponent, BvModalEvent } from './components/modal'
-import { BToastComponent } from './components/toast'
-import { BToasterComponent } from './components/toaster'
+import { BToastComponent, BToasterComponent } from './components/toast'
 
 export { BvEvent, BvModalEvent }
 
-export interface ComponentOptions {
-  [key: string]?: any
-}
-
-export interface InstallOptions {
-  breakpoints?: string[]
-  [key: string]?: ComponentOptions
-}
-
-export function install (vue: typeof Vue, options: InstallOptions): void
-
-// TypeScrip cannot merge imported class with namespace, so declare subclasses instead
+// TypeScript cannot merge imported class with namespace, so declare subclasses instead
 
 export class BModal extends BModalComponent {}
 export class BToast extends BToastComponent {}
