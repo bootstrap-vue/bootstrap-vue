970bd082e5ba8755d7bfcb7d94997b8a6f4d5096
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 18:22:55 -0300

    Create form-input.d.ts

---
+++ types/components/form-input.d.ts
@@ -0,0 +1,13 @@
+//
+// Form Input
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const FormInput: FormInput
+export default FormInput
+export interface FormInput extends BvPlugin {}
+
+// Component: b-form-input
+export interface BFormInput extends Vue {}
