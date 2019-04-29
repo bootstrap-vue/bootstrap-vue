918a5fbd7d0ff106022d4d997696d12ec29abccd
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:51:14 -0300

    Create carousel.d.ts

---
+++ types/components/carousel.d.ts
@@ -0,0 +1,23 @@
+//
+// Carousel
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Carousel: Carousel
+export default Carousel
+export interface Carousel extends BvPlugin {}
+
+// Component: b-carousel
+export interface BCarousel extends Vue {
+  setSlide: (slide: number) => void
+  prev: () => void
+  next: () => void
+  start: () => void
+  pause: () => void
+  restart: () => void
+}
+
+// Component: b-carousel-slide
+export interface BCarouselSlide extends Vue {}
