cc2dd6e2c0da55ab6c6c39b873c5c56043e0539f
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:42:13 -0300

    Create button-toolbar.d.ts

---
+++ types/components/button-toolbar.d.ts
@@ -0,0 +1,13 @@
+//
+// Button Toolbar
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const ButtonToolbar: ButtonToolbar
+export default ButtonToolbar
+export interface ButtonToolbar extends BvPlugin {}
+
+// Component: b-button-toolbar
+export interface BButtonToolbar extends Vue {}
