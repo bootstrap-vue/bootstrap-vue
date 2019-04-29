245f868e9036e79b09422489edd31e0ce0081e02
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:34:14 -0300

    Update alert.d.ts

--- types/components/alert.d.ts
+++ types/components/alert.d.ts
@@ -8,7 +8,6 @@ import { BvPlugin } from '../bv-plugin'
 declare const Alert: Alert
 export default Alert
 export interface Alert extends BvPlugin {}
-export { Alert }
 
 // Component: b-alert
 export interface BAlert extends Vue {}
