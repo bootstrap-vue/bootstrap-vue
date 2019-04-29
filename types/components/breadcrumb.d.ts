7e5e02cef92ad9896f129d80ff6f6ff74970cf85
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:32:14 -0300

    Create breadcrumb.d.ts

---
+++ types/components/breadcrumb.d.ts
@@ -0,0 +1,20 @@
+//
+// Breadcrumb
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Breadcrumb: Breadcrumb
+export default Breadcrumb
+export interface Breadcrumb extends BvPlugin {}
+export { Breadcrumb }
+
+// Component: b-breadcrumb
+export interface BBreadcrumb extends Vue {}
+
+// Component: b-breadcrumb-item
+export interface BBreadcrumbItem extends Vue {}
+
+// Component: b-breadcrumb-link
+export interface BBreadcrumbLink extends Vue {}
