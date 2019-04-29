d3c9dbd519d31dd2f579324c9690fade95dd07eb
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:40:44 -0300

    Create button-group.d.ts

---
+++ types/components/button-group.d.ts
@@ -0,0 +1,13 @@
+//
+// Button Group
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const ButtonGroup: ButtonGroup
+export default ButtonGroup
+export interface ButtonGroup extends BvPlugin {}
+
+// Component: b-button-group
+export interface BButtonGroup extends Vue {}
