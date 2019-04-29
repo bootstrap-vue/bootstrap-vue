b8b3ec9bdc8cbffc170230977d7a3212167d6ee7
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 15:13:03 -0300

    Update bootstrap-vue.d.ts

--- types/bootstrap-vue.d.ts
+++ types/bootstrap-vue.d.ts
@@ -2,19 +2,26 @@ declare module 'bootstrap-vue' {
   import { Component, AsyncComponent, DirectiveOptions } from 'vue'
   import { BvPlugin } from './bv-plugin'
 
+  import Modal, { BModalComponent as BModal } from './components/modal'
+  import Toast, { BToastComponent as BToast, BToasterComponent as BToaster } from './components/modal'
+
   export const Alert: BvPlugin
   export const BAlert: Component | AsyncComponent
 
   export const Badge: BvPlugin
   export const BBadge: Component | AsyncComponent
 
-  export const Modal: BvPlugin
-  export const BModal: Component | AsyncComponent
+  export {
+    Modal,
+    BModal
+  }
 
   export const Spinner: BvPlugin
   export const BSpinner: Component | AsyncComponent
 
-  export const Toast: BvPlugin
-  export const BToast: Component | AsyncComponent
-  export const BToaster: Component | AsyncComponent
+  export {
+    Toast,
+    BToast,
+    BToaster
+  }
 }
