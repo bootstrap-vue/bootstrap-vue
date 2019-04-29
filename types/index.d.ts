71e0aeb3fd041e70c4650a0a4fb1bf7ebc77e7cc
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 13:18:16 -0300

    Update index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,7 +2,6 @@ import Vue, { PluginFunction, PluginObject } from 'vue'
 import { BvPlugin } from './bv-plugin'
 import './vue-injections'
 
-
 declare const BootstrapVue: BootstrapVue
 export default BootstrapVue
 export interface BootstrapVue extends BvPlugin {}
