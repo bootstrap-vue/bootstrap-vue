3b37ee2eff00a1b06a1caccffeaf3d0151b61fff
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 18:21:56 -0300

    Create form-group.d.ts

---
+++ types/components/form-group.d.ts
@@ -0,0 +1,13 @@
+//
+// Form Group
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const FormGroup: FormGroup
+export default FormGroup
+export interface FormGroup extends BvPlugin {}
+
+// Component: b-form-group
+export interface BFormGroup extends Vue {}
