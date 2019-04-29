7eef36ebec3bfe11c0ace160195bfb8dffa2196c
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 18:47:05 -0300

    Create jumbotron.d.ts

---
+++ types/components/jumbotron.d.ts
@@ -0,0 +1,13 @@
+//
+// Jumbotron
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Jumbotron: Jumbotron
+export default Jumbotron
+export interface Jumbotron extends BvPlugin {}
+
+// Component: b-jumbotron
+export interface BJumbotron extends Vue {}
