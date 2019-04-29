7c5a7534293132f651d80526388159971a0b2077
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:35:02 -0300

    Update modal.d.ts

--- types/components/modal.d.ts
+++ types/components/modal.d.ts
@@ -9,7 +9,6 @@ import { BvPlugin } from '../bv-plugin'
 declare const Modal: Modal
 export default Modal
 export interface Modal extends BvPlugin {}
-export { Modal }
 
 // Component: b-modal
 export interface BModal extends Vue {
