ed08e8812d60ef180d5929e60585b6e710fb3d34
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 17:34:03 -0300

    Create spinner.d.ts

---
+++ types/components/spinner.d.ts
@@ -0,0 +1,13 @@
+//
+// Spinner
+//
+import Vue from 'vue'
+import { BvPlugin } from '../bv-plugin'
+
+// Plugin
+declare const Spinner: Spinner
+export default Spinner
+export interface Spinner extends BvPlugin {}
+
+// Component: b-alert
+export interface BAlert extends Vue {}
