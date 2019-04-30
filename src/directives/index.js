97639bac7f75b8cabd377262a24f014258382cc9
T Morehouse <troymore@nbnet.nb.ca>
2019-04-30 17:44:09 -0300

    Update index.js

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,7 +1,19 @@
-import TogglePlugin from './toggle'
-import ModalPlugin from './modal'
-import ScrollspyPlugin from './scrollspy'
-import TooltipPlugin from './tooltip'
-import PopoverPlugin from './popover'
+import { installFactory } from '../utils/plugins'
 
-export { TogglePlugin, ModalPlugin, ScrollspyPlugin, TooltipPlugin, PopoverPlugin }
+// Export a named eport 'plugins' which is an object of all plugins
+// Note this is may not be tree shake-able if user imports plugins from
+// this named export.
+import * as plugins from './plugins'
+export { plugins }
+
+// Named exports of all directives
+export * from './toggle'
+export * from './modal'
+export * from './scrollspy'
+export * from './tooltip'
+export * from './popover'
+
+// Default export is a plugin that installs all plugins
+export default {
+  install: installFactory({ plugins })
+}
