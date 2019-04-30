ba75d03768f8488209a999a601e4ebf53e6f8f57
T Morehouse <troymore@nbnet.nb.ca>
2019-04-30 18:43:55 -0300

    Update index.js

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,10 +1,6 @@
 import { installFactory } from '../utils/plugins'
+import * as directivePlugins from './plugins'
 
-// Export a named eport 'plugins' which is an object of all plugins
-// Note this is may not be tree shake-able if user imports plugins from
-// this named export.
-import * as plugins from './plugins'
-export { plugins }
 
 // Named exports of all directives
 export * from './toggle'
@@ -15,5 +11,5 @@ export * from './popover'
 
 // Default export is a plugin that installs all plugins
 export default {
-  install: installFactory({ plugins })
+  install: installFactory({ plugins: directivePlugins })
 }
