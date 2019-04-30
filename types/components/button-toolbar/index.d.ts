958c77865a386e8c1218a4ef3310c8996a6449e4
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 21:59:45 -0300

    Update and rename types/components/button-toolbar.d.ts to types/components/button-toolbar/index.d.ts

--- types/components/button-toolbar/index.d.ts
+++ types/components/button-toolbar/index.d.ts
@@ -2,7 +2,7 @@
 // Button Toolbar
 //
 import Vue from 'vue'
-import { BvPlugin } from '../bv-plugin'
+import { BvPlugin } from '../../bv-plugin'
 
 // Plugin
 declare const ButtonToolbar: ButtonToolbar
