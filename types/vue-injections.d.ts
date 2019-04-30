43ff8c05795707192a633b94c45d8666cc7e640f
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 22:18:18 -0300

    Update vue-injections.d.ts

--- types/vue-injections.d.ts
+++ types/vue-injections.d.ts
@@ -1,11 +1,11 @@
 /**
  * Augment the typings of Vue.js
  */
-import Vue from "vue"
-import { BvModal } from "./components/modal"
-import { BvToast } from "./components/toast"
+import Vue from 'vue'
+import { BvModal } from './components/modal'
+import { BvToast } from './components/toast'
 
-declare module "vue/types/vue" {
+declare module 'vue/types/vue' {
   interface Vue {
     $BvModal: BvModal
     $bvToast: BvToast
