7176617e4f8fb9e7dc8c6e356b474b59b62b1c4e
T Morehouse <troymore@nbnet.nb.ca>
2019-04-29 22:44:39 -0300

    Update index.js

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -17,14 +17,7 @@ const components = {
   BFormValidFeedback
 }
 
-export {
-  BForm,
-  BFormDatalist,
-  BFormRow,
-  BFormText,
-  BFormInvalidFeedback,
-  BFormValidFeedback
-}
+export { BForm, BFormDatalist, BFormRow, BFormText, BFormInvalidFeedback, BFormValidFeedback }
 
 export default {
   install: installFactory({ components })
