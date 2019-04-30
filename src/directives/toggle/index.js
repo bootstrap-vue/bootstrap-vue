dd6062004cae31220572a8d9d7d0b3927928ad38
T Morehouse <troymore@nbnet.nb.ca>
2019-04-30 18:54:06 -0300

    Update index.js

--- src/directives/toggle/index.js
+++ src/directives/toggle/index.js
@@ -1,11 +1,11 @@
-import BToggleDirective from './toggle'
+import VBToggleDirective from './toggle'
 import { installFactory } from '../../utils/plugins'
 
 const directives = {
-  BToggle: BToggleDirective
+  BToggle: VBToggleDirective
 }
 
-export { BToggleDirective as BToggle }
+export { VBToggleDirective as VBToggle }
 
 export default {
   install: installFactory({ directives })
