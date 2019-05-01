77bf5f94b246f307d3e6e36e51eeb89cfd5d6f06
T Morehouse <troymore@nbnet.nb.ca>
2019-05-01 15:54:20 -0300

    Create index.d.ts

---
+++ types/directives/index.d.ts
@@ -0,0 +1,19 @@
+import { BvPlugin } from ../bv-plugin'
+
+// Default export is a plugin that installs all plugins
+declare const BDirectivesPlugin: BDirectivesPlugin
+export default BDirectivesPlugin
+export interface BDirectivesPlugin extends BvPlugin {}
+
+// Export all directive group plugins as named exports
+export * from './plugins'
+
+// Export all legacy named directive group plugins as named exports
+export * from './plugins-legacy'
+
+// Named exports of all directives
+export * from './toggle'
+export * from './modal'
+export * from './scrollspy'
+export * from './tooltip'
+export * from './popover'
