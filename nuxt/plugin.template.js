dcbe3078a653d27886ecd07a59981015efd75abc
T Morehouse <troymore@nbnet.nb.ca>
2019-04-30 23:25:20 -0300

    Update plugin.template.js
    add support for individual components and directives

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
-<% if (options.componentPlugins.length || options.directivePlugins.length) { %><%=
+<% if (
+  options.componentPlugins.length ||
+  options.directivePlugins.length ||
+  options.components.length ||
+  options.directives.length
+) { %><%=
 options.componentPlugins.reduce((acc, p) => (acc += `import { ${p} } from 'bootstrap-vue/${options.dist}/components'\n` ), '') %><%=
-options.directivePlugins.reduce((acc, p) => (acc += `import { ${p} } from 'bootstrap-vue/${options.dist}/directives'\n` ), '') %>
+options.directivePlugins.reduce((acc, p) => (acc += `import { ${p} } from 'bootstrap-vue/${options.dist}/directives'\n` ), '') %><%=
+options.components.reduce((acc, c) => (acc += `import { ${c} } from 'bootstrap-vue/${options.dist}/components'\n` ), '') %><%=
+options.directives.reduce((acc, d) => (acc += `import { ${d} } from 'bootstrap-vue/${options.dist}/directives'\n` ), '') %>
 
 <% if (options.config) { %>
 import BVConfigPlugin from 'bootstrap-vue/<%= options.dist %>/bv-config'
@@ -11,7 +18,10 @@ Vue.use(BVConfigPlugin, <%= JSON.stringify(options.config, undefined, 2) %>)
 
 <%=
 options.componentPlugins.reduce((acc, p) => (acc += `Vue.use(${p})\n` ), '') %><%=
-options.directivePlugins.reduce((acc, p) => (acc += `Vue.use(${p})\n` ), '') %>
+options.directivePlugins.reduce((acc, p) => (acc += `Vue.use(${p})\n` ), '') %><%=
+options.components.reduce((acc, c) => (acc += `Vue.component('${c}', ${c})\n` ), '') %><%=
+options.directives.reduce((acc, d) => (acc += `Vue.directive('${d.replace(/^VB/, 'B')}', ${d})\n` ), '') %>
+
 <% } else { %>
 import BootstrapVue from 'bootstrap-vue/<%= options.dist %>'
 
