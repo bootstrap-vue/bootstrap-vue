c92bd132e6e3d61e86c99383ce95cf45e1cc7062
T Morehouse <troymore@nbnet.nb.ca>
2019-05-01 15:49:46 -0300

    Update scrollspy.d.ts

--- types/directives/scrollspy.d.ts
+++ types/directives/scrollspy.d.ts
@@ -9,5 +9,5 @@ declare const VBScrollspyPlugin: VBScrollspyPlugin
 export default VBScrollspyPlugin
 export interface VBScrollspyPlugin extends BvPlugin {}
 
-// directive: v-b-modal
+// directive: v-b-scrollspy
 export interface VBScrollspy extends DirectiveOptions {}
