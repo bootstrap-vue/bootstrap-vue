f09545ec75755c28d62838ddd795e726ca863885
T Morehouse <troymore@nbnet.nb.ca>
2019-05-01 15:09:07 -0300

    Update index.d.ts

--- types/components/form-radio/index.d.ts
+++ types/components/form-radio/index.d.ts
@@ -5,9 +5,9 @@ import Vue from 'vue'
 import { BvPlugin } from '../../bv-plugin'
 
 // Plugin
-declare const FormRadio: FormRadio
-export default FormRadio
-export interface FormRadio extends BvPlugin {}
+declare const FormRadioPlugin: FormRadioPlugin
+export default FormRadioPlugin
+export interface FormRadioPlugin extends BvPlugin {}
 
 // Component: b-form-radio
 export interface BFormRadio extends Vue {}
