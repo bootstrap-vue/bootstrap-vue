c6c8de512ae520577a17e5d736a6b29a074816c2
T Morehouse <troymore@nbnet.nb.ca>
2019-05-01 15:03:40 -0300

    Update index.d.ts

--- types/components/button/index.d.ts
+++ types/components/button/index.d.ts
@@ -5,9 +5,9 @@ import Vue from 'vue'
 import { BvPlugin } from '../../bv-plugin'
 
 // Plugin
-declare const Button: Button
-export default Button
-export interface Button extends BvPlugin {}
+declare const ButtonPlugin: ButtonPlugin
+export default ButtonPlugin
+export interface ButtonPlugin extends BvPlugin {}
 
 // Component: b-button
 export interface BButton extends Vue {}
