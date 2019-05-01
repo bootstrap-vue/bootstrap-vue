79ee26005392e4ad4775d50bc3a746408683abef
T Morehouse <troymore@nbnet.nb.ca>
2019-05-01 15:10:12 -0300

    Update index.d.ts

--- types/components/form-textarea/index.d.ts
+++ types/components/form-textarea/index.d.ts
@@ -5,9 +5,9 @@ import Vue from 'vue'
 import { BvPlugin } from '../../bv-plugin'
 
 // Plugin
-declare const FormTextarea: FormTextarea
-export default FormTextarea
-export interface FormTextarea extends BvPlugin {}
+declare const FormTextareaPlugin: FormTextareaPlugin
+export default FormTextareaPlugin
+export interface FormTextareaPlugin extends BvPlugin {}
 
 // Component: b-form-textarea
 export interface BFormTextarea extends Vue {
