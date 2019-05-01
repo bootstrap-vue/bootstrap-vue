ba508170031125a6c7eb5c61f80f03e4c5c8c8de
T Morehouse <troymore@nbnet.nb.ca>
2019-05-01 15:08:37 -0300

    Update index.d.ts

--- types/components/form-input/index.d.ts
+++ types/components/form-input/index.d.ts
@@ -5,9 +5,9 @@ import Vue from 'vue'
 import { BvPlugin } from '../../bv-plugin'
 
 // Plugin
-declare const FormInput: FormInput
-export default FormInput
-export interface FormInput extends BvPlugin {}
+declare const FormInputPlugin: FormInputPlugin
+export default FormInputPlugin
+export interface FormInputPlugin extends BvPlugin {}
 
 // Component: b-form-input
 export interface BFormInput extends Vue {
