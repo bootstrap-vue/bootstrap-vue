ab64c94c8c9ab77cf8ade67f4d6d14f33fd96cc5
T Morehouse <troymore@nbnet.nb.ca>
2019-04-30 21:14:02 -0300

    Update importdoc.vue

--- docs/components/importdoc.vue
+++ docs/components/importdoc.vue
@@ -164,7 +164,7 @@ export default {
       const pluginLocation = this.isComponentRoute ? 'components' : 'directives'
       return [
         '// Importing the named export',
-        `import { ${this.pluginName} } from 'bootstrap-vue/es/${pluginLocation}`,
+        `import { ${this.pluginName} } from 'bootstrap-vue/es/${pluginLocation}'`,
         `Vue.use(${this.pluginName})`,
         '',
         '// Or importing the default export (legacy)',
