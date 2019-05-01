a1b0a1b78e820f10e168dfed9e25a0942328f24d
T Morehouse <troymore@nbnet.nb.ca>
2019-04-30 22:07:34 -0300

    Update plugin.template.js

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 <% if (options.componentPlugins.length || options.directivePlugins.length) { %><%=
-options.componentPlugins.reduce((acc, p) => (acc += `import ${p[0]} from 'bootstrap-vue/${options.dist}/components/${p[1]}'\n` ), '') %><%=
-options.directivePlugins.reduce((acc, p) => (acc += `import ${p[0]} from 'bootstrap-vue/${options.dist}/directives/${p[1]}'\n` ), '') %>
+options.componentPlugins.reduce((acc, p) => (acc += `import { ${p} } from 'bootstrap-vue/${options.dist}/components'\n` ), '') %><%=
+options.directivePlugins.reduce((acc, p) => (acc += `import { ${p} } from 'bootstrap-vue/${options.dist}/directives'\n` ), '') %>
 
 <% if (options.config) { %>
 import BVConfigPlugin from 'bootstrap-vue/<%= options.dist %>/bv-config'
@@ -10,8 +10,8 @@ Vue.use(BVConfigPlugin, <%= JSON.stringify(options.config, undefined, 2) %>)
 <% } %>
 
 <%=
-options.componentPlugins.reduce((acc, p) => (acc += `Vue.use(${p[0]})\n` ), '') %><%=
-options.directivePlugins.reduce((acc, p) => (acc += `Vue.use(${p[0]})\n` ), '') %>
+options.componentPlugins.reduce((acc, p) => (acc += `Vue.use(${p})\n` ), '') %><%=
+options.directivePlugins.reduce((acc, p) => (acc += `Vue.use(${p})\n` ), '') %>
 <% } else { %>
 import BootstrapVue from 'bootstrap-vue/<%= options.dist %>'
 
