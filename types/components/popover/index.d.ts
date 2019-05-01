6b857bb4016e3847e947f7877923c79c1b18e7fe
T Morehouse <troymore@nbnet.nb.ca>
2019-05-01 15:17:04 -0300

    Update index.d.ts

--- types/components/popover/index.d.ts
+++ types/components/popover/index.d.ts
@@ -5,9 +5,9 @@ import Vue from 'vue'
 import { BvPlugin } from '../../bv-plugin'
 
 // Plugin
-declare const Popover: Popover
-export default Popover
-export interface Popover extends BvPlugin {}
+declare const PopoverPlugin: PopoverPlugin
+export default PopoverPlugin
+export interface PopoverPlugin extends BvPlugin {}
 
 // Component: b-popover
 export interface BPopover extends Vue {}
