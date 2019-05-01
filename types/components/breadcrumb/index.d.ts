ce3d2393e425228df40ef4e5cc559d3e54b31e6c
T Morehouse <troymore@nbnet.nb.ca>
2019-05-01 15:02:33 -0300

    Update index.d.ts

--- types/components/breadcrumb/index.d.ts
+++ types/components/breadcrumb/index.d.ts
@@ -5,9 +5,9 @@ import Vue from 'vue'
 import { BvPlugin } from '../../bv-plugin'
 
 // Plugin
-declare const Breadcrumb: Breadcrumb
-export default Breadcrumb
-export interface Breadcrumb extends BvPlugin {}
+declare const BreadcrumbPlugin: BreadcrumbPlugin
+export default BreadcrumbPlugin
+export interface BreadcrumbPlugin extends BvPlugin {}
 
 // Component: b-breadcrumb
 export interface BBreadcrumb extends Vue {}
