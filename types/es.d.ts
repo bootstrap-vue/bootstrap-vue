682cfcabe174e922c5ceed27c61df770cc7a33db
T Morehouse <troymore@nbnet.nb.ca>
2019-05-04 14:38:38 -0300

    Update es.d.ts

--- types/es.d.ts
+++ types/es.d.ts
@@ -11,91 +11,33 @@ import {
 } from 'vue'
 import { BvPlugin } from './bv-plugin'
 
+type ComponentOrPlugin = Component & BvPlugin
+
+type DirectiveOrPlugin = DirectiveOptions & BvPlugin
+
 declare module 'bootstrap-vue/es' {
-  type Cmp = VueConstructor | Component | ComponentOptions | AsyncComponent
-  type Dir = DirectiveOptions | DirectiveFunction
+  import BoostrapVue from 'bootstrap-vue'
 
-  const BootstrapVuePlugin: {
-    default: BvPlugin
-    [key: string]: Cmp | Dir | BvPlugin
-  }
-
-  export = BootstrapVuePlugin
-}
-
-// components/index.js file
-declare module 'bootstrap-vue/es/components' {
-  interface BvPluginsObject {
-    [key: string]: BvPlugin
-  }
-
-  const BootstrapVueComponentPlugin: {
-    // Default export is a plugin that installs all component plugins
-    default: BvPlugin
-    // plugins contains an object of all component group plugins
-    plugins: BvPluginsObject
-    // Named exports are components
-    [key: string]: VueConstructor | Component | ComponentOptions | AsyncComponent
-  }
-
-  export = BootstrapVueComponentPlugin
+  export default BootstrapVue
 }
 
 declare module 'bootstrap-vue/es/components/*' {
-  const BootstrapVueComponentPlugin: {
-    // Default export is a plugin
-    default: BvPlugin
-    // Named exports are cmponents
-    [key: string]: VueConstructor | Component | ComponentOptions | AsyncComponent
-  }
-
-  export = BootstrapVueComponentPlugin
-}
-
-declare module 'bootstrap-vue/es/components/*/*' {
   const BootstrapVueComponent: {
-    // Default export is a component
-    default: VueConstructor | Component | AsyncComponent | ComponentOptions | BvPlugin
-    // Other named exports (which shouldn't be used by users)
-    [key: string]: any
+    // Default export is a plugin that installs all component plugins
+    default: ComponentOrPlugin & VueConstructor
+    // Named exports are components
+    [key: string]: ComponentOrPlugin & VueConstructor
   }
 
   export = BootstrapVueComponent
 }
 
-// directives/index.js
-declare module 'bootstrap-vue/es/directives' {
-  interface BvPluginsObject {
-    [key: string]: BvPlugin
-  }
-
-  const BootstrapVueDirectivePlugin: {
-    // Default export is a plugin that installs all directive plugins
-    default: BvPlugin
-    // plugins export is an object of plugins
-    plugins: BvPluginsObject
-    // Named export(s) are individual directives
-    [key: string]: DirectiveOptions | DirectiveFunction
-  }
-
-  export = BootstrapVueDirectivePlugin
-}
-
 declare module 'bootstrap-vue/es/directives/*' {
-  const BootstrapVueDirectivePlugin: {
-    // Default export is the plugin
-    default: BvPlugin
-    // Named export(s) are individual directives
-    [key: string]: DirectiveOptions | DirectiveFunction
-  }
-
-  export = BootstrapVueDirectivePlugin
-}
-
-declare module 'bootstrap-vue/es/directives/*/*' {
   const BootstrapVueDirective: {
-    default: DirectiveOptions | DirectiveFunction | BvPlugin
-    [key: string]: any
+    // Default export is the plugin
+    default: DirectiveOrPlugin
+    // Named export(s) are individual directives
+    [key: string]: DirectiveOrPlugin
   }
 
   export = BootstrapVueDirective
