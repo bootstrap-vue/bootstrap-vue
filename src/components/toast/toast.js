5884c0fd5434f5427ec15a2fda13b113d840d5e2
T Morehouse <troymore@nbnet.nb.ca>
2019-05-03 22:33:14 -0300

    Update toast.js

--- src/components/toast/toast.js
+++ src/components/toast/toast.js
@@ -178,11 +178,11 @@ export default Vue.extend({
         this.$emit('change', newVal)
       }
     },
-    toaster(newVal) {
+    toaster(newVal) /* istanbul ignore next */ {
       // If toaster target changed, make sure toaster exists
       this.$nextTick(() => this.ensureToaster)
     },
-    static(newVal) {
+    static(newVal) /* istanbul ignore next */ {
       // If static changes to true, and the toast is showing,
       // ensure the toaster target exists
       if (newVal && this.localShow) {
@@ -212,6 +212,7 @@ export default Vue.extend({
       }
     })
     // Make sure we hide when toaster is destroyed
+    /* istanbul ignore next: difficult to test */ 
     this.listenOnRoot('bv::toaster::destroyed', toaster => {
       if (toaster === this.computedToaster) {
         this.hide()
