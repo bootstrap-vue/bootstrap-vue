20d4f0468a02fa92b7426d0d762739f5d92abfd6
T Morehouse <troymore@nbnet.nb.ca>
2019-05-04 13:26:54 -0300

    Update bv-plugin.d.ts

--- types/bv-plugin.d.ts
+++ types/bv-plugin.d.ts
@@ -2,12 +2,15 @@ import Vue, { PluginFunction, PluginObject } from 'vue'
 
 export type BvComponentOptionValue = string | string[] | number | number[] | boolean | object | null | any
 
+export type BvBreakpointsValue = string[]
+
 export interface BvComponentOptions {
   [key: string]: BvComponentOptionValue
 }
 
 export interface BvConfigOptions {
   [key: string]: BvComponentOptions
+  breakpoints?: BvBreakpointsValue
 }
 
 export interface BvPlugin {
