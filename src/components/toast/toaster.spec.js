2bc27946779032343c81ed243b48b51cc4389790
T Morehouse <troymore@nbnet.nb.ca>
2019-05-03 21:58:27 -0300

    Create toaster.spec.js

---
+++ src/components/toast/toaster.spec.js
@@ -0,0 +1,59 @@
+import { mount } from '@vue/test-utils'
+import BToaster from './toaster'
+
+describe('b-toaster', () => {
+  it ('has expected structure', async () => {
+    const wrapper = mount(Btoaster, {
+      attachToDocument: true,
+      propsData: {
+        name: 'foobar'
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('b-toast')
+    expect(wrapper.classes()).toContain('foobar')
+    expect(wrapper.classes().length).toBe(2)
+    expect(wrapper.attributes('id')).toBe('foobar')
+    expect(wrapper.find('.b-toaster-slot').exists()).toBe(true)
+    const $slot = wrapper.find('.b-toaster-slot')
+    expect($slot.is('div')).toBe(true)
+    expect($slot.classes()).toContain('b-toaster-slot')
+    expect($slot.classes()).toContain('vue-portal-target')
+    expect($slot.classes().length).toBe(2)
+    expect($slot.attributes('aria-live')).toEqual('polite')
+    expect($slot.attributes('aria-atomic')).toEqual('true')
+    expect($slot.attributes('role')).not.toBeDefined()
+    expect($slot.text()).toEqual('')
+
+    wrapper.destroy()
+  })
+
+  it ('accepts aria props', async () => {
+    const wrapper = mount(Btoaster, {
+      attachToDocument: true,
+      propsData: {
+        name: 'foobar',
+        ariaLive: 'assertive',
+        ariaAtomic: 'false',
+        role: 'alert'
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.find('.b-toaster-slot').exists()).toBe(true)
+    const $slot = wrapper.find('.b-toaster-slot')
+    expect($slot.is('div')).toBe(true)
+    expect($slot.classes()).toContain('b-toaster-slot')
+    expect($slot.classes()).toContain('vue-portal-target')
+    expect($slot.classes().length).toBe(2)
+    expect($slot.attributes('aria-live')).toEqual('assertive')
+    expect($slot.attributes('aria-atomic')).toEqual('false')
+    expect($slot.attributes('role')).toEqual('alert')
+    expect($slot.text()).toEqual('')
+
+    wrapper.destroy()
+  })
+})
