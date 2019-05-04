9b2e8fa2b7a267010479eb1c9f378741908905f1
T Morehouse <troymore@nbnet.nb.ca>
2019-05-04 12:57:40 -0300

    Update bv-plugin.d.ts

--- types/bv-plugin.d.ts
+++ types/bv-plugin.d.ts
@@ -9,28 +9,28 @@ import Vue, {
 } from 'vue'
 
 interface BvComponentsObject {
-  [key: string]?: Component | AsyncComponent | ComponentOptions
+  [key: string]: Component | AsyncComponent | ComponentOptions
 }
 
 interface BvDirectivesObject {
-  [key: string]?: DirectiveOptions | DirectiveFunction
+  [key: string]: DirectiveOptions | DirectiveFunction
 }
 
 interface BvPluginsObject {
-  [key: string]?: BvPlugin
+  [key: string]: BvPlugin
 }
 
 export interface BvComponentOptions {
-  [key: string]?: string | number | boolean | any
+  [key: string]: string | number | boolean | any
 }
 
-export interface BvInstallOptions {
+export interface BvConfigOptions {
   breakpoints?: string[]
   [key: string]?: BvComponentOptions
 }
 
 export interface BvPlugin {
-  install: PluginFunction<BvInstallOptions>
+  install: PluginFunction<BvConfigOptions>
   components?: BvComponentsObject
   directives?: BvDirectivesObject
   plugins?: BvPluginsObject
