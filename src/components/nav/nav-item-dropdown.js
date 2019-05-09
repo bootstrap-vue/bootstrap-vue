9d7d100f9563a0290f1a28cdbc56a9fa6b17d771
T Morehouse <troymore@nbnet.nb.ca>
2019-05-09 18:21:41 -0300

    Update nav-item-dropdown.js

--- src/components/nav/nav-item-dropdown.js
+++ src/components/nav/nav-item-dropdown.js
@@ -56,7 +56,7 @@ export default Vue.extend({
         this.extraToggleClasses, // Deprecated
         this.toggleClass,
         {
-          'disabled': this.disabled,
+          disabled: this.disabled,
           'dropdown-toggle-no-caret': this.noCaret
         }
       ]
