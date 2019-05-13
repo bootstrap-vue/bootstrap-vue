5c7252004945cf68b1d280d9894235f716893172
T Morehouse <troymore@nbnet.nb.ca>
2019-05-13 18:13:30 -0300

    chore(types): declare `/es` modules

--- types/es.d.ts
+++ types/es.d.ts
@@ -1,45 +1,31 @@
 //
 // ES build directory
 //
-// Note: this declaration file has issues and is not currently used
-//
-import {
-  VueConstructor,
-  Component,
-  AsyncComponent,
-  ComponentOptions,
-  DirectiveOptions,
-  DirectiveFunction
-} from 'vue'
-import { BvPlugin } from './bv-plugin'
-import { BoostrapVue } from './'
+// '/es' modules
 
-type ComponentOrPlugin = Component & BvPlugin
+declare module 'bootstrap-vue/es/components' {
+  export * from './components'
+}
 
-type DirectiveOrPlugin = DirectiveOptions & BvPlugin
+declare module 'bootstrap-vue/es/directives' {
+  export * from './directives'
+}
+
+declare module 'bootstrap-vue/es/bv-config' {
+  export * from './bv-config'
+}
 
 declare module 'bootstrap-vue/es' {
+  import { BvPlugin, BvConfigOptions } from './bv-plugin'
+
+  export * from './bv-config'
+  export * from './bv-event'
+  export * from './components'
+  export * from './directives'
+
+  declare const BootstrapVue: BootstrapVue
   export default BootstrapVue
-}
-
-declare module 'bootstrap-vue/es/components/*' {
-  const BootstrapVueComponent: {
-    // Default export is a plugin that installs all component plugins
-    default: ComponentOrPlugin & VueConstructor
-    // Named exports are components
-    [key: string]: ComponentOrPlugin & VueConstructor
+  export interface BootstrapVue extends BvPlugin {
+    setConfig: (config: BvConfigOptions) => void
   }
-
-  export = BootstrapVueComponent
-}
-
-declare module 'bootstrap-vue/es/directives/*' {
-  const BootstrapVueDirective: {
-    // Default export is the plugin
-    default: DirectiveOrPlugin
-    // Named export(s) are individual directives
-    [key: string]: DirectiveOrPlugin
-  }
-
-  export = BootstrapVueDirective
 }
