cbe92b3d1283c09bccccba46b85ff6288cb2ed5f
T Morehouse <troymore@nbnet.nb.ca>
2019-05-13 13:33:02 -0300

    Update img-lazy.spec.js

--- src/components/image/img-lazy.spec.js
+++ src/components/image/img-lazy.spec.js
@@ -58,36 +58,62 @@ describe('img-lazy', () => {
     wrapper.destroy()
   })
 
-  it('triggers check on resize event event', async () => {
-    const src = 'https://picsum.photos/1024/400/?image=41'
-    const wrapper = mount(BImgLazy, {
-      attachToDocument: true,
-      propsData: {
-        src: src
-      }
-    })
-    expect(wrapper.is('img')).toBe(true)
+  // These tests are wrapped in a new describe to limit the scope of the getBCR Mock
+  describe('scroll events', () => {
+    const origGetBCR = Element.prototype.getBoundingClientRect
 
-    expect(wrapper.attributes('src')).toBeDefined()
-    expect(wrapper.attributes('src')).toContain('data:image/svg+xml;charset=UTF-8')
-
-    expect(wrapper.vm.scrollTimeout).toBe(null)
-
-    const resizeEvt = new UIEvent('resize')
-    window.dispatchEvent(resizeEvt)
-
-    expect(wrapper.vm.scrollTimeout).not.toBe(null)
-
-    // Since JEST doesnt support getBCR, we fake it by setting
-    // the data prop to shown
-    wrapper.setData({
-      isShown: true
+    afterEach(() => {
+      // Restore prototype
+      Element.prototype.getBoundingClientRect = origGetBCR
     })
 
-    window.dispatchEvent(resizeEvt)
+    it('triggers check on resize event event', async () => {
+      const src = 'https://picsum.photos/1024/400/?image=41'
+      const wrapper = mount(BImgLazy, {
+        attachToDocument: true,
+        propsData: {
+          src: src
+        }
+      })
+      expect(wrapper.is('img')).toBe(true)
 
-    expect(wrapper.vm.scrollTimeout).toBe(null)
+      expect(wrapper.attributes('src')).toBeDefined()
+      expect(wrapper.attributes('src')).toContain('data:image/svg+xml;charset=UTF-8')
 
-    wrapper.destroy()
+      expect(wrapper.vm.scrollTimeout).toBe(null)
+
+      // Fake getBCR
+      Element.prototype.getBoundingClientRect = jest.fn(() => {
+        return {
+          width: 24,
+          height: 24,
+          top: 0,
+          left: 0,
+          bottom: 10,
+          right: 10
+        }
+      })
+
+      const resizeEvt = new UIEvent('resize')
+      window.dispatchEvent(resizeEvt)
+
+      await wrapper.vm.$nextTick()
+
+      expect(wrapper.vm.scrollTimeout).not.toBe(null)
+
+      // Since JSDOM doesnt support getBCR, we fake it by setting
+      // the data prop to shown
+      // wrapper.setData({
+      //   isShown: true
+      // })
+
+      expect(wrapper.attributes('src')).not.toContain('data:image/svg+xml;charset=UTF-8')
+
+      window.dispatchEvent(resizeEvt)
+
+      expect(wrapper.vm.scrollTimeout).toBe(null)
+
+      wrapper.destroy()
+    })
   })
 })
