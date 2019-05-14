4effb9249b80282b77462095caf28b059d543a62
T Morehouse <troymore@nbnet.nb.ca>
2019-05-14 18:30:01 -0300

    Update index.d.ts

--- types/components/layout/index.d.ts
+++ types/components/layout/index.d.ts
@@ -10,13 +10,13 @@ export default LayoutPlugin
 export interface LayoutPlugin extends BvPlugin {}
 
 // Component: b-container
-export interface BContainer extends Vue {}
+export declare class BContainer extends Vue {}
 
 // Component: b-row
-export interface BRow extends Vue {}
+export declare class BRow extends Vue {}
 
 // Component: b-container
-export interface BCol extends Vue {}
+export declare class BCol extends Vue {}
 
 // Component: b-form-row
-export interface BFormRow extends Vue {}
+export declare class BFormRow extends Vue {}
