56d00f6bdfb698b530413c36ce7034a911d4dc22
T Morehouse <troymore@nbnet.nb.ca>
2019-05-14 13:57:43 -0300

    Update es.d.ts

--- types/es.d.ts
+++ types/es.d.ts
@@ -15,6 +15,18 @@ declare module 'bootstrap-vue/es/components' {
   export = BVComponents
 }
 
+declare module 'bootstrap-vue/es/components/*' {
+  import { Component, VueConstructor } from 'vue'
+  import { BvPlugin, BvConfigOptions } from './bv-plugin'
+  
+  const BVComponents: {
+    default: Component & BvPlugin & VueConstructor
+    [key: string]: Component & BvPlugin & VueConstructor
+  }
+
+  export = BVComponents
+}
+
 declare module 'bootstrap-vue/es/directives' {
   import { VueConstructor, DirectiveOptions } from 'vue'
   import { BvPlugin, BvConfigOptions } from './bv-plugin'
@@ -27,19 +39,29 @@ declare module 'bootstrap-vue/es/directives' {
   export = BVDirectives
 }
 
+declare module 'bootstrap-vue/es/directives/*' {
+  import { VueConstructor, DirectiveOptions } from 'vue'
+  import { BvPlugin, BvConfigOptions } from './bv-plugin'
+
+  const BVDirectives: {
+    default: DirectiveOptions & BvPlugin & VueConstructor
+    [key: string]: DirectiveOptions & BvPlugin & VueConstructor
+  }
+
+  export = BVDirectives
+}
+
+
 declare module 'bootstrap-vue/es/bv-config' {
   import { BvPlugin, BvConfigOptions } from './bv-plugin'
 
   const BVConfigPlugin: BvPlugin
+
   export default BVConfigPlugin
 }
 
 declare module 'bootstrap-vue/es' {
-  import { BvPlugin, BvConfigOptions } from './bv-plugin'
+  import BootstrapVue from 'bootstrap-vue'
 
-  declare const BootstrapVue: BootstrapVue
   export default BootstrapVue
-  export interface BootstrapVue extends BvPlugin {
-    setConfig: (config: BvConfigOptions) => void
-  }
 }
