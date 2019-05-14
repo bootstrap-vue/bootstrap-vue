c14e9a11f9d608beee4524ee25dc2db84e4ce0dc
T Morehouse <troymore@nbnet.nb.ca>
2019-05-14 19:28:25 -0300

    Update index.d.ts

--- types/directives/scrollspy/index.d.ts
+++ types/directives/scrollspy/index.d.ts
@@ -5,9 +5,8 @@ import Vue, { DirectiveOptions } from 'vue'
 import { BvPlugin } from '../../bv-plugin'
 
 // Plugin
-declare const VBScrollspyPlugin: VBScrollspyPlugin
+export declare const VBScrollspyPlugin: BvPlugin
 export default VBScrollspyPlugin
-export interface VBScrollspyPlugin extends BvPlugin {}
 
 // directive: v-b-scrollspy
 export interface VBScrollspy extends DirectiveOptions {}
