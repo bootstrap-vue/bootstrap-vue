726eb857a14ddadeb4744b688a62ec5d551304c8
T Morehouse <troymore@nbnet.nb.ca>
2019-05-14 19:35:48 -0300

    Update index.d.ts

--- types/directives/modal/index.d.ts
+++ types/directives/modal/index.d.ts
@@ -9,4 +9,4 @@ export declare const VBModalPlugin: BvPlugin
 export default VBModalPlugin
 
 // directive: v-b-modal
-export interface VBModal extends DirectiveOptions {}
+export declare const VBModal: DirectiveOptions
