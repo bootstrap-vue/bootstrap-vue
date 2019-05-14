8a6239e21376932733f1d74a6e7319d4d969aca0
T Morehouse <troymore@nbnet.nb.ca>
2019-05-14 18:34:30 -0300

    Update index.d.ts

--- types/components/nav/index.d.ts
+++ types/components/nav/index.d.ts
@@ -10,16 +10,16 @@ export default NavPlugin
 export interface NavPlugin extends BvPlugin {}
 
 // Component: b-nav
-export interface BNav extends Vue {}
+export declare class BNav extends Vue {}
 
 // Component: b-nav-form
-export interface BNavForm extends Vue {}
+export declare class BNavForm extends Vue {}
 
 // Component: b-nav-item
-export interface BNavItem extends Vue {}
+export declare class BNavItem extends Vue {}
 
 // Component: b-nav-item-dropdown
-export interface BNavItemDropdown extends Vue {}
+export declare class BNavItemDropdown extends Vue {}
 
 // Component: b-nav-text
-export interface BNavText extends Vue {}
+export declare class BNavText extends Vue {}
