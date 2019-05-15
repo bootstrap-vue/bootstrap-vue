83d360042fe30e95ccec9a3d2d78eb04c10ef0e0
T Morehouse <troymore@nbnet.nb.ca>
2019-05-14 23:12:56 -0300

    Update bv-config.d.ts

--- types/bv-config.d.ts
+++ types/bv-config.d.ts
@@ -1,6 +1,5 @@
 import Vue, { PluginFunction, PluginObject } from 'vue'
 import { BvPlugin } from './bv-plugin'
 
-declare const BVConfigPlugin: BVConfigPlugin
+export declare const BVConfigPlugin: BvPlugin
 export default BVConfigPlugin
-export interface BVConfigPlugin extends BvPlugin {}
