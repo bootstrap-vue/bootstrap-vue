b9ac121a28fd19eee969d9eea65957c6f1ed7b4e
T Morehouse <troymore@nbnet.nb.ca>
2019-05-14 23:08:10 -0300

    Update index.d.ts

--- types/components/spinner/index.d.ts
+++ types/components/spinner/index.d.ts
@@ -5,9 +5,8 @@ import Vue from 'vue'
 import { BvPlugin } from '../../bv-plugin'
 
 // Plugin
-declare const SpinnerPlugin: SpinnerPlugin
+export declare const SpinnerPlugin: BvPlugin
 export default SpinnerPlugin
-export interface SpinnerPlugin extends BvPlugin {}
 
 // Component: b-alert
 export declare class BSpinner extends Vue {}
