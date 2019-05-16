7cf3f610c309a9c16e13b165be21e3d863ac75b7
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:20:45 -0300

    Update index.d.ts

--- types/components/form-checkbox/index.d.ts
+++ types/components/form-checkbox/index.d.ts
@@ -2,14 +2,14 @@
 // Form Checkbox
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const FormCheckboxPlugin: BvPlugin
 export default FormCheckboxPlugin
 
 // Component: b-form-checkbox
-export declare class BFormCheckbox extends Vue {}
+export declare class BFormCheckbox extends BvComponent {}
 
 // Component: b-form-checkbox-group
-export declare class BFormCheckboxGroup extends Vue {}
+export declare class BFormCheckboxGroup extends BvComponent {}
