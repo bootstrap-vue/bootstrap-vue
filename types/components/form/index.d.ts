52a2e289d6bfd33c0d079acb2dd2ab989698d10f
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:25:21 -0300

    Update index.d.ts

--- types/components/form/index.d.ts
+++ types/components/form/index.d.ts
@@ -2,23 +2,23 @@
 // Form
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const FormPlugin: BvPlugin
 export default FormPlugin
 
 // Component: b-form
-export declare class BForm extends Vue {}
+export declare class BForm extends BvComponent {}
 
 // Component: b-form-text
-export declare class BFormText extends Vue {}
+export declare class BFormText extends BvComponent {}
 
 // Component: b-form-invalid-feedback
-export declare class BFormInvalidFeedback extends Vue {}
+export declare class BFormInvalidFeedback extends BvComponent {}
 
 // Component: b-form-valid-feedback
-export declare class BFormValidFeedback extends Vue {}
+export declare class BFormValidFeedback extends BvComponent {}
 
 // Component: b-form-datalist
-export declare class BFormDatalist extends Vue {}
+export declare class BFormDatalist extends BvComponent {}
