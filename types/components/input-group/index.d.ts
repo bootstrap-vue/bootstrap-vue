f8fcb9d8ddc5aa3082e9e709bfe10badde16e61e
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:26:33 -0300

    Update index.d.ts

--- types/components/input-group/index.d.ts
+++ types/components/input-group/index.d.ts
@@ -2,20 +2,20 @@
 // InputGroup
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const InputGroupPlugin: BvPlugin
 export default InputGroupPlugin
 
 // Component: b-input-group-append
-export declare class BInputGroupAppend extends Vue {}
+export declare class BInputGroupAppend extends BvComponent {}
 
 // Component: b-input-group-prepend
-export declare class BInputGroupPrepend extends Vue {}
+export declare class BInputGroupPrepend extends BvComponent {}
 
 // Component: b-input-group-text
-export declare class BInputGroupText extends Vue {}
+export declare class BInputGroupText extends BvComponent {}
 
 // Component: b-input-group-addon
-export declare class BInputGroupAddon extends Vue {}
+export declare class BInputGroupAddon extends BvComponent {}
