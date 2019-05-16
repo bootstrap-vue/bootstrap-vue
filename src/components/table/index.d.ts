1f3c5b315ee09e2554d7688892f6c3836753269a
T Morehouse <troymore@nbnet.nb.ca>
2019-05-16 02:41:49 -0300

    Update index.d.ts

--- src/components/table/index.d.ts
+++ src/components/table/index.d.ts
@@ -14,7 +14,20 @@ export declare class BTable extends BvComponent {
   refresh: () => void
   clearSelected: () => void
   // Props
+  id?: string
+  items: Array<any> | BvTableProviderCallback
   fields?: BvTableFieldObject | BvTableFieldArray
+  primaryKey?: string
+  sortBy?: string | null
+  sortDesc?: boolean
+  sortDirection?: BvTableSortDirection
+  sortCompare?: BvTableSortCompareCallback
+  perPage?: number | string
+  currentPage?: number | string
+  filter?: string | Array<any> | RegExp | object | any
+  filterFunction?: BvTableFilterCallback
+  busy?: boolean
+  tbodyTrClass?: string | Array<any> | object | BvTableTbodyTrClassCallback
 }
 
 export type BvTableVariant =
@@ -31,6 +44,27 @@ export type BvTableVariant =
 export type BvTableSortDirection = 'asc' | 'desc' | 'last'
 
 export type BvTableFormatterCallback = ((value: any, key: string, item: any) => any)
+
+export type BvTableTbodyTrClassCallback = ((item: any, type: string) => any)
+
+export type BvTableFilterCallback = ((item: any, filter: any) => boolean)
+
+export type BvTableSortCompareCallback = ((a: any, b: any, field: string) => any)
+
+export interface BvTableCtxObject {
+  currentPage: number
+  perPage: number
+  filter: string | RegExp | BvTableFilterCallback | null
+  sortBy: string | null
+  sortDesc: boolean
+  apiUrl: string | null
+  [key: string]: any
+}
+
+export interface BvTableProviderCallback {
+  (ctx: BvTableCtxObject): any
+  (ctx: BvTableCtxObject, callback: () => Array<any>): null
+}
 
 export interface BvTableField {
   label?: string
