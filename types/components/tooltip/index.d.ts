0e7e2b2a78bfd8caf30919e55a0757f5e05d39ad
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:55:38 -0300

    Update index.d.ts

--- types/components/tooltip/index.d.ts
+++ types/components/tooltip/index.d.ts
@@ -2,11 +2,11 @@
 // Tooltip
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const TooltipPlugin: BvPlugin
 export default TooltipPlugin
 
 // Component: b-tooltip
-export declare class BTooltip extends Vue {}
+export declare class BTooltip extends BvComponent {}
