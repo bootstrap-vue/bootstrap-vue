6b9456c63e5a559df3ba8ef248bcf738548092f4
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:15:08 -0300

    Update index.d.ts

--- types/components/button/index.d.ts
+++ types/components/button/index.d.ts
@@ -2,14 +2,14 @@
 // Buttons
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const ButtonPlugin: BvPlugin
 export default ButtonPlugin
 
 // Component: b-button
-export declare class BButton extends Vue {}
+export declare class BButton extends BvComponent {}
 
 // Component: b-button-close
-export declare class BButtonClose extends Vue {}
+export declare class BButtonClose extends BvComponent {}
