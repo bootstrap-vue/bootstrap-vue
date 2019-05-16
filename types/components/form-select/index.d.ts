baf4305130721473f622b6abfcc2d457f91fc006
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:23:30 -0300

    Update index.d.ts

--- types/components/form-select/index.d.ts
+++ types/components/form-select/index.d.ts
@@ -2,11 +2,11 @@
 // Form Select
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const FormSelectPlugin: BvPlugin
 export default FormSelectPlugin
 
 // Component: b-form-select
-export declare class BFormSelect extends Vue {}
+export declare class BFormSelect extends BvComponent {}
