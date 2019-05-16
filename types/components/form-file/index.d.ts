17afee22298fe82f9be9d37e28fe0c9b52159ddf
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:21:29 -0300

    Update index.d.ts

--- types/components/form-file/index.d.ts
+++ types/components/form-file/index.d.ts
@@ -2,14 +2,14 @@
 // Form File
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const FormFilePlugin: BvPlugin
 export default FormFilePlugin
 
 // Component: b-form-file
-export declare class BFormFile extends Vue {
+export declare class BFormFile extends BvComponent {
   focus: () => void
   reset: () => void
 }
