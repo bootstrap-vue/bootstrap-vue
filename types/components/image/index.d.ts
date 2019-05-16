c1a8e2f41e6de5fc1e8e4623dd0d1bde5074d2d1
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:25:58 -0300

    Update index.d.ts

--- types/components/image/index.d.ts
+++ types/components/image/index.d.ts
@@ -2,14 +2,14 @@
 // Image
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const ImagePlugin: BvPlugin
 export default ImagePlugin
 
 // Component: b-img
-export declare class BImg extends Vue {}
+export declare class BImg extends BvComponent {}
 
 // Component: b-img-lazy
-export declare class BImgLazy extends Vue {}
+export declare class BImgLazy extends BvComponent {}
