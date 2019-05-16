faef1b5d4d1dfecb4081a2c76ac24e25bc983a50
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:49:20 -0300

    Update index.d.ts

--- types/components/progress/index.d.ts
+++ types/components/progress/index.d.ts
@@ -2,14 +2,14 @@
 // Progress
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const ProgressPlugin: BvPlugin
 export default ProgressPlugin
 
 // Component: b-progress
-export declare class BProgress extends Vue {}
+export declare class BProgress extends BvComponent {}
 
 // Component: b-progress-bar
-export declare class BProgressBar extends Vue {}
+export declare class BProgressBar extends BvComponent {}
