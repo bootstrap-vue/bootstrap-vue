356a6d059c56811ac875353ac7d9272f1426c8f0
T Morehouse <troymore@nbnet.nb.ca>
2019-05-15 21:28:05 -0300

    Update index.d.ts

--- types/components/layout/index.d.ts
+++ types/components/layout/index.d.ts
@@ -2,20 +2,20 @@
 // Layout
 //
 import Vue from 'vue'
-import { BvPlugin } from '../../bv-plugin'
+import { BvPlugin, BvComponent } from '../../'
 
 // Plugin
 export declare const LayoutPlugin: BvPlugin
 export default LayoutPlugin
 
 // Component: b-container
-export declare class BContainer extends Vue {}
+export declare class BContainer extends BvComponent {}
 
 // Component: b-row
-export declare class BRow extends Vue {}
+export declare class BRow extends BvComponent {}
 
 // Component: b-container
-export declare class BCol extends Vue {}
+export declare class BCol extends BvComponent {}
 
 // Component: b-form-row
-export declare class BFormRow extends Vue {}
+export declare class BFormRow extends BvComponent {}
