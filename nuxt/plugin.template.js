fd315199b9f69804123eac2a64538b0544252f96
T Morehouse <troymore@nbnet.nb.ca>
2019-05-18 03:39:52 -0300

    Update plugin.template.js

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,4 +1,4 @@
-import Vue from 'vue'
+import Vue from 'vue';
 <% if (
   options.componentPlugins.length ||
   options.directivePlugins.length ||
@@ -8,32 +8,32 @@ import Vue from 'vue'
 <% if (options.componentPlugins.length || options.components.length) { %>
 import {
   <%= [].concat(options.componentPlugins, options.components).filter(Boolean).join(',\n  ') %>
-} from 'bootstrap-vue/<%= options.dist %>/components'
+} from 'bootstrap-vue/<%= options.dist %>/components';
 <% } %>
 <% if (options.directivePlugins.length || options.directives.length) { %>
 import {
   <%= [].concat(options.directivePlugins, options.directives).filter(Boolean).join(',\n  ') %>
-} from 'bootstrap-vue/<%= options.dist %>/directives'
+} from 'bootstrap-vue/<%= options.dist %>/directives';
 <% } %>
 
 <% if (options.config) { %>
-import BVConfigPlugin from 'bootstrap-vue/<%= options.dist %>/bv-config'
+import BVConfigPlugin from 'bootstrap-vue/<%= options.dist %>/bv-config';
 
-Vue.use(BVConfigPlugin, <%= JSON.stringify(options.config, undefined, 2) %>)
+Vue.use(BVConfigPlugin, <%= JSON.stringify(options.config, undefined, 2) %>)'
 <% } %>
 
 <%=
-options.componentPlugins.reduce((acc, cp) => (acc += `Vue.use(${cp})\n` ), '')
+options.componentPlugins.reduce((acc, cp) => (acc += `Vue.use(${cp});\n` ), '')
 %><%=
-options.directivePlugins.reduce((acc, dp) => (acc += `Vue.use(${dp})\n` ), '')
+options.directivePlugins.reduce((acc, dp) => (acc += `Vue.use(${dp});\n` ), '')
 %><%=
-options.components.reduce((acc, c) => (acc += `Vue.component('${c}', ${c})\n` ), '')
+options.components.reduce((acc, c) => (acc += `Vue.component('${c}', ${c});\n` ), '')
 %><%=
-options.directives.reduce((acc, d) => (acc += `Vue.directive('${d.replace(/^VB/, 'B')}', ${d})\n` ), '')
+options.directives.reduce((acc, d) => (acc += `Vue.directive('${d.replace(/^VB/, 'B')}', ${d});\n` ), '')
 %>
 
 <% } else { %>
-import BootstrapVue from 'bootstrap-vue/<%= options.dist %>'
+import BootstrapVue from 'bootstrap-vue/<%= options.dist %>';
 
-Vue.use(BootstrapVue, <%= JSON.stringify(options.config || {}, undefined, 2) %>)
+Vue.use(BootstrapVue, <%= JSON.stringify(options.config || {}, undefined, 2) %>);
 <% } %>
