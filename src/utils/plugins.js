96f02cf84d03e18c69af79e25a09f04411b73028
T Morehouse <troymore@nbnet.nb.ca>
2019-05-20 19:13:52 -0300

    Update plugins.js

--- src/utils/plugins.js
+++ src/utils/plugins.js
@@ -26,20 +26,20 @@ export const checkMultipleVue = Vue => {
  * @returns {function} plugin install function
  */
 export const installFactory = ({ components, directives, plugins }) => {
-  let _installed = false
-
   const install = (Vue, config = {}) => {
-    if (_installed) {
+    if (install.installed) {
       /* istanbul ignore next */
       return
     }
-    _installed = true
+    install.installed = true
     checkMultipleVue(Vue)
     setConfig(config)
     registerComponents(Vue, components)
     registerDirectives(Vue, directives)
     registerPlugins(Vue, plugins)
   }
+
+  install.installed = false
 
   return install
 }
