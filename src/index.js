1d9e1f84a9d26b3563d4a5fdf186d70fa4743769
T Morehouse <troymore@nbnet.nb.ca>
2019-05-20 19:27:19 -0300

    fix: adjust main plugin installer `installed` flag (closes #3338)
    ### Describe the PR
    
    Minor adjustment to main BootstrapVue plugin installed flag
    
    Closes #3338
    
    ### PR checklist
    
    <!-- (Update "[ ]" to "[x]" to check a box) -->
    
    **What kind of change does this PR introduce?** (check at least one)
    
    - [ ] Bugfix
    - [ ] Feature
    - [ ] Enhancement
    - [ ] ARIA accessibility
    - [ ] Documentation update
    - [x] Other (please describe) minor refactor on main plugin installation
    
    **Does this PR introduce a breaking change?** (check one)
    
    - [x] No
    - [ ] Yes (please describe)
    
    **The PR fulfills these requirements:**
    
    - [x] It's submitted to the `dev` branch, **not** the `master` branch
    - [x] When resolving a specific issue, it's referenced in the PR's title (i.e. `[...] (fixes #xxx[,#xxx])`, where "xxx" is the issue number)
    - [x] It should address only one issue or feature. If adding multiple features or fixing a bug and adding a new feature, break them into separate PRs if at all possible.
    - [x] The title should follow the [**Conventional Commits**](https://www.conventionalcommits.org/) naming convention (i.e. `fix(alert): not alerting during SSR render`, `docs(badge): update pill examples, fix typos`, `chore: fix typo in README`, etc). **This is very important, as the `CHANGELOG` is generated from these messages.**
    
    **If new features/enhancement/fixes are added or changed:**
    
    - [ ] Includes documentation updates (including updating the component's `package.json` for slot and event changes)
    - [ ] New/updated tests are included and passing (if required)
    - [x] Existing test suites are passing
    - [x] The changes have not impacted the functionality of other components or directives
    - [ ] ARIA Accessibility has been taken into consideration (Does it affect screen reader users or keyboard only users? Clickable items should be in the tab index, etc.)
    
    **If adding a new feature, or changing the functionality of an existing feature, the PR's
    description above includes:**
    
    - [ ] A convincing reason for adding this feature (to avoid wasting your time, it's best to open a suggestion issue first and wait for approval before working on it)

--- src/index.js
+++ src/index.js
@@ -1,29 +1,10 @@
 import componentsPlugin from './components'
 import directivesPlugin from './directives'
-import { vueUse } from './utils/plugins'
+import { installFactory, vueUse } from './utils/plugins'
 import { setConfig } from './utils/config'
 
-const install = (Vue, config = {}) => {
-  if (install.installed) {
-    /* istanbul ignore next */
-    return
-  }
-  install.installed = true
-
-  // Configure BootstrapVue
-  setConfig(config)
-
-  // Install all component plugins
-  Vue.use(componentsPlugin)
-
-  // Install all directive plugins
-  Vue.use(directivesPlugin)
-}
-
-install.installed = false
-
 const BootstrapVue = {
-  install: install,
+  install: installFactory({ plugins: { componentsPlugin, directivesPlugin } }),
   setConfig: setConfig
 }
 
