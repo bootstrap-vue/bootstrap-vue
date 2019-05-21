c11fbca239660ccdb3d6c6dad1bda5296405857c
T Morehouse <troymore@nbnet.nb.ca>
2019-05-21 00:41:42 -0300

    Update form-select.spec.js

--- src/components/form-select/form-select.spec.js
+++ src/components/form-select/form-select.spec.js
@@ -564,9 +564,9 @@ describe('form-select', () => {
     await wrapper.vm.$nextTick()
 
     const input = wrapper.find('select')
-    expect(input).toBeDefined()
+    expect(input.exists()).toBe(true)
     expect(document).toBeDefined()
-    expect(document.activeElement).toBe(input)
+    expect(document.activeElement).toBe(input.element)
 
     wrapper.destroy()
   })
