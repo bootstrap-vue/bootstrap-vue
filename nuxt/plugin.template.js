ef71a3b04746c673c1c4c19efcee9685651b3e1a
T Morehouse <troymore@nbnet.nb.ca>
2019-05-21 02:06:59 -0300

    feat(nuxt module): optimize imports into single import statements (#3325)

--- nuxt/plugin.template.js
+++ nuxt/plugin.template.js
@@ -1,29 +1,39 @@
-import Vue from 'vue'
+import Vue from 'vue';
 <% if (
   options.componentPlugins.length ||
   options.directivePlugins.length ||
   options.components.length ||
   options.directives.length
-) { %><%=
-options.componentPlugins.reduce((acc, p) => (acc += `import { ${p} } from 'bootstrap-vue/${options.dist}/components'\n` ), '') %><%=
-options.directivePlugins.reduce((acc, p) => (acc += `import { ${p} } from 'bootstrap-vue/${options.dist}/directives'\n` ), '') %><%=
-options.components.reduce((acc, c) => (acc += `import { ${c} } from 'bootstrap-vue/${options.dist}/components'\n` ), '') %><%=
-options.directives.reduce((acc, d) => (acc += `import { ${d} } from 'bootstrap-vue/${options.dist}/directives'\n` ), '') %>
+) { %>
+<% if (options.componentPlugins.length || options.components.length) { %>
+import {
+  <%= [].concat(options.componentPlugins, options.components).filter(Boolean).join(',\n  ') %>
+} from 'bootstrap-vue/<%= options.dist %>/components';
+<% } %>
+<% if (options.directivePlugins.length || options.directives.length) { %>
+import {
+  <%= [].concat(options.directivePlugins, options.directives).filter(Boolean).join(',\n  ') %>
+} from 'bootstrap-vue/<%= options.dist %>/directives';
+<% } %>
 
 <% if (options.config) { %>
-import BVConfigPlugin from 'bootstrap-vue/<%= options.dist %>/bv-config'
+import BVConfigPlugin from 'bootstrap-vue/<%= options.dist %>/bv-config';
 
-Vue.use(BVConfigPlugin, <%= JSON.stringify(options.config, undefined, 2) %>)
+Vue.use(BVConfigPlugin, <%= JSON.stringify(options.config, undefined, 2) %>)'
 <% } %>
 
 <%=
-options.componentPlugins.reduce((acc, p) => (acc += `Vue.use(${p})\n` ), '') %><%=
-options.directivePlugins.reduce((acc, p) => (acc += `Vue.use(${p})\n` ), '') %><%=
-options.components.reduce((acc, c) => (acc += `Vue.component('${c}', ${c})\n` ), '') %><%=
-options.directives.reduce((acc, d) => (acc += `Vue.directive('${d.replace(/^VB/, 'B')}', ${d})\n` ), '') %>
+options.componentPlugins.reduce((acc, cp) => (acc += `Vue.use(${cp});\n` ), '')
+%><%=
+options.directivePlugins.reduce((acc, dp) => (acc += `Vue.use(${dp});\n` ), '')
+%><%=
+options.components.reduce((acc, c) => (acc += `Vue.component('${c}', ${c});\n` ), '')
+%><%=
+options.directives.reduce((acc, d) => (acc += `Vue.directive('${d.replace(/^VB/, 'B')}', ${d});\n` ), '')
+%>
 
 <% } else { %>
-import BootstrapVue from 'bootstrap-vue/<%= options.dist %>'
+import BootstrapVue from 'bootstrap-vue/<%= options.dist %>';
 
-Vue.use(BootstrapVue, <%= JSON.stringify(options.config || {}, undefined, 2) %>)
+Vue.use(BootstrapVue, <%= JSON.stringify(options.config || {}, undefined, 2) %>);
 <% } %>
