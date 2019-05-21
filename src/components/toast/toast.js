06c93f9f092486b745f63fe4f3b76d6f9823e2ff
T Morehouse <troymore@nbnet.nb.ca>
2019-05-21 20:07:57 -0300

    Update toast.js

--- src/components/toast/toast.js
+++ src/components/toast/toast.js
@@ -368,7 +368,7 @@ export default Vue.extend({
       let $header = h(false)
       if ($headerContent.length > 0) {
         $header = h(
-          'header',
+          'div',
           { staticClass: 'toast-header', class: this.headerClass },
           $headerContent
         )
