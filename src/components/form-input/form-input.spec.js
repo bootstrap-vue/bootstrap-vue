7f026f8a165aeb5baeffe6866a3c7d45a576d3cc
T Morehouse <troymore@nbnet.nb.ca>
2019-05-21 00:40:37 -0300

    Update form-input.spec.js

--- src/components/form-input/form-input.spec.js
+++ src/components/form-input/form-input.spec.js
@@ -779,9 +779,9 @@ describe('form-input', () => {
     await wrapper.vm.$nextTick()
 
     const input = wrapper.find('input')
-    expect(input).toBeDefined()
+    expect(input.exists()).toBe(true)
     expect(document).toBeDefined()
-    expect(document.activeElement).toBe(input)
+    expect(document.activeElement).toBe(input.element)
 
     wrapper.destroy()
   })
