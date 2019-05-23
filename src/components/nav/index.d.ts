17ddd4bccd9384adc57e16d10b95aaa75e696696
T Morehouse <troymore@nbnet.nb.ca>
2019-05-23 16:58:50 -0300

    Update index.d.ts

--- src/components/nav/index.d.ts
+++ src/components/nav/index.d.ts
@@ -18,7 +18,11 @@ export declare class BNavForm extends BvComponent {}
 export declare class BNavItem extends BvComponent {}
 
 // Component: b-nav-item-dropdown
-export declare class BNavItemDropdown extends BvComponent {}
+export declare class BNavItemDropdown extends BvComponent {
+  // Public methods
+  show: () => void
+  hide: (refocus?: boolean) => void
+}
 
 // Component: b-nav-text
 export declare class BNavText extends BvComponent {}
