ba3cac1f829b751bb211e06c06924b6609ac9752
T Morehouse <troymore@nbnet.nb.ca>
2019-05-28 17:55:14 -0300

    Update browser.js

--- src/browser.js
+++ src/browser.js
@@ -1,12 +1,6 @@
 // Main entry point for the browser build
 
-import componentsPlugin from './components'
-import directivesPlugin from './directives'
-import { installFactory, vueUse } from './utils/plugins'
-
-const BootstrapVue = {
-  install: installFactory({ plugins: { componentsPlugin, directivesPlugin } }),
-}
+import BootstrapVue from './index'
 
 // Auto installation only occurs if window.Vue exists
 vueUse(BootstrapVue)
