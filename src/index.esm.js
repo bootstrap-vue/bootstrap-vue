0c7ed1330c08570b14b6a4b1d121f12a611cf032
T Morehouse <troymore@nbnet.nb.ca>
2019-05-28 19:22:45 -0300

    Update index.esm.js

--- src/index.esm.js
+++ src/index.esm.js
@@ -3,17 +3,22 @@ import { setConfig } from './utils/config'
 // Once es dir build is gone, we will move index.esm.js to index.js
 import { componentsPlugin } from './components/index.esm'
 import { directivesPlugin } from './directives/index.esm'
+import BvConfig from './bv-config'
 
-// Named exports of all compoents and component group plugins
+// Named exports of all components and component group plugins
 export * from './components/index.esm'
 
 // Named exports of all directives and directive group plugins
 export * from './directives/index.esm'
 
-export const BootstrapVue = {
+// BootstrapVue plugin
+const BootstrapVue = {
   install: installFactory({ plugins: { componentsPlugin, directivesPlugin } }),
   setConfig: setConfig
 }
+
+// Named exports for BvConfig and BootstrapVue
+export { BvConfig, BootstrapVue }
 
 // Default export is the BootstrapVue plugin
 export default BootstrapVue
