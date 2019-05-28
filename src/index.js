7a084a4d256124a60570999cf64bd55b8bdca127
T Morehouse <troymore@nbnet.nb.ca>
2019-05-28 19:01:39 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
+// This index file will be replaced by the index.esm.js file
+// once teh `es/` build has been removed.
 import { installFactory } from './utils/plugins'
 import { setConfig } from './utils/config'
 import { componentsPlugin } from './components'
 import { directivesPlugin } from './directives'
-
-// Named exports of all compoents and component group plugins
-// Once es dir build is gone, we will move index.esm.js to index.js
-export * from './components/index.esm'
-
-// Named exports of all directives and directive group plugins
-// Once es dir build is gone, we will move index.esm.js to index.js
-export * from './directives/index.esm'
-
-export const BootstrapVue = {
-  install: installFactory({ plugins: { componentsPlugin, directivesPlugin } }),
-  setConfig: setConfig
-}
 
 // Default export is the BootstrapVue plugin
 export default BootstrapVue
