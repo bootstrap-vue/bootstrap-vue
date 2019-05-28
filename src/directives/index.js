5020319660bd4d66d37a6eaa8c79233043a98cf0
T Morehouse <troymore@nbnet.nb.ca>
2019-05-28 17:40:30 -0300

    Update index.js

--- src/directives/index.js
+++ src/directives/index.js
@@ -1,20 +1,16 @@
-import { installFactory } from '../utils/plugins'
-import * as directivePlugins from './plugins'
+// Legacy index file supporting leagacy plugin names.
+// This file is only here from transpilation purposes for `es/` build.
+// src/index import /src/components/index.esm so that we don't
+// have top-level duplicate plugin names.
+
+// Import teh main plugin
+import directivesPlugin from './index.esm'
 
 // Export all directive group plugins as named exports
-export * from './plugins'
+export * from './index.esm'
 
 // Export all legacy named directive group plugins as named exports
 export * from './plugins-legacy'
 
-// Named exports of all directives
-export * from './toggle'
-export * from './modal'
-export * from './scrollspy'
-export * from './tooltip'
-export * from './popover'
-
 // Default export is a plugin that installs all plugins
-export default {
-  install: installFactory({ plugins: directivePlugins })
-}
+export default directivesPlugin
