c0968524e89398a565734872085f02a30b1d2b7e
T Morehouse <troymore@nbnet.nb.ca>
2019-05-28 18:01:07 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import componentsPlugin from './components'
 import directivesPlugin from './directives'
-import { installFactory, vueUse } from './utils/plugins'
+import { installFactory } from './utils/plugins'
 import { setConfig } from './utils/config'
+
+// Named export of all compoents and component group plugins
+export * from './components/index.esm'
+
+// Named export of all directives and directive group plugins
+export * from './directives/index.esm'
 
 const BootstrapVue = {
   install: installFactory({ plugins: { componentsPlugin, directivesPlugin } }),
   setConfig: setConfig
 }
-
-// Auto installation only occurs if window.Vue exists
-vueUse(BootstrapVue)
 
 export default BootstrapVue
