31ae7e86af3d2f153d52f75328d5dca4f9c01324
T Morehouse <troymore@nbnet.nb.ca>
2019-05-29 17:04:50 -0300

    Update index.js

--- nuxt/index.js
+++ nuxt/index.js
@@ -56,15 +56,9 @@ module.exports = function nuxtBootstrapVue(moduleOptions = {}) {
       // by aliasing bootstrap-vue to the source files.
       // We prepend a $ to ensure that it is only used for
       // `import from 'bootstrap-vue'` not `import from 'bootstrap-vue/*'`
-      const srcIndex = require.resolve('bootstrap-vue/src/index.esm.js')
-      if (this.options.alias) {
-        // Nuxt 2.6+
-        this.options.alias['bootstrap-vue$'] = srcIndex
-      } else {
-        // Nuxt < 2.6
-        this.externalBuild((config, { isServer }) => {
-          config.alias['bootstrap-vue$'] = srcIndex
-        })
+      const srcIndex = 'bootstrap-vue/src/index.esm.js'
+      this.externalBuild((config, { isServer }) => {
+        config.alias['bootstrap-vue$'] = require.resolve('srcIndex')
       }
     }
 
