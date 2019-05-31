38c490b854f9d9caa479ae49eef18fcdee1c564c
T Morehouse <troymore@nbnet.nb.ca>
2019-05-31 18:21:43 -0300

    fix(table): ensure provider is refreshed when filter is an object (closes #3428)
    Possible fix for #3428
    
    Ensure provider check context properties exist to ensure reactivity of sub properties.

--- src/components/table/helpers/mixin-provider.js
+++ src/components/table/helpers/mixin-provider.js
@@ -34,7 +34,12 @@ export default {
       // regular this.context is sent to the provider during fetches though, as they
       // may need all the prop info.
       const ctx = {
-        apiUrl: this.apiUrl
+        apiUrl: this.apiUrl,
+        filter: null,
+        sortBy: null,
+        sortDesc: null,
+        perPage: null,
+        currentPage: null
       }
       if (!this.noProviderFiltering) {
         // Either a string, or could be an object or array.
