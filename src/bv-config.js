575b23e4f8f1e94704b0bc2bada02fbf0f87577e
T Morehouse <troymore@nbnet.nb.ca>
2019-06-05 03:37:14 -0300

    Update bv-config.js

--- src/bv-config.js
+++ src/bv-config.js
@@ -1,11 +1,11 @@
 //
 // Utility Plugin for setting the configuration
 //
-import { setConfig } from './utils/config'
+import { setConfig } from './utils/config-set'
 
 const BVConfigPlugin = {
   install(Vue, config = {}) {
-    setConfig(config)
+    setConfig(config, Vue)
   }
 }
 
