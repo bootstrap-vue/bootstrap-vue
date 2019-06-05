5b26ed1ad487f657e8481b3675f7c84fc7e3a5ee
T Morehouse <troymore@nbnet.nb.ca>
2019-06-05 05:03:12 -0300

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -4,56 +4,61 @@ import get from './get'
 import memoize from './memoize'
 import DEFAULTS from './config-defaults'
 
-// Reset the user config to default
-// For testing purposes only
+// --- Constants ---
+
+const PROP_NAME = '$bvConfig'
+
+// --- Methods ---
+
+// Reset the user config to default. For testing purposes only.
 // TODO: MOVE to config-set.js
 export const resetConfig = () => {
-  Vue.prototype.$bvConfig && Vue.prototype.$bvConfig.resetConfig()
+  Vue.prototype[PROP_NAME] && Vue.prototype[PROP_NAME].resetConfig()
 }
 
 // Method to get a deep clone (immutable) copy of the defaults
 // For testing purposes only
+// TODO: remove cloneDeep, update tests
 export const getDefaults = () => {
   return cloneDeep(DEFAULTS)
 }
 
-// Get the current user config
-// For testing purposes only
+// Get the current user config. For testing purposes only
 export const getConfig = () => {
-  return Vue.prototype.$bvConfig ? Vue.prototype.$bvConfig.getConfig() : {}
+  return Vue.prototype[PROP_NAME] ? Vue.prototype[PROP_NAME].getConfig() : {}
 }
 
 // Method to grab a config value based on a dotted/array notation key
-// Returns a deep clone (immutable) copy
+// Returns a deep clone (immutable) copy.
 export const getConfigValue = key => {
-  return Vue.prototype.$bvConfig
-    ? Vue.prototype.$bvConfig.getConfigValue(key)
+  return Vue.prototype[PROP_NAME]
+    ? Vue.prototype[PROP_NAME].getConfigValue(key)
     : cloneDeep(get(DEFAULTS, key))
 }
 
 // Method to grab a config value for a particular component.
-// Returns a deep clone (immutable) copy
+// Returns a deep clone (immutable) copy.
 export const getComponentConfig = (cmpName, key = null) => {
   // Return the particular config value for key for if specified,
   // otherwise we return the full config (or an empty object if not found)
   return key ? getConfigValue(`${cmpName}.${key}`) : getConfigValue(cmpName) || {}
 }
 
-// Convenience method for getting all breakpoint names
-// Returns a deep clone (immutable) copy
+// Convenience method for getting all breakpoint names.
+// Returns a deep clone (immutable) copy.
 export const getBreakpoints = () => {
   return getConfigValue('breakpoints')
 }
 
-// Private function for caching / locking-in breakpoint names
+// Private function for caching / locking-in breakpoint names.
 // Returns a deep clone (immutable) copy
 const _getBreakpointsCached = memoize(() => {
   return getBreakpoints()
 })
 
-// Convenience method for getting all breakpoint names
-// Caches the results after first access
-// Returns a deep clone (immutable) copy
+// Convenience method for getting all breakpoint names.
+// Caches the results after first access.
+// Returns a deep clone (immutable) copy.
 export const getBreakpointsCached = () => {
   return cloneDeep(_getBreakpointsCached())
 }
