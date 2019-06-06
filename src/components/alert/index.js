e8e91e005a095319df8cb819b17cda1aba807fe6
T Morehouse <troymore@nbnet.nb.ca>
2019-06-06 11:28:04 -0300

    Update index.js

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -1,7 +1,7 @@
 import BAlert from './alert'
 import { pluginFactory } from '../../utils/plugins'
 
-const AlertPlugin = /#__PURE__*/pluginFactory({
+const AlertPlugin = /*#__PURE__*/pluginFactory({
   components: { BAlert }
 })
 
