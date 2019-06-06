cffc13a44bd3ba702bcb317b37bd73b18630f625
T Morehouse <troymore@nbnet.nb.ca>
2019-06-06 11:43:24 -0300

    Update index.js

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -1,9 +1,8 @@
 import BAlert from './alert'
 import { pluginFactory } from '../../utils/plugins'
 
-// eslint-disable-next-line spaced-comment
 // prettier-ignore
-const AlertPlugin = /*#__PURE__*/pluginFactory({
+const AlertPlugin = /*@__PURE__*/pluginFactory({
   components: { BAlert }
 })
 
