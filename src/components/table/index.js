2fe9a1a0f0a19c139a343d9522a0ff1e09ea88ce
T Morehouse <troymore@nbnet.nb.ca>
2019-06-06 15:21:54 -0300

    Update index.js

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,14 +1,11 @@
 import BTable from './table'
 import BTableLite from './table-lite'
-import { installFactory } from '../../utils/plugins'
+import { pluginFactory } from '../../utils/plugins'
 
-const components = {
-  BTable,
-  BTableLite
-}
+const TablePlugin = /*#__PURE__*/ pluginFactory({
+  components { BTable, BTableLite}
+})
 
 export { BTable, BTableLite }
 
-export default {
-  install: installFactory({ components })
-}
+export default TablePlugin
