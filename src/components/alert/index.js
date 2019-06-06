8bb73efc77b61fa3eee88c1e35c9aec6fb05e924
T Morehouse <troymore@nbnet.nb.ca>
2019-06-06 11:46:47 -0300

    Update index.js

--- src/components/alert/index.js
+++ src/components/alert/index.js
@@ -2,7 +2,7 @@ import BAlert from './alert'
 import { pluginFactory } from '../../utils/plugins'
 
 // prettier-ignore
-const AlertPlugin = /*@__PURE__*/pluginFactory({
+const AlertPlugin = /*#__PURE__*/pluginFactory({
   components: { BAlert }
 })
 
