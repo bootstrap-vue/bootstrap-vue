6278c026c4d51c6bb1a2b07029cc95fd07eea468
T Morehouse <troymore@nbnet.nb.ca>
2019-06-06 18:03:27 -0300

    Update index.js

--- src/components/carousel/index.js
+++ src/components/carousel/index.js
@@ -1,14 +1,14 @@
 import BCarousel from './carousel'
 import BCarouselSlide from './carousel-slide'
-import { installFactory } from '../../utils/plugins'
+import { pluginFactory } from '../../utils/plugins'
 
-const components = {
-  BCarousel,
-  BCarouselSlide
-}
+const CarouselPlugin = /*#__PURE*/ pluginFactory({
+  components: {
+    BCarousel,
+    BCarouselSlide
+  }
+})
 
 export { BCarousel, BCarouselSlide }
 
-export default {
-  install: installFactory({ components })
-}
+export default CarouselPlugin
