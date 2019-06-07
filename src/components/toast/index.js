cd2cef27f655c739c2da580968219a0e335ae5cc
T Morehouse <troymore@nbnet.nb.ca>
2019-06-06 23:24:04 -0300

    Update index.js

--- src/components/toast/index.js
+++ src/components/toast/index.js
@@ -1,20 +1,14 @@
 import BToast from './toast'
 import BToaster from './toaster'
 import BVToastPlugin from './helpers/bv-toast'
-import { installFactory } from '../../utils/plugins'
+import { pluginFactory } from '../../utils/plugins'
 
-const components = {
-  BToast,
-  BToaster
-}
-
-const plugins = {
+const ToastPlugin = /*#__PURE__*/ pluginFactory({
+  components: { BToast, BToaster },
   // $bvToast injection
-  BVToastPlugin
-}
+  plugins: { BVToastPlugin }
+})
 
 export { BToast, BToaster }
 
-export default {
-  install: installFactory({ components, plugins })
-}
+export default ToastPlugin
