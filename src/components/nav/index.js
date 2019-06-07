65be72a3d9fde8bb36d3a1f5db02d7430e1a2d43
T Morehouse <troymore@nbnet.nb.ca>
2019-06-06 22:55:04 -0300

    Update index.js

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -4,25 +4,24 @@ import BNavText from './nav-text'
 import BNavForm from './nav-form'
 import BNavItemDropdown from './nav-item-dropdown'
 import DropdownPlugin from '../dropdown'
-import { installFactory } from '../../utils/plugins'
+import { pluginFactory } from '../../utils/plugins'
 
-const components = {
-  BNav,
-  BNavItem,
-  BNavText,
-  BNavForm,
-  BNavItemDropdown,
-  BNavItemDd: BNavItemDropdown,
-  BNavDropdown: BNavItemDropdown,
-  BNavDd: BNavItemDropdown
-}
-
-const plugins = {
-  DropdownPlugin
-}
+const NavPlugin = /*#__PURE__*/ pluginFactory({
+  components: {
+    BNav,
+    BNavItem,
+    BNavText,
+    BNavForm,
+    BNavItemDropdown,
+    BNavItemDd: BNavItemDropdown,
+    BNavDropdown: BNavItemDropdown,
+    BNavDd: BNavItemDropdown
+  },
+  plugins: {
+    DropdownPlugin
+  }
+})
 
 export { BNav, BNavItem, BNavText, BNavForm, BNavItemDropdown }
 
-export default {
-  install: installFactory({ components, plugins })
-}
+export default NavPlugin
