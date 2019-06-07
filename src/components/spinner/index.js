605a979d763d0b5c736d95a14be449eff171f7f3
T Morehouse <troymore@nbnet.nb.ca>
2019-06-06 23:13:56 -0300

    Update index.js

--- src/components/spinner/index.js
+++ src/components/spinner/index.js
@@ -1,12 +1,10 @@
 import BSpinner from './spinner'
-import { installFactory } from '../../utils/plugins'
+import { pluginFactory } from '../../utils/plugins'
 
-const components = {
-  BSpinner
-}
+const SpinnerPlugin = /*#__PURE__*/ pluginFactory({
+  components: { BSpinner }
+})
 
 export { BSpinner }
 
-export default {
-  install: installFactory({ components })
-}
+export default SpinnerPlugin
