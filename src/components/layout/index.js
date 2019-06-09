750ffb9481fa3a4a4d1949ca10cbb0fbedfca9bf
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 18:54:37 -0300

    Update index.js

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,7 +1,7 @@
-import BContainer from './container'
-import BRow from './row'
-import BCol from './col'
-import BFormRow from './form-row'
+import { BContainer } from './container'
+import { BRow } from './row'
+import { BCol } from './col'
+import { BFormRow } from './form-row'
 import { pluginFactory } from '../../utils/plugins'
 
 const LayoutPlugin = /*#__PURE__*/ pluginFactory({
