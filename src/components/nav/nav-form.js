f5cd8e248186aee16627ed4ff7e6fb12ca6fbd94
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 20:09:28 -0300

    Update nav-form.js

--- src/components/nav/nav-form.js
+++ src/components/nav/nav-form.js
@@ -1,12 +1,12 @@
 import Vue from '../../utils/vue'
 import { mergeData } from 'vue-functional-data-merge'
-import BForm, { props as BFormProps } from '../form/form'
 import { omit } from '../../utils/object'
+import { BForm, props as BFormProps } from '../form/form'
 
 export const props = omit(BFormProps, ['inline'])
 
 // @vue/component
-const BNavForm = /*#__PURE__*/ Vue.extend({
+export const BNavForm = /*#__PURE__*/ Vue.extend({
   name: 'BNavForm',
   functional: true,
   props,
