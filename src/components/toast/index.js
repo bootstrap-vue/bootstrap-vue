869ca64bf6516e861be578005172e45e97c8baa2
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 19:19:26 -0300

    Update index.js

--- src/components/toast/index.js
+++ src/components/toast/index.js
@@ -1,6 +1,6 @@
-import BToast from './toast'
-import BToaster from './toaster'
 import BVToastPlugin from './helpers/bv-toast'
+import { BToast } from './toast'
+import { BToaster } from './toaster'
 import { pluginFactory } from '../../utils/plugins'
 
 const ToastPlugin = /*#__PURE__*/ pluginFactory({
