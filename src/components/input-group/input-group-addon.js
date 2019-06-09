5bc042bfd1af171d1ff59fd860e83d63a241c393
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 19:04:11 -0300

    Update input-group-addon.js

--- src/components/input-group/input-group-addon.js
+++ src/components/input-group/input-group-addon.js
@@ -1,6 +1,6 @@
 import Vue from '../../utils/vue'
 import { mergeData } from 'vue-functional-data-merge'
-import InputGroupText from './input-group-text'
+import { InputGroupText } from './input-group-text'
 
 export const commonProps = {
   id: {
@@ -18,7 +18,7 @@ export const commonProps = {
 }
 
 // @vue/component
-const BInputGroupAddon = /*#__PURE__*/ Vue.extend({
+export const BInputGroupAddon = /*#__PURE__*/ Vue.extend({
   name: 'BInputGroupAddon',
   functional: true,
   props: {
