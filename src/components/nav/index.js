aaed032fb52d638c6c6c155695eb704644fbc560
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 20:10:32 -0300

    Update index.js

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,8 +1,8 @@
-import BNav from './nav'
-import BNavItem from './nav-item'
-import BNavText from './nav-text'
-import BNavForm from './nav-form'
-import BNavItemDropdown from './nav-item-dropdown'
+import { BNav } from './nav'
+import { BNavItem } from './nav-item'
+import { BNavText } from './nav-text'
+import { BNavForm } from './nav-form'
+import { BNavItemDropdown } from './nav-item-dropdown'
 import DropdownPlugin from '../dropdown'
 import { pluginFactory } from '../../utils/plugins'
 
