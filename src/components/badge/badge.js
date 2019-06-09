3570264138d1589731ff564719c8155c96382d9a
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 18:15:44 -0300

    Update badge.js

--- src/components/badge/badge.js
+++ src/components/badge/badge.js
@@ -2,7 +2,7 @@ import Vue from '../../utils/vue'
 import { mergeData } from 'vue-functional-data-merge'
 import { getComponentConfig } from '../../utils/config'
 import pluckProps from '../../utils/pluck-props'
-import Link, { propsFactory as linkPropsFactory } from '../link/link'
+import { BLink, propsFactory as linkPropsFactory } from '../link/link'
 
 const NAME = 'BBadge'
 
@@ -32,7 +32,7 @@ export const BBadge = /*#__PURE__*/ Vue.extend({
   functional: true,
   props,
   render(h, { props, data, children }) {
-    const tag = !props.href && !props.to ? props.tag : Link
+    const tag = !props.href && !props.to ? props.tag : BLink
 
     const componentData = {
       staticClass: 'badge',
