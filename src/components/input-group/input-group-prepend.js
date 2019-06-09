03ad69e0c0b6a7d0c0a34fb73dd4879fa11181be
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 19:08:35 -0300

    Update input-group-prepend.js

--- src/components/input-group/input-group-prepend.js
+++ src/components/input-group/input-group-prepend.js
@@ -1,6 +1,6 @@
 import Vue from '../../utils/vue'
 import { mergeData } from 'vue-functional-data-merge'
-import InputGroupAddon, { commonProps } from './input-group-addon'
+import { BInputGroupAddon, commonProps } from './input-group-addon'
 
 // @vue/component
 const BInputGroupPrepend = /*#__PURE__*/ Vue.extend({
@@ -10,7 +10,7 @@ const BInputGroupPrepend = /*#__PURE__*/ Vue.extend({
   render(h, { props, data, children }) {
     // pass all our props/attrs down to child, and set`append` to false
     return h(
-      InputGroupAddon,
+      BInputGroupAddon,
       mergeData(data, {
         props: { ...props, append: false }
       }),
