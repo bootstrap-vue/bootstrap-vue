2c210cddb86d6247cfc2237b5b7b9fa161e15eff
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 20:01:02 -0300

    Update index.js

--- src/components/form-file/index.js
+++ src/components/form-file/index.js
@@ -1,4 +1,4 @@
-import BFormFile from './form-file'
+import { BFormFile } from './form-file'
 import { pluginFactory } from '../../utils/plugins'
 
 const FormFilePlugin = /*#__PURE__*/ pluginFactory({
