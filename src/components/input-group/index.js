6c94cf3989dda3a550cb2daa06408e598f009b64
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 19:10:46 -0300

    Update index.js

--- src/components/input-group/index.js
+++ src/components/input-group/index.js
@@ -1,8 +1,8 @@
-import BInputGroup from './input-group'
-import BInputGroupAddon from './input-group-addon'
-import BInputGroupPrepend from './input-group-prepend'
-import BInputGroupAppend from './input-group-append'
-import BInputGroupText from './input-group-text'
+import { BInputGroup } from './input-group'
+import { BInputGroupAddon } from './input-group-addon'
+import { BInputGroupPrepend } from './input-group-prepend'
+import { BInputGroupAppend } from './input-group-append'
+import { BInputGroupText } from './input-group-text'
 import { pluginFactory } from '../../utils/plugins'
 
 const InputGroupPlugin = /*#__PURE__*/ pluginFactory({
