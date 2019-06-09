44485d5b85a1946e8cc32b56c0a040aa7f7d8ca7
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 18:40:50 -0300

    Update index.js

--- src/components/collapse/index.js
+++ src/components/collapse/index.js
@@ -1,5 +1,5 @@
-import BCollapse from './collapse'
-import VBToggle from '../../directives/toggle/toggle'
+import { BCollapse } from './collapse'
+import { VBToggle } from '../../directives/toggle/toggle'
 import { pluginFactory } from '../../utils/plugins'
 
 const CollapsePlugin = /*#__PURE__*/ pluginFactory({
