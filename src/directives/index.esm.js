ecc7d405f1151ef600930264285d51031d844d85
T Morehouse <troymore@nbnet.nb.ca>
2019-06-09 22:35:06 -0300

    Update index.esm.js

--- src/directives/index.esm.js
+++ src/directives/index.esm.js
@@ -1,27 +1,43 @@
 // Index file used for the main builds, which does not include legacy plugin names
 // Once es/ buld is removed, then this file will be renamed to index.js
 import { pluginFactory } from '../utils/plugins'
-import * as directivePlugins from './plugins'
 
-// Export all directive group plugins as named exports (VB<name>Plugin)
-export * from './plugins'
-
-// Named exports of all directives (VB<Name>)
-
-// export * from './toggle'
-export { VBToggle } from './toggle/toggle'
-
-// export * from './modal'
-export { VBModal } from './modal/modal'
-
-// export * from './scrollspy'
-export { VBScrollspy } from './scrollspy/scrollspy'
-
-// export * from './tooltip'
-export { VBTooltip } from './tooltip/tooltip'
-
-// export * from './popover'
-export { VBPopover } from './popover'
+import { default as VBModalPlugin } from './modal'
+import { default as VBPopoverPlugin } from './popover'
+import { default as VBScrollspyPlugin } from './scrollspy'
+import { default as VBTogglePlugin } from './toggle'
+import { default as VBTooltipPlugin } from './tooltip'
 
 // Main plugin for installing all directive plugins
-export const directivesPlugin = /*#__PURE__*/ pluginFactory({ plugins: directivePlugins })
+export const directivesPlugin = /*#__PURE__*/ pluginFactory({
+  plugins: {
+    VBModalPlugin,
+    VBPopoverPlugin,
+    VBScrollspyPlugin,
+    VBTogglePlugin,
+    VBTooltipPlugin
+  }
+})
+
+// Named exports of all directives (VB<Name>) and Plugins (VB<name>Plugin)
+// See src/compinents/index.esm.js for notes/comment
+
+// export * from './modal'
+export { VBModalPlugin } from './modal/modal'
+export { VBModal } from './modal/modal'
+
+// export * from './popover'
+export { VBPopoverPlugin } from './popover'
+export { VBPopover } from './popover/popover'
+
+// export * from './scrollspy'
+export { VBScrollspyPlugin } from './scrollspy'
+export { VBScrollspy } from './scrollspy/scrollspy'
+
+// export * from './toggle'
+export { VBTogglePlugin } from './toggle'
+export { VBToggle } from './toggle/toggle'
+
+// export * from './tooltip'
+export { VBTooltipPlugin } from './tooltip'
+export { VBTooltip } from './tooltip/tooltip'
