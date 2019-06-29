2e4f448e478de0ac283d91125e47142dd35025f7
Jacob Müller <jacob.mueller.elz@gmail.com>
2019-06-29 11:42:30 +0200

    fix typos

--- src/components/table/helpers/default-sort-compare.js
+++ src/components/table/helpers/default-sort-compare.js
@@ -4,10 +4,12 @@ import stringifyObjectValues from './stringify-object-values'
 
 // Default sort compare routine
 //
-// TODO: add option to sort by multiple columns (tri-state per column, plus order of columns in sort)
-//  where sortBy could be an array of objects [ {key: 'foo', sortDir: 'asc'}, {key:'bar', sortDir: 'desc'} ...]
-//  or an array of arrays [ ['foo','asc'], ['bar','desc'] ]
-//  Multisort will most likey be handled in mixin-sort.js by calling this method for each sortBy
+// TODO: Add option to sort by multiple columns (tri-state per column,
+//       plus order of columns in sort)  where sortBy could be an array
+//       of objects [ {key: 'foo', sortDir: 'asc'}, {key:'bar', sortDir: 'desc'} ...]
+//       or an array of arrays [ ['foo','asc'], ['bar','desc'] ]
+//       Multisort will most likely be handled in mixin-sort.js by
+//       calling this method for each sortBy
 
 export default function defaultSortCompare(a, b, sortBy, formatter, localeOpts, locale) {
   let aa = get(a, sortBy, '')
@@ -19,7 +21,7 @@ export default function defaultSortCompare(a, b, sortBy, formatter, localeOpts, 
   aa = isUndefined(aa) || isNull(aa) ? '' : aa
   bb = isUndefined(bb) || isNull(bb) ? '' : bb
   if ((isDate(aa) && isDate(bb)) || (isNumber(aa) && isNumber(bb))) {
-    // Special case for comparing Dates and Numbers
+    // Special case for comparing dates and numbers
     // Internally dates are compared via their epoch number values
     return aa < bb ? -1 : aa > bb ? 1 : 0
   } else {
