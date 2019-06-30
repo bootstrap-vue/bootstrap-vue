14a41c16c095fd978b905a466789e2a27b30e366
Troy Morehouse <troymore@nbnet.nb.ca>
2019-06-30 19:36:53 -0300

    Update docs-publish.sh

--- scripts/docs-publish.sh
+++ scripts/docs-publish.sh
@@ -7,9 +7,8 @@ git config --global user.email vuebootstrap@gmail.com
 git config --global user.name BootstrapVue
 
 mkdir -p ~/.ssh
+chown -R $USER:$USER ~/.ssh
 chmod 600 ~/.ssh
-touch ~/.ssh/known_hosts
-chmod 644 ~/.ssh/known_hosts
 ssh-keyscan github.com >> ~/.ssh/known_hosts
 
 gh-pages -t -d docs-dist -b master -r git@github.com:bootstrap-vue/bootstrap-vue.github.io.git
