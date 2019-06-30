3f008b2060c15c9dd898d871f0e5841129112bc7
Troy Morehouse <troymore@nbnet.nb.ca>
2019-06-30 19:59:06 -0300

    Update docs-publish.sh

--- scripts/docs-publish.sh
+++ scripts/docs-publish.sh
@@ -4,12 +4,7 @@ set -e
 git config --global user.email vuebootstrap@gmail.com
 git config --global user.name BootstrapVue
 
-# mkdir -p ~/.ssh
-# chown -R $USER:$USER ~/.ssh
-# chmod 600 ~/.ssh
-# ssh-keyscan github.com >> ~/.ssh/known_hosts
-
-# generate the docs
+# Generate the docs
 yarn docs-gen
 
 # Publish the docs
