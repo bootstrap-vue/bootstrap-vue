0304e5510d6c9af02ff154b3fd7c03b9bd190ead
Jacob Müller <jacob.mueller.elz@gmail.com>
2019-07-03 21:51:12 +0200

    Update index.vue

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -297,40 +297,40 @@
       </b-card-group>
 
       <b-card class="border-0" body-class="p-4" tag="article" bg-variant="light">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="3em"
-            height="3em"
-            viewBox="0 0 400 400"
-            class="d-block mx-auto mb-2"
-            version="1"
-            aria-hidden="true"
-            focusable="false"
-            role="img"
-          >
-            <title>Nuxt.js logo</title>
-            <g transform="translate(0 49)" fill="none" fill-rule="evenodd">
-              <path
-                fill="#00C58E"
-                d="M123 292l-1-1c-2-4-2-8-2-12H25L167 27l59 107 19-14-59-107c-1-2-8-13-20-13-5 0-13 2-19 13L4 268c-1 2-7 14-1 24 2 5 8 10 21 10h120c-13 0-19-5-21-10z"
-              />
-              <path
-                fill="#108775"
-                d="M395 269L280 62c-2-2-8-13-20-13-5 0-12 2-19 13l-15 24v48l34-59 114 204h-43a20 20 0 0 1-2 12v1c-6 10-19 10-21 10h68c2 0 15 0 21-10 2-5 4-13-2-23z"
-              />
-              <path
-                fill="#2F495E"
-                fill-rule="nonzero"
-                d="M332 292v-1l1-2c1-3 2-7 1-10l-4-11-90-158-13-24h-1l-13 24-91 158-3 11a21 21 0 0 0 2 13c3 5 9 10 21 10h168c3 0 16 0 22-10zM226 134l83 145H144l82-145z"
-              />
-            </g>
-          </svg>
-          <b-card-text class="text-center">
-            BootstrapVue's documentation is created using
-            <span class="bd-text-purple-bright">BootstrapVue</span> components and
-            generated with <a href="https://nuxtjs.org" target="_blank">Nuxt.js</a>
-          </b-card-text>
-       </b-card>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="3em"
+          height="3em"
+          viewBox="0 0 400 400"
+          class="d-block mx-auto mb-2"
+          version="1"
+          aria-hidden="true"
+          focusable="false"
+          role="img"
+        >
+          <title>Nuxt.js logo</title>
+          <g transform="translate(0 49)" fill="none" fill-rule="evenodd">
+            <path
+              fill="#00C58E"
+              d="M123 292l-1-1c-2-4-2-8-2-12H25L167 27l59 107 19-14-59-107c-1-2-8-13-20-13-5 0-13 2-19 13L4 268c-1 2-7 14-1 24 2 5 8 10 21 10h120c-13 0-19-5-21-10z"
+            />
+            <path
+              fill="#108775"
+              d="M395 269L280 62c-2-2-8-13-20-13-5 0-12 2-19 13l-15 24v48l34-59 114 204h-43a20 20 0 0 1-2 12v1c-6 10-19 10-21 10h68c2 0 15 0 21-10 2-5 4-13-2-23z"
+            />
+            <path
+              fill="#2F495E"
+              fill-rule="nonzero"
+              d="M332 292v-1l1-2c1-3 2-7 1-10l-4-11-90-158-13-24h-1l-13 24-91 158-3 11a21 21 0 0 0 2 13c3 5 9 10 21 10h168c3 0 16 0 22-10zM226 134l83 145H144l82-145z"
+            />
+          </g>
+        </svg>
+        <b-card-text class="text-center">
+          BootstrapVue's documentation is created using
+          <span class="bd-text-purple-bright">BootstrapVue</span> components and
+          generated with <a href="https://nuxtjs.org" target="_blank">Nuxt.js</a>
+        </b-card-text>
+      </b-card>
     </b-container>
   </main>
 </template>
