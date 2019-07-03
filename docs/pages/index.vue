c9bcd829200031335014f9dfcf6fe3a8e1e0bf6f
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-03 17:29:49 -0300

    Update index.vue

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -331,6 +331,47 @@
           generated with <a href="https://nuxtjs.org" target="_blank">Nuxt.js</a>
         </b-card-text>
       </b-card>
+
+      <div class="my-4 py-4 text-center">
+        <b-button href="https://opencollective.com/bootstrap-vue#backer" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 40 41"
+            width="1em" height="1em"
+            fill="currentColor"
+            fill-rule="evenodd"
+            aria-hidden="true"
+            focusable="false"
+            role="img"
+          >
+            <path
+              fill-opacity=".4"
+              d="M32.8 21c0 2.4-.8 4.9-2 6.9l5.1 5.1c2.5-3.4 4.1-7.6 4.1-12 0-4.6-1.6-8.8-4-12.2L30.7 14c1.2 2 2 4.3 2 7z"
+            />
+            <path d="M20 33.7a12.8 12.8 0 0 1 0-25.6c2.6 0 5 .7 7 2.1L32 5a20 20 0 1 0 .1 31.9l-5-5.2a13 13 0 0 1-7 2z" />
+          </svg>
+          Become a backer
+        </b-button>
+        <b-button href="https://opencollective.com/bootstrap-vue#sponsor" target="_blank">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 40 41"
+            width="1em" height="1em"
+            fill="currentColor"
+            fill-rule="evenodd"
+            aria-hidden="true"
+            focusable="false"
+            role="img"
+          >
+            <path
+              fill-opacity=".4"
+              d="M32.8 21c0 2.4-.8 4.9-2 6.9l5.1 5.1c2.5-3.4 4.1-7.6 4.1-12 0-4.6-1.6-8.8-4-12.2L30.7 14c1.2 2 2 4.3 2 7z"
+            />
+            <path d="M20 33.7a12.8 12.8 0 0 1 0-25.6c2.6 0 5 .7 7 2.1L32 5a20 20 0 1 0 .1 31.9l-5-5.2a13 13 0 0 1-7 2z" />
+          </svg>
+          Become a sponsor
+        </b-button>
+      </div>
     </b-container>
   </main>
 </template>
