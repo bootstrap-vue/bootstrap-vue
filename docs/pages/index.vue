918cfb4a3dc74d04f9e46b20069d42b2d012acda
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-03 17:34:48 -0300

    Update index.vue

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -337,7 +337,8 @@
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 40 41"
-            width="1em" height="1em"
+            width="1em"
+            height="1em"
             fill="currentColor"
             fill-rule="evenodd"
             aria-hidden="true"
@@ -356,7 +357,8 @@
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 40 41"
-            width="1em" height="1em"
+            width="1em"
+            height="1em"
             fill="currentColor"
             fill-rule="evenodd"
             aria-hidden="true"
