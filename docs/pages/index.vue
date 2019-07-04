dbca64e00db291be563d91313ef7bce5353090cf
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-04 00:35:53 -0300

    Update index.vue

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -346,8 +346,28 @@
     </section>
 
     <section class="bv-section-white">
-      <b-container tag="article" class="py-5 text-center">
-        <h5 class="text-center bd-text-purple-bright">Support BootstrapVue on Open Collective</h5>
+      <b-container tag="article" class="text-center">
+        <h5 class="text-center bd-text-purple-bright">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 40 41"
+            width="2.5em"
+            height="2.5em"
+            class="d-block mx-auto mb-2"
+            fill="currentColor"
+            fill-rule="evenodd"
+            aria-hidden="true"
+            focusable="false"
+            role="img"
+          >
+            <path
+              fill-opacity=".4"
+              d="M32.8 21c0 2.4-.8 4.9-2 6.9l5.1 5.1c2.5-3.4 4.1-7.6 4.1-12 0-4.6-1.6-8.8-4-12.2L30.7 14c1.2 2 2 4.3 2 7z"
+            />
+            <path d="M20 33.7a12.8 12.8 0 0 1 0-25.6c2.6 0 5 .7 7 2.1L32 5a20 20 0 1 0 .1 31.9l-5-5.2a13 13 0 0 1-7 2z" />
+          </svg>
+          Support BootstrapVue on Open Collective
+        </h5>
         <b-button
           href="https://opencollective.com/bootstrap-vue#backer"
           size="sm"
