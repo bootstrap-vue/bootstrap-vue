b4366feab2b0a1a922a949733365080d632407b9
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-03 21:27:54 -0300

    Update index.vue

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -298,7 +298,7 @@
             </b-card-text>
 
             <b-card-text class="text-center">
-              Open sourced on <a href="https://github.com/bootstrap-vue/bootstrap-vue/" target="_blank">GitHub</a>, MIT Lisence
+              Open sourced on <a href="https://github.com/bootstrap-vue/bootstrap-vue/" target="_blank">GitHub</a>, MIT License
             </b-card-text>
           </b-card>
         </b-card-group>
