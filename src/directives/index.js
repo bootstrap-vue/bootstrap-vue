1d7fcab8d7aa20c1611c096ccde787300938af11
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-05 20:48:44 -0300

    Update index.js

--- src/directives/index.js
+++ src/directives/index.js
@@ -16,32 +16,3 @@ export const directivesPlugin = /*#__PURE__*/ pluginFactory({
     VBTooltipPlugin
   }
 })
-
-// Named exports of all directives (VB<Name>) and Plugins (VB<name>Plugin)
-//
-// Webpack 4 has optimization difficulties with re-eport of re-exports, so
-// we import the directives individulaly here for better tree shaking,
-//
-// Webpack v5 fixes the optimizations with re-export of re-exports so this
-// can be reverted back to `export * from './scrollspy'` when Webpack v5 is released.
-// https://github.com/webpack/webpack/pull/9203 (available in Webpack v5.0.0-alpha.15)
-
-// export * from './modal'
-export { VBModalPlugin } from './modal'
-export { VBModal } from './modal/modal'
-
-// export * from './popover'
-export { VBPopoverPlugin } from './popover'
-export { VBPopover } from './popover/popover'
-
-// export * from './scrollspy'
-export { VBScrollspyPlugin } from './scrollspy'
-export { VBScrollspy } from './scrollspy/scrollspy'
-
-// export * from './toggle'
-export { VBTogglePlugin } from './toggle'
-export { VBToggle } from './toggle/toggle'
-
-// export * from './tooltip'
-export { VBTooltipPlugin } from './tooltip'
-export { VBTooltip } from './tooltip/tooltip'
