f3b36e2cce11592aa9b5b38a3a231153931edde9
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-04 22:26:39 -0300

    Update footer.vue

--- docs/components/footer.vue
+++ docs/components/footer.vue
@@ -1,20 +1,34 @@
 <template>
   <footer class="bd-footer text-muted">
-    <b-container fluid>
-      <ul class="bd-footer-links">
-        <li><b-link to="/">Home</b-link></li>
-        <li><b-link to="/docs/">Documentation</b-link></li>
-        <li>
-          <a href="https://github.com/bootstrap-vue/bootstrap-vue" target="_blank">
-            GitHub
-          </a>
-        </li>
-        <li>
-          <a href="https://discord.gg/j2Mtcny" target="_blank">
-            Discord Chat Forum
-          </a>
-        </li>
-      </ul>
+    <b-container>
+      <b-row>
+        <b-col cols="12" sm="auto" class="text-center text-sm-left mr-sm-auto">
+          <b-img src="~static/logo.svg" width="100" alt="BootstrapVue logo" fluid></b-img>
+        </b-col>
+
+        <b-col cols="12" sm="auto">
+          <h5 class="bd-text-purple-bright">Documentation</h5>
+          <ul class="list-unstyled ml-3">
+            <li><b-link to="/" exact>Home</b-link></li>
+            <li><b-link to="/docs" exact>Getting started</b-link></li>
+            <li><b-link to="/docs/components" exact>Components</b-link></li>
+            <li><b-link to="/docs/directives" exact>Directives</b-link></li>
+            <li><b-link to="/docs/reference" exact>Reference</b-link></li>
+            <li><b-link to="/docs/misc" exact>Miscelaneous</b-link></li>
+            <li><b-link to="/play" exact>Playground</b-link></li>
+          </ul>
+        </b-col>
+
+        <b-col cols="12" sm="auto">
+          <h5 class="bd-text-purple-bright">Communnity</h5>
+          <ul class="list-unstyled ml-3">
+            <li><a href="https://github.com/bootstrap-vue/bootstrap-vue" target="_blank">GitHub</a></li>
+            <li><a href="https://discord.gg/j2Mtcny" target="_blank">Discord chat</a></li>
+            <li><a href="https://twitter.com/BootstrapVue" target="_blank">Twitter</a></li>
+            <li><a href="https://opencollective.com/bootstrap-vue/" target="_blank">Open Collective</a></li>
+          </ul>
+        </b-col>
+      </b-row>
 
       <p>
         Designed and built with all the love in the world. Maintained by the
