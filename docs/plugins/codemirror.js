c31b2fb63043b6a8c38c97a1dc1a8a6d608fb013
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-16 03:36:49 -0300

    Update codemirror.js

--- docs/plugins/codemirror.js
+++ docs/plugins/codemirror.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
 
-// Lazy load coadmorror, so that it apperas in a separate chunk
-Vue.component('codemirror', () = import('../components/codemirror'  /* webpackChunkName: "codemirror" */))
+// Lazy load coadmorror, so that it appears in a separate chunk
+Vue.component('codemirror', () => {
+  return {
+    component: import('../components/codemirror' /* webpackChunkName: "codemirror" */),
+    loading: {
+      render(h) {
+        return h('div', { style: { minHeight: '300px' } }, [h()])
+      }
+    }
+  }
+})
