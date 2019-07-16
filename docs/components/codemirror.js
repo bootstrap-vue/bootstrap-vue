a7786aef45490e0fa8ed278af2ab366012d13152
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-16 11:56:31 -0300

    Update codemirror.js

--- docs/components/codemirror.js
+++ docs/components/codemirror.js
@@ -1,15 +1,3 @@
-let CodeMirror
-if (typeof window !== 'undefined') {
-  CodeMirror = require('codemirror')
-  require('codemirror/mode/javascript/javascript')
-  // require('codemirror/mode/shell/shell')
-  require('codemirror/mode/vue/vue')
-  require('codemirror/mode/htmlmixed/htmlmixed')
-  require('codemirror/addon/edit/closetag')
-  require('codemirror/addon/edit/closebrackets')
-  require('codemirror/addon/fold/xml-fold')
-}
-
 export default {
   name: 'CodeMirror',
   props: {
