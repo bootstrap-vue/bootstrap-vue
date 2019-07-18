10b89eb592fa55966092c4010e264b8545847e16
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-17 22:15:58 -0300

    Update tooltip.class.js

--- src/utils/tooltip.class.js
+++ src/utils/tooltip.class.js
@@ -919,6 +919,11 @@ class ToolTip {
 
   // Enter handler
   enter(e) {
+    // Prevent showing if tip/popover is on a dropdown and the menu is open
+    if (hasClass(this.$element, DROPDOWN_CLASS) && select(DROPDOWN_OPEN_SELECTOR, this.$element)) {
+      /* istanbul ignore next */
+      return
+    }
     if (e) {
       this.$activeTrigger[e.type === 'focusin' ? 'focus' : 'hover'] = true
     }
