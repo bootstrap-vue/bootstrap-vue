b87d86f175b4fa82b13afc9fca1903a91a9b6920
Troy Morehouse <troymore@nbnet.nb.ca>
2019-07-28 15:33:42 -0300

    Update index.vue

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -573,8 +573,8 @@ $bv-angle-padding-md: 6rem;
   margin-left: auto;
   margin-right: auto;
   margin-top: 0;
-  transform-style: preserve-3d;
-  animation: logo-splash-alpha 0.35s 1 ease-in-out;
+  // transform-style: preserve-3d;
+  // animation: logo-splash-alpha 0.35s 1 ease-in-out;
 
   .logo-purple-v {
     transform-style: preserve-3d;
@@ -625,18 +625,22 @@ $bv-angle-padding-md: 6rem;
 
 @keyframes logo-splash-purple {
   0% {
+    opacity: 0.1;
     transform: traslateY(-8%);
   }
   100% {
+    opacity: 1;
     transform: traslateY(0);
   }
 }
 
 @keyframes logo-splash-green {
   0% {
+    opacity: 0.1;
     transform: traslateY(8%);
   }
   100% {
+    opacity: 1;
     transform: traslateY(0);
   }
 }
