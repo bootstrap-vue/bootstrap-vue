e6a934042eaa723e89381adb317a48eebd36419c
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-02 16:31:51 -0300

    feat(table): create table child element components

---
+++ src/components/table/helpers/table-cell.js
@@ -0,0 +1,72 @@
+import Vue from '../../../utils/vue'
+import normalizeSlotMixin from '../../../mixins/normalize-slot'
+
+export const BTableCell = /*#__PURE__*/ Vue.extend({
+  name: 'BTableCell',
+  inheritAttrs: false
+  mixins: [nomalizeSlotMixin],
+  inject: {
+    bvTable: {
+      default: null
+    },
+    bvTableHead: {
+      default: null
+    },
+    bvTableFoot: {
+      default: null
+    },
+  },
+  props: {
+    header: {
+      type: Boolean,
+      default: false
+    },
+    variant: {
+      type: String,
+      default: null
+    },
+    colspan: {
+      type: [Number, String],
+      default: null,
+    },
+    rowspan: {
+      type: [Number, String],
+      default: null
+    }
+  },
+  computed: {
+    isDark() {
+      return this.bvTable && this.bvTable.dark
+    },
+    cellClasses() {
+      return [
+        this.variant ? `${this.isDark ? 'bg' : 'table'}-${this.variant}` : null
+      ]
+    }
+  },
+  render(h) {
+    return h(
+      this.header ? 'th' : 'td',
+      {
+        class: this.cellClasses,
+        attrs: {
+          role: this.bvTableHead || this.bvTableFoot
+            ? 'columnheader'
+            : this.header ? 'rowheader' : 'cell',
+          scope: this.bvTableHead || this.bvTableFoot ?
+            ? 'col'
+            : this.header ? 'row' : null,
+          colspan: this.colspan,
+          rowspan: this.rowspan,
+          // Allow users to override role/scope plus add other attributes
+          ...this.$attrs
+        },
+        // Transfer any native listeners
+        on: this.$listeners
+      },
+      this.noramlizeSlot('default')
+    )
+  }
+})
+
+export default BTableCell
