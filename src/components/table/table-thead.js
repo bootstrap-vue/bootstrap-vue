4c65f1f1eba31af0746ccb17cf9926be15d9b568
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-02 18:53:33 -0300

    Create table-thead.js

---
+++ src/components/table/table-thead.js
@@ -0,0 +1,48 @@
+import Vue from '../../utils/vue'
+import normalizeSlotMixin from '../../mixins/normalize-slot'
+
+export const props = {
+  headVariant: {
+    type: String, // supported values: 'lite', 'dark', or null
+    default: null
+  }
+}
+
+// @vue/component
+export const BTableThead = /*#__PURE__*/ Vue.extend({
+  name: 'BTableThead',
+  mixins: [normalizeSlotMixin],
+  inheritAttrs: false,
+  provide() /* istanbul ignore next: until tests are written */ {
+    return {
+      bvTableThead: this
+    }
+  },
+  inject: {
+    bvTable: {
+      default: null
+    }
+  },
+  props: props,
+  computed() {
+    theadClasses() /* istanbul ignore next: until tests are written */ {
+      return [this.headVariant ? `thead-${this.headVariant}` : null]
+    },
+    theadAttrs() /* istanbul ignore next: until tests are written */ {
+      return { role: 'rowgroup', ...this.$attrs }
+    }
+  },
+  render(h) /* istanbul ignore next: until tests are written */ {
+    return(
+      'thead',
+      {
+        class: this.theadClasses,
+        attrs: this.theadAttrs,
+        // Pass down any native listeners
+        on: this.$listeners
+      },
+      this.normalizeSlot('default', {})
+    )
+  }
+})
+  
