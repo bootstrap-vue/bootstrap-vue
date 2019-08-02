d24a9bec130801a1314905a0f4e960f69a0cce96
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-02 17:59:49 -0300

    Update index.js

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -2,12 +2,21 @@ import { BTable } from './table'
 import { BTableLite } from './table-lite'
 import { BTableSimple } from './table-simple'
 import { BTableTd } from './table-td'
+import { BTableTh } from './table-th'
 import { pluginFactory } from '../../utils/plugins'
 
 const TablePlugin = /*#__PURE__*/ pluginFactory({
-  components: { BTable, BTableLite, BTableSimple, BTableTd, BTd: BTableTd }
+  components: {
+    BTable,
+    BTableLite,
+    BTableSimple,
+    BTableTd,
+    BTd: BTableTd,
+    BTableTh,
+    BTh: BTableTh
+  }
 })
 
-export { TablePlugin, BTable, BTableLite, BTableSimple, BTableTd }
+export { TablePlugin, BTable, BTableLite, BTableSimple, BTableTd, BTableTh }
 
 export default TablePlugin
