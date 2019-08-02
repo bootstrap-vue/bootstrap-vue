08840ce96cb6be1740e351cb48d84464eb846153
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-02 19:57:31 -0300

    Update table-tbody.js

--- src/components/table/table-tbody.js
+++ src/components/table/table-tbody.js
@@ -1,7 +1,16 @@
 import Vue from '../../utils/vue'
 import normalizeSlotMixin from '../../mixins/normalize-slot'
 
-export const props = {}
+export const props = {
+  tbodyTransitionProps: {
+    type: Object
+    // default: undefined
+  },
+  tbodyTransitionHandlers: {
+    type: Object
+    // default: undefined
+  }
+}
 
 // @vue/component
 export const BTableTbody = /*#__PURE__*/ Vue.extend({
@@ -20,17 +29,28 @@ export const BTableTbody = /*#__PURE__*/ Vue.extend({
   },
   props: props,
   computed: {
+    isTransitionGroup() {
+      return this.tbodyTransitionProps || this.tbodyTransitionHandlers
+    },
     tbodyAttrs() /* istanbul ignore next: until tests are written */ {
       return { role: 'rowgroup', ...this.$attrs }
+    },
+    tbodyProps() {
+      return this.tbodyTransitionProps ? { ...this.tbodyTransitionProps, tag: 'tbody' } : {}
+    },
+    tbodyListeners() {
+      const handlers = this.tbodyTransitionHandlers || {}
+      return { this.$listeners, ...handlers }
     }
   },
   render(h) /* istanbul ignore next: until tests are written */ {
     return h(
-      'tbody',
+      this.isTransitionGroup ? 'transition-group' : 'tbody',
       {
+        props: this.tbodyProps,
         attrs: this.theadAttrs,
-        // Pass down any native listeners
-        on: this.$listeners
+        // Pass down any listeners
+        on: this.tbodyListeners
       },
       this.normalizeSlot('default', {})
     )
