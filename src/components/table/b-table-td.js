13dec7adca8b67d3e8976aabbd5d81189593f754
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-02 17:22:16 -0300

    Create b-table-td.js

---
+++ src/components/table/b-table-td.js
@@ -0,0 +1,15 @@
+import Vue from '../../utils/vue'
+import { omit } from '../../utils/object'
+import { props as cellProps, BTableCell } from './helpers/table-cell'
+
+export const props = omit(cellProps, ['header'])
+
+// @vue/component
+export const BTableTd = /#__pure__/ Vue.extend({
+  functional: true,
+  props: props,
+  render(h, { props, data, children }) {
+    data.props = { ...props, header: false }
+    return h(BTableCell, data, children)
+  }
+})
