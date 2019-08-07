275cb32d686b109f4250bf50e56732859b05b69b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-07 13:13:09 -0300

    Update default-sort-compare.js

--- src/components/table/helpers/default-sort-compare.js
+++ src/components/table/helpers/default-sort-compare.js
@@ -17,22 +17,21 @@ const defaultSortCompare = (a, b, sortBy, sortDesc, formatter, localeOpts, local
     aa = formatter(aa, sortBy, a)
     bb = formatter(bb, sortBy, b)
   }
-  // Special handling when `null`/`undefined` sorted last
-  if (nullLast && isUndefinedOrNull(aa) && !isUndefinedOrNull(bb)) {
-    return 1
-  } else if (nullLast && !isUndefinedOrNull(aa) && isUndefinedOrNull(bb)) {
-    return -1
-  }
   aa = isUndefinedOrNull(aa) ? '' : aa
   bb = isUndefinedOrNull(bb) ? '' : bb
   if ((isDate(aa) && isDate(bb)) || (isNumber(aa) && isNumber(bb))) {
     // Special case for comparing dates and numbers
     // Internally dates are compared via their epoch number values
     return aa < bb ? -1 : aa > bb ? 1 : 0
-  } else {
-    // Do localized string comparison
-    return stringifyObjectValues(aa).localeCompare(stringifyObjectValues(bb), locale, localeOpts)
+  } else if (nullLast && aa === '' && bb !== '') {
+      // Special case when sorting null/undefined/empty string last
+      return 1
+  } else if (nullLast && aa !== '' && bb === '') {
+      // Special case when sorting null/undefined/empty string last
+      return -1
   }
+  // Do localized string comparison
+  return stringifyObjectValues(aa).localeCompare(stringifyObjectValues(bb), locale, localeOpts)
 }
 
 export default defaultSortCompare
