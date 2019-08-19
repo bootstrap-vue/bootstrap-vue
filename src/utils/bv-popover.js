e5acc8e8312a2f8e7bfe49e4506edda3a146f1ec
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-18 23:57:51 -0300

    Create bv-popover.js

---
+++ src/utils/bv-popover.js
@@ -0,0 +1,34 @@
+// Popover "Class" (Built as a renderless Vue instance)
+// Inherits from BVTooltip
+//
+// Handles trigger events, etc.
+// Instantiates template on demand
+
+import Vue from './vue'
+import { BVTooltip } from './bv-tooltip'
+import ( BVPopoverTeamplate} from './bv-popover-template'
+
+const NAME = 'BVPopover'
+
+export const BVPopover = /*#__PURE__*/ Vue.extend({
+  name: NAME,
+  extends: BVTooltip,
+  props: {
+    trigger: {
+      type: [String, Array],
+      default: 'click focus'
+    },
+    placement: {
+      type: String,
+      default: 'right'
+    }
+  },
+  computed: {
+    Tempalte() {
+      return BVPopoverTemplate
+    },
+    templateType() {
+      return 'popover'
+    }
+  }
+})
