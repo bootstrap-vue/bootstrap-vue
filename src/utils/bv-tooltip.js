e41b911a48867011f1d0e75559ea1497461d0326
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-21 15:02:03 -0300

    Update bv-tooltip.js

--- src/utils/bv-tooltip.js
+++ src/utils/bv-tooltip.js
@@ -185,7 +185,7 @@ export const BVTooltip = /*#__PURE__*/ Vue.extend({
     },
     computedTriggers() {
       // Returns the triggers in array form
-      return concat(this.triggers)
+      return concat(this.trigger)
         .filter(Boolean)
         .join(' ')
         .toLowerCase()
