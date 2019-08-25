b3df052fbd0e76c6220c1006120ebdbee87159d2
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-25 08:21:33 -0300

    Update tooltip.js

--- src/directives/tooltip/tooltip.js
+++ src/directives/tooltip/tooltip.js
@@ -186,7 +186,6 @@ const applyTooltip = (el, bindings, vnode) => {
     el[BV_TOOLTIP].__bv_prev_data__ = null
   }
   const data = {
-    target: el,
     title: config.title,
     triggers: config.trigger,
     placement: config.placement,
@@ -201,7 +200,7 @@ const applyTooltip = (el, bindings, vnode) => {
   }
   if (!looseEqual(data, el[BV_TOOLTIP].__bv_prev_data__)) {
     // We only update the instance if data has changed
-    el[BV_TOOLTIP].updateData(data)
+    el[BV_TOOLTIP].updateData({ ...data, target: el })
     el[BV_TOOLTIP].__bv_prev_data__ = data
   }
 }
