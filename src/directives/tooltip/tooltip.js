d062d265ad08624649fd1235f9ba2d22098079f8
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-25 05:13:06 -0300

    Update tooltip.js

--- src/directives/tooltip/tooltip.js
+++ src/directives/tooltip/tooltip.js
@@ -1,9 +1,8 @@
-import Popper from 'popper.js'
-import ToolTip from '../../utils/tooltip.class'
-import warn from '../../utils/warn'
+import { BVToolTip } from '../../utils/bv-tooltip'
+import { concat } from '../../utils//array'
 import { getComponentConfig } from '../../utils/config'
 import { isBrowser } from '../../utils/env'
-import { isFunction, isObject, isString } from '../../utils/inspect'
+import { isFunction, isObject, isString, isUndefined } from '../../utils/inspect'
 import { keys } from '../../utils/object'
 
 // Key which we use to store tooltip object on element
@@ -14,22 +13,24 @@ const validTriggers = {
   focus: true,
   hover: true,
   click: true,
-  blur: true
+  blur: true,
+  manual: true
 }
 
 // Directive modifier test regular expressions. Pre-compile for performance
-const htmlRE = /^html$/
+const htmlRE = /^html$/i
 const noFadeRE = /^nofade$/i
-const placementRE = /^(auto|top(left|right)?|bottom(left|right)?|left(top|bottom)?|right(top|bottom)?)$/
-const boundaryRE = /^(window|viewport|scrollParent)$/
-const delayRE = /^d\d+$/
-const offsetRE = /^o-?\d+$/
-const variantRE = /^v-.+$/
+const placementRE = /^(auto|top(left|right)?|bottom(left|right)?|left(top|bottom)?|right(top|bottom)?)$/i
+const boundaryRE = /^(window|viewport|scrollParent)$/i
+const delayRE = /^d\d+$/i
+const delayShowRE = /^ds\d+$/i
+const delayHideRE = /^dh\d+$/i
+const offsetRE = /^o-?\d+$/i
+const variantRE = /^v-.+$/i
 
 // Build a ToolTip config based on bindings (if any)
 // Arguments and modifiers take precedence over passed value config object
-/* istanbul ignore next: not easy to test */
-const parseBindings = bindings => /* istanbul ignore next: not easy to test */ {
+const parseBindings = (bindings, vnode) => /* istanbul ignore next: not easy to test */ {
   // We start out with a basic config
   const NAME = 'BTooltip'
   let config = {
@@ -37,7 +38,9 @@ const parseBindings = bindings => /* istanbul ignore next: not easy to test */ {
     boundary: String(getComponentConfig(NAME, 'boundary')),
     boundaryPadding: parseInt(getComponentConfig(NAME, 'boundaryPadding'), 10) || 0,
     variant: getComponentConfig(NAME, 'variant'),
-    customClass: getComponentConfig(NAME, 'customClass')
+    customClass: getComponentConfig(NAME, 'customClass'),
+    noFade: false,
+    offset: 0
   }
 
   // Process bindings.value
@@ -50,6 +53,21 @@ const parseBindings = bindings => /* istanbul ignore next: not easy to test */ {
   } else if (isObject(bindings.value)) {
     // Value is config object, so merge
     config = { ...config, ...bindings.value }
+  }
+
+  // If title is not provided, try title attribute
+  if (isUndefined(config.title)) {
+    // try attribute
+    const data = vnode.data || {}
+    config.title = data.attrs && data.attrs.title ? data.attrs.title : ''
+  }
+
+  // Normalize delay
+  if (!isObject(config.delay)) {
+    config.delay = {
+      show: config.delay,
+      hide: config.delay
+    }
   }
 
   // If argument, assume element ID of container element
@@ -66,25 +84,28 @@ const parseBindings = bindings => /* istanbul ignore next: not easy to test */ {
       config.html = true
     } else if (noFadeRE.test(mod)) {
       // No animation
-      config.animation = false
+      config.noFade = true
     } else if (placementRE.test(mod)) {
       // Placement of tooltip
       config.placement = mod
     } else if (boundaryRE.test(mod)) {
       // Boundary of tooltip
+      mod = mod === 'scrollparent' ? 'scrollParent' : mod
       config.boundary = mod
     } else if (delayRE.test(mod)) {
       // Delay value
       const delay = parseInt(mod.slice(1), 10) || 0
-      if (delay) {
-        config.delay = delay
-      }
+      config.delay.show = delay
+      config.delay.hide = delay
+    } else if (delayShowRE.test(mod)) {
+      // Delay show value
+      config.delay.show = parseInt(mod.slice(2), 10) || 0
+    } else if (delayHideRE.test(mod)) {
+      // Delay hide value
+      config.delay.hide = parseInt(mod.slice(2), 10) || 0
     } else if (offsetRE.test(mod)) {
       // Offset value, negative allowed
-      const offset = parseInt(mod.slice(1), 10) || 0
-      if (offset) {
-        config.offset = offset
-      }
+      config.offset = parseInt(mod.slice(1), 10) || 0
     } else if (variantRE.test(mod)) {
       // Variant
       config.variant = mod.slice(2) || null
@@ -96,12 +117,17 @@ const parseBindings = bindings => /* istanbul ignore next: not easy to test */ {
   const selectedTriggers = {}
 
   // Parse current config object trigger
-  const triggers = isString(config.trigger) ? config.trigger.trim().split(/\s+/) : []
-  triggers.forEach(trigger => {
-    if (validTriggers[trigger]) {
-      selectedTriggers[trigger] = true
-    }
-  })
+  const triggers = concat(config.trigger)
+    .filter(Boolean)
+    .join(' ')
+    .toLowerCase()
+    .trim()
+    .split(/\s+/)
+    .forEach(trigger => {
+      if (validTriggers[trigger]) {
+        selectedTriggers[trigger] = true
+      }
+    })
 
   // Parse modifiers for triggers
   keys(validTriggers).forEach(trigger => {
@@ -117,32 +143,47 @@ const parseBindings = bindings => /* istanbul ignore next: not easy to test */ {
     config.trigger = 'focus'
   }
   if (!config.trigger) {
-    // Remove trigger config
+    // Remove trigger config to use default
     delete config.trigger
   }
 
+  // If title is a string, and the html option is true, then
+  // generate a div container with innerHTML set
+  if (isString(config.title) && config.html) {
+    config.title = vnode.context.$createElement('div', { domProps: { innerHTML: config.title } })
+  }
+
+  // return the config
   return config
 }
 
-// Add or update ToolTip on our element
+// Add ToolTip on our element
 const applyTooltip = (el, bindings, vnode) => {
   if (!isBrowser) {
     /* istanbul ignore next */
     return
   }
-  if (!Popper) {
-    // Popper is required for ToolTips to work
-    /* istanbul ignore next */
-    warn('v-b-tooltip: Popper.js is required for ToolTips to work')
-    /* istanbul ignore next */
-    return
+  const config = parseBindings(bindings, vnode)
+  if (!el[BV_TOOLTIP]) {
+    el[BV_TOOLTIP] = new BVTooltip({
+      parent: vnode.context
+    })
   }
-  const config = parseBindings(bindings)
-  if (el[BV_TOOLTIP]) {
-    el[BV_TOOLTIP].updateConfig(config)
-  } else {
-    el[BV_TOOLTIP] = new ToolTip(el, config, vnode.context)
-  }
+  // The updateData method only updates values that have changed
+  el[BV_TOOLTIP].updateData({
+    target: el,
+    title: config.title,
+    triggers: config.trigger,
+    placement: config.placement,
+    fallbackPlacement: config.fallbackPlacement,
+    variant: config.variant,
+    customClass: config.customClass,
+    container: config.container,
+    boundary: config.boundary,
+    delay: config.delay,
+    offset: config.offset,
+    noFade: config.noFade
+  })
 }
 
 // Remove ToolTip on our element
@@ -150,8 +191,8 @@ const removeTooltip = el => {
   if (el[BV_TOOLTIP]) {
     el[BV_TOOLTIP].destroy()
     el[BV_TOOLTIP] = null
-    delete el[BV_TOOLTIP]
   }
+  delete el[BV_TOOLTIP]
 }
 
 /*
@@ -161,18 +202,8 @@ export const VBTooltip = {
   bind(el, bindings, vnode) {
     applyTooltip(el, bindings, vnode)
   },
-  inserted(el, bindings, vnode) {
-    applyTooltip(el, bindings, vnode)
-  },
   update(el, bindings, vnode) /* istanbul ignore next: not easy to test */ {
-    if (bindings.value !== bindings.oldValue) {
-      applyTooltip(el, bindings, vnode)
-    }
-  },
-  componentUpdated(el, bindings, vnode) /* istanbul ignore next: not easy to test */ {
-    if (bindings.value !== bindings.oldValue) {
-      applyTooltip(el, bindings, vnode)
-    }
+    applyTooltip(el, bindings, vnode)
   },
   unbind(el) {
     removeTooltip(el)
