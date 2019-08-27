cc555188b458bf300b250326e39117bcd47884a4
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-27 14:37:21 -0300

    Update popover.js

--- src/directives/popover/popover.js
+++ src/directives/popover/popover.js
@@ -1,4 +1,4 @@
-import { BVPopover } from '../../utils/bv-popover'
+import { BVPopover } from '../../omponents/popover/helpers/bv-popover'
 import looseEqual from '../../utils/loose-equal'
 import { concat } from '../../utils/array'
 import { getComponentConfig } from '../../utils/config'
