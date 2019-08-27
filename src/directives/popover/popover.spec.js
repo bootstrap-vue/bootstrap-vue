eb228a727f4a4f259a73ffa993b49d811193b87f
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-27 14:47:11 -0300

    Update popover.spec.js

--- src/directives/popover/popover.spec.js
+++ src/directives/popover/popover.spec.js
@@ -1,7 +1,7 @@
 import { mount, createLocalVue as CreateLocalVue } from '@vue/test-utils'
 import { waitNT, waitRAF } from '../../../tests/utils'
-import { BVPopover } from '../../utils/bv-popover'
 import { VBPopover } from './popover'
+import { BVPopover } from '../../components/popover/helpers/bv-popover'
 
 // Key which we use to store tooltip object on element
 const BV_POPOVER = '__BV_Popover__'
