513c3c057236e18708da62cf16e2d29a9a6b0ba2
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-29 17:38:41 -0300

    Update table-primarykey.spec.js

--- src/components/table/table-primarykey.spec.js
+++ src/components/table/table-primarykey.spec.js
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import BTable from './table'
+import { BTable } from './table'
 
 const testItems = [{ a: 1, b: 2, c: 3 }, { a: 5, b: 5, c: 6 }, { a: 7, b: 8, c: 9 }]
 
