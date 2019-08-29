7691b5066845e7d09783ba1b8af5114c8c786d5a
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-28 23:22:04 -0300

    Update get-scope-id.js
    add comment re what `_scopeId` is

--- src/utils/get-scope-id.js
+++ src/utils/get-scope-id.js
@@ -1,3 +1,6 @@
+// This method returns the parent component's scoped style attribute name `data-v-xxxxxxx`
+// The `_scopeId` options property is added by vue-loader when using scoped styles
+
 const getScopeId = (vm, defaultValue = null) => {
   return vm ? vm.$options._scopeId || defaultValue : defaultValue
 }
