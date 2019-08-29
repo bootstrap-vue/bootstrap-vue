8f3eb90a3451256cd70449a2a0b5d22d5afc6b26
Troy Morehouse <troymore@nbnet.nb.ca>
2019-08-29 16:29:43 -0300

    Update tab.spec.js

--- src/components/tabs/tab.spec.js
+++ src/components/tabs/tab.spec.js
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import { waitNT, waitRAF } from '../../../tests/utils'
-import BTab from './tab'
+import { BTab } from './tab'
 
 describe('tab', () => {
   it('default has expected classes, attributes and structure', async () => {
