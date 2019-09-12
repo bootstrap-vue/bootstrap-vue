7b320859b22c812664c175714bc09a923198dcb9
Troy Morehouse <troymore@nbnet.nb.ca>
2019-09-12 20:07:06 -0300

    Update backers-donors.vue

--- docs/components/backers-donors.vue
+++ docs/components/backers-donors.vue
@@ -1,33 +1,39 @@
 <template>
   <div>
     <template v-if="backers.length > 0">
-      <h3 class="mx-auto">Backers</h3>
+      <h5 class="mx-auto">Backers</h5>
       <div class="backer-container d-flex flex-wrap mx-n2 text-center font-weight-bold">
         <div v-for="backer in backers" :key="backer.slug" class="m-2 position-relative">
           <div class="img-thumbnail bg-light mx-auto d-flex align-items-center justify-content-center overflow-hidden">
             <b-img-lazy :src="backer.imageUrl" fluid block aria-hidden="true"></b-img-lazy>
           </div>
-          <h4 class="h6 pt-2">
+          <h6 class="pt-2">
             <b-link v-if="backer.url" :href="backer.url" class="stretched-link text-reset">
               {{ backer.name }}
             </b-link>
             <template v-else>
               {{ backer.name }}
             </template>
-          </h4>
+          </h6>
         </div>
       </div>
     </template>
 
     <template v-if="donors.length > 0">
-      <h3 class="mx-auto">Donors</h3>
+      <h5 class="mx-auto">Donors</h5>
       <div class="donor-container d-flex flex-wrap mx-n1 text-center">
         <div v-for="donor in donors" :key="donor.slug" class="m-1 position-relative">
           <div class="img-thumbnail d-flex align-items-center justify-content-center overflow-hidden">
             <b-link v-if="donor.url" :href="donor.url" class="stretched-link" :title="donor.name">
               <b-img-lazy :src="donor.imageUrl" fluid block :alt="donor.name"></b-img-lazy>
             </b-link>
-            <b-img-lazy v-else :src="donor.imageUrl" fluid block :alt="donor.name"></b-img-lazy>
+            <b-img-lazy
+              v-else
+              :src="donor.imageUrl"
+              :alt="donor.name"
+              fluid
+              block 
+            ></b-img-lazy>
           </div>
         </div>
       </div>
@@ -86,19 +92,20 @@
         xhr.send()
       },
       processOcNodes(nodes = []) {
-        return nodes.map(n => {
-          return {
-            name: n.fromAccount.name,
-            slug: n.fromAccount.slug,
-            imageUrl: n.fromAccount.imageUrl,
-            website: n.fromAccount.website,
-            amount: n.totalDonations,
-            status: n.status,
-            frequency: n.frequency,
-            tier: n.tier.slug,
-            date: new Date(n.createdAt)
-          }
-        })
+        return nodes
+          .map(n => {
+            return {
+              name: n.fromAccount.name,
+              slug: n.fromAccount.slug,
+              imageUrl: n.fromAccount.imageUrl,
+              website: n.fromAccount.website,
+              amount: n.totalDonations,
+              status: n.status,
+              frequency: n.frequency,
+              tier: n.tier.slug,
+              date: new Date(n.createdAt)
+            }
+          })
       },
       sortCompare(a = {}, b = {}) {
         // sort first by amount, then by date
