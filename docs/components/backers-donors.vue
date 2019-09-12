37ea1de20f94a88d498573ab0c68273a946e3580
Troy Morehouse <troymore@nbnet.nb.ca>
2019-09-12 20:14:20 -0300

    Update backers-donors.vue

--- docs/components/backers-donors.vue
+++ docs/components/backers-donors.vue
@@ -54,89 +54,89 @@
 </style>
 
 <script>
-  const baseURL = 'https://rest.opencollective.com/v2/bootstrap-vue/orders/incoming/'
+const baseURL = 'https://rest.opencollective.com/v2/bootstrap-vue/orders/incoming/'
 
-  export default {
-    data() {
-      return {
-        backers: [],
-        donors: []
-      }
-    },
-    mounted() {
-      // Backers are people/organizations with recurring (active) donations
-      this.requestOC(this.processBackers, 'active')
-      // Donors are  are people/organizations with one-time (paid) donations
-      this.requestOC(this.processDonors, 'paid')
-    },
-    methods: {
-      requestOC(cb, type = 'active') {
-        // Do the async request
-        const ocURL = `${baseURL}${type}?limit=100`
-        const xhr = new XMLHttpRequest()
+export default {
+  data() {
+    return {
+      backers: [],
+      donors: []
+    }
+  },
+  mounted() {
+    // Backers are people/organizations with recurring (active) donations
+    this.requestOC(this.processBackers, 'active')
+    // Donors are  are people/organizations with one-time (paid) donations
+    this.requestOC(this.processDonors, 'paid')
+  },
+  methods: {
+    requestOC(cb, type = 'active') {
+      // Do the async request
+      const ocURL = `${baseURL}${type}?limit=100`
+      const xhr = new XMLHttpRequest()
 
-        xhr.open('GET', ocURL, true)
-        xhr.addEventListener('load', function () {
-          if (xhr.readyState !== 4) {
-            return
+      xhr.open('GET', ocURL, true)
+      xhr.addEventListener('load', () => {
+        if (xhr.readyState !== 4) {
+          return
+        }
+        if (xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText) || {}
+          cb(this.processOcNodes(response.nodes), null)
+        } else {
+          // We just return an empty node list rather than spew an error
+          // eslint-disable-next-line standard/no-callback-literal
+          cb([], xhr.statusText)
+        }
+      })
+      // Initiate the request
+      xhr.send()
+    },
+    processOcNodes(nodes = []) {
+      return nodes.map(n => {
+        return {
+          name: n.fromAccount.name,
+          slug: n.fromAccount.slug,
+          imageUrl: n.fromAccount.imageUrl,
+          website: n.fromAccount.website,
+          amount: n.totalDonations,
+          status: n.status,
+          frequency: n.frequency,
+          tier: n.tier.slug,
+          date: new Date(n.createdAt)
+        }
+      })
+    },
+    sortCompare(a = {}, b = {}) {
+      // sort first by amount, then by date
+      return (a.amount || 0) - (b.amount || 0) || (a.date || 0) - (b.date || 0)
+    },
+    processBackers(backers = []) {
+      // Backers are provided in reverse chronological order
+      // so we sort by larger amount first, then by date
+      // Limit to top 10 backers
+      this.backers = backers
+        .filter(b => b.status === 'ACTIVE')
+        .sort(this.sortCompare)
+        .slice(0, 10)
+    },
+    processDonors(donors = []) {
+      // Donors are provided in reverse chronological order,
+      // but donors can be listed more than once (for each individual donation),
+      // although the totalDonations is the same on each entry.
+      // We sort by larger amount first, then by date
+      // Limit to top 20 most recent donors
+      this.donors = donors
+        .filter(d => d.status === 'PAID')
+        .reduce((arr, d) => {
+          if (arr.map(i => i.slug).indexOf(d.slug) === -1) {
+            arr.push(d)
           }
-          if (xhr.status === 200) {
-            const response = JSON.parse(xhr.responseText) || {}
-            cb(processOcNodes(response.nodes))
-          } else {
-            // We just return an empty node list rather than spew an error
-            cb([])
-          }
-        })
-        // Initiate the request
-        xhr.send()
-      },
-      processOcNodes(nodes = []) {
-        return nodes
-          .map(n => {
-            return {
-              name: n.fromAccount.name,
-              slug: n.fromAccount.slug,
-              imageUrl: n.fromAccount.imageUrl,
-              website: n.fromAccount.website,
-              amount: n.totalDonations,
-              status: n.status,
-              frequency: n.frequency,
-              tier: n.tier.slug,
-              date: new Date(n.createdAt)
-            }
-          })
-      },
-      sortCompare(a = {}, b = {}) {
-        // sort first by amount, then by date
-        return (a.amount || 0) - (b.amount || 0) || (a.date || 0) - (b.date || 0)
-      },
-      processBackers(backers = []) {
-        // Backers are provided in reverse chronological order
-        // so we sort by larger amount first, then by date
-        // Limit to top 10 backers
-        this.backers = backers
-          .filter(b => b.status === 'ACTIVE')
-          .sort(this.sortCompare)
-          .slice(0, 10)
-      },
-      processDonors(donors = []) {
-        // Donors are provided in reverse chronological order,
-        // but donors can be listed more than once (for each individual donation),
-        // although the totalDonations is the same on each entry.
-        // We sort by larger amount first, then by date
-        // Limit to top 20 most recent donors
-        this.donors = donors
-          .filter(d => d.status === 'PAID')
-          .reduce((arr, d) => {
-            if (arr.map(i => i.slug).indexOf(d.slug) === -1) {
-              arr.push(d)
-            }
-            return arr
-          }, [])
-          .sort(this.sortCompare)
-          .slice(0, 20)
-      }
+          return arr
+        }, [])
+        .sort(this.sortCompare)
+        .slice(0, 20)
     }
   }
+}
 </script>
