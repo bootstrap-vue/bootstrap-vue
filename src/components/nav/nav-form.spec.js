ce507b8dfc96f364795e8b326c7e951a0275920f
Troy Morehouse <troymore@nbnet.nb.ca>
2019-09-15 11:18:01 -0300

    Update nav-form.spec.js

--- src/components/nav/nav-form.spec.js
+++ src/components/nav/nav-form.spec.js
@@ -5,10 +5,17 @@ describe('nav > nav-form', () => {
   it('has expected default structure', async () => {
     const wrapper = mount(BNavForm)
 
-    expect(wrapper.is('form')).toBe(true)
+    expect(wrapper.is('li')).toBe(true)
     expect(wrapper.classes()).toContain('form-inline')
     expect(wrapper.classes().length).toBe(1)
+
+    const $form = wrapper.find('form')
+    expect($form.exists()).toBe(true)
+    expect($form.classes()).toContain('form-inline')
+    expect($form.classes().length).toBe(1)
     expect(wrapper.text()).toEqual('')
+
+    wrapper.destroy()
   })
 
   it('renders default slot content', async () => {
@@ -18,9 +25,69 @@ describe('nav > nav-form', () => {
       }
     })
 
-    expect(wrapper.is('form')).toBe(true)
+    expect(wrapper.is('li')).toBe(true)
     expect(wrapper.classes()).toContain('form-inline')
     expect(wrapper.classes().length).toBe(1)
-    expect(wrapper.text()).toEqual('foobar')
+
+    const $form = wrapper.find('form')
+    expect($form.exists()).toBe(true)
+    expect($form.classes()).toContain('form-inline')
+    expect($form.text()).toEqual('foobar')
+
+    wrapper.destroy()
+  })
+
+  it('applies ID to form when prop ID is set', async () => {
+    const wrapper = mount(BNavForm, {
+      propsData: {
+        id: 'baz'
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+
+    expect(wrapper.is('li')).toBe(true)
+    expect(wrapper.classes()).toContain('form-inline')
+    expect(wrapper.classes().length).toBe(1)
+
+    const $form = wrapper.find('form')
+    expect($form.exists()).toBe(true)
+    expect($form.classes()).toContain('form-inline')
+    expect($form.text()).toEqual('foobar')
+    expect($form.attributes('id')).toEqual('baz')
+
+    wrapper.destroy()
+  })
+
+  it('listeners are bound to form element', async () => {
+    const onSubmit = jest.$fn()
+    const wrapper = mount(BNavForm, {
+      propsData: {
+        id: 'baz'
+      },
+      on: {
+        submit: onSubmit
+      },
+      slots: {
+        default: 'foobar'
+      }
+    })
+
+    expect(wrapper.is('li')).toBe(true)
+    expect(wrapper.classes()).toContain('form-inline')
+    expect(wrapper.classes().length).toBe(1)
+
+    const $form = wrapper.find('form')
+    expect($form.exists()).toBe(true)
+    expect($form.classes()).toContain('form-inline')
+    expect($form.text()).toEqual('foobar')
+
+    expect(onSubmit).not.toHaveBeenCalled()
+
+    $form.trigger('submit')
+    expect(onSubmit).toHaveBeenCalled()
+
+    wrapper.destroy()
   })
 })
