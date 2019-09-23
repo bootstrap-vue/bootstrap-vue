427b72879e4e00bcb4f81da8ac811b0092f9aea2
Troy Morehouse <troymore@nbnet.nb.ca>
2019-09-22 21:32:31 -0300

    Update safe-types.js

--- src/utils/safe-types.js
+++ src/utils/safe-types.js
@@ -7,3 +7,5 @@ import { hasWindowSupport } from './env'
 const w = hasWindowSupport ? window : {}
 
 export const HTMLElement = hasWindowSupport ? w.HTMLElement : class HTMLElement extends Object {}
+
+export const File = hasWindowSupport ? w.File : class File extends Object {}
