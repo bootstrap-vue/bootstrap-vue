14d01e8eff76a28988b0add30bddfb331d8c6f42
Troy Morehouse <troymore@nbnet.nb.ca>
2019-09-25 01:22:36 -0300

    Update th.js

--- src/components/table/th.js
+++ src/components/table/th.js
@@ -1,17 +1,13 @@
 import Vue from '../../utils/vue'
-import { omit } from '../../utils/object'
-import { props as cellProps, BTableCell } from './helpers/table-cell'
-
-export const props = omit(cellProps, ['header'])
+import { BTableCell } from './helpers/table-cell'
 
 // @vue/component
 export const BTh = /*#__PURE__*/ Vue.extend({
   name: 'BTh',
-  functional: true,
-  props,
-  render(h, { props, data, children }) {
-    // `data` already includes any listeners
-    data.props = { ...props, header: true }
-    return h(BTableCell, data, children)
+  extends: BTableCell,
+  computed: {
+    tag() {
+      return 'th'
+    }
   }
 })
