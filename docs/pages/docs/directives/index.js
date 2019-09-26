6bf3e6fb7520d1aac8daf154e0a6193bfc0afb5d
Troy Morehouse <troymore@nbnet.nb.ca>
2019-09-25 22:24:54 -0300

    Update index.js

--- docs/pages/docs/directives/index.js
+++ docs/pages/docs/directives/index.js
@@ -1,6 +1,8 @@
 import SectionToc from '~/components/section-toc.vue'
+import docsMixin from '~/plugins/docs-mixin'
 
 export default {
   name: 'BVDocsDirectivesIndex',
+  mixins: [docsMixin],
   extends: SectionToc
 }
