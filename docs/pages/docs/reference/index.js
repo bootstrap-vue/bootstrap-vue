653a8362825f505706b14e79d5c84ad8f9adef9e
Troy Morehouse <troymore@nbnet.nb.ca>
2019-09-25 22:25:54 -0300

    Update index.js

--- docs/pages/docs/reference/index.js
+++ docs/pages/docs/reference/index.js
@@ -1,6 +1,8 @@
 import SectionToc from '~/components/section-toc.vue'
+import docsMixin from '~/plugins/docs-mixin'
 
 export default {
   name: 'BVDocsReferenceIndex',
+  mixins: [docsMixin],
   extends: SectionToc
 }
