6040beb8578f9e4f135639a3e7dd4cc11a65fc09
Troy Morehouse <troymore@nbnet.nb.ca>
2019-09-27 00:46:32 -0300

    Update componentdoc.vue

--- docs/components/componentdoc.vue
+++ docs/components/componentdoc.vue
@@ -86,7 +86,7 @@
         <p class="mb-0 small">
           Props that support HTML strings (<code class="notranslate" translate="no">*-html</code>) can
           be vulerable to
-          <b-link href="https://en.wikipedia.org/wiki/Cross-site_scripting" class="alert-link" target=_blank">
+          <b-link href="https://en.wikipedia.org/wiki/Cross-site_scripting" class="alert-link" target="_blank">
             Cross Site Scripting (XSS) attacks
           </b-link>
           when using user supplied values. You must properly
