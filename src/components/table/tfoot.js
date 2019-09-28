ea60ae21ab9df35cd75c3bde9027a535fdabf95e
Troy Morehouse <troymore@nbnet.nb.ca>
2019-09-28 02:05:36 -0300

    Update tfoot.js

--- src/components/table/tfoot.js
+++ src/components/table/tfoot.js
@@ -42,7 +42,7 @@ export const BTfoot = /*#__PURE__*/ Vue.extend({
     },
     isResponsive() {
       // Sniffed by <b-tr> / <b-td> / <b-th>
-      return !this.isStacked && this.bvTable.isResponsive
+      return this.bvTable.isResponsive
     },
     isStickyHeader() {
       // Sniffed by <b-tr> / <b-td> / <b-th>
