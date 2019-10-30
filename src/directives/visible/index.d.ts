0d3804abef166f7cb6ed279fbc970a65757ed6bc
Troy Morehouse <troymore@nbnet.nb.ca>
2019-10-30 18:58:54 -0300

    Update index.d.ts

--- src/directives/visible/index.d.ts
+++ src/directives/visible/index.d.ts
@@ -7,5 +7,5 @@ import { BvPlugin } from '../../'
 // Plugin
 export declare const VBVisiblePlugin: BvPlugin
 
-// Directive: v-b-popover
+// Directive: v-b-visible
 export declare const VBVisible: DirectiveOptions
