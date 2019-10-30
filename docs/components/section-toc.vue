07e025687e75b098888ba03c99eb7e30b74e387f
Troy Morehouse <troymore@nbnet.nb.ca>
2019-10-30 19:02:21 -0300

    Update section-toc.vue

--- docs/components/section-toc.vue
+++ docs/components/section-toc.vue
@@ -16,7 +16,7 @@
         >
           <strong class="text-primary">{{ page.title }}</strong> &mdash;
           <b-badge v-if="page.new" variant="success">NEW</b-badge>
-          <b-badge v-if="page.version" variant="secondary">{{ page.version }}</b-badge>
+          <b-badge v-if="page.version" variant="secondary">v{{ page.version }}</b-badge>
           <span class="text-muted">{{ page.description }}</span>
         </b-list-group-item>
       </b-list-group>
