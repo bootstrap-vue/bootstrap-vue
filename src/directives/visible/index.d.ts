d28fc4cbe2574a6aed49fa47f1a25dbfd112bcf2
Troy Morehouse <troymore@nbnet.nb.ca>
2019-10-30 04:31:59 -0300

    Create index.d.ts

---
+++ src/directives/visible/index.d.ts
@@ -0,0 +1,11 @@
+//
+// VBVisible
+//
+import Vue, { DirectiveOptions } from 'vue'
+import { BvPlugin } from '../../'
+
+// Plugin
+export declare const VBVisiblePlugin: BvPlugin
+
+// directive: v-b-popover
+export declare const VBVisible: DirectiveOptions
