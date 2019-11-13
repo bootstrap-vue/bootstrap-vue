4c58fdcc5d202930f1940bd4e486ca7db44dd6be
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-13 13:18:50 -0400

    Update bv-collapse.js

--- src/utils/bv-collapse.js
+++ src/utils/bv-collapse.js
@@ -5,13 +5,15 @@
 //   during the enter/leave transition phases only
 import Vue from './vue'
 import { mergeData } from 'vue-functional-data-merge'
-import { getBCR, reflow } from './dom'
+import { getBCR, reflow, requestAF } from './dom'
 
 // Transition event handler helpers
 const onEnter = el => {
   el.style.height = 0
-  reflow(el)
-  el.style.height = `${el.scrollHeight}px`
+  requestAF(() => {
+    reflow(el)
+    el.style.height = `${el.scrollHeight}px`
+  })
 }
 
 const onAfterEnter = el => {
