017073c80fedd0581b41893f87f90b292e492344
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-15 17:44:11 -0400

    Update has-listener.js

--- src/mixins/has-listener.js
+++ src/mixins/has-listener.js
@@ -1,6 +1,7 @@
 // Mixin to determine if an event listener has been registered
 // either via `v-on:name` (in the parent) or programmatically
 // via `vm.$on('name', ...)`
+// See: https://github.com/vuejs/vue/issues/10825
 import { isArray } from '../utils/array'
 import { isUndefined } from '../utils/inspect'
 
