9965086fa6a6a33728e3d787579ab86be9a4bcc2
Jacob Müller <jacob.mueller.elz@gmail.com>
2019-11-17 15:08:55 +0100

    Update has-listener.js

--- src/mixins/has-listener.js
+++ src/mixins/has-listener.js
@@ -2,9 +2,9 @@
 // either via `v-on:name` (in the parent) or programmatically
 // via `vm.$on('name', ...)`
 // See: https://github.com/vuejs/vue/issues/10825
-import { isArray } from '../utils/array'
-import { isUndefined } from '../utils/inspect'
+import { isArray, isUndefined } from '../utils/inspect'
 
+// @vue/component
 export default {
   methods: {
     hasListener(name) {
@@ -14,7 +14,8 @@ export default {
       // Note this property is not part of the public Vue API, but it is
       // the only way to determine if a listener was added via `vm.$on`
       const $events = this._events || {}
-      // Registered listeners in this._events are always an array (but might be 0 length)
+      // Registered listeners in `this._events` are always an array,
+      // but might be zero length
       return !isUndefined($listeners[name]) || (isArray($events[name]) && $events[name].length > 0)
     }
   }
