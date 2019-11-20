0811f3c953ef841a150e053e18f0b2fe251a7c5b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-19 23:03:24 -0400

    Update collapse.js

--- src/components/collapse/collapse.js
+++ src/components/collapse/collapse.js
@@ -232,7 +232,7 @@ export const BCollapse = /*#__PURE__*/ Vue.extend({
   },
   render(h) {
     const scope = {
-      close: this.toggle
+      close: () => (this.show = false)
     }
     const content = h(
       this.tag,
