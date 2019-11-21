9fdb66c40b0c0218c483bb2e56836fe9abcc80e8
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-20 22:40:24 -0400

    Create index.d.ts

---
+++ src/components/form-tags/index.d.ts
@@ -0,0 +1,14 @@
+//
+// Form Tags
+//
+import Vue from 'vue'
+import { BvPlugin, BvComponent } from '../../'
+
+// Plugin
+export declare const FormTagsPlugin: BvPlugin
+
+// Component: b-form-tags
+export declare class BFormTags extends BvComponent {
+  focus: () => void
+  blur: () => void
+}
