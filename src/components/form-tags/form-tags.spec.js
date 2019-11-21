75b14d2f92ae268f6d69efa345d436589b07aa09
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-20 23:42:33 -0400

    Update form-tags.spec.js

--- src/components/form-tags/form-tags.spec.js
+++ src/components/form-tags/form-tags.spec.js
@@ -9,4 +9,15 @@ describe('form-tags', () => {
 
     wrapper.destroy()
   })
+
+  it('has tags when value is set', async () => {
+    const wrapper = mount(BFormTags, {
+      propsData: {
+        value: ['apple', 'orange']
+      }
+    })
+    expect(wrapper.is('div')).toBe(true)
+
+    wrapper.destroy()
+  })
 })
