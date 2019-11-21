d6e877580ee0012ae68c652f43ff275e8713c0c7
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-21 12:32:22 -0400

    Update form-tags.spec.js

--- src/components/form-tags/form-tags.spec.js
+++ src/components/form-tags/form-tags.spec.js
@@ -45,6 +45,49 @@ describe('form-tags', () => {
     })
     expect(wrapper.is('div')).toBe(true)
 
+    const $hidden = wrapper.findAll('input[type=hidden]')
+    expect($hidden.length).toBe(2)
+
+    expect($hidden.at(0).attributes('value')).toEqual('apple')
+    expect($hidden.at(0).attributes('name')).toEqual('foo')
+    expect($hidden.at(1).attributes('value')).toEqual('orange')
+    expect($hidden.at(1).attributes('name')).toEqual('foo')
+
+    wrapper.destroy()
+  })
+
+  it('adds new tags from user input', async () => {
+    const wrapper = mount(BFormTags, {
+      propsData: {
+        value: ['apple', 'orange']
+      }
+    })
+    expect(wrapper.is('div')).toBe(true)
+
+    expect(wrapper.vm.tags).toEqual(['apple', 'orange'])
+    expect(wrapper.vm.newTag).toEqual('')
+
+    const $input = wrapper.find('input')
+
+    expect($input.exists()).toBe(true)
+    expect($input.element.value).toBe('')
+
+    $input.element.value = 'pear'
+    $input.trigger('input')
+    expect(wrapper.vm.newTag).toEqual('pear')
+    expect(wrapper.vm.tags).toEqual(['apple', 'orange'])
+    $input.trigger('change')
+    expect(wrapper.vm.newTag).toEqual('')
+    expect(wrapper.vm.tags).toEqual(['apple', 'orange', 'pear'])
+
+    $input.element.value = 'peach'
+    $input.trigger('input')
+    expect(wrapper.vm.newTag).toEqual('peach')
+    expect(wrapper.vm.tags).toEqual(['apple', 'orange', 'pear'])
+    $input.trigger('keydown.enter')
+    expect(wrapper.vm.newTag).toEqual('')
+    expect(wrapper.vm.tags).toEqual(['apple', 'orange', 'pear', 'peach'])
+
     wrapper.destroy()
   })
 })
