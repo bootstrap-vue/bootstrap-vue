e6d586b63e714fa90f40f1d5d2429b666b989eed
bencelaszlo <bencelaszlo@protonmail.com>
2019-11-26 17:13:12 +0100

    docs: fix typo in compontendoc.vue.

--- docs/components/componentdoc.vue
+++ docs/components/componentdoc.vue
@@ -121,7 +121,7 @@
       <div v-if="hasHtmlProps" class="alert alert-warning">
         <p class="mb-0 small">
           <strong>Caution:</strong> Props that support HTML strings
-          (<code class="notranslate" translate="no">*-html</code>) can be vulerable to
+          (<code class="notranslate" translate="no">*-html</code>) can be vulnerable to
           <b-link href="https://en.wikipedia.org/wiki/Cross-site_scripting" class="alert-link" target="_blank">
             Cross Site Scripting (XSS) attacks
           </b-link>
