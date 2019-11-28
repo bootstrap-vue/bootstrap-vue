85e05e20e2dfbf00ee0b6948295d8c6d453a3cf4
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-28 01:15:34 -0400

    Update to-string.js

--- src/utils/to-string.js
+++ src/utils/to-string.js
@@ -1,14 +1,4 @@
-import { isArray, isPlainObject, isUndefinedOrNull } from './inspect'
-
-/**
- * Convert a value to a string that can be rendered.
- */
-const toString = (val, spaces = 2) => {
-  return isUndefinedOrNull(val)
-    ? ''
-    : isArray(val) || (isPlainObject(val) && val.toString === Object.prototype.toString)
-      ? JSON.stringify(val, null, spaces)
-      : String(val)
-}
+// To be removed in favour of importing from `utils/string`
+import { toString } from './string'
 
 export default toString
