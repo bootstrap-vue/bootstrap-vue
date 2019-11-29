13c20bddf3face484b36f351018a9762cc1cad1b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-29 16:30:49 -0400

    Update listen-on-document.js

--- src/mixins/listen-on-document.js
+++ src/mixins/listen-on-document.js
@@ -10,32 +10,34 @@ const PROP = '$_bv_documentHandlers_'
 
 // @vue/component
 export default {
-  beforeCreate() {
+  beforeMount() {
     // Declare non-reactive property
     // Object of arrays, keyed by event name,
     // where value is an array of handlers
-    this[PROP] = {}
-  },
-  beforeDestroy() {
-    if (isBrowser) {
-      const items = this[PROP]
+    // Prop will be defined on client only
+    // We check for browser support in case testing
+    // in vue-test-utils without `attachToDocoument: true`
+    this[PROP] = isBrowser ? {} : null
+    // Set up our beforeDestroy handler (client only)
+    this.$once('hook:beforeDestroy', () => {
+      const items = this[PROP] || {}
       // Immediately delete this[PROP] to prevent the
       // listenOn/Off methods from running (which may occur
-      // due to requestAnimationFrame delays)
+      // due to requestAnimationFrame/transition delays)
       delete this[PROP]
       // Remove all registered event handlers
       keys(items).forEach(evtName => {
         const handlers = items[evtName] || []
         handlers.forEach(handler => eventOff(document, evtName, handler, eventOptions))
       })
-    }
+    })
   },
   methods: {
     listenDocument(on, evtName, handler) {
       on ? this.listenOnDocument(evtName, handler) : this.listenOffDocument(evtName, handler)
     },
     listenOnDocument(evtName, handler) {
-      if (isBrowser && this[PROP] && isString(evtName) && isFunction(handler)) {
+      if (this[PROP] && isString(evtName) && isFunction(handler)) {
         this[PROP][evtName] = this[PROP][evtName] || []
         if (!arrayIncludes(this[PROP][evtName], handler)) {
           this[PROP][evtName].push(handler)
@@ -44,7 +46,7 @@ export default {
       }
     },
     listenOffDocument(evtName, handler) {
-      if (isBrowser && this[PROP] && isString(evtName) && isFunction(handler)) {
+      if (this[PROP] && isString(evtName) && isFunction(handler)) {
         eventOff(document, evtName, handler, eventOptions)
         this[PROP][evtName] = (this[PROP][evtName] || []).filter(h => h !== handler)
       }
