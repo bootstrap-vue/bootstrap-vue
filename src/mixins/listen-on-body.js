451fc5e99cb6a572ada11d0bb8fffc2656654d3d
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-29 01:44:24 -0400

    chore: create listen on body mixin

---
+++ src/mixins/listen-on-body.js
@@ -0,0 +1,43 @@
+import { concat } from '../utils/array'
+import { eventOff, eventOn } from '../utils/dom'
+import { isBrowser } from '../utils/env'
+import { isString, isFunction } from '../utils/inspect'
+import { keys } from '../utils/object'
+
+const eventOptions = { passive: true, capture: false }
+
+const PROP = '$_bv_bodyHandlers_'
+
+// @vue/component
+export default {
+  beforeCreate() {
+    // Declare non-reactive property
+    // Object of arrays, keyed by event name,
+    // where value is an array of handlers
+    this[PROP] = {}
+  },
+  beforeDestroy() {
+    if (isBrowser) {
+      keys(this[PROP]).forEach(type => {
+        (this[PROP][type] || []).forEach(handler => {
+          this.listenOffBody(type, handler)
+        })
+      })
+      delete this[PROP]
+    }
+  },
+  methods: {
+    listenOnBody(type, handler) {
+      if (isBrowser && isString(type) && isFunction(handler)) {
+        this[PROP][type] = concat(this[PROP][type] || [], handler)
+        eventOn(document.body, type, handler, eventOptions)
+      }
+    },
+    listenOffBody(type, handler) {
+      if (isBrowser && isString(type) && isFunction(handler)) {
+        eventOff(document.body, type, handler, eventOptions)
+        this[PROP][type] = (this[PROP][type] || []).filter(h => h !== handler)
+      }
+    }
+  }
+}
