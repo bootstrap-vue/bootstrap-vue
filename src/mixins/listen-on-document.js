5843e5772525fecc74a57f0e2e711bc3bd6d4137
Troy Morehouse <troymore@nbnet.nb.ca>
2019-11-29 02:30:30 -0400

    Create listen-on-document.js

---
+++ src/mixins/listen-on-document.js
@@ -0,0 +1,44 @@
+import { concat } from '../utils/array'
+import { eventOff, eventOn } from '../utils/dom'
+import { isBrowser } from '../utils/env'
+import { isString, isFunction } from '../utils/inspect'
+import { keys } from '../utils/object'
+
+const eventOptions = { passive: true, capture: false }
+
+const PROP = '$_bv_documentHandlers_'
+
+// @vue/component
+export default {
+  beforeCreate() {
+    // Declare non-reactive property
+    // Object of arrays, keyed by event name,
+    // where value is an array of handlers
+    this[PROP] = {}
+  },
+  beforeDestroy() {
+    if (isBrowser) {
+      keys(this[PROP]).forEach(type => {
+        const handlers = this[PROP][type] || []
+        handlers.forEach(handler => {
+          this.listenOffWindow(type, handler)
+        })
+      })
+      delete this[PROP]
+    }
+  },
+  methods: {
+    listenOnDocument(type, handler) {
+      if (isBrowser && isString(type) && isFunction(handler)) {
+        this[PROP][type] = concat(this[PROP][type] || [], handler)
+        eventOn(document, type, handler, eventOptions)
+      }
+    },
+    listenOffDocument(type, handler) {
+      if (isBrowser && isString(type) && isFunction(handler)) {
+        eventOff(document, type, handler, eventOptions)
+        this[PROP][type] = (this[PROP][type] || []).filter(h => h !== handler)
+      }
+    }
+  }
+}
