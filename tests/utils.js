f46c0dec5fc018e802bb4b887a79b22aa670d57b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-03 09:54:32 -0400

    Update utils.js

--- tests/utils.js
+++ tests/utils.js
@@ -2,8 +2,8 @@
  * Utils for testing
  */
 
-import FakeTransition from './fake-tansition'
-import FakeTransitionGroup from './fake-tansition-group'
+import FakeTransition from './stubs/fake-tansition'
+import FakeTransitionGroup from './stubs/fake-tansition-group'
 
 export const waitNT = ctx => new Promise(resolve => ctx.$nextTick(resolve))
 export const waitRAF = () => new Promise(resolve => requestAnimationFrame(resolve))
