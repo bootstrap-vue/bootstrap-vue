03385344a01599e758c6aad1711f9488c2a51125
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-11 00:23:23 -0400

    Update compile-js.js

--- docs/utils/compile-js.js
+++ docs/utils/compile-js.js
@@ -1,11 +1,56 @@
 // Utility for tranpiling ES6 code into ES5 for playground and v-play
+// Imported only on demand when needed
 import { transform, disableScriptTags } from '@babel/standalone'
 
+// Babel broke the standalone version via PR https://github.com/babel/babel/pull/10420
+// Which assumes the browser supports String.prototype.trimLeft/Right
+// IE 11 does not support either, and polyfill.io does not polyfill them
+// So we do it here (as this file is only loaded if we need transpilation):
+if (typeof window !== 'undefined') {
+  const String = window.String
+  const Proto = String.prototype
+
+  // Ensure we have a `trimStart` method
+  ((obj, prop) => {
+    if (!(prop in obj && obj[prop])) {
+      const rx = /^\s+/
+      obj[prop] = obj.trimLeft || function() {
+        return this.replace(rx, '')
+      }
+    }
+  })(Proto,'trimStart')
+
+  // Ensure we have a `trimLeft` method
+  ((obj, prop)=>{
+    if (!(prop in obj && obj[prop])) {
+      obj[prop] = obj.trimStart
+    }
+  })(Proto,'trimLeft')
+
+  // Ensure we have a `trimEnd` method
+  ((obj, prop) => {
+    if (!(prop in obj && obj[prop])) {
+      const rx = /\s+$/
+      obj[prop] = obj.trimRight || function() {
+        return this.replace(rx, '')
+      }
+    }
+  })(Proto,'trimEnd')
+
+  // Ensure we have a `trimRight` method
+  ((obj, prop) => {
+    if (!(prop in obj && obj[prop])) {
+      obj[prop] = obj.trimEnd
+    }
+  })(Proto,'trimRight')
+}
+
+// Prevent Babel/Standalone from processing <script> tag insertions
 if (typeof window !== 'undefined' && window && window.removeEventListener) {
-  // Prevent Babel/Standalone from processing <script> tag insertions
   disableScriptTags()
 }
 
+// Our babel transform options
 const transformOptions = {
   sourceType: 'script',
   presets: ['es2015', 'es2016', 'es2017'],
@@ -16,10 +61,10 @@ const transformOptions = {
   ]
 }
 
+// Our traspilation compiler method
 export default function compileJs(code) {
   if (!code) {
     return ''
   }
-  const result = transform(code, transformOptions)
-  return result && result.code ? result.code : ''
+  return transform(code, transformOptions).code || ''
 }
