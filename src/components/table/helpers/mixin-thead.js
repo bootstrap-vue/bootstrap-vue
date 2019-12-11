90b0d837e5e27ec7e996e183c447b67dfe1a7ba4
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-11 17:08:37 -0400

    Update mixin-thead.js

--- src/components/table/helpers/mixin-thead.js
+++ src/components/table/helpers/mixin-thead.js
@@ -135,7 +135,7 @@ export default {
           clearSelected
         }
         const content = this.normalizeSlot(slotNames, scope) ||
-          field.labelHtml ? h('div', { domProps: htmlOrText(field.labelHtml) }) : field.label
+          (field.labelHtml ? h('div', { domProps: htmlOrText(field.labelHtml) }) : field.label)
         const srLabel = sortLabel ? h('span', { staticClass: 'sr-only' }, sortLabel) : null
         // Return the header cell
         return h(BTh, data, [content, srLabel].filter(identity))
