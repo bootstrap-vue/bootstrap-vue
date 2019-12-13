2396fb4f119df05a6d8bb201911160dac9f7ca72
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-12 21:17:49 -0400

    Update icons-table.vue

--- docs/components/icons-table.vue
+++ docs/components/icons-table.vue
@@ -31,7 +31,7 @@
       <b-col
         v-for="icon in filteredIcons"
         :key="`_icon_${icon.name}`"
-        v-b-tooltip.hover
+        v-b-tooltip.hover.ds200
         tag="li"
         :title="icon.component"
         class="mb-3 text-center"
