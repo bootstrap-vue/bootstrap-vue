07aa2fea1b25eb3e0fc8cc9acfb03a5ed6ad6d29
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-13 01:32:33 -0400

    Create index.d.ts

---
+++ src/icons/index.d.ts
@@ -0,0 +1,14 @@
+//
+// Icons
+//
+import Vue from 'vue'
+import { BvPlugin, BvComponent } from '../'
+
+// Plugin
+export declare const IconsPlugin: BvPlugin
+
+// Component: b-icon
+export declare class BIcon extends BvComponent {}
+
+// TODO:
+//   Add all individual BIcon{IConName} components
