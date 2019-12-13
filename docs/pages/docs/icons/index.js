87a7cc98ac599a2430f6d0d12fa4aee2e8fdbcbd
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-12 22:07:29 -0400

    Update index.js

--- docs/pages/docs/icons/index.js
+++ docs/pages/docs/icons/index.js
@@ -1,6 +1,9 @@
+import AnchoredHeading from '~/components/anchored-heading'
+import Componentdoc from '~/components/componentdoc'
+import IconsTable from '~/components/icons-table'
+import Importdoc from '~/components/importdoc'
 import Main from '~/components/main'
 import Section from '~/components/section'
-import IconsTable from '~/components/icons-table'
 import docsMixin from '~/plugins/docs-mixin'
 import { icons as iconsMeta } from '~/content'
 import readme from '~/../src/icons/README.md'
@@ -9,11 +12,33 @@ export default {
   name: 'BDVIcons',
   layout: 'docs',
   // We use a string template here so that the docs README can do interpolation
-  template: `<Main><Section>${readme}</Section></Main>`,
+  template: `
+    <Main class="bd-components">
+      <Section play>${readme}</Section>
+      <Section class="bd-component-reference">
+        <AnchoredHeading id="component-reference">Component reference</AnchoredHeading>
+        <template v-for="c in meta.components">
+          <Componentdoc
+            :key="c.component"
+            :component="c.component"
+            :events="c.events"
+            :root-event-listeners="c.rootEventListeners"
+            :slots=c.slots"
+            :aliases="c.aliases"
+            :props="c.propsMeta"
+            :version="c.version"
+          ></Componentdoc>
+        </template>
+        <Importdoc :meta="meta"></ImportDoc>
+      </Section>
+    </Main>`,
   components: {
+    AnchoredHeading,
+    Componentdoc,
+    IconsTable,
+    Importdoc,
     Main,
-    Section,
-    IconsTable
+    Section
   },
   mixins: [docsMixin],
   data() {
