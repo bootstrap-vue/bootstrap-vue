d3a555d4532f1ba6bc3d6be44e361dae8b9cc42c
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-15 19:42:02 -0400

    Update index.js

--- src/icons/index.js
+++ src/icons/index.js
@@ -4,15 +4,15 @@ import { pluginFactory } from '../utils/plugins'
 import { BIcon } from './icon'
 import * as iconComponents from './icons'
 
-// This re-export is not currently used, as Webpack 4 has
+// Thes re-exports are not currently used, as Webpack 4 has
 // issues with tree shaking re-exports of re-exports
 // /src/index.js does a single level re-export * from ./icons/icons/icons
 // export * from './icons'
+//
+// export { BIcon }
 
-export {
-  BIcon,
-  // Plugin
-  IconsPlugin: /*#__PURE__*/ pluginFactory({ components: { BIcon, ...iconComponents } }),
-  // Array of icon names (for use by the docs)
-  iconNames: keys(iconComponents)
-}
+// Plugin
+export const IconsPlugin = /*#__PURE__*/ pluginFactory({ components: { BIcon, ...iconComponents } })
+
+// Array of icon names (for use by the docs)
+eport const iconNames = /*#__PURE__*/ keys(iconComponents)
