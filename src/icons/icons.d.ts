bbeec3f7c43c2ee0a0a03e18495a1aace9dd6141
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-15 19:29:23 -0400

    Create icons.d.ts

---
+++ src/icons/icons.d.ts
@@ -0,0 +1,55 @@
+// --- START OF AUTO GENERATED FILE ---
+//
+
+import Vue from 'vue'
+import { BvComponent } from '../'
+
+// Component: b-icon-blank
+export declare class BIconBlank extends BvComponent {}
+
+// Component: b-icon-alarm
+export declare class BIconAlarm extends BvComponent {}
+
+// Component: b-icon-alarm-fill
+export declare class BIconAlarmFill extends BvComponent {}
+
+// TODO:
+//   Add remaining icon components
+
+// Component: b-icon-wallet
+export declare class BIconWallet extends BvComponent {}
+
+// Component: b-icon-watch
+export declare class BIconWatch extends BvComponent {}
+
+// Component: b-icon-wifi
+export declare class BIconWifi extends BvComponent {}
+
+// Component: b-icon-window
+export declare class BIconWindow extends BvComponent {}
+
+// Component: b-icon-wrench
+export declare class BIconWrench extends BvComponent {}
+
+// Component: b-icon-x
+export declare class BIconX extends BvComponent {}
+
+// Component: b-icon-x-circle
+export declare class BIconXCircle extends BvComponent {}
+
+// Component: b-icon-x-circle-fill
+export declare class BIconXCircleFill extends BvComponent {}
+
+// Component: b-icon-x-octagon
+export declare class BIconXOctagon extends BvComponent {}
+
+// Component: b-icon-x-octagon-fill
+export declare class BIconXOctagonFill extends BvComponent {}
+
+// Component: b-icon-x-square
+export declare class BIconXSquare extends BvComponent {}
+
+// Component: b-icon-x-square-fill
+export declare class BIconXSquareFill extends BvComponent {}
+
+// --- END OF AUTO GENERATED FILE ---
