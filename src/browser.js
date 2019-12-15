f865b56a7c2c59f93f39397fe76c7019dd13c3f7
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-15 19:34:59 -0400

    Update browser.js

--- src/browser.js
+++ src/browser.js
@@ -1,9 +1,10 @@
 // Main entry point for the browser build
 import { vueUse } from './utils/plugins'
 
-import { BootstrapVue } from './index'
+import { BootstrapVue, IconsPlugin } from './index'
 
 // Auto installation only occurs if window.Vue exists
 vueUse(BootstrapVue)
+vueUse(IconsPlugin)
 
 export default BootstrapVue
