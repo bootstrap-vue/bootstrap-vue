a6e42bb065c9f9152fd92d4c1aec2e730d217d50
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-16 11:31:40 -0400

    Update icons-table.vue

--- docs/components/icons-table.vue
+++ docs/components/icons-table.vue
@@ -8,7 +8,7 @@
     <b-row align-v="start">
       <b-col md="5" lg="6">
         <div id="bv-icons-table-title" class="h3 text-muted mb-3 mb-md-0">
-          Icons explorer
+          Icon explorer
         </div>
       </b-col>
       <b-col md="7" lg="6">
