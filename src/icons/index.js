e7721a13546016fff10dc62e4ac12be230022e22
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-17 15:30:33 -0400

    Update index.js

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,12 +1,11 @@
-import { keys } from '../utils/object'
-import { iconComponents } from './plugin'
+// Icons Plugin
 
 // These re-exports are not currently used, as Webpack 4 has
 // issues with tree shaking re-exports of re-exports
 // `/src/index.js` does a single level re-export `* from './icons/icons/icons'`
 // export * from './icons'
 //
-// export { BIcon }
+// export { BIcon } from './icon'
 
 // Plugin (an iconNames for docs)
 export { IconsPlugin, iconNames } from './plugin'
