29729dc25f8ec160f87b3691d5290c97ce705533
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-18 16:58:54 -0400

    Update index.js

--- docs/pages/docs/icons/index.js
+++ docs/pages/docs/icons/index.js
@@ -32,8 +32,6 @@ export default {
         <div class="alert alert-info small">
           <p class="mb-0">
             Individual icon components are not listed here due to the large number of components.
-            All individual icon components have the same props as <code>&lt;b-icon&gt;</code> other
-            than the <code>icon</code> prop.
           </p>
         </div>
         <Importdoc :meta="importMeta"></ImportDoc>
@@ -58,17 +56,9 @@ export default {
   },
   computed: {
     componentMeta() {
-      // We only return the first BIcon component, plus one extra example
-      // icon component which we modify the icon name to be `BIcon{IconName}`
-      // We sort the array to ensure `BIcon` appears first
-      const components = this.meta.components
-        .sort((a, b) => (a < b ? -1 : a > b ? 1 : 0))
-        .slice(0, 2)
-        .map(c => ({ ...c }))
-      // Change the name of the example icon component
-      components[1].component = 'BIcon{IconName}'
-      // Return the shortend list of components
-      return components
+      // docs/content/index.js massages the list of icon components
+      // to include only BIcon and an example component
+      return = this.meta.components
     },
     importMeta() {
       return { ...this.meta, slug: 'icons', components: this.componentMeta }
