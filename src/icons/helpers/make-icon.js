07e159d99f3f0472c9c67e397deaf0b1d5b7673e
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-19 04:08:00 -0400

    add shift-h and shift-v props

--- src/icons/helpers/make-icon.js
+++ src/icons/helpers/make-icon.js
@@ -29,6 +29,14 @@ export const commonIconProps = {
   flipV: {
     type: Boolean,
     default: false
+  },
+  shiftH: {
+    type: [Number, String],
+    default: 0
+  },
+  shiftV: {
+    type: [Number, String],
+    default: 0
   }
 }
 
@@ -56,14 +64,17 @@ const BVIconBase = {
     const fontScale = toFloat(props.fontScale) || 1
     const scale = toFloat(props.scale) || 1
     const rotate = toFloat(props.rotate) || 0
+    const shiftH = toFloat(props.shiftV) || 0
+    const shiftV = toFloat(props.shiftV) || 0
     const flipH = props.flipH
     const flipV = props.flipV
     // Compute the transforms. Note that order is important.
     // CSS transforms are applied in order from right to left
-    // and we want flipping to occur before rotation
-    // TODO:
-    //   If we add x/y "shifting", it would be listed first here (applied last)
+    // and we want flipping to occur before rotation, and
+    // shifting is applied last
     const transforms = [
+      shiftH ? `translateX(${shiftH * -1/16}%)` : null,
+      shiftV ? `translateY(${shiftV * -1/16}%)` : null,
       rotate ? `rotate(${rotate}deg)` : null,
       flipH || flipV || scale !== 1
         ? `scale(${(flipH ? -1 : 1) * scale}, ${(flipV ? -1 : 1) * scale})`
@@ -74,7 +85,7 @@ const BVIconBase = {
     const $inner = h('g', {
       style: {
         transform: transforms.join(' ') || null,
-        transformOrigin: transforms.length > 0 ? '50% 50%' : null
+        transformOrigin: transforms.length > 0 ? 'center' : null
       },
       domProps: { innerHTML: props.content || '' }
     })
