c01a4a65a4902ec0fdb34cbe495484c8457ef68b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-22 18:49:05 -0400

    Create boolean.spec.js

---
+++ src/utils/boolean.spec.js
@@ -0,0 +1,20 @@
+import { toBoolean } from './boolean'
+
+describe('utils/boolean', () => {
+  it('toBoolean works', async () => {
+    expect(toBoolean(true)).toBe(true)
+    expect(toBoolean(false)).toBe(false)
+    expect(toBoolean('')).toBe(false)
+    expect(toBoolean(0)).toBe(false)
+    expect(toBoolean(1)).toBe(true)
+    expect(toBoolean(1000)).toBe(true)
+    expect(toBoolean(null)).toBe(false)
+    expect(toBoolean(undefined)).toBe(false)
+    expect(toBoolean(NaN)).toBe(false)
+    expect(toBoolean(void 0)).toBe(false)
+    expect(toBoolean('0')).toBe(true)
+    expect(toBoolean([])).toBe(true)
+    expect(toBoolean({})).toBe(true)
+    expect(toBoolean(() => {})).toBe(true)
+  })
+})
