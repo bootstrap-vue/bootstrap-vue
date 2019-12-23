35674885317be5608a5d775b00a1ef83c426e917
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-23 04:12:40 -0400

    Update sanitize-row.js

--- src/components/table/helpers/sanitize-row.js
+++ src/components/table/helpers/sanitize-row.js
@@ -1,18 +1,18 @@
 import { keys } from '../../../utils/object'
 import { arrayIncludes } from '../../../utils/array'
-import { isFunction } from '../../../utils/inspect'
+import { isArray, isFunction } from '../../../utils/inspect'
 import { IGNORED_FIELD_KEYS } from './constants'
 
 // Return a copy of a row after all reserved fields have been filtered out
-const sanitizeRow = (row, ignoreFields = [], includeFields = [], fieldsObj = {}) =>
+const sanitizeRow = (row, ignoreFields, includeFields, fieldsObj = {}) =>
   keys(row).reduce((obj, key) => {
     // Ignore special fields that start with `_`
     // Ignore fields in the `ignoreFields` array
     // Include only fields in the `includeFields` array
     if (
       !IGNORED_FIELD_KEYS[key] &&
-      !(ignoreFields.length > 0 && arrayIncludes(ignoreFields, key)) &&
-      !(includeFields.length > 0 && !arrayIncludes(includeFields, key))
+      !(isArray(ignoreFields) && ignoreFields.length > 0 && arrayIncludes(ignoreFields, key)) &&
+      !(isArray(includeFields) && includeFields.length > 0 && !arrayIncludes(includeFields, key))
     ) {
       const f = fieldsObj[key] || {}
       const val = row[key]
