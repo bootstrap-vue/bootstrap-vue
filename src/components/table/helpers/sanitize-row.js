3d19ac26257486f955a891af314e254150fc490b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-23 03:52:17 -0400

    Update sanitize-row.js

--- src/components/table/helpers/sanitize-row.js
+++ src/components/table/helpers/sanitize-row.js
@@ -4,15 +4,15 @@ import { isFunction } from '../../../utils/inspect'
 import { IGNORED_FIELD_KEYS } from './constants'
 
 // Return a copy of a row after all reserved fields have been filtered out
-const sanitizeRow = (row, ignoreFields, includeFields, fieldsObj = {}) =>
+const sanitizeRow = (row, ignoreFields = [], includeFields = [], fieldsObj = {}) =>
   keys(row).reduce((obj, key) => {
     // Ignore special fields that start with `_`
     // Ignore fields in the `ignoreFields` array
     // Include only fields in the `includeFields` array
     if (
       !IGNORED_FIELD_KEYS[key] &&
-      !(ignoreFields && ignoreFields.length > 0 && arrayIncludes(ignoreFields, key)) &&
-      !(includeFields && includeFields.length > 0 && !arrayIncludes(includeFields, key))
+      !(ignoreFields.length > 0 && arrayIncludes(ignoreFields, key)) &&
+      !(includeFields.length > 0 && !arrayIncludes(includeFields, key))
     ) {
       const f = fieldsObj[key] || {}
       const val = row[key]
