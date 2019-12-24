0f0ab4988ea3f22f76e29adfb11499ff8843c0f1
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-24 02:38:11 -0400

    Update stringify-record-values.js

--- src/components/table/helpers/stringify-record-values.js
+++ src/components/table/helpers/stringify-record-values.js
@@ -3,7 +3,10 @@ import sanitizeRow from './sanitize-row'
 import stringifyObjectValues from './stringify-object-values'
 
 // Stringifies the values of a record, ignoring any special top level field keys
-// TODO: Add option to stringify `scopedSlot` items
+// TODO:
+//   - Format the row first, make nested keys 'top level', then sanitize,
+//     perhaps we need a format row helper method
+//   - Add option to stringify `scopedSlot` items
 const stringifyRecordValues = (row, ignoreFields, includeFields, fieldsObj) => {
   return isObject(row)
     ? stringifyObjectValues(sanitizeRow(row, ignoreFields, includeFields, fieldsObj))
