11a73921aa0326a25ea525497b9f2946f244f96b
Troy Morehouse <troymore@nbnet.nb.ca>
2019-12-23 23:57:30 -0400

    Update sanitize-row.js

--- src/components/table/helpers/sanitize-row.js
+++ src/components/table/helpers/sanitize-row.js
@@ -1,19 +1,21 @@
+import identity from '../../../utils/identity'
 import { keys } from '../../../utils/object'
-import { arrayIncludes } from '../../../utils/array'
+import { arrayIncludes, concat } from '../../../utils/array'
 import { isArray, isFunction } from '../../../utils/inspect'
 import { IGNORED_FIELD_KEYS } from './constants'
 
+// Convert object to array of ignored keys
+const IGNORED = keys(IGNORED_FIELD_KEYS)
+
 // Return a copy of a row after all reserved fields have been filtered out
-const sanitizeRow = (row, ignoreFields, includeFields, fieldsObj = {}) =>
-  keys(row).reduce((obj, key) => {
+const sanitizeRow = (row, ignoreFields, includeFields, fieldsObj = {}) => {
+  const ignore = concat(IGNORED, ignoreFields).filter(identity)
+  const include = isArray(includeFields) && includeFields.length > 0 ? includeFields : null
+  return keys(row).reduce((obj, key) => {
     // Ignore special fields that start with `_`
     // Ignore fields in the `ignoreFields` array
-    // Include only fields in the `includeFields` array
-    if (
-      !IGNORED_FIELD_KEYS[key] &&
-      !(isArray(ignoreFields) && arrayIncludes(ignoreFields, key)) &&
-      !(isArray(includeFields) && includeFields.length > 0 && !arrayIncludes(includeFields, key))
-    ) {
+    // Include only fields in the `includeFields` array (if present)
+    if (!arrayIncludes(ignore, key) && !(include && !arrayIncludes(include, key))) {
       const f = fieldsObj[key] || {}
       const val = row[key]
       // `f.filterByFormatted` will either be a function or boolean
@@ -28,5 +30,6 @@ const sanitizeRow = (row, ignoreFields, includeFields, fieldsObj = {}) =>
     }
     return obj
   }, {})
+}
 
 export default sanitizeRow
