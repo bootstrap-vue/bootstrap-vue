d17a6f53b8141655ffe5b175a788831d4a855414
Troy Morehouse <troymore@nbnet.nb.ca>
2020-01-21 16:39:35 -0400

    Update iconstack.spec.js

--- src/icons/iconstack.spec.js
+++ src/icons/iconstack.spec.js
@@ -21,13 +21,34 @@ describe('icons > b-iconstack', () => {
     expect(wrapper.attributes('style')).not.toBeDefined()
     expect(wrapper.element.style.fontSize).toEqual('')
     expect(wrapper.find('svg > g').exists()).toBe(true)
+    expect(wrapper.find('svg > g').attributes('transform')).not.toBeDefined()
     expect(wrapper.find('svg > g > g').exists()).toBe(false)
   })
 
-  it('has renders default slot', async () => {
+  it('b-iconstack variant works', async () => {
     const wrapper = mount(BIconstack, {
-      slots: {
-        default: '<svg class="foo"></svg>'
+      propsData: {
+        variant: 'danger'
+      }
+    })
+
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.is('svg')).toBe(true)
+    expect(wrapper.classes()).toContain('b-icon')
+    expect(wrapper.classes()).toContain('bi')
+    expect(wrapper.classes()).toContain('text-danger')
+    expect(wrapper.classes().length).toBe(3)
+    expect(wrapper.attributes('role')).toBe('img')
+    expect(wrapper.attributes('alt')).toBe('icon')
+    expect(wrapper.attributes('focusable')).toBe('false')
+    expect(wrapper.find('svg > g').exists()).toBe(true)
+    expect(wrapper.find('svg > g').attributes('transform')).not.toBeDefined()
+  })
+
+  it('b-iconstack font-scale prop works', async () => {
+    const wrapper = mount(BIconstack, {
+      propsData: {
+        fontScale: '1.25'
       }
     })
 
@@ -39,16 +60,48 @@ describe('icons > b-iconstack', () => {
     expect(wrapper.attributes('role')).toBe('img')
     expect(wrapper.attributes('alt')).toBe('icon')
     expect(wrapper.attributes('focusable')).toBe('false')
-    expect(wrapper.attributes('xmlns')).toBe('http://www.w3.org/2000/svg')
-    expect(wrapper.attributes('width')).toBe('1em')
-    expect(wrapper.attributes('height')).toBe('1em')
-    expect(wrapper.attributes('viewBox')).toBe('0 0 20 20')
-    expect(wrapper.attributes('fill')).toBe('currentColor')
-    expect(wrapper.attributes('style')).not.toBeDefined()
-    expect(wrapper.element.style.fontSize).toEqual('')
+    expect(wrapper.attributes('style')).toBeDefined()
+    expect(wrapper.element.style.fontSize).toEqual('125%')
     expect(wrapper.find('svg > g').exists()).toBe(true)
-    expect(wrapper.find('svg > g > g').exists()).toBe(false)
-    expect(wrapper.find('svg > g > svg').exists()).toBe(true)
-    expect(wrapper.find('svg > g > svg.foo').exists()).toBe(true)
+    expect(wrapper.find('svg > g').attributes('transform')).not.toBeDefined()
+  })
+
+  it('b-icons rotate prop works', async () => {
+    const wrapper = mount(BIconstack, {
+      propsData: {
+        rotate: '45'
+      }
+    })
+
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.is('svg')).toBe(true)
+    expect(wrapper.classes()).toContain('b-icon')
+    expect(wrapper.classes()).toContain('bi')
+    expect(wrapper.classes().length).toBe(2)
+    expect(wrapper.find('svg > g').exists()).toBe(true)
+    expect(wrapper.find('svg > g').attributes('transform')).toBeDefined()
+    expect(wrapper.find('svg > g').attributes('transform')).toEqual(
+      'translate(10 10) rotate(45) translate(-10 -10)'
+    )
+  })
+
+  it('b-iconstack scale prop works', async () => {
+    const wrapper = mount(BIconstack, {
+      propsData: {
+        scale: '1.5'
+      }
+    })
+
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.is('svg')).toBe(true)
+    expect(wrapper.classes()).toContain('b-icon')
+    expect(wrapper.classes()).toContain('bi')
+    expect(wrapper.classes()).toContain('bi-alert-circle-fill')
+    expect(wrapper.classes().length).toBe(3)
+    expect(wrapper.find('svg > g').exists()).toBe(true)
+    expect(wrapper.find('svg > g').attributes('transform')).toBeDefined()
+    expect(wrapper.find('svg > g').attributes('transform')).toEqual(
+      'translate(10 10) scale(1.5 1.5) translate(-10 -10)'
+    )
   })
 })
