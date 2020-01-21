9a4e68d3127d4a0644a6f55c01fb0a067aee97b4
Troy Morehouse <troymore@nbnet.nb.ca>
2020-01-21 15:37:07 -0400

    Update iconstack.js

--- src/icons/iconstack.js
+++ src/icons/iconstack.js
@@ -1,6 +1,6 @@
 import Vue from '../utils/vue'
 import { mergeData } from 'vue-functional-data-merge'
-import { commonIconProps } from './helpers/make-icon'
+import { commonIconProps, BVIconBase } from './helpers/make-icon'
 
 // @vue/component
 export const BIconstack = /*#__PURE__*/ Vue.extend({
@@ -8,10 +8,6 @@ export const BIconstack = /*#__PURE__*/ Vue.extend({
   functional: true,
   props: { ...commonIconProps },
   render(h, { data, props, children }) {
-    return h(
-      BVIconBase,
-      mergeData(data, { props: { ...props, stacked: false } }),
-      children
-    )
+    return h(BVIconBase, mergeData(data, { props: { ...props, stacked: false } }), children)
   }
 })
