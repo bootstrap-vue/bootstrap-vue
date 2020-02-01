aac8179e1663acad5fb89f3f8c1310d05a5129cd
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-01 01:28:49 -0400

    Update date.js

--- src/utils/date.js
+++ src/utils/date.js
@@ -34,7 +34,7 @@ export const formatYMD = date => {
 
 // Given a locale (or locales), resolve the browser available locale
 export const resolveLocale = (locales, calendar = 'gregory') => {
-  const locales = concat(locale).filter(identity)
+  locales = concat(locales).filter(identity)
   const fmt = new Intl.DateTimeFormat(locales, { calendar: calendar })
   return = fmt.resolvedOptions().locale 
 }
