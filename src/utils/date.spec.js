29becd7d56bceb70215579ee230535100718c266
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-01 02:54:21 -0400

    Update date.spec.js

--- src/utils/date.spec.js
+++ src/utils/date.spec.js
@@ -4,13 +4,17 @@ describe('utils/date', () => {
   it('parseYMD works', async () => {
     const date1 = parseYMD('2020-01-15')
     const date2 = new Date(2020, 0, 15)
+
     expect(date1.toISOString()).toEqual(date2.toISOString())
+    expect(parseYMD('yyyy-mm-dd')).toequal(null)
   })
 
   it('formatYMD works', async () => {
     expect(formatYMD(new Date(2020, 0, 15))).toEqual('2020-01-15')
     expect(formatYMD('2020-01-15')).toEqual('2020-01-15')
     expect(formatYMD('2020-01-32')).toEqual('2020-02-01')
+    expect(formatYMD('adsadsad')).toEqual(null)
+    expect(formatYMD('x2020-01-15')).toEqual(null)
   })
 
   it('datesEqual works', async () => {
