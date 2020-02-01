acbca60b2346e2a6f7e7ee106759c952e1601fc6
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-01 02:44:34 -0400

    Update date.spec.js

--- src/utils/date.spec.js
+++ src/utils/date.spec.js
@@ -14,8 +14,8 @@ describe('utils/date', () => {
   })
 
   it('datesEqual works', async () => {
-    expect(datesEqual('2020-01-15', new Date(2020, 0, 15))).toBe(true)
     expect(datesEqual('2020-01-15', '2020-12-15')).toBe(false)
     expect(datesEqual(new Date(2020, 0, 15), new Date(2020, 0, 15, 5, 4, 3))).toBe(true)
+    expect(datesEqual('2020-01-15', new Date(2020, 0, 15))).toBe(true)
   })
 })
