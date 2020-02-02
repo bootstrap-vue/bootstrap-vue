1230f9db0ee96bfe844b26c35527365d0ed7650a
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-02 18:57:17 -0400

    Update calendar.js

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -116,7 +116,7 @@ export const BCalendar = Vue.extend({
     width: {
       // Has no effect if prop `block` is set
       type: String,
-      default: '280px'
+      default: '270px'
     },
     block: {
       // Makes calendar the full width of its parent container
