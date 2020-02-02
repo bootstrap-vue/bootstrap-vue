3c9553cb5349f19061530471b21965d5cc824291
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-01 22:42:28 -0400

    Update calendar.spec.js

--- src/components/calendar/calendar.spec.js
+++ src/components/calendar/calendar.spec.js
@@ -1,11 +1,93 @@
 import { mount } from '@vue/test-utils'
+import { waitNT, waitRAF } from '../../../tests/utils'
 import { BCalendar } from './calendar'
 
+//  Note that JSDOM only supports `en-US` (`en`) locale for Intl
+
 describe('calendar', () => {
-  it('works', async () => {
+  it('has expected base structure', async () => {
     const wrapper = mount(BCalendar)
 
     expect(wrapper.isVueInstance()).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    wrapper.destroy()
+  })
+
+  it('has expected struture when value is set', async() => {
+    const wrapper = mount(BCalendar, {
+      propsData: {
+        value: '2020-02-15' // Leap year
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    // TBD
+
+    wrapper.destroy()
+  })
+
+  it('date navigation buttons work', async() => {
+    const wrapper = mount(BCalendar, {
+      attachToDocument: true,
+      propsData: {
+        value: '2020-02-15' // Leap year
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    // TBD
+
+    wrapper.destroy()
+  })
+
+  it('focus method works', async() => {
+    const wrapper = mount(BCalendar, {
+      attachToDocument: true,
+      propsData: {
+        value: '2020-02-15' // Leap year
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    const $grid = wrapper.find('[role="grid"]')
+    expect($grid.exists()).toBe(true)
+    expect($grid.is('div')).toBe(true)
+
+    expect(document.activeElement).not.toBe($grid.element)
+
+    wrapper.vm.focus()
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    expect(document.activeElement).toBe($grid.element)
+    
+    wrapper.destroy()
+  })
+
+  it('keyboard navigation works', async() => {
+    const wrapper = mount(BCalendar, {
+      attachToDocument: true,
+      propsData: {
+        value: '2020-02-15' // Leap year
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    // TBD
 
     wrapper.destroy()
   })
