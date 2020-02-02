03f8c6bacb98bfec193aea56f43d88ffa434a699
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-02 01:45:38 -0400

    Update calendar.spec.js

--- src/components/calendar/calendar.spec.js
+++ src/components/calendar/calendar.spec.js
@@ -40,6 +40,29 @@ describe('calendar', () => {
     wrapper.destroy()
   })
 
+
+  it('reacts to changes in value', async () => {
+    const wrapper = mount(BCalendar, {
+      attachToDocument: true,
+      propsData: {
+        value: '2020-01-01' // Leap year
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    // TBD
+
+    wrapper.setProps({
+      value: '2020-01-15'
+    })
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    wrapper.destroy()
+  })
   it('date navigation buttons work', async () => {
     const wrapper = mount(BCalendar, {
       attachToDocument: true,
