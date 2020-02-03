85e53e58279d633042f268217ef29f451d7fe882
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-03 02:36:55 -0400

    Create form-date.spec.js

---
+++ src/components/form-date/form-date.spec.js
@@ -0,0 +1,25 @@
+import { mount } from '@vue/test-utils'
+// import { waitNT, waitRAF } from '../../../tests/utils'
+import { BFormDate } from './form-date'
+
+//  Note that JSDOM only supports `en-US` (`en`) locale for Intl
+
+describe('form-date', () => {
+  it('has expected base structure', async () => {
+    const wrapper = mount(BFormDate, {
+      attachToDocument: true
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.is('div')).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    // TBD
+
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    wrapper.destroy()
+  })
+})
