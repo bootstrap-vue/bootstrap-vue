8e872758298d98de023dd14e9775fd88cb12c3a4
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-03 02:19:05 -0400

    Create form-date.js

---
+++ src/components/form-date/form-date.js
@@ -0,0 +1,8 @@
+import Vue from '../../utils/vue'
+
+export const BFormDate = /*#__PURE__*/ Vue.extend({
+  name: 'BFormDate',
+  render(h) {
+    return h('div', { staticClass: 'b-form-date' })
+  }
+})
