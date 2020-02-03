b820f83a0430790a1e2fbd28819493a178dcb4b0
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-03 02:21:47 -0400

    Create index.d.ts

---
+++ src/components/form-date/index.d.ts
@@ -0,0 +1,12 @@
+// --- Form Date ---
+import Vue from 'vue'
+import { BvPlugin, BvComponent } from '../../'
+
+// Plugin
+export declare const FormDatePlugin: BvPlugin
+
+// Component: b-form-date
+export declare class BFormDate extends BvComponent {
+  focus: () => void
+  blur: () => void
+}
