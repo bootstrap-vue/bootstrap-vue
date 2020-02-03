8c997b1c9f1db6c952bc8f0caf0641396ed61725
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-03 02:24:44 -0400

    Update index.js

--- src/components/form-date/index.js
+++ src/components/form-date/index.js
@@ -1,8 +1,8 @@
 import { BFormDate } from './form-date'
 import { pluginFactory } from '../../utils/plugins'
 
-const FormdatePlugin = /*#__PURE__*/ pluginFactory({
+const FormDatePlugin = /*#__PURE__*/ pluginFactory({
   components: { BFormDate }
 })
 
-export { ForMDatePlugin, BFormDate }
+export { FormDatePlugin, BFormDate }
