0f474036f3d7eb64dcae80dcd0778ae3f3e0eb05
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-03 03:04:23 -0400

    Update form-date.js

--- src/components/form-date/form-date.js
+++ src/components/form-date/form-date.js
@@ -2,6 +2,10 @@ import Vue from '../../utils/vue'
 
 export const BFormDate = /*#__PURE__*/ Vue.extend({
   name: 'BFormDate',
+  model: {
+    prop: 'value',
+    event: 'input'
+  },
   render(h) {
     return h('div', { staticClass: 'b-form-date' })
   }
