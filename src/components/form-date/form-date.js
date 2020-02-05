25a8413cdf93e4321877cfdffaa8b42077bfd15a
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-05 00:08:07 -0400

    Update form-date.js

--- src/components/form-date/form-date.js
+++ src/components/form-date/form-date.js
@@ -441,7 +441,7 @@ export const BFormDate = /*#__PURE__*/ Vue.extend({
     /* istanbul ignore if: until tests are written */
     if ($controls.length > 0) {
       // TODO: May want to adjust layout based on number of buttons
-      $controls = h('div', { staticClass: 'd-flex justify-content-between flex-wrap' }, $controls)
+      $controls = [h('div', { staticClass: 'd-flex justify-content-between flex-wrap' }, $controls)]
     }
 
     const $calendar = h(
