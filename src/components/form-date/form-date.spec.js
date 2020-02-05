3555b85d71dc8ea5962b4a03423753453e9764cd
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-05 19:49:06 -0400

    Update form-date.spec.js

--- src/components/form-date/form-date.spec.js
+++ src/components/form-date/form-date.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import { waitNT, waitRAF } from '../../../tests/utils'
 import { BFormDate } from './form-date'
-// import { formatYMD } from '../../utils/date'
+import { formatYMD } from '../../utils/date'
 
 // Note that JSDOM only supports `en-US` (`en`) locale for `Intl`
 
@@ -59,7 +59,8 @@ describe('form-date', () => {
     await waitNT(wrapper.vm)
     await waitRAF()
 
-    // TBD
+    expect(wrapper.emitted('input')).not.toBeDefined()
+
     wrapper.setProps({
       value: '2020-01-20'
     })
@@ -67,37 +68,12 @@ describe('form-date', () => {
     await waitNT(wrapper.vm)
     await waitRAF()
 
-    wrapper.destroy()
-  })
-
-  it('emits new value when date selected', async () => {
-    const wrapper = mount(BFormDate, {
-      attachToDocument: true,
-      propsData: {
-        value: ''
-      }
-    })
-
-    expect(wrapper.isVueInstance()).toBe(true)
-    expect(wrapper.is('div')).toBe(true)
-    await waitNT(wrapper.vm)
-    await waitRAF()
+    expect(wrapper.emitted('input')).not.toBeDefined()
 
     // TBD
 
-    // Simulate picking a date on the calendar
-    wrapper.setData({
-      localYMD: '2020-01-20'
-    })
-
-    // TBD
-
-    await waitNT(wrapper.vm)
-    await waitRAF()
-
     wrapper.destroy()
   })
-
   it('focus and blur methods work', async () => {
     const wrapper = mount(BFormDate, {
       attachToDocument: true,
@@ -173,6 +149,68 @@ describe('form-date', () => {
     await waitRAF()
 
     expect($menu.classes()).not.toContain('show')
+
+    wrapper.destroy()
+  })
+
+  it('emits new value when date updated', async () => {
+    const wrapper = mount(BFormDate, {
+      attachToDocument: true,
+      propsData: {
+        value: '',
+        id: 'test-emit-input'
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.is('div')).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    expect(wrapper.emitted('input')).not.toBeDefined()
+
+    const $toggle = wrapper.find('button#test-emit-input')
+    const $menu = wrapper.find('.dropdown-menu')
+
+    expect($toggle.exists()).toBe(true)
+    expect($toggle.is('button')).toBe(true)
+    expect($menu.exists()).toBe(true)
+    expect($menu.classes()).not.toContain('show')
+    expect(wrapper.find('.b-calendar').exists()).toBe(false)
+
+    $toggle.trigger('click')
+    await waitNT(wrapper.vm)
+    await waitRAF()
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    expect($menu.classes()).toContain('show')
+    expect(wrapper.find('.b-calendar').exists()).toBe(true)
+
+    expect(wrapper.emitted('context')).toBeDefined()
+    const lastIndex = wrapper.emitted('context').length - 1
+    // activeYMD will be the date that caneldar has focused by default
+    const activeYMD = wrapper.emitted('context')[lastIndex][0]['activeYMD']
+
+    expect(activeYMD).not.toBe(null)
+    expect(activeYMD).not.toBe('')
+    expect(/^\d+-\d\d-\d\d$/.test(activeYMD)).toBe(true)
+
+    const $grid = wrapper.find('[role="application"]')
+    expect($grid.exists()).toBe(true)
+
+    // Simulate picking todays date on calendar by keydown.enter on grid
+    // The calendar has today's date as the default calendar day button
+    $grid.trigger('keydown.enter')
+    await waitNT(wrapper.vm)
+    await waitRAF()
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    expect($menu.classes()).not.toContain('show')
+    expect(wrapper.emitted('input')).toBeDefined()
+    expect(wrapper.emitted('input').length).toBe(1)
+    expect(wrapper.emitted('input')[0][0]).toBe(activeYMD)
 
     wrapper.destroy()
   })
