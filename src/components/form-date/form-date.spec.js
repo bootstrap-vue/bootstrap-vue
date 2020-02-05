7b5ba1802c1ac92999502525f74fe666ce450c7e
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-05 19:07:56 -0400

    Update form-date.spec.js

--- src/components/form-date/form-date.spec.js
+++ src/components/form-date/form-date.spec.js
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils'
 import { waitNT, waitRAF } from '../../../tests/utils'
 import { BFormDate } from './form-date'
+import { formatYMD } from '../../utils/date'
 
 // Note that JSDOM only supports `en-US` (`en`) locale for `Intl`
 
@@ -107,6 +108,40 @@ describe('form-date', () => {
     await waitRAF()
 
     expect(document.activeElement).not.toBe($toggle.element)
+
+    wrapper.destroy()
+  })
+
+  it('opens calendar when toggle button clicked', async () => {
+    const wrapper = mount(BFormDate, {
+      attachToDocument: true,
+      propsData: {
+        value: ''
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.is('div')).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    const $toggle = wrapper.find('button#test-focus-blur')
+
+    expect($toggle.exists()).toBe(true)
+    expect($toggle.is('button')).toBe(true)
+
+    const $menu = wrapper.find('.dropdown-menu')
+
+    expect($menu.exists()).toBe(true)
+    expect($menu.classes()).not.toContain('show')
+
+    $toggle.trigger('click')
+    await waitNT(wrapper.vm)
+    await waitRAF()
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    expect($menu.classes()).toContain('show')
 
     wrapper.destroy()
   })
