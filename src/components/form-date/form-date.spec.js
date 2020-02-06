3523f1544145c87d84cfcc0ba6a821821bb51f59
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-05 20:05:39 -0400

    Update form-date.spec.js

--- src/components/form-date/form-date.spec.js
+++ src/components/form-date/form-date.spec.js
@@ -106,6 +106,7 @@ describe('form-date', () => {
 
     wrapper.destroy()
   })
+
   it('focus and blur methods work', async () => {
     const wrapper = mount(BFormDate, {
       attachToDocument: true,
@@ -240,6 +241,69 @@ describe('form-date', () => {
     await waitRAF()
 
     expect($menu.classes()).not.toContain('show')
+    expect(wrapper.emitted('input')).toBeDefined()
+    expect(wrapper.emitted('input').length).toBe(1)
+    expect(wrapper.emitted('input')[0][0]).toBe(activeYMD)
+
+    wrapper.destroy()
+  })
+
+  it('does not close popup when prop `no-close-on-select` is set', async () => {
+    const wrapper = mount(BFormDate, {
+      attachToDocument: true,
+      propsData: {
+        value: '',
+        id: 'test-no-close',
+        noCloseOnSelect: true
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.is('div')).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    const $toggle = wrapper.find('button#test-no-close')
+    const $menu = wrapper.find('.dropdown-menu')
+
+    expect($toggle.exists()).toBe(true)
+    expect($toggle.is('button')).toBe(true)
+    expect($menu.exists()).toBe(true)
+    expect($menu.classes()).not.toContain('show')
+    expect(wrapper.find('.b-calendar').exists()).toBe(false)
+
+    $toggle.trigger('click')
+    await waitNT(wrapper.vm)
+    await waitRAF()
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    expect($menu.classes()).toContain('show')
+    expect(wrapper.find('.b-calendar').exists()).toBe(true)
+
+    expect(wrapper.emitted('context')).toBeDefined()
+    const lastIndex = wrapper.emitted('context').length - 1
+    // activeYMD will be the date that caneldar has focused by default
+    const activeYMD = wrapper.emitted('context')[lastIndex][0].activeYMD
+
+    expect(activeYMD).not.toBe(null)
+    expect(activeYMD).not.toBe('')
+    expect(/^\d+-\d\d-\d\d$/.test(activeYMD)).toBe(true)
+
+    const $grid = wrapper.find('[role="application"]')
+    expect($grid.exists()).toBe(true)
+
+    // Simulate picking todays date on calendar by keydown.enter on grid
+    // The calendar has today's date as the default calendar day button
+    $grid.trigger('keydown.enter')
+    await waitNT(wrapper.vm)
+    await waitRAF()
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    // Calendar should remain open
+    expect($menu.classes()).toContain('show')
+
     expect(wrapper.emitted('input')).toBeDefined()
     expect(wrapper.emitted('input').length).toBe(1)
     expect(wrapper.emitted('input')[0][0]).toBe(activeYMD)
