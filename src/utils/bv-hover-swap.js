ec4c23901583d845e470ae54441338e199cd3a00
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-13 02:03:03 -0400

    chore: create BVHoverSwap utility helper component

---
+++ src/utils/bv-hover-swap.js
@@ -0,0 +1,30 @@
+import Vue from './vue'
+
+export const BVHoverSwap = /*#__PURE__*/ Vue.extend({
+  name: 'BVHoverSwap',
+  props: {
+    tag: {
+      type: String,
+      default: 'div'
+    }
+  },
+  data() {
+    return {
+      isHovered: false
+    }
+  },
+  methods: {
+    handleHover(evt) {
+      this.isHovered = evt.type === 'mouseenter'
+    }
+  },
+  render(h) {
+    const $scoped = this.$scopedSlots
+    const $default = $scoped.default || () => h()
+    const $hovered = $scoped.hovered || $default
+    return h(
+      tag,
+      { on: { mouseenter: this.handleHover, mouseleave: this.handleHover } },
+      [isHovered ? $hovered() : $default()]
+  }
+})
