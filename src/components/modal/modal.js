27b8c7b2e4969171562a7ae033a17a419860572f
Philip Mountifield <philip@mountifield.org>
2020-02-17 10:10:21 +0000

    fix(b-modal): inconsistent show transition behaviour

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -604,9 +604,13 @@ export const BModal = /*#__PURE__*/ Vue.extend({
     },
     onEnter() {
       this.isBlock = true
-      // We add show class 1 frame after
+      // We add show class 1 frame after, requestAF runs the callback before
+      // the next repaint so we need two calls to guarantee the next frame has
+      // been rendered
       requestAF(() => {
-        this.isShow = true
+        requestAF(() => {
+          this.isShow = true
+        })
       })
     },
     onAfterEnter() {
