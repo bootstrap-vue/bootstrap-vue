e673855da37e414824c0e22ee187a9a6013a20eb
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-18 05:08:17 -0400

    Update time.js

--- src/components/time/time.js
+++ src/components/time/time.js
@@ -22,7 +22,7 @@ const NAME = 'BTime'
 const NUMERIC = 'numeric'
 
 // Time string RegExpr (optional seconds)
-const RE_TIME = /^([0-1][0-9]|2[0-3]):[0-5][0-9](:[0-5][0-9])?$/
+const RE_TIME = /^([0-1]?[0-9]|2[0-3]):[0-5]?[0-9](:[0-5]?[0-9])?$/
 
 // --- Helpers ---
 
