6580f51ddcf9e884fe419ad7e2dde619e788018d
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-18 00:43:33 -0400

    Update time.spec.js

--- src/components/time/time.spec.js
+++ src/components/time/time.spec.js
@@ -5,7 +5,7 @@ import { BTime } from './time'
 //  Note that JSDOM only supports `en-US` (`en`) locale for Intl
 
 describe('time', () => {
-  it('Has expected default structure', async () => {
+  it('has expected default structure', async () => {
     const wrapper = mount(BTime)
 
     expect(wrapper.isVueInstance()).toBe(true)
@@ -17,7 +17,7 @@ describe('time', () => {
     wrapper.destroy()
   })
 
-  it('Has expected default structure when value supplied', async () => {
+  it('has expected structure when `value` supplied', async () => {
     const wrapper = mount(BTime, {
       propsData: {
         value: '13:14:15'
@@ -33,6 +33,38 @@ describe('time', () => {
     wrapper.setProps({
       value: '01:02:03'
     })
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    // TBD
+
+    wrapper.destroy()
+  })
+
+  it('has expected structure when `hour12` is `true` supplied', async () => {
+    const wrapper = mount(BTime, {
+      propsData: {
+        hour12: true
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
+    await waitNT(wrapper.vm)
+    await waitRAF()
+
+    // TBD
+
+    wrapper.destroy()
+  })
+
+  it('has expected structure when `hour12` is `false` supplied', async () => {
+    const wrapper = mount(BTime, {
+      propsData: {
+        hour12: false
+      }
+    })
+
+    expect(wrapper.isVueInstance()).toBe(true)
     await waitNT(wrapper.vm)
     await waitRAF()
 
