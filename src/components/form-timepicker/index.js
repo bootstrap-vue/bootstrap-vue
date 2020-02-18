96b069ac5dc77b0cb352618fe87e4175ab347058
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-18 05:50:41 -0400

    Create index.js

---
+++ src/components/form-timepicker/index.js
@@ -0,0 +1,8 @@
+import { BFormTimepicker } from './form-timepicker'
+import { pluginFactory } from '../../utils/plugins'
+
+const FormTimepickerPlugin = /*#__PURE__*/ pluginFactory({
+  components: { BFormTimepicker }
+})
+
+export { FormTimepickerPlugin, BFormTimepicker }
