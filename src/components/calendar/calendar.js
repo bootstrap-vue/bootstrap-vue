91b77bc9a6b1a4796698ce3185c0b354156ce563
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-24 12:07:17 -0400

    feat(b-form-datepicker): add calendar-width prop (closes #4817) (#4822)

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -855,7 +855,7 @@ export const BCalendar = Vue.extend({
           'small',
           {
             key: idx,
-            staticClass: 'col',
+            staticClass: 'col text-truncate',
             class: { 'text-muted': this.disabled },
             attrs: {
               title: d.label === d.text ? null : d.label,
@@ -997,6 +997,7 @@ export const BCalendar = Vue.extend({
     const $widget = h(
       'div',
       {
+        staticClass: 'b-calendar-inner',
         class: this.block ? 'd-block' : 'd-inline-block',
         style: this.block ? {} : { width: this.width },
         attrs: {
