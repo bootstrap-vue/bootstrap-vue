7ae53a1c0143d861d7112b34889e1d4845b91850
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-25 18:54:19 -0400

    Update bind-attrs.js

--- src/mixins/bind-attrs.js
+++ src/mixins/bind-attrs.js
@@ -7,14 +7,16 @@ const LISTENERS_ATTRIBUTE_NAME = 'listeners$'
 
 // --- Utility methods ---
 
-const makeWatcher = property => ((newVal, oldVal) => {
-  for (const prop in oldVal) {
-    if (!hasOwnProperty(newVal, prop)) {
-      this.$delete(this.$data[property], prop)
+const makeWatcher = property => ({
+  handler(newVal, oldVal) {
+    for (const prop in oldVal) {
+      if (!hasOwnProperty(newVal, prop)) {
+        this.$delete(this.$data[property], prop)
+      }
     }
-  }
-  for (const prop in newVal) {
-    this.$set(this.$data[property], prop, newVal[prop])
+    for (const prop in newVal) {
+      this.$set(this.$data[property], prop, newVal[prop])
+    }
   }
 })
 
