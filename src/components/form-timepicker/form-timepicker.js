d119576cb86d15321bb0286832e87470d3218a8e
Troy Morehouse <troymore@nbnet.nb.ca>
2020-02-28 19:18:32 -0400

    Update form-timepicker.js

--- src/components/form-timepicker/form-timepicker.js
+++ src/components/form-timepicker/form-timepicker.js
@@ -413,7 +413,7 @@ export const BFormTimepicker = /*#__PURE__*/ Vue.extend({
           rtl: this.isRTL,
           lang: this.computedLang,
           value: localHMS || '',
-          formattedValue: localHMS ? this.formattedValue : this.labelNoTimeSelected || ''
+          formattedValue: localHMS ? this.formattedValue :  ''
         },
         on: {
           show: this.onShow,
