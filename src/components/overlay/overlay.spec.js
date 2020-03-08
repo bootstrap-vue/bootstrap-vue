55a865c2d5632162de33a19d6d7a0be2e1f5a602
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-08 00:13:31 -0400

    Update overlay.spec.js

--- src/components/overlay/overlay.spec.js
+++ src/components/overlay/overlay.spec.js
@@ -5,25 +5,29 @@ import { BOverlay } from './overlay'
 describe('overlay', () => {
   it('has expected default structure', async () => {
     const wrapper = mount(BOverlay)
-    
+
     expect(wrapper.isVueInstance()).toBe(true)
     await waitNT(wrapper.vm)
     await waitRAF()
-    
+
+    // TBD
+
     wrapper.destroy()
   })
-  
+
   it('has expected default structure when `no-wrap` is set', async () => {
     const wrapper = mount(BOverlay, {
       propsData: {
         noWrap: true
       }
     })
-    
+
     expect(wrapper.isVueInstance()).toBe(true)
     await waitNT(wrapper.vm)
     await waitRAF()
-    
+
+    // TBD
+
     wrapper.destroy()
   })
 })
