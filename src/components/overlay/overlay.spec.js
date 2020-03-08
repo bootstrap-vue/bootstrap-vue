23411700b17885c0daba90f051376634e073f0b4
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-08 20:52:15 -0300

    Update overlay.spec.js

--- src/components/overlay/overlay.spec.js
+++ src/components/overlay/overlay.spec.js
@@ -4,13 +4,23 @@ import { BOverlay } from './overlay'
 
 describe('overlay', () => {
   it('has expected default structure', async () => {
-    const wrapper = mount(BOverlay)
+    const wrapper = mount(BOverlay, {
+      slots: {
+        default: '<span>Foobar</span>'
+      }
+    })
 
     expect(wrapper.isVueInstance()).toBe(true)
     await waitNT(wrapper.vm)
     await waitRAF()
 
-    // TBD
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('b-overlay-wrap')
+    expect(wrapper.classes()).toContain('position-relative')
+    expect(wrapper.attributes('aria-busy')).not.toBe('true')
+    expect(wrapper.text()).toContain('foobar')
+    expect(wrapper.find('.b-overlay').exists()).toBe(false)
+    expect(wrapper.find('.spinner-border').exists()).toBe(false)
 
     wrapper.destroy()
   })
@@ -19,6 +29,9 @@ describe('overlay', () => {
     const wrapper = mount(BOverlay, {
       propsData: {
         show: true
+      },
+      slots: {
+        default: '<span>Foobar</span>'
       }
     })
 
@@ -26,7 +39,31 @@ describe('overlay', () => {
     await waitNT(wrapper.vm)
     await waitRAF()
 
-    // TBD
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('b-overlay-wrap')
+    expect(wrapper.classes()).toContain('position-relative')
+    expect(wrapper.attributes('aria-busy')).toBe('true')
+    expect(wrapper.text()).toContain('foobar')
+
+    const $overlay = wrapper.find('.b-overlay')
+    expect($overlay.exists()).toBe(true)
+    expect($overlay.classes()).toContain('position-absolute')
+
+    const $children = $overlay.findAll('div:not(.b-overlay)')
+    expect($children.length).toBe(2)
+
+    expect($children.at(0).classes()).toContain('position-absolute')
+    expect($children.at(0).classes()).toContain('bg-light')
+    expect($children.at(0).text()).toBe('')
+
+    expect($children.at(1).classes()).toContain('position-absolute')
+    expect($children.at(1).classes()).not.toContain('bg-light')
+    expect(
+      $children
+        .at(1)
+        .find('.spinner-border')
+        .exists()
+    ).toBe(true)
 
     wrapper.destroy()
   })
@@ -42,7 +79,8 @@ describe('overlay', () => {
     await waitNT(wrapper.vm)
     await waitRAF()
 
-    // TBD
+    expect(wrapper.is('div')).toBe(false)
+    expect(wrapper.find('.spinner-border').exists()).toBe(false)
 
     wrapper.destroy()
   })
@@ -59,7 +97,27 @@ describe('overlay', () => {
     await waitNT(wrapper.vm)
     await waitRAF()
 
-    // TBD
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('b-overlay')
+    expect(wrapper.classes()).toContain('position-absoute')
+    expect(wrapper.classes()).not.toContain('b-overlay-wrap')
+    expect(wrapper.classes()).not.toContain('position-relative')
+
+    const $children = wrapper.findAll('div:not(.b-overlay)')
+    expect($children.length).toBe(2)
+
+    execpt($children.at(0).classes()).toContain('position-absolute')
+    execpt($children.at(0).classes()).toContain('bg-light')
+    execpt($children.at(0).text()).toBe('')
+
+    execpt($children.at(1).classes()).toContain('position-absolute')
+    execpt($children.at(1).classes()).not.toContain('bg-light')
+    expect(
+      $children
+        .at(1)
+        .find('.spinner-border')
+        .exists()
+    ).toBe(true)
 
     wrapper.destroy()
   })
