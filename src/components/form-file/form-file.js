f68bf541878572c3f6f4783dd8028279d74292fa
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-09 11:49:03 -0300

    Update form-file.js

--- src/components/form-file/form-file.js
+++ src/components/form-file/form-file.js
@@ -43,7 +43,13 @@ export const BFormFile = /*#__PURE__*/ Vue.extend({
         return (
           isUndefinedOrNull(val) ||
           isFile(val) ||
-          (isArray(val) && (val.length === 0 || val.every(v => isArray(v) || isFile(v))))
+          (
+            isArray(val) &&
+            (
+              val.length === 0 ||
+              val.every(v => /* istanbul ignore next */ { return isArray(v) || isFile(v) })
+            )
+          )
         )
       }
     },
