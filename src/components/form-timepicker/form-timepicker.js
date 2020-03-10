788968b72afc8f9f31f4aaf4046fb8d203f6b85e
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-10 01:32:25 -0300

    Update form-timepicker.js

--- src/components/form-timepicker/form-timepicker.js
+++ src/components/form-timepicker/form-timepicker.js
@@ -249,7 +249,12 @@ export const BFormTimepicker = /*#__PURE__*/ Vue.extend({
       this.localHMS = newVal || ''
     },
     localHMS(newVal) {
-      this.$emit('input', newVal || '')
+      // We only update hte v-model value when the timepicker
+      // is open, to prevent cursor jumps when bound to a
+      // text input in button only mode
+      if (this.isVisible) {
+        this.$emit('input', newVal || '')
+      }
     }
   },
   methods: {
