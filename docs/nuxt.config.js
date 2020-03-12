f8e4fd0a36dfe93d86b5d01c7d1fd2d3d4afb88b
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-11 22:54:02 -0300

    Update nuxt.config.js

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -146,7 +146,7 @@ module.exports = {
   build: {
     extractCSS: true,
     cssSourceMap: true,
-    parallel: Boolean(process.env.PULL_REQUEST && process.env.NETLIFY),
+    parallel: Boolean(process.env.NETLIFY && process.env.PULL_REQUEST),
     postcss: {
       preset: {
         autoprefixer: {
