9608ce101878e78d6a0c7dd381ac2c42bc087c34
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-03-21 04:11:23 +0100

    Update avatar.spec.js

--- src/components/avatar/avatar.spec.js
+++ src/components/avatar/avatar.spec.js
@@ -1,6 +1,6 @@
 import { mount, createLocalVue as CreateLocalVue } from '@vue/test-utils'
-import { BAvatar } from './avatar'
 import { BIconPerson } from '../../icons/icons'
+import { BAvatar } from './avatar'
 
 describe('avatar', () => {
   it('should have expected default structure', async () => {
@@ -125,5 +125,31 @@ describe('avatar', () => {
     const $icon = wrapper.find('.b-icon')
     expect($icon.exists()).toBe(true)
     expect($icon.classes()).toContain('bi-person')
+  })
+
+  it('`size` prop should work as expected', async () => {
+    const wrapper1 = mount(BAvatar)
+    expect(wrapper1.attributes('style')).toEqual('width: 2.5em; height: 2.5em;')
+
+    const wrapper2 = mount(BAvatar, { propsData: { size: 'sm' } })
+    expect(wrapper2.attributes('style')).toEqual('width: 1.5em; height: 1.5em;')
+
+    const wrapper3 = mount(BAvatar, { propsData: { size: 'md' } })
+    expect(wrapper3.attributes('style')).toEqual('width: 2.5em; height: 2.5em;')
+
+    const wrapper4 = mount(BAvatar, { propsData: { size: 'lg' } })
+    expect(wrapper4.attributes('style')).toEqual('width: 3.5em; height: 3.5em;')
+
+    const wrapper5 = mount(BAvatar, { propsData: { size: 20 } })
+    expect(wrapper5.attributes('style')).toEqual('width: 20px; height: 20px;')
+
+    const wrapper6 = mount(BAvatar, { propsData: { size: '24.5' } })
+    expect(wrapper6.attributes('style')).toEqual('width: 24.5px; height: 24.5px;')
+
+    const wrapper7 = mount(BAvatar, { propsData: { size: '5em' } })
+    expect(wrapper7.attributes('style')).toEqual('width: 5em; height: 5em;')
+
+    const wrapper8 = mount(BAvatar, { propsData: { size: '36px' } })
+    expect(wrapper8.attributes('style')).toEqual('width: 36px; height: 36px;')
   })
 })
