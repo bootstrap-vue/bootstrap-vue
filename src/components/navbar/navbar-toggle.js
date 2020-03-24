027fefe7675bd0a7fa1fa9e86f4e9518a2742ca4
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-03-24 08:58:11 +0100

    Update navbar-toggle.js

--- src/components/navbar/navbar-toggle.js
+++ src/components/navbar/navbar-toggle.js
@@ -1,17 +1,22 @@
 import Vue from '../../utils/vue'
+import { getComponentConfig } from '../../utils/config'
+import { toString } from '../../utils/string'
 import listenOnRootMixin from '../../mixins/listen-on-root'
 import normalizeSlotMixin from '../../mixins/normalize-slot'
-import { getComponentConfig } from '../../utils/config'
 import { EVENT_TOGGLE, EVENT_STATE, EVENT_STATE_SYNC } from '../../directives/toggle/toggle'
 
-const NAME = 'BNavbarToggle'
-
 // TODO:
-//  Switch to using `VBToggle` directive, will reduce code footprint
-//  Although the `click` event will no longer be cancellable
-//  Instead add `disabled` prop, and have `VBToggle` check element
-//  disabled state
+//   Switch to using `VBToggle` directive, will reduce code footprint
+//   Although the `click` event will no longer be cancellable
+//   Instead add `disabled` prop, and have `VBToggle` check element
+//   disabled state
 
+// --- Constants ---
+
+const NAME = 'BNavbarToggle'
+const CLASS_NAME = 'navbar-toggler'
+
+// --- Main component ---
 // @vue/component
 export const BNavbarToggle = /*#__PURE__*/ Vue.extend({
   name: NAME,
@@ -53,16 +58,19 @@ export const BNavbarToggle = /*#__PURE__*/ Vue.extend({
     return h(
       'button',
       {
-        class: ['navbar-toggler'],
+        staticClass: CLASS_NAME,
         attrs: {
           type: 'button',
           'aria-label': this.label,
           'aria-controls': this.target,
-          'aria-expanded': expanded ? 'true' : 'false'
+          'aria-expanded': toString(expanded)
         },
         on: { click: this.onClick }
       },
-      [this.normalizeSlot('default', { expanded }) || h('span', { class: ['navbar-toggler-icon'] })]
+      [
+        this.normalizeSlot('default', { expanded }) ||
+          h('span', { staticClass: `${CLASS_NAME}-icon` })
+      ]
     )
   }
 })
