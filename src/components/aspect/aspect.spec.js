5878eb06503623e13994ea9d62709cca5a4fea94
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-24 22:01:11 -0300

    Update aspect.spec.js

--- src/components/aspect/aspect.spec.js
+++ src/components/aspect/aspect.spec.js
@@ -14,7 +14,65 @@ describe('aspect', () => {
     expect($sizer.exists()).toBe(true)
     expect($sizer.is('div')).toBe(true)
     expect($sizer.classes()).toContain('flex-grow-1')
+    // Default aspect ratio is 1:1
     expect($sizer.attributes('style')).toContain('padding-bottom: 100%;')
+
+    const $content = wrapper.find('.b-aspect-content')
+    expect($content.exists()).toBe(true)
+    expect($content.is('div')).toBe(true)
+    expect($content.classes()).toContain('flex-grow-1')
+    expect($content.classes()).toContain('w-100')
+    expect($content.classes()).toContain('mw-100')
+    expect($content.attributes('style')).toContain('margin-left: -100%;')
+
+    wrapper.destroy()
+  })
+
+  it('should have expected structure when aspect is set to "4:3"', async () => {
+    const wrapper = mount(BAspect, {
+      propsData: {
+        aspect: '4:3'
+      }
+    })
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('b-aspect')
+    expect(wrapper.classes()).toContain('d-flex')
+    expect(wrapper.classes().length).toBe(2)
+
+    const $sizer = wrapper.find('.b-aspect-sizer')
+    expect($sizer.exists()).toBe(true)
+    expect($sizer.is('div')).toBe(true)
+    expect($sizer.classes()).toContain('flex-grow-1')
+    expect($sizer.attributes('style')).toContain('padding-bottom: 75%;')
+
+    const $content = wrapper.find('.b-aspect-content')
+    expect($content.exists()).toBe(true)
+    expect($content.is('div')).toBe(true)
+    expect($content.classes()).toContain('flex-grow-1')
+    expect($content.classes()).toContain('w-100')
+    expect($content.classes()).toContain('mw-100')
+    expect($content.attributes('style')).toContain('margin-left: -100%;')
+
+    wrapper.destroy()
+  })
+  it('should have expected structure when aspect is set to `4/3`', async () => {
+    const wrapper = mount(BAspect, {
+      propsData: {
+        aspect: 4 / 3
+      }
+    })
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.is('div')).toBe(true)
+    expect(wrapper.classes()).toContain('b-aspect')
+    expect(wrapper.classes()).toContain('d-flex')
+    expect(wrapper.classes().length).toBe(2)
+
+    const $sizer = wrapper.find('.b-aspect-sizer')
+    expect($sizer.exists()).toBe(true)
+    expect($sizer.is('div')).toBe(true)
+    expect($sizer.classes()).toContain('flex-grow-1')
+    expect($sizer.attributes('style')).toContain('padding-bottom: 75%;')
 
     const $content = wrapper.find('.b-aspect-content')
     expect($content.exists()).toBe(true)
