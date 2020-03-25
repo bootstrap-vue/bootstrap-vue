b72bbe96fd519f39ad2c49c9d31f0ede8fa2554d
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-24 23:35:42 -0300

    Update aspect.spec.js

--- src/components/aspect/aspect.spec.js
+++ src/components/aspect/aspect.spec.js
@@ -28,6 +28,36 @@ describe('aspect', () => {
     wrapper.destroy()
   })
 
+  it('should have expected structure when prop `tag` is set', async () => {
+    const wrapper = mount(BAspect, {
+      propsData: {
+        tag: 'section'
+      }
+    })
+    expect(wrapper.isVueInstance()).toBe(true)
+    expect(wrapper.is('section')).toBe(true)
+    expect(wrapper.classes()).toContain('b-aspect')
+    expect(wrapper.classes()).toContain('d-flex')
+    expect(wrapper.classes().length).toBe(2)
+
+    const $sizer = wrapper.find('.b-aspect-sizer')
+    expect($sizer.exists()).toBe(true)
+    expect($sizer.is('div')).toBe(true)
+    expect($sizer.classes()).toContain('flex-grow-1')
+    // Default aspect ratio is 1:1
+    expect($sizer.attributes('style')).toContain('padding-bottom: 100%;')
+
+    const $content = wrapper.find('.b-aspect-content')
+    expect($content.exists()).toBe(true)
+    expect($content.is('div')).toBe(true)
+    expect($content.classes()).toContain('flex-grow-1')
+    expect($content.classes()).toContain('w-100')
+    expect($content.classes()).toContain('mw-100')
+    expect($content.attributes('style')).toContain('margin-left: -100%;')
+
+    wrapper.destroy()
+  })
+
   it('should have expected structure when aspect is set to "4:3"', async () => {
     const wrapper = mount(BAspect, {
       propsData: {
