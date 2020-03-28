6674ca8ea5b1ac52ae36a6ecf13a3a71d9a5d3b6
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-28 03:59:12 -0300

    Update _slug.js

--- docs/pages/docs/components/_slug.js
+++ docs/pages/docs/components/_slug.js
@@ -1,13 +1,10 @@
 import AnchoredHeading from '~/components/anchored-heading'
-import CarbonAd from '~/components/ad'
 import Componentdoc from '~/components/componentdoc'
 import Importdoc from '~/components/importdoc'
-import Main from '~/components/main'
+import MainDocs from '~/components/main-docs'
 import Section from '~/components/section'
-import QuickLinks from '~/components/quick-links.vue'
 import docsMixin from '~/plugins/docs-mixin'
 import { components as componentsMeta } from '~/content'
-import { splitReadme } from '~/utils'
 
 const getReadMe = name =>
   import(`~/../src/components/${name}/README.md` /* webpackChunkName: "docs/components" */)
@@ -21,25 +18,10 @@ export default {
   },
   async asyncData({ params }) {
     const readme = (await getReadMe(params.slug)).default
-    const { titleLead, body } = splitReadme(readme)
     const meta = componentsMeta[params.slug]
-    return { meta, titleLead, body }
+    return { meta, readme }
   },
   render(h) {
-    // Lead section
-    const $leadSection = h(Section, {
-      props: { play: false },
-      domProps: { innerHTML: this.titleLead }
-    })
-    // CarbonAd
-    const $carbonAd = h(CarbonAd, { key: `ad-${this.$route.path}` })
-    // Quick links
-    const $quickLinks = h(QuickLinks, { key: `quick-${this.$route.path}` })
-    // Body section
-    const $bodySection = h(Section, {
-      props: { play: true },
-      domProps: { innerHTML: this.body }
-    })
     // Reference section
     const $referenceSection = h(Section, { class: ['bd-component-reference'] }, [
       // Heading
@@ -54,11 +36,7 @@ export default {
       // Component importing information
       h(Importdoc, { props: { meta: this.meta } })
     ])
-    return h(Main, { staticClass: 'bd-components' }, [
-      $leadSection,
-      $carbonAd,
-      $quickLinks,
-      $bodySection,
+    return h(MainDocs, { staticClass: 'bd-components', props: { readme: this.readme} }, [
       $referenceSection
     ])
   }
