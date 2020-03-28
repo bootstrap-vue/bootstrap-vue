3a111c3e21a7b0fb27beb431e4217e2f96691b36
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-28 04:20:06 -0300

    Update _slug.js

--- docs/pages/docs/reference/_slug.js
+++ docs/pages/docs/reference/_slug.js
@@ -1,10 +1,6 @@
-import CarbonAd from '~/components/ad'
-import Main from '~/components/main'
-import QuickLinks from '~/components/quick-links.vue'
-import Section from '~/components/section'
+import MainDocs from '~/components/main-docs'
 import docsMixin from '~/plugins/docs-mixin'
 import { reference as referenceMeta } from '~/content'
-import { splitReadme } from '~/utils'
 
 const getReadMe = name =>
   import(`~/markdown/reference/${name}/README.md` /* webpackChunkName: "docs/reference" */)
@@ -18,30 +14,10 @@ export default {
   },
   async asyncData({ params }) {
     const readme = (await getReadMe(params.slug)).default
-    const { titleLead, body } = splitReadme(readme)
     const meta = referenceMeta[params.slug]
-    return { meta, titleLead, body }
+    return { meta, readme }
   },
   render(h) {
-    // Lead section
-    const $leadSection = h(Section, {
-      props: { play: false },
-      domProps: { innerHTML: this.titleLead }
-    })
-    // CarbonAd
-    const $carbonAd = h(CarbonAd, { key: `ad-${this.$route.path}` })
-    // Quick links
-    const $quickLinks = h(QuickLinks, { key: `quick-${this.$route.path}` })
-    // Body section
-    const $bodySection = h(Section, {
-      props: { play: true },
-      domProps: { innerHTML: this.body }
-    })
-    return h(Main, { staticClass: 'bd-components' }, [
-      $leadSection,
-      $carbonAd,
-      $quickLinks,
-      $bodySection
-    ])
+    return h(MainDocs, { staticClass: 'bd-components', prop: { readme: this.readme } })
   }
 }
