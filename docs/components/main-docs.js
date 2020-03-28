575d52b0f4b6158def3740cc5da6a8d9a5262dd8
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-28 03:47:45 -0300

    Create main-docs.js

---
+++ docs/components/main-docs.js
@@ -0,0 +1,58 @@
+import CarbonAd from '~/components/ad'
+import Main from '~/compoent/main'
+import QuickLinks from '~/components/quick-links.vue'
+import Section from '~/component/section'
+import { mergeData } from 'vue-functional-data-merge'
+import { splitReadme } from '~/utils'
+
+export default {
+  name: 'BVMainDocs',
+  props: {
+    readme: {
+      type: String,
+      default: ''
+    },
+    tag: {
+      type: String,
+      default: 'main'
+    }
+  },
+  computed: {
+    docs() {
+      return { titleLead, body } = splitReadme(this.readme)
+    },
+    titleLead() {
+      return this.docs.titleLead
+    },
+    body() {
+      return this.docs.body
+    },
+    docsPath() {
+      return this.$route.path
+    }
+  },
+  render(h) {
+    // Lead section
+    const $leadSection = h(Section, {
+      props: { play: false },
+      domProps: { innerHTML: this.titleLead }
+    })
+    // CarbonAd
+    const $carbonAd = h(CarbonAd, { key: `ad-${this.$route.path}` })
+    // Quick links
+    const $quickLinks = h(QuickLinks, { key: `quick-${this.$route.path}` })
+    // Body section
+    const $bodySection = h(Section, {
+      props: { play: true },
+      domProps: { innerHTML: this.body }
+    })
+
+    return h(Main, { props: { tag: this.tag }, staticClass: 'bd-main' }, [
+      $leadSection,
+      $carbonAd,
+      $quickLinks,
+      $bodySection,
+      this.$scopedSlots.default()
+    ])
+  }
+}
