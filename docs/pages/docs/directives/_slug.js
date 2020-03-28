c0cca17060fa3b2fa2be3474ce0d17e1320e0b24
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-28 02:51:57 -0300

    Update _slug.js

--- docs/pages/docs/directives/_slug.js
+++ docs/pages/docs/directives/_slug.js
@@ -1,6 +1,8 @@
 import AnchoredHeading from '~/components/anchored-heading'
+import CarbonAd from '~/components/ad'
 import Importdoc from '~/components/importdoc'
 import Main from '~/components/main'
+import QuickLinks from '~/components/quick-links.vue'
 import Section from '~/components/section'
 import docsMixin from '~/plugins/docs-mixin'
 import { directives as directivesMeta } from '~/content'
@@ -17,14 +19,24 @@ export default {
   },
   async asyncData({ params }) {
     const readme = (await getReadMe(params.slug)).default
-    const meta = directivesMeta[params.slug]
-    return { readme, meta }
+    const { titleLead, body } = splitReadme(readme)
+    const meta = componentsMeta[params.slug]
+    return { meta, titleLead, body }
   },
   render(h) {
-    // Readme section
-    const $readmeSection = h(Section, {
+    // Lead section
+    const $leadSection = h(Section, {
+      props: { play: false },
+      domProps: { innerHTML: this.titleLead }
+    })
+    // CarbonAd
+    const $carbonAd = h(CarbonAd, { key: `ad-${this.$route.path}` })
+    // Quick links
+    const $quickLinks = h(QuickLinks, { key: `quick-${this.$route.path}` })
+    // Body section
+    const $bodySection = h(Section, {
       props: { play: true },
-      domProps: { innerHTML: this.readme }
+      domProps: { innerHTML: this.body }
     })
     // Reference section
     const $referenceSection = h(Section, { class: ['bd-component-reference'] }, [
@@ -33,6 +45,12 @@ export default {
       // Directive importing information
       h(Importdoc, { props: { meta: this.meta } })
     ])
-    return h(Main, { staticClass: 'bd-components' }, [$readmeSection, $referenceSection])
+    return h(Main, { staticClass: 'bd-components' }, [
+      $leadSection,
+      $carbonAd,
+      $quickLinks,
+      $bodySection,
+      $referenceSection
+    ])
   }
 }
