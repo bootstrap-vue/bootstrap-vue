158bdb30c37ec16b99b1b489d05d8ed002e638a5
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-28 04:01:17 -0300

    Update _slug.js

--- docs/pages/docs/directives/_slug.js
+++ docs/pages/docs/directives/_slug.js
@@ -1,8 +1,6 @@
 import AnchoredHeading from '~/components/anchored-heading'
-import CarbonAd from '~/components/ad'
 import Importdoc from '~/components/importdoc'
-import Main from '~/components/main'
-import QuickLinks from '~/components/quick-links.vue'
+import MainDocs from '~/components/main-docs'
 import Section from '~/components/section'
 import docsMixin from '~/plugins/docs-mixin'
 import { directives as directivesMeta } from '~/content'
@@ -20,37 +18,17 @@ export default {
   },
   async asyncData({ params }) {
     const readme = (await getReadMe(params.slug)).default
-    const { titleLead, body } = splitReadme(readme)
     const meta = directivesMeta[params.slug]
-    return { meta, titleLead, body }
+    return { meta, readme }
   },
   render(h) {
-    // Lead section
-    const $leadSection = h(Section, {
-      props: { play: false },
-      domProps: { innerHTML: this.titleLead }
-    })
-    // CarbonAd
-    const $carbonAd = h(CarbonAd, { key: `ad-${this.$route.path}` })
-    // Quick links
-    const $quickLinks = h(QuickLinks, { key: `quick-${this.$route.path}` })
-    // Body section
-    const $bodySection = h(Section, {
-      props: { play: true },
-      domProps: { innerHTML: this.body }
-    })
-    // Reference section
     const $referenceSection = h(Section, { class: ['bd-component-reference'] }, [
       // Heading
       h(AnchoredHeading, { props: { id: 'directive-reference' } }, 'Directive reference'),
       // Directive importing information
       h(Importdoc, { props: { meta: this.meta } })
     ])
-    return h(Main, { staticClass: 'bd-components' }, [
-      $leadSection,
-      $carbonAd,
-      $quickLinks,
-      $bodySection,
+    return h(MainDocs, { staticClass: 'bd-components', props: { readme: this.readme } }, [
       $referenceSection
     ])
   }
