89f598aaacdad67e13be35b2153f3f9c18406a34
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-28 03:17:39 -0300

    Update _slug.js

--- docs/pages/docs/misc/_slug.js
+++ docs/pages/docs/misc/_slug.js
@@ -1,8 +1,11 @@
 import hljs from '~/utils/hljs'
+import CarbonAd from '~/components/ad'
 import Main from '~/components/main'
+import QuickLinks from '~/components/quick-links.vue'
 import Section from '~/components/section'
 import docsMixin from '~/plugins/docs-mixin'
 import { misc as miscMeta, defaultConfig } from '~/content'
+import { splitReadme } from '~/utils'
 
 const getReadMe = name =>
   import(`~/markdown/misc/${name}/README.md` /* webpackChunkName: "docs/misc" */)
@@ -20,12 +23,31 @@ export default {
       '{{ defaultConfig }}',
       hljs.highlight('json', JSON.stringify(defaultConfig || {}, undefined, 2)).value
     )
+    const { titleLead, body } = splitReadme(readme)
     const meta = miscMeta[params.slug]
-    return { readme, meta }
+    
+    return { meta, titleLead, body }
   },
   render(h) {
-    // Readme section
-    const $readmeSection = h(Section, { domProps: { innerHTML: this.readme } })
-    return h(Main, { staticClass: 'bd-components' }, [$readmeSection])
+    // Lead section
+    const $leadSection = h(Section, {
+      props: { play: false },
+      domProps: { innerHTML: this.titleLead }
+    })
+    // CarbonAd
+    const $carbonAd = h(CarbonAd, { key: `ad-${this.$route.path}` })
+    // Quick links
+    const $quickLinks = h(QuickLinks, { key: `quick-${this.$route.path}` })
+    // Body section
+    const $bodySection = h(Section, {
+      props: { play: true },
+      domProps: { innerHTML: this.body }
+    })
+    return h(Main, { staticClass: 'bd-components' }, [
+      $leadSection,
+      $carbonAd,
+      $quickLinks,
+      $bodySection,
+    ])
   }
 }
