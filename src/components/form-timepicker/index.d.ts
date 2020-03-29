ae841184dc3037b5d6f365311cc668bccb0e85da
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-03-29 19:39:56 +0200

    fix(types): add missing declaration for `b-form-timepicker` (closes #5035) (#5036)
    * fix(types): add missing decleration for `b-form-timepicker`
    
    * Fix title comment

---
+++ src/components/form-timepicker/index.d.ts
@@ -0,0 +1,12 @@
+// --- Form Timepicker ---
+import Vue from 'vue'
+import { BvPlugin, BvComponent } from '../../'
+
+// Plugin
+export declare const FormTimepickerPlugin: BvPlugin
+
+// Component: b-form-timepicker
+export declare class BFormTimepicker extends BvComponent {
+  focus: () => void
+  blur: () => void
+}
