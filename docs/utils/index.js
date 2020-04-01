dc9551baf65090b023d1a52c7f8ded6441e698d6
Troy Morehouse <troymore@nbnet.nb.ca>
2020-03-31 23:03:48 -0300

    Update index.js

--- docs/utils/index.js
+++ docs/utils/index.js
@@ -82,14 +82,13 @@ export const relativeUrl = url => {
 
 // Splits an HTML README into two parts: Title+Lead and Body
 // So that we can place ads after the lead section
-// const RX_TITLE_LEAD_BODY = /^\s*(<h1 .+?<\/h1>)\s*(<p class="?bd-lead.+?<\/p>)?(.+)/i
-// const RX_TITLE_LEAD_BODY = /^\s*(<h1 .+?<\/h1>)\s*(<p class="?bd-lead.+?<\/p>)?(.*)$/is
 const RX_TITLE_LEAD_BODY = /^\s*(<h1 .+?<\/h1>)\s*(<p class="?bd-lead[\s\S]+?<\/p>)?([\s\S]*)$/i
 export const parseReadme = (readme = '') => {
   const [, title = '', lead = '', body = ''] = readme.match(RX_TITLE_LEAD_BODY)
+  const hasTitleLead = title || lead
   return {
-    titleLead: `${title} ${lead}`,
-    body: body || readme
+    titleLead: hasTitleLead ? `${title} ${lead}` : '',
+    body: hasTitleLead ? body : readme || ''
   }
 }
 
