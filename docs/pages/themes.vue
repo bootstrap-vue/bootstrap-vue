af57dbaadf262b3d2405bb440c52088e6fff6d82
Troy Morehouse <troymore@nbnet.nb.ca>
2020-04-16 05:52:16 -0300

    Update themes.vue

--- docs/pages/themes.vue
+++ docs/pages/themes.vue
@@ -21,7 +21,7 @@
       >
         <b-card no-body bg-variant="light">
           <b-row no-gutters>
-            <b-col md="7" lg="4" xl="4" aria-hidden="true">
+            <b-col md="7" lg="5" xl="4" aria-hidden="true">
               <b-card-img
                 :src="theme.img"
                 width="400"
