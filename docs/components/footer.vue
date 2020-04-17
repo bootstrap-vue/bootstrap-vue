ac7e14cb9ecf7b7164f68c95eae2ae0aab3d6379
Troy Morehouse <troymore@nbnet.nb.ca>
2020-04-17 01:20:26 -0300

    Update footer.vue

--- docs/components/footer.vue
+++ docs/components/footer.vue
@@ -43,7 +43,7 @@
 
       <hr>
 
-      <template  v-if="isZeitNow">
+      <template v-if="isZeitNow">
         <p class="mb-3 text-center">
           <strong>Depolyment preview hosting provided by</strong>
           <br>
@@ -57,12 +57,22 @@
               focusable="false"
               alt="Zeit Logo"
             >
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M107.2 37H86.75V33.9L101.85 12.35H87V8.80002H106.95V11.9L91.85 33.45H107.2V37ZM146.95 37H128.6V8.80002H146.95V12.35H132.75V20.85H145.1V24.4H132.75V33.45H146.95V37ZM169.05 37H186.95V33.45H180.1V12.35H186.95V8.80002H169.05V12.35H175.95V33.45H169.05V37ZM220.55 37H216.4V12.35H206.9V8.80002H230.15V12.35H220.55V37Z" fill="var(--geist-foreground)" />
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M25.6882 0.470947L50.9832 45.3291H0.393188L25.6882 0.470947Z" fill="url(#logotype21)" />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M107.2 37H86.75V33.9L101.85 12.35H87V8.80002H106.95V11.9L91.85 33.45H107.2V37ZM146.95 37H128.6V8.80002H146.95V12.35H132.75V20.85H145.1V24.4H132.75V33.45H146.95V37ZM169.05 37H186.95V33.45H180.1V12.35H186.95V8.80002H169.05V12.35H175.95V33.45H169.05V37ZM220.55 37H216.4V12.35H206.9V8.80002H230.15V12.35H220.55V37Z"
+                fill="#000"
+              />
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M25.6882 0.470947L50.9832 45.3291H0.393188L25.6882 0.470947Z"
+                fill="url(#logotype21)"
+              />
               <defs>
                 <linearGradient id="logotype21" x1="99.8392" y1="103.113" x2="36.06" y2="15.3616" gradientUnits="userSpaceOnUse">
-                  <stop stop-color="var(--geist-background)" />
-                  <stop offset="1" stop-color="var(--geist-foreground)" />
+                  <stop stop-color="#fff" />
+                  <stop offset="1" stop-color="#000" />
                 </linearGradient>
               </defs>
             </svg>
