8456fa2ecd1cac9a1844d18b4d66d934b7b043a5
Troy Morehouse <troymore@nbnet.nb.ca>
2020-04-16 21:18:17 -0300

    Update themes.vue

--- docs/pages/themes.vue
+++ docs/pages/themes.vue
@@ -4,8 +4,8 @@
       <header class="bd-content pb-4">
         <h1>Custom themes and dashboards</h1>
         <p class="lead">
-          With the below themes and dashboards built by our partners, you can build eye catching
-          apps and pages &mdash; all using BootstrapVue! The folloing items have been curated by
+          With the below themes and dashboards built by our partners, you can build eye-catching
+          apps and pages &mdash; all using BootstrapVue! The following items have been curated by
           the BootstrapVue team.
         </p>
       </header>
