aed708f289395b692229169d5ec806fa9d7f89b7
Troy Morehouse <troymore@nbnet.nb.ca>
2020-04-19 05:59:09 -0300

    Update sidebar.spec.js

--- src/components/sidebar/sidebar.spec.js
+++ src/components/sidebar/sidebar.spec.js
@@ -100,7 +100,7 @@ describe('sidebar', () => {
 
     const $sidebar = wrapper.find('.b-sidebar')
     expect($sidebar.exists()).toBe(true)
-    const $backdrop = wrapper.find('.b-sideba-backdrop')
+    const $backdrop = wrapper.find('.b-sidebar-backdrop')
     expect($backdrop.exists()).toBe(true)
 
     $sidebar.trigger('click')
