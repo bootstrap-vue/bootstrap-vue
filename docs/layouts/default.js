3b79a25daeaed1ac43ff822bb4a5620a2ec3c224
Troy Morehouse <troymore@nbnet.nb.ca>
2020-04-20 22:38:36 -0300

    Update default.js

--- docs/layouts/default.js
+++ docs/layouts/default.js
@@ -1,8 +1,22 @@
 import Footer from '~/components/footer'
 import Header from '~/components/header'
 
+const BASE_URL = 'https://bootstrap-vue.org'
+
 export default {
   name: 'BVDefaultLayout',
-  functional: true,
-  render: h => [h(Header), h('nuxt'), h(Footer)]
+  render: h => [h(Header), h('nuxt'), h(Footer)],
+  head() {
+    // Add conanonical URL so all site variations are
+    // indexed to the same primary URL
+    return {
+      link: [
+        {
+          hid: 'canonical',
+          rel: 'canonical',
+          href: `${BASE_URL}${this.$route.path}`
+        }
+      ]
+    }
+  }
 }
