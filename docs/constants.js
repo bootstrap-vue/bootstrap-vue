840df31f95820677489c3f9501870d19786594a8
Troy Morehouse <troymore@nbnet.nb.ca>
2020-04-22 18:47:24 -0300

    Update constants.js

--- docs/constants.js
+++ docs/constants.js
@@ -6,7 +6,7 @@ export const GA_TRACKING_ID = 'UA-89526435-1'
 
 export const TWITTER_HANDLE = '@BootstrapVue'
 
-// --- Google Webmaster tools google-site-verification codes --
+// --- Google Webmaster tools google-site-verification codes ---
 // bootstrap-vue.js.org
 // <meta name="google-site-verification" content="H9Mn7ie5Z5KJSjRfsn9nilLn5kFgn7BMEqL_sUhVpzg" />
 export const GWT_JS_ORG = 'H9Mn7ie5Z5KJSjRfsn9nilLn5kFgn7BMEqL_sUhVpzg'
