81d41ae6176afad43be9c7c844e2a9d604ea2b72
Troy Morehouse <troymore@nbnet.nb.ca>
2020-04-22 18:36:47 -0300

    Update default.js

--- docs/layouts/default.js
+++ docs/layouts/default.js
@@ -1,8 +1,35 @@
+import { BASE_URL, GWT_BV_ORG, GWT_JS_ORG } from '~/constants'
 import Footer from '~/components/footer'
 import Header from '~/components/header'
 
 export default {
   name: 'BVDefaultLayout',
-  functional: true,
-  render: h => [h(Header), h('nuxt'), h(Footer)]
+  render(h) {
+    return h('div', [h(Header), h('nuxt'), h(Footer)])
+  },
+  head() {
+    // Add canonical URL so all site variations are
+    // indexed to the same primary URL
+    return {
+      link: [
+        {
+          hid: 'canonical',
+          rel: 'canonical',
+          href: `${BASE_URL}${this.$route.path}`
+        }
+      ],
+      meta: [
+        {
+          hid: 'google-site-verification-bv-org',
+          name: 'google-site-verification',
+          content: GWT_BV_ORG
+        },
+        {
+          hid: 'google-site-verification-js-org',
+          name: 'google-site-verification',
+          content: GWT_JS_ORG
+        }
+      ]
+    }
+  }
 }
