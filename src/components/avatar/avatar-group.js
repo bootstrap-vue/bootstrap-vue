e55e021e3510989fe7c88cf86ee711fa9b9f23a4
Troy Morehouse <troymore@nbnet.nb.ca>
2020-04-28 17:24:25 -0300

    Create avatar-group.js

---
+++ src/components/avatar/avatar-group.js
@@ -0,0 +1,33 @@
+import Vue from '../../utils/vue'
+import normalizeSlotMixin from '../../mixins/normalize-slot'
+
+const NAME = 'BAvatarGroup'
+
+export const BAvatarGroup = /*#__PURE__*/ Vue.extend({
+  name: NAME,
+  mixins: [normalizeSlotMixin],
+  provide() {
+    return { bvAvatarGroup: this }
+  },
+  props: {
+    size: {
+      // Child avatars will always use this over their own size
+      type: String,
+      default: undefined
+    },
+    variant: {
+      // Child avatars will prefer this variant over their own
+      type: String,
+      default: undefined
+    },
+    tag: {
+      type: String,
+      default: 'div'
+    }
+  },
+  render(h) {
+    return h(this.tag, { staticClass: 'b-avatar-group', attrs: { role: 'group' } }, [
+      this.normalizeSlot('default')
+    ])
+  }
+})
