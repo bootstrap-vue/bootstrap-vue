c5587af206f101dc5c2dde3db7dfbbb1f86bd360
Troy Morehouse <troymore@nbnet.nb.ca>
2020-05-02 02:53:33 -0300

    Update sidebar.vue

--- docs/components/sidebar.vue
+++ docs/components/sidebar.vue
@@ -72,7 +72,7 @@ export default {
   },
   methods: {
     buildUrl(basePath, parts = []) {
-      parts = parts.filter(Boolean).join('/')
+      parts = parts.filter(Boolean).join('/').replace(/\/$/, '')
       const path = [basePath, parts].filter(Boolean).join('/')
       return path.replace(/(https?:\/\/)|(\/)+/g, '$1$2')
     }
