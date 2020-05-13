8d0d28e2ab0860d259465960c3225efcf2b3f18e
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-05-13 19:04:36 +0200

    Update html.js

--- src/utils/html.js
+++ src/utils/html.js
@@ -1,9 +1,9 @@
-const stripTagsRegex = /(<([^>]+)>)/gi
+const RX_HTML_TAGS = /(<([^>]+)>)/gi
 
-// Removes any thing that looks like an HTML tag from the supplied string
-export const stripTags = (text = '') => String(text).replace(stripTagsRegex, '')
+// Removes anything that looks like an HTML tag from the supplied string
+export const stripTags = (text = '') => String(text).replace(RX_HTML_TAGS, '')
 
-// Generate a domProps object for either innerHTML, textContent or nothing
+// Generate a `domProps` object for either `innerHTML`, `textContent` or an empty object
 export const htmlOrText = (innerHTML, textContent) => {
   return innerHTML ? { innerHTML } : textContent ? { textContent } : {}
 }
