97ed355c4d2e975a13138b9325752d0f0693d0d3
Troy Morehouse <troymore@nbnet.nb.ca>
2020-05-14 16:39:14 -0300

    feat(types): create declarations for `<b-calendar>` and `<b-time>` context event objects

--- src/components/calendar/index.d.ts
+++ src/components/calendar/index.d.ts
@@ -5,6 +5,20 @@ import { BvPlugin, BvComponent } from '../../'
 // Plugin
 export declare const CalendarPlugin: BvPlugin
 
+// Calendar context object
+export interface BCalendarContextEvent {
+  readonly selectedFormatted: string
+  readonly selectedYMD: string
+  readonly selectedDate: Date | null
+  readonly activeFormatted: string
+  readonly activeYMD: string
+  readonly activeDate: Date | null
+  readonly disabled: boolean
+  readonly locale: string
+  readonly calendarLocale: string
+  readonly rtl: boolean
+}
+
 // Component: b-calendar
 export declare class BCalendar extends BvComponent {
   focus: () => void
