f3e287ecd6bea8f175114007733039ef08bfd709
Rasmus Nielsen - Nova Consult ApS <rni@nova-c.dk>
2020-07-06 15:42:41 +0200

    initial

--- nuxt/index.js
+++ nuxt/index.js
@@ -1,4 +1,4 @@
-const { resolve } = require('path')
+const { resolve, join } = require('path')
 
 // --- Constants ---
 
@@ -183,3 +183,23 @@ module.exports = function nuxtBootstrapVue(moduleOptions = {}) {
 }
 
 module.exports.meta = require('../package.json')
+
+// --- Nuxt Components Hook ---
+export default function() {
+  const { nuxt } = this
+
+  // Make sure components is enabled
+  if (!nuxt.options.components) {
+    throw new Error(
+      'please set `components: true` inside `nuxt.config` and ensure using `nuxt >= 2.13.0`'
+    )
+  }
+
+  this.nuxt.hook('components:dirs', dirs => {
+    // Add ./components dir to the list
+    dirs.push({
+      path: join(__dirname, '../src/components'),
+      prefix: 'kru'
+    })
+  })
+}
