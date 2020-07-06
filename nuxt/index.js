83f47f61d19a124ee53c8c5b6fdc8eb5dde7c27b
Rasmus Nielsen - Nova Consult ApS <rni@nova-c.dk>
2020-07-06 15:50:58 +0200

    remove prefix

--- nuxt/index.js
+++ nuxt/index.js
@@ -198,8 +198,7 @@ export default function() {
   this.nuxt.hook('components:dirs', dirs => {
     // Add ./components dir to the list
     dirs.push({
-      path: join(__dirname, '../src/components'),
-      prefix: 'kru'
+      path: join(__dirname, '../src/components')
     })
   })
 }
