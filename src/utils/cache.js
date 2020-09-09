f424a39b0b8b8ae87ebba7e12cbb60e03bfdd94b
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-09-09 18:35:30 +0200

    Update cache.js

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -1,29 +1,35 @@
-import { hasOwnProperty } from './object'
+import cloneDeep from './clone-deep'
+import looseEqual from './loose-equal'
+import { hasOwnProperty, keys } from './object'
+
+const isEmpty = value => !value || keys(value).length === 0
 
 export const makePropWatcher = propName => ({
-  handler(newVal, oldVal) {
-    for (const key in oldVal) {
-      if (!hasOwnProperty(newVal, key)) {
+  handler(newValue, oldValue) {
+    if (looseEqual(newValue, oldValue)) {
+      return
+    }
+    if (isEmpty(newValue) || isEmpty(oldValue)) {
+      this[propName] = cloneDeep(newValue || {})
+      return
+    }
+    for (const key in oldValue) {
+      if (!hasOwnProperty(newValue, key)) {
         this.$delete(this.$data[propName], key)
       }
     }
-    for (const key in newVal) {
-      this.$set(this.$data[propName], key, newVal[key])
+    for (const key in newValue) {
+      this.$set(this.$data[propName], key, newValue[key])
     }
   }
 })
 
 export const makePropCacheMixin = (propName, proxyPropName) => ({
   data() {
-    return {
-      [proxyPropName]: {}
-    }
+    return { [proxyPropName]: cloneDeep(this[propName] || {}) }
   },
   watch: {
     // Work around unwanted re-renders: https://github.com/vuejs/vue/issues/10115
     [propName]: makePropWatcher(proxyPropName)
-  },
-  created() {
-    this[proxyPropName] = { ...this[propName] }
   }
 })
