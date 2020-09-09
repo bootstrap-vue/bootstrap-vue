5cc1cd63c72917c4aa5aa8c40892766e44da58cf
Rasmus Nielsen <rni@nova-c.dk>
2020-09-09 15:23:02 +0200

    change class name

--- src/components/form-tags/form-tags.js
+++ src/components/form-tags/form-tags.js
@@ -657,7 +657,7 @@ export const BFormTags = /*#__PURE__*/ Vue.extend({
         'li',
         {
           key: '__li-input__',
-          staticClass: 'flex-grow-1 b-form-tags-input-li',
+          staticClass: 'flex-grow-1 b-form-tags-li-input',
           attrs: {
             role: 'none',
             'aria-live': 'off',
