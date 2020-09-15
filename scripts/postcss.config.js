7a47f3e0ba5d669e68b4a1add1b1e58eaecc2446
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-09-16 00:07:46 +0200

    Update postcss.config.js

--- scripts/postcss.config.js
+++ scripts/postcss.config.js
@@ -1,12 +1,10 @@
+const autoprefixer = require('autoprefixer')
+
 module.exports = () => ({
   map: {
     inline: false,
     annotation: true,
     sourcesContent: true
   },
-  plugins: {
-    autoprefixer: {
-      cascade: false
-    }
-  }
+  plugins: [autoprefixer({ cascade: false })]
 })
