761bc9381ba24aed751726c8213651e2014aa746
Hiws <hiws@live.dk>
2020-10-01 11:22:16 +0200

    fix(b-carousel): fix glitching when switching slides fast (closes #5810) (#5845)
    * fix glitchingg
    
    * Update carousel.js
    
    Co-authored-by: Jacob Müller <jacob.mueller.elz@gmail.com>

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -10,6 +10,7 @@ import {
   getActiveElement,
   reflow,
   removeClass,
+  requestAF,
   selectAll,
   setAttr
 } from '../../utils/dom'
@@ -269,7 +270,10 @@ export const BCarousel = /*#__PURE__*/ Vue.extend({
       // Don't change slide while transitioning, wait until transition is done
       if (this.isSliding) {
         // Schedule slide after sliding complete
-        this.$once('sliding-end', () => this.setSlide(slide, direction))
+        this.$once('sliding-end', () => {
+          // Wrap in `requestAF()` to allow the slide to properly finish to avoid glitching
+          requestAF(() => this.setSlide(slide, direction))
+        })
         return
       }
       this.direction = direction
