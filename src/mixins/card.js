069e5168151f436a643a10baa5f72de33249be8a
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-10-30 18:26:47 +0100

    Update card.js

--- src/mixins/card.js
+++ src/mixins/card.js
@@ -1,21 +1,27 @@
+import { NAME_CARD } from '../constants/components'
+import { makePropsConfigurable } from '../utils/config'
+
 // @vue/component
 export default {
-  props: {
-    tag: {
-      type: String,
-      default: 'div'
+  props: makePropsConfigurable(
+    {
+      tag: {
+        type: String,
+        default: 'div'
+      },
+      bgVariant: {
+        type: String
+        // default: null
+      },
+      borderVariant: {
+        type: String
+        // default: null
+      },
+      textVariant: {
+        type: String
+        // default: null
+      }
     },
-    bgVariant: {
-      type: String
-      // default: null
-    },
-    borderVariant: {
-      type: String
-      // default: null
-    },
-    textVariant: {
-      type: String
-      // default: null
-    }
-  }
+    NAME_CARD
+  )
 }
