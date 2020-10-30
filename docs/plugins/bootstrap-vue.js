92ef9eee4552724215c9c7c06d7f7270acff6080
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-10-30 08:14:46 +0100

    Update bootstrap-vue.js

--- docs/plugins/bootstrap-vue.js
+++ docs/plugins/bootstrap-vue.js
@@ -1,9 +1,5 @@
 import Vue from 'vue'
 import { BootstrapVue, BootstrapVueIcons } from '../../src'
 
-Vue.use(BootstrapVue, {
-  BButton: {
-    size: 'lg'
-  }
-})
+Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
