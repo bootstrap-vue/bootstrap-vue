4baa858ba485c79e1ee571b504c5a90f92b978e3
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-11-06 11:01:29 +0100

    Update avatar.js

--- src/components/avatar/avatar.js
+++ src/components/avatar/avatar.js
@@ -14,6 +14,7 @@ import { BIconPersonFill } from '../../icons/icons'
 import normalizeSlotMixin from '../../mixins/normalize-slot'
 
 // --- Constants ---
+
 const CLASS_NAME = 'b-avatar'
 
 const SIZES = ['sm', null, 'lg']
