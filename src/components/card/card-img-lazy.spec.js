b333eb74e48647cd1743c472a8cb2466078f1655
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-11-12 00:41:01 +0100

    Update card-img-lazy.spec.js

--- src/components/card/card-img-lazy.spec.js
+++ src/components/card/card-img-lazy.spec.js
@@ -1,8 +1,8 @@
 import { mount } from '@vue/test-utils'
 import { BCardImgLazy } from './card-img-lazy'
 
-describe('card-image', () => {
-  it('default has tag "img"', async () => {
+describe('card-img-lazy', () => {
+  it('has expected default structure', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25'
@@ -10,51 +10,17 @@ describe('card-image', () => {
     })
 
     expect(wrapper.element.tagName).toBe('IMG')
-    expect(wrapper.attributes('src')).toBeDefined()
-
-    wrapper.unmount()
-  })
-
-  it('default does not have alt attribute', async () => {
-    const wrapper = mount(BCardImgLazy, {
-      props: {
-        src: 'https://picsum.photos/600/300/?image=25'
-      }
-    })
-
+    expect(wrapper.classes()).toContain('card-img')
+    expect(wrapper.classes().length).toBe(1)
+    expect(wrapper.attributes('src')).toBe('https://picsum.photos/600/300/?image=25')
     expect(wrapper.attributes('alt')).not.toBeDefined()
-
-    wrapper.unmount()
-  })
-
-  it('default has attributes width and height set to 1', async () => {
-    const wrapper = mount(BCardImgLazy, {
-      props: {
-        src: 'https://picsum.photos/600/300/?image=25'
-      }
-    })
-
-    expect(wrapper.attributes('width')).toBeDefined()
     expect(wrapper.attributes('width')).toBe('1')
-    expect(wrapper.attributes('height')).toBeDefined()
     expect(wrapper.attributes('height')).toBe('1')
 
     wrapper.unmount()
   })
 
-  it('default has class "card-img"', async () => {
-    const wrapper = mount(BCardImgLazy, {
-      props: {
-        src: 'https://picsum.photos/600/300/?image=25'
-      }
-    })
-
-    expect(wrapper.classes()).toContain('card-img')
-
-    wrapper.unmount()
-  })
-
-  it('has class "card-img-top" when prop top=true', async () => {
+  it('has class "card-img-top" when prop `top` is `true`', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25',
@@ -63,11 +29,12 @@ describe('card-image', () => {
     })
 
     expect(wrapper.classes()).toContain('card-img-top')
+    expect(wrapper.classes().length).toBe(1)
 
     wrapper.unmount()
   })
 
-  it('has class "card-img-bottom" when prop bottom=true', async () => {
+  it('has class "card-img-bottom" when prop `bottom` is `true`', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25',
@@ -76,11 +43,12 @@ describe('card-image', () => {
     })
 
     expect(wrapper.classes()).toContain('card-img-bottom')
+    expect(wrapper.classes().length).toBe(1)
 
     wrapper.unmount()
   })
 
-  it('has class "card-img-top" when props top=true and bottom=true', async () => {
+  it('has class "card-img-top" when props `top` and `bottom` is `true`', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25',
@@ -90,11 +58,12 @@ describe('card-image', () => {
     })
 
     expect(wrapper.classes()).toContain('card-img-top')
+    expect(wrapper.classes().length).toBe(1)
 
     wrapper.unmount()
   })
 
-  it('has class "card-img-left" when prop left=true', async () => {
+  it('has class "card-img-left" when prop `left` is `true`', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25',
@@ -103,11 +72,12 @@ describe('card-image', () => {
     })
 
     expect(wrapper.classes()).toContain('card-img-left')
+    expect(wrapper.classes().length).toBe(1)
 
     wrapper.unmount()
   })
 
-  it('has class "card-img-right" when prop right=true', async () => {
+  it('has class "card-img-right" when prop `right` is `true`', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25',
@@ -116,11 +86,12 @@ describe('card-image', () => {
     })
 
     expect(wrapper.classes()).toContain('card-img-right')
+    expect(wrapper.classes().length).toBe(1)
 
     wrapper.unmount()
   })
 
-  it('has attribute alt when prop alt set', async () => {
+  it('has `alt` attribute when `alt` prop set', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25',
@@ -128,13 +99,13 @@ describe('card-image', () => {
       }
     })
 
-    expect(wrapper.attributes('alt')).toBeDefined()
+    expect(wrapper.classes()).toContain('card-img')
     expect(wrapper.attributes('alt')).toBe('image')
 
     wrapper.unmount()
   })
 
-  it('has attribute alt when prop `alt` is empty', async () => {
+  it('has `alt` attribute when `alt` prop is empty', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25',
@@ -142,13 +113,14 @@ describe('card-image', () => {
       }
     })
 
+    expect(wrapper.classes()).toContain('card-img')
     expect(wrapper.attributes('alt')).toBeDefined()
     expect(wrapper.attributes('alt')).toBe('')
 
     wrapper.unmount()
   })
 
-  it('has attribute width when prop width set', async () => {
+  it('has `width` attribute when `width` prop set', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25',
@@ -156,13 +128,13 @@ describe('card-image', () => {
       }
     })
 
-    expect(wrapper.attributes('width')).toBeDefined()
+    expect(wrapper.classes()).toContain('card-img')
     expect(wrapper.attributes('width')).toBe('600')
 
     wrapper.unmount()
   })
 
-  it('has attribute height when prop height set', async () => {
+  it('has `height` attribute when `height` prop set', async () => {
     const wrapper = mount(BCardImgLazy, {
       props: {
         src: 'https://picsum.photos/600/300/?image=25',
@@ -170,7 +142,7 @@ describe('card-image', () => {
       }
     })
 
-    expect(wrapper.attributes('height')).toBeDefined()
+    expect(wrapper.classes()).toContain('card-img')
     expect(wrapper.attributes('height')).toBe('300')
 
     wrapper.unmount()
