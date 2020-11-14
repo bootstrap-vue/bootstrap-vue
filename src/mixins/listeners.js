c1daf8fddcc51d8480f03bed7abc21c12d90684b
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-11-14 12:58:02 +0100

    Update listeners.js

--- src/mixins/listeners.js
+++ src/mixins/listeners.js
@@ -1,15 +1,29 @@
 import { isVue2 } from '../vue'
 import { makePropCacheMixin } from '../utils/cache'
+import { isPlainObject } from '../utils/inspect'
 import { keys } from '../utils/object'
+import { lowerFirst } from '../utils/string'
 
+// --- Constants ---
+
+const LISTENER_KEY_PREFIX = 'on'
+
+// --- Helper methods ---
+
+const normalizeProp = data =>
+  isPlainObject(data)
+    ? keys(data).reduce(
+        (result, key) =>
+          key.indexOf(LISTENER_KEY_PREFIX) === 0
+            ? { ...result, [lowerFirst(key.substring(LISTENER_KEY_PREFIX.length))]: data[key] }
+            : result,
+        {}
+      )
+    : data
+
+// --- Mixin ---
 export default makePropCacheMixin(
-  isVue2 ? '$attrs' : '$listeners',
+  isVue2 ? '$listeners' : '$attrs',
   'bvListeners',
-  isVue2
-    ? data =>
-        keys(data).reduce(
-          (result, key) => (key.indexOf('on') === 0 ? { ...result, [key]: data[key] } : result),
-          {}
-        )
-    : null
+  isVue2 ? null : normalizeProp
 )
