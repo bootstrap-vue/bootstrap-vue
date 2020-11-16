af5fd0835381535e5a581663e6ce476eb18ad333
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-11-16 12:22:55 +0100

    Update form-select-option.js

--- src/components/form-select/form-select-option.js
+++ src/components/form-select/form-select-option.js
@@ -1,12 +1,13 @@
 import { defineComponent, h, mergeProps } from '../../vue'
 import { NAME_FORM_SELECT_OPTION } from '../../constants/components'
+import { PROP_NAME_MODEL_VALUE } from '../../constants/props'
 import { makePropsConfigurable } from '../../utils/config'
 
 // --- Props ---
 
 export const props = makePropsConfigurable(
   {
-    value: {
+    [PROP_NAME_MODEL_VALUE]: {
       // type: [String, Number, Boolean, Object],
       required: true
     },
@@ -25,12 +26,13 @@ export const BFormSelectOption = /*#__PURE__*/ defineComponent({
   functional: true,
   props,
   render(_, { props, data, children }) {
-    const { value, disabled } = props
+    const { disabled } = props
+
     return h(
       'option',
       mergeProps(data, {
         attrs: { disabled },
-        domProps: { value }
+        domProps: { value: props[PROP_NAME_MODEL_VALUE] }
       }),
       children
     )
