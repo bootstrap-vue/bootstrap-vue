fd08f9b3c0cf9347e1d319fef86fe9f4801035b7
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-11-29 22:57:00 +0100

    chore(refactor): improved code sharing between form components (#6100)
    * chore(refactor): improved code sharing between form components
    
    * Update form-checkbox-group.spec.js
    
    * Update form-radio-group.spec.js
    
    * Update form-radio-group.js
    
    * Update form-radio-check-group.js
    
    * Update form-radio-group.js

--- src/components/form-checkbox/form-checkbox-group.js
+++ src/components/form-checkbox/form-checkbox-group.js
@@ -1,60 +1,41 @@
 import Vue from '../../vue'
 import { NAME_FORM_CHECKBOX_GROUP } from '../../constants/components'
 import { makePropsConfigurable } from '../../utils/config'
-import formControlMixin, { props as formControlProps } from '../../mixins/form-control'
-import formOptionsMixin, { props as formOptionsProps } from '../../mixins/form-options'
 import formRadioCheckGroupMixin, {
   props as formRadioCheckGroupProps
 } from '../../mixins/form-radio-check-group'
-import formSizeMixin, { props as formSizeProps } from '../../mixins/form-size'
-import formStateMixin, { props as formStateProps } from '../../mixins/form-state'
-import idMixin from '../../mixins/id'
 
 // --- Props ---
 
 export const props = makePropsConfigurable(
   {
-    ...formControlProps,
-    ...formOptionsProps,
     ...formRadioCheckGroupProps,
-    ...formSizeProps,
-    ...formStateProps,
+    checked: {
+      type: Array,
+      default: () => []
+    },
     switches: {
       // Custom switch styling
       type: Boolean,
       default: false
-    },
-    checked: {
-      type: Array,
-      default: null
     }
   },
   NAME_FORM_CHECKBOX_GROUP
 )
 
 // --- Main component ---
+
 // @vue/component
 export const BFormCheckboxGroup = /*#__PURE__*/ Vue.extend({
   name: NAME_FORM_CHECKBOX_GROUP,
-  mixins: [
-    idMixin,
-    formControlMixin,
-    formRadioCheckGroupMixin, // Includes render function
-    formOptionsMixin,
-    formSizeMixin,
-    formStateMixin
-  ],
+  // Includes render function
+  mixins: [formRadioCheckGroupMixin],
   provide() {
     return {
       bvCheckGroup: this
     }
   },
   props,
-  data() {
-    return {
-      localChecked: this.checked || []
-    }
-  },
   computed: {
     isRadioGroup() {
       return false
--- src/components/form-radio/form-radio-group.js
+++ src/components/form-radio/form-radio-group.js
@@ -1,55 +1,26 @@
 import Vue from '../../vue'
 import { NAME_FORM_RADIO_GROUP } from '../../constants/components'
 import { makePropsConfigurable } from '../../utils/config'
-import idMixin from '../../mixins/id'
-import formControlMixin, { props as formControlProps } from '../../mixins/form-control'
-import formOptionsMixin, { props as formOptionsProps } from '../../mixins/form-options'
 import formRadioCheckGroupMixin, {
   props as formRadioCheckGroupProps
 } from '../../mixins/form-radio-check-group'
-import formSizeMixin, { props as formSizeProps } from '../../mixins/form-size'
-import formStateMixin, { props as formStateProps } from '../../mixins/form-state'
 
 // --- Props ---
 
-export const props = makePropsConfigurable(
-  {
-    ...formControlProps,
-    ...formOptionsProps,
-    ...formRadioCheckGroupProps,
-    ...formSizeProps,
-    ...formStateProps,
-    checked: {
-      // type: [String, Number, Boolean, Object],
-      default: null
-    }
-  },
-  NAME_FORM_RADIO_GROUP
-)
+export const props = makePropsConfigurable(formRadioCheckGroupProps, NAME_FORM_RADIO_GROUP)
 
 // --- Main component ---
+
 // @vue/component
 export const BFormRadioGroup = /*#__PURE__*/ Vue.extend({
   name: NAME_FORM_RADIO_GROUP,
-  mixins: [
-    idMixin,
-    formControlMixin,
-    formRadioCheckGroupMixin, // Includes render function
-    formOptionsMixin,
-    formSizeMixin,
-    formStateMixin
-  ],
+  mixins: [formRadioCheckGroupMixin],
   provide() {
     return {
       bvRadioGroup: this
     }
   },
   props,
-  data() {
-    return {
-      localChecked: this.checked
-    }
-  },
   computed: {
     isRadioGroup() {
       return true
