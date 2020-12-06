97e5c207810a3dfde11fc35e8961222806e9e84c
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-12-06 19:06:48 +0100

    Update props.spec.js

--- src/utils/props.spec.js
+++ src/utils/props.spec.js
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import { setConfig } from './config-set'
-import { copyProps, makePropsConfigurable } from './props'
+import { copyProps, makeProp, makePropsConfigurable } from './props'
 
 describe('utils/props', () => {
   it('copyProps() works with array props', async () => {
@@ -29,6 +29,49 @@ describe('utils/props', () => {
     // Except for primatives
     expect(copyProps(props).c).toBe(props.c)
     expect(copyProps(props).c).toEqual(props.c)
+  })
+
+  it('makeProp() works', async () => {
+    expect(makeProp()).toEqual({})
+    expect(makeProp(undefined)).toEqual({})
+    expect(makeProp(null)).toEqual({})
+    expect(makeProp('')).toEqual({})
+
+    expect(makeProp(Array)).toEqual({ type: Array })
+    expect(makeProp(Boolean)).toEqual({ type: Boolean })
+    expect(makeProp(Function)).toEqual({ type: Function })
+    expect(makeProp(Number)).toEqual({ type: Number })
+    expect(makeProp(Object)).toEqual({ type: Object })
+    expect(makeProp(String)).toEqual({ type: String })
+
+    expect(makeProp(Array, null)).toEqual({ type: Array, default: null })
+    expect(makeProp(Boolean, false)).toEqual({ type: Boolean, default: false })
+    expect(makeProp(Function, null)).toEqual({ type: Function, default: null })
+    expect(makeProp(Number, 0)).toEqual({ type: Number, default: 0 })
+    expect(makeProp(Object, null)).toEqual({ type: Object, default: null })
+    expect(makeProp(String, '')).toEqual({ type: String, default: '' })
+
+    expect(typeof makeProp(Array, []).default).toEqual('function')
+    expect(makeProp(Array, []).default()).toEqual([])
+
+    const fn = () => {}
+    expect(typeof makeProp(Function, fn).default).toEqual('function')
+    expect(makeProp(Function, fn).default).toEqual(fn)
+
+    expect(typeof makeProp(Object, {}).default).toEqual('function')
+    expect(makeProp(Object, {}).default()).toEqual({})
+
+    expect(makeProp(Array, true)).toEqual({ type: Array, required: true })
+    expect(makeProp(Boolean, true)).toEqual({ type: Boolean, default: true })
+    expect(makeProp(Function, true)).toEqual({ type: Function, required: true })
+    expect(makeProp(Number, true)).toEqual({ type: Number, required: true })
+    expect(makeProp(Object, true)).toEqual({ type: Object, required: true })
+    expect(makeProp(String, true)).toEqual({ type: String, required: true })
+
+    const validator = value => !!value
+    expect(makeProp(String, '', undefined)).toEqual({ type: String, default: '' })
+    expect(makeProp(String, '', validator)).toEqual({ type: String, default: '', validator })
+    expect(makeProp(String, undefined, validator)).toEqual({ type: String, validator })
   })
 
   it('makePropsConfigurable() works', async () => {
