7cbd170060c270616321604f73217701bccfca2a
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-12-06 19:29:02 +0100

    Update tfoot.js

--- src/components/table/tfoot.js
+++ src/components/table/tfoot.js
@@ -33,8 +33,7 @@ export const BTfoot = /*#__PURE__*/ Vue.extend({
   inject: {
     // Sniffed by `<b-tr>` / `<b-td>` / `<b-th>`
     bvTable: {
-      /* istanbul ignore next */
-      default: () => ({})
+      default: /* istanbul ignore next */ () => ({})
     }
   },
   inheritAttrs: false,
