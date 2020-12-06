4917dc30934f6bae21836fd5ef10c01ae78494c0
Jacob Müller <jacob.mueller.elz@gmail.com>
2020-12-06 20:50:38 +0100

    Update form-radio.js

--- src/components/form-radio/form-radio.js
+++ src/components/form-radio/form-radio.js
@@ -1,6 +1,5 @@
 import { Vue } from '../../vue'
 import { NAME_FORM_RADIO } from '../../constants/components'
-import { EVENT_NAME_CHANGE } from '../../constants/events'
 import { looseEqual } from '../../utils/loose-equal'
 import { sortKeys } from '../../utils/object'
 import { makePropsConfigurable } from '../../utils/props'
@@ -46,42 +45,11 @@ export const BFormRadio = /*#__PURE__*/ Vue.extend({
     }
   },
   props,
-  computed: {
-    isChecked() {
-      return looseEqual(this.value, this.computedLocalChecked)
-    },
-    isRadio() {
-      return true
-    },
-    isCheck() {
-      return false
-    }
-  },
   watch: {
     computedLocalChecked(newValue, oldValue) {
       if (!looseEqual(newValue, oldValue)) {
         this.$emit(MODEL_EVENT_NAME, newValue)
       }
-    }
-  },
-  methods: {
-    handleChange({ target: { checked } }) {
-      const { value } = this
-      const localChecked = checked ? value : null
-
-      this.computedLocalChecked = value
-
-      // Fire events in a `$nextTick()` to ensure the `v-model` is updated
-      this.$nextTick(() => {
-        // Change is only emitted on user interaction
-        this.$emit(EVENT_NAME_CHANGE, localChecked)
-
-        // If this is a child of `<form-radio-group>`,
-        // we emit a change event on it as well
-        if (this.isGroup) {
-          this.bvGroup.$emit(EVENT_NAME_CHANGE, localChecked)
-        }
-      })
     }
   }
 })
