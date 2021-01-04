12b6f092b5def8d8360b091bbb845d16c9f83c7f
jenemde <53625399+jenemde@users.noreply.github.com>
2021-01-04 10:00:00 +0100

    Update tabs.js

--- src/components/tabs/tabs.js
+++ src/components/tabs/tabs.js
@@ -199,8 +199,8 @@ export const props = makePropsConfigurable(
     // Synonym for 'bottom'
     end: makeProp(PROP_TYPE_BOOLEAN, false),
     // This prop is sniffed by the `<b-tab>` child
-    hidePosinset: makeProp(PROP_TYPE_BOOLEAN, false),
-    hideSetsize: makeProp(PROP_TYPE_BOOLEAN, false),
+    hidePosInSet: makeProp(PROP_TYPE_BOOLEAN, false),
+    hideSetSize: makeProp(PROP_TYPE_BOOLEAN, false),
     lazy: makeProp(PROP_TYPE_BOOLEAN, false),
     navClass: makeProp(PROP_TYPE_ARRAY_OBJECT_STRING),
     navWrapperClass: makeProp(PROP_TYPE_ARRAY_OBJECT_STRING),
@@ -538,8 +538,8 @@ export const BTabs = /*#__PURE__*/ Vue.extend({
       end,
       fill,
       firstTab,
-      hidePosinset,
-      hideSetsize,
+      hidePosInSet,
+      hideSetSize,
       justified,
       lastTab,
       nextTab,
@@ -578,8 +578,8 @@ export const BTabs = /*#__PURE__*/ Vue.extend({
           controls: safeId ? safeId() : null,
           id: $tab.controlledBy || (safeId ? safeId(`_BV_tab_button_`) : null),
           noKeyNav,
-          posInSet: hidePosinset ? null : index + 1,
-          setSize: hideSetsize ? null : $tabs.length,
+          posInSet: hidePosInSet ? null : index + 1,
+          setSize: hideSetSize ? null : $tabs.length,
           tab: $tab,
           tabIndex
         },
