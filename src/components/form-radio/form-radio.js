5d64a657bfbcd04d0d7b1ca8819d4eec0ee27a31
TomSmith27 <tom.smith2711@googlemail.com>
2021-01-27 13:20:27 +0000

    fix(b-form-radio-group)  make sure to pass state to form radio options for contextual feedback (closes #6357) (#6366)
    * fix(b-form-radio-group) make sure to pass state to form radio options for contextual feedback
    
    * Update form-radio.js
    
    * Update form-radio-check-group.js
    
    * Update form-radio-group.spec.js
    
    * Update form-checkbox-group.spec.js
    
    * Update form-radio-group.spec.js
    
    Co-authored-by: TomSmith <tom.smith@3squared.com>
    Co-authored-by: Jacob Müller <jacob.mueller.elz@gmail.com>

--- src/components/form-radio/form-radio.js
+++ src/components/form-radio/form-radio.js
@@ -1,43 +1,23 @@
 import { Vue } from '../../vue'
 import { NAME_FORM_RADIO } from '../../constants/components'
 import { looseEqual } from '../../utils/loose-equal'
-import { sortKeys } from '../../utils/object'
 import { makePropsConfigurable } from '../../utils/props'
-import { formControlMixin, props as formControlProps } from '../../mixins/form-control'
 import {
   MODEL_EVENT_NAME,
   formRadioCheckMixin,
   props as formRadioCheckProps
 } from '../../mixins/form-radio-check'
-import { formSizeMixin, props as formSizeProps } from '../../mixins/form-size'
-import { formStateMixin, props as formStateProps } from '../../mixins/form-state'
-import { idMixin, props as idProps } from '../../mixins/id'
 
 // --- Props ---
 
-export const props = makePropsConfigurable(
-  sortKeys({
-    ...idProps,
-    ...formControlProps,
-    ...formRadioCheckProps,
-    ...formSizeProps,
-    ...formStateProps
-  }),
-  NAME_FORM_RADIO
-)
+export const props = makePropsConfigurable(formRadioCheckProps, NAME_FORM_RADIO)
 
 // --- Main component ---
 
 // @vue/component
 export const BFormRadio = /*#__PURE__*/ Vue.extend({
   name: NAME_FORM_RADIO,
-  mixins: [
-    idMixin,
-    formRadioCheckMixin, // Includes shared render function
-    formControlMixin,
-    formSizeMixin,
-    formStateMixin
-  ],
+  mixins: [formRadioCheckMixin],
   inject: {
     bvGroup: {
       from: 'bvRadioGroup',
