1482c052b42b8233748c5bf2f588c351764ca014
Jacob Müller <jacob.mueller.elz@gmail.com>
2021-01-28 00:37:18 +0100

    Update section.js

--- docs/components/section.js
+++ docs/components/section.js
@@ -3,34 +3,34 @@ import { scrollTargetIntoView } from '~/utils'
 
 // --- Utility methods ---
 
-// Convert local links to router push or scrollIntoView
+// Convert local links to router push or scroll target into view
 const linkToRouter = event => {
   if (!event || event.type !== 'click') {
     return
   }
-  const target = event.target && event.target.closest ? event.target.closest('a[href]') : null
+  const $target = event.target && event.target.closest ? event.target.closest('a[href]') : null
+  // Early exit if click inside an example, not a link,
+  // or default prevented or is a Vue instance
   if (
-    !target ||
+    !$target ||
     event.type !== 'click' ||
-    target.__vue__ ||
-    target.closest('.bd-example') ||
-    target.closest('pre') ||
+    $target.__vue__ ||
+    $target.closest('.bd-example') ||
+    $target.closest('pre') ||
     event.defaultPrevented
   ) {
-    // Early exit if click inside an example, not a link, or
-    // default prevented or is a Vue instance
     return
   }
-  const href = target.getAttribute('href')
+  const href = $target.getAttribute('href')
   if (href && href.indexOf('/') === 0 && href.indexOf('//') !== 0) {
-    // if local page-to-page-docs link, convert click to `$router.push()`
+    // If local page-to-page-docs link, convert click to `$router.push()`
     event.preventDefault()
     if (typeof window !== 'undefined' && window.$nuxt) {
-      // Since we are a functional component, we can't use this.$router
+      // Since we are a functional component, we can't use `this.$router`
       window.$nuxt.$router.push(href)
     }
   } else if (href && href.indexOf('#') === 0) {
-    // In page anchor link, so use scrollIntoView utility method
+    // In page anchor link, so use `scrollTargetIntoView` utility method
     scrollTargetIntoView(event, href)
   }
   // Else, normal browser link handling (i.e. external links)
