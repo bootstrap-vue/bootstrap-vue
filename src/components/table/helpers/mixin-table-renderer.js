e4d752cb6df0b509bc7c085d81ab2463ec02d481
Andrei Gheorghiu <arhicub@gmail.com>
2021-01-31 17:32:15 +0000

    w.i.p.

--- src/components/table/helpers/mixin-table-renderer.js
+++ src/components/table/helpers/mixin-table-renderer.js
@@ -135,7 +135,7 @@ export const tableRendererMixin = Vue.extend({
         ...this.bvAttrs,
         // Now we can override any `$attrs` here
         id: this.safeId(),
-        role: 'table',
+        role: this.bvAttrs.role || 'grid',
         ...ariaAttrs,
         ...selectableTableAttrs
       }
