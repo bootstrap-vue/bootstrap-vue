4f5df28a06551c7c30bd91410d489614ab07d032
Illya Klymov <xanf@xanf.me>
2021-07-25 04:06:43 +0300

    chore(compat): remove parentComponent usage
    parentComponent is not supported in VTU v2

--- tests/utils.js
+++ tests/utils.js
@@ -1,4 +1,16 @@
 // --- Utils for testing ---
 
+export const wrapWithMethods = (Component, methods) => ({
+  inheritAttrs: false,
+  components: { wrappedComponent: Component },
+  methods,
+  template: `
+    <wrapped-component v-bind="$attrs" v-on="$listeners">
+      <slot v-for="(_, name) in $slots" :name="name" :slot="name" />
+      <template v-for="(_, name) in $scopedSlots" :slot="name" slot-scope="slotData"><slot :name="name" v-bind="slotData" /></template>
+    </wrapped-component>
+  `
+})
+
 export const waitNT = ctx => new Promise(resolve => ctx.$nextTick(resolve))
 export const waitRAF = () => new Promise(resolve => requestAnimationFrame(resolve))
