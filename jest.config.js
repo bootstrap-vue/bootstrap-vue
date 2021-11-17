e993c7153e12bc045834d4014cdcb6282e588339
Illya Klymov <xanf@xanf.me>
2021-11-17 17:18:27 +0200

    chore(compat): introduce Vue3 testing infrastructure
    Add basic setup for running tests with vue3 compat

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,20 @@
+const useVue2 = 'USE_VUE2' in process.env
+
+const moduleNameMapper = useVue2
+  ? {}
+  : {
+      '^vue$': '@vue/compat',
+      '^@vue/test-utils$': '@vue/test-utils-vue3'
+    }
+
 module.exports = {
   testRegex: 'spec.js$',
   moduleFileExtensions: ['js', 'vue'],
+  moduleNameMapper,
   transform: {
-    '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
+    '^.+\\.js$': 'babel-jest'
   },
+  transformIgnorePatterns: ['/node_modules(?![\\\\/]vue-test-utils-compat[\\\\/])'],
   coverageDirectory: './coverage/',
   testEnvironmentOptions: {
     pretendToBeVisual: true
