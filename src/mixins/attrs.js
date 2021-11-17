d4eadd8c9028812cd6619960fb3c55cf6699e9dc
Illya Klymov <xanf@xanf.me>
2021-11-17 18:29:59 +0200

    chore(compat): update attrs mixin for vue3

--- src/mixins/attrs.js
+++ src/mixins/attrs.js
@@ -1,3 +1,13 @@
 import { makePropCacheMixin } from '../utils/cache'
+import { Vue, isVue3 } from '../vue'
 
-export const attrsMixin = makePropCacheMixin('$attrs', 'bvAttrs')
+const attrsMixinVue2 = makePropCacheMixin('$attrs', 'bvAttrs')
+const attrsMixinVue3 = Vue.extend({
+  computed: {
+    bvAttrs() {
+      return this.$attrs
+    }
+  }
+})
+
+export const attrsMixin = isVue3 ? attrsMixinVue3 : attrsMixinVue2
