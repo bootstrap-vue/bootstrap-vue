2ce667248a558f93efbab36465e5d415ef364e96
Illya Klymov <xanf@xanf.me>
2021-11-17 17:18:27 +0200

    chore(compat): introduce Vue3 testing infrastructure
    Add basic setup for running tests with vue3 compat

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,18 @@
 import '@testing-library/jest-dom'
-import { config as vtuConfig } from '@vue/test-utils'
+import Vue from 'vue'
+import * as VTU from '@vue/test-utils'
+import { installCompat as installVTUCompat, fullCompatConfig } from 'vue-test-utils-compat'
+
+const useVue2 = 'USE_VUE2' in process.env
+if (!useVue2) {
+  Vue.configureCompat({
+    MODE: 2
+  })
+
+  const compatH = new Vue({}).$createElement
+  installVTUCompat(VTU, fullCompatConfig, compatH)
+}
 
 // Don't stub `<transition>` and `<transition-group>` components
-vtuConfig.stubs.transition = false
-vtuConfig.stubs['transition-group'] = false
+VTU.config.stubs.transition = false
+VTU.config.stubs['transition-group'] = false
