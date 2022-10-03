d8edafc9ec10cb1bd134334425698e37969bcf28
Illya Klymov <xanf@xanf.me>
2022-10-03 13:16:39 +0300

    chore(test): simplify Vue3 detection

--- tests/setup.js
+++ tests/setup.js
@@ -3,8 +3,7 @@ import Vue from 'vue'
 import * as VTU from '@vue/test-utils'
 import { installCompat as installVTUCompat, fullCompatConfig } from 'vue-test-utils-compat'
 
-const useVue3 = 'USE_VUE3' in process.env
-if (useVue3) {
+if (Vue.configureCompat) {
   Vue.configureCompat({
     MODE: 2,
     ATTR_FALSE_VALUE: 'suppress-warning',
