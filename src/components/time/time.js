440462cda2c7c8d3c5928cc06a2b3e62d759bdc8
Stanislav Lashmanov <stasvarenkin@gmail.com>
2022-10-14 17:22:50 +0300

    Fix b-time hydration error
    Since AM\PM spinner depends on client's setting it shouldn't be rendered on server.

--- src/components/time/time.js
+++ src/components/time/time.js
@@ -504,7 +504,8 @@ export const BTime = /*#__PURE__*/ extend({
     }
 
     // AM/PM ?
-    if (this.is12Hour) {
+    // depends on client settings, shouldn't be rendered on server
+    if (this.isLive && this.is12Hour) {
       // TODO:
       //   If locale is RTL, unshift this instead of push?
       //   And switch class `ml-2` to `mr-2`
