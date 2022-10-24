b4424713dc7f0719a7c3a74a2d39ef26593e206f
Illya Klymov <xanf@xanf.me>
2022-10-24 23:10:01 +0300

    chore: remove unused vars

--- src/components/form-radio/form-radio.js
+++ src/components/form-radio/form-radio.js
@@ -1,12 +1,7 @@
 import { extend } from '../../vue'
 import { NAME_FORM_RADIO } from '../../constants/components'
-import { looseEqual } from '../../utils/loose-equal'
 import { makePropsConfigurable } from '../../utils/props'
-import {
-  MODEL_EVENT_NAME,
-  formRadioCheckMixin,
-  props as formRadioCheckProps
-} from '../../mixins/form-radio-check'
+import { formRadioCheckMixin, props as formRadioCheckProps } from '../../mixins/form-radio-check'
 
 // --- Props ---
 
