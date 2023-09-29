d6f1fff42e591e685fa3bb94970962eec2b1991b
Beckerin <paivadiego7797@gmail.com>
2023-09-29 16:27:32 +0100

    feat(injections.d.ts): Adding support to vue3/optionsApi lint

--- src/vue-injections.d.ts
+++ src/vue-injections.d.ts
@@ -11,3 +11,18 @@ declare module 'vue/types/vue' {
     readonly $bvToast: BvToast
   }
 }
+
+
+// TODO: figure out why it cannot be 'vue' like said in docs
+// @ts-ignore: works on Vue 3, fails in Vue 2
+declare module '@vue/runtime-core' {
+  export interface ComponentCustomProperties {
+    /**
+     * Access to the plugins related functions on Modal/Toast
+     */
+    $bvModal: BvModal extends Record<'$bvModal', infer T> ? T : BvModal
+    $bvToast: BvToast extends Record<'$bvToast', infer T> ? T : BvToast
+
+
+  }
+}
